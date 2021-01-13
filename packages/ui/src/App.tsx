6c321c78c277d51038b279d93a2e348411949410
Maciej Gołaszewski <jodator@jodator.net>
2021-01-13 15:56:56 +0100

    Add browser router

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Profile } from './pages/Profile/Profile'
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom'
 
 export function App() {
-  return <Profile />
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/profile" component={Profile} />
+        <Redirect exact from="/" to="/profile" />
+      </Switch>
+    </BrowserRouter>
+  )
 }
