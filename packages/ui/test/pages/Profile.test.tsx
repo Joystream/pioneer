44d0180365742c45f131f087ccfe291b9e81dc10
Maciej Gołaszewski <jodator@jodator.net>
2021-01-15 17:10:48 +0100

    Add test for listing accounts

--- packages/ui/test/pages/Profile.test.tsx
+++ packages/ui/test/pages/Profile.test.tsx
@@ -1,10 +1,95 @@
 import React from 'react'
+import { expect } from 'chai'
 import { render, screen } from '@testing-library/react'
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import { TypeRegistry } from '@polkadot/types'
+import metaStatic from '@polkadot/metadata/static'
+import { Metadata } from '@polkadot/metadata'
+import keyring from '@polkadot/ui-keyring'
+import { KeyringJson, KeyringStore } from '@polkadot/ui-keyring/types'
 import { Profile } from '../../src/pages/Profile/Profile'
+import { SubstrateContext } from '../../src/providers/substrate/context'
+import { State, types } from '../../src/providers/substrate/reducer'
+
+export function createApiWithAugmentations(): ApiPromise {
+  const registry = new TypeRegistry()
+  const metadata = new Metadata(registry, metaStatic)
+
+  registry.setMetadata(metadata)
+
+  const api = new ApiPromise({ provider: new WsProvider('ws://', false), registry })
+
+  api.injectMetadata(metadata, true)
+
+  return api
+}
+
+type AccountsMap = Record<string, KeyringJson>
+
+export class MemoryStore implements KeyringStore {
+  private accounts: AccountsMap = {}
+
+  all(cb: (key: string, value: KeyringJson) => void): void {
+    Object.keys(this.accounts).forEach((accountsKey) => cb(accountsKey, this.accounts[accountsKey]))
+  }
+
+  get(key: string, cb: (value: KeyringJson) => void): void {
+    cb(this.accounts[key])
+  }
+
+  remove(key: string, cb: (() => void) | undefined): void {
+    delete this.accounts[key]
+
+    if (cb) {
+      cb()
+    }
+  }
+
+  set(key: string, value: KeyringJson, cb: (() => void) | undefined): void {
+    this.accounts[key] = value
+
+    if (cb) {
+      cb()
+    }
+  }
+}
+
+import { Keyring } from '@polkadot/keyring'
+import { KeyringPair } from '@polkadot/keyring/types'
+
+export function aliceSigner(): KeyringPair {
+  const keyring = new Keyring({ type: 'sr25519' })
+
+  return keyring.addFromUri('//Alice')
+}
 
 describe('UI: Profile', () => {
+  before(() => {
+    keyring.loadAll({ isDevelopment: true, store: new MemoryStore() })
+  })
+
   it('Shows loading', () => {
     render(<Profile />)
-    screen.getByText('Loading...')
+    expect(screen.getByText('Loading...')).to.not.be.empty
+  })
+
+  it('Renders accounts list for known addresses', () => {
+    const state: State = {
+      keyringState: 'READY',
+      api: createApiWithAugmentations(),
+      keyring: keyring,
+      socket: 'ws://',
+      apiState: 'READY',
+      jsonrpc: {},
+      types: types,
+    }
+
+    render(
+      <SubstrateContext.Provider value={state}>
+        <Profile />
+      </SubstrateContext.Provider>
+    )
+    const alice = aliceSigner().address
+    expect(screen.getByText(alice)).to.not.be.empty
   })
 })
