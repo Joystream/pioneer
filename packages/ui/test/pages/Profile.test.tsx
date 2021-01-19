836a8c355b1a1f1a893341a26089f5cba6d7e82e
Maciej Gołaszewski <jodator@jodator.net>
2021-01-19 10:50:06 +0100

    Add test for loading screen

--- packages/ui/test/pages/Profile.test.tsx
+++ packages/ui/test/pages/Profile.test.tsx
@@ -1,46 +1,60 @@
 import React from 'react'
 import { expect } from 'chai'
+import sinon from 'sinon'
 import { render } from '@testing-library/react'
-import keyring, { Keyring } from '@polkadot/ui-keyring'
+import { Keyring } from '@polkadot/ui-keyring'
 import { Profile } from '../../src/pages/Profile/Profile'
 import { aliceSigner, MemoryStore } from '../mocks/keyring'
 import { KeyringContext } from '../../src/providers/keyring/context'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useAccountsModule = require('../../src/hooks/useAccounts')
+
 describe('UI: Profile', () => {
-  before(() => {
-    keyring.loadAll({ isDevelopment: true, store: new MemoryStore() })
+  context('with empty keyring', () => {
+    after(() => {
+      sinon.restore()
+    })
+
+    it('Shows loading screen', () => {
+      sinon.stub(useAccountsModule, 'useAccounts').returns({
+        hasAccounts: false,
+        allAccounts: [],
+      })
+
+      const profile = render(<Profile />)
+      expect(profile.getByText('Loading...')).to.exist
+    })
   })
 
-  it.skip('Shows loading', () => {
-    const keyring = new Keyring()
+  context('with development accounts', () => {
+    let keyring: Keyring
 
-    const profile = render(
-      <KeyringContext.Provider value={keyring}>
-        <Profile />
-      </KeyringContext.Provider>
-    )
-    expect(profile.getByText('Loading...')).to.not.be.empty
+    before(() => {
+      keyring = new Keyring()
+      keyring.loadAll({ isDevelopment: true, store: new MemoryStore() })
+    })
+
+    it('Renders accounts list for known addresses', () => {
+      const profilePage = renderProfile()
+
+      const [, accountsRowGroup] = [...profilePage.getAllByRole('rowgroup')]
+      expect(accountsRowGroup.childNodes).to.have.length(8)
+    })
+
+    it("Displays account's data", () => {
+      const profilePage = renderProfile()
+
+      const alice = aliceSigner().address
+      expect(profilePage.getByText(alice)?.previousSibling?.textContent).to.equal('alice')
+    })
+
+    function renderProfile() {
+      return render(
+        <KeyringContext.Provider value={keyring}>
+          <Profile />
+        </KeyringContext.Provider>
+      )
+    }
   })
-
-  it('Renders accounts list for known addresses', () => {
-    const profilePage = renderProfile()
-
-    const [, accountsRowGroup] = [...profilePage.getAllByRole('rowgroup')]
-    expect(accountsRowGroup.childNodes).to.have.length(8)
-  })
-
-  it("Displays account's data", () => {
-    const profilePage = renderProfile()
-
-    const alice = aliceSigner().address
-    expect(profilePage.getByText(alice)?.previousSibling?.textContent).to.equal('alice')
-  })
-
-  function renderProfile() {
-    return render(
-      <KeyringContext.Provider value={keyring}>
-        <Profile />
-      </KeyringContext.Provider>
-    )
-  }
 })
