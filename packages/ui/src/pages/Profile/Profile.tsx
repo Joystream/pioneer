a9745dfb47a7f3e918151d01265924d0ef3a5727
Maciej Gołaszewski <jodator@jodator.net>
2021-01-20 13:26:02 +0100

    Add loading styled component

--- packages/ui/src/pages/Profile/Profile.tsx
+++ packages/ui/src/pages/Profile/Profile.tsx
@@ -4,12 +4,13 @@ import { Navigation } from '../../components/page/Navigation'
 import { PageContent } from '../../components/page/PageContent'
 import { Accounts } from './Accounts'
 import { useAccounts } from '../../hooks/useAccounts'
+import styled from 'styled-components'
 
 export function Profile() {
   const { hasAccounts, allAccounts } = useAccounts()
 
   if (!hasAccounts) {
-    return <div>Loading...</div>
+    return <Loading>Loading...</Loading>
   }
 
   return (
@@ -21,3 +22,7 @@ export function Profile() {
     </Page>
   )
 }
+
+const Loading = styled.div`
+  font-size: 2em;
+`
