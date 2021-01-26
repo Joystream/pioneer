bba0b599670ed7037ef35b3c90c356c47c5846dc
Maciej Gołaszewski <jodator@jodator.net>
2021-01-26 18:06:50 +0100

    Use proper decimals for JOY token

--- packages/ui/src/utils/formatters.ts
+++ packages/ui/src/utils/formatters.ts
@@ -1,11 +1,8 @@
 import BN from 'bn.js'
 import { formatBalance } from '@polkadot/util'
 
-const DECIMALS = 12
-
 export const formatTokenValue = (value: BN | number) => {
   return formatBalance(new BN(value), {
-    decimals: DECIMALS,
     withUnit: 'JOY',
   })
 }
