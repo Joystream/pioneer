133b56f53ea09214d8e4d3c68594696d020a8eb8
Maciej Gołaszewski <jodator@jodator.net>
2021-01-27 17:59:14 +0100

    Extract SignTransferModal

--- packages/ui/src/components/TransferModal.tsx
+++ packages/ui/src/components/TransferModal.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import BN from 'bn.js'
 import { BorderRad, Colors } from '../constants'
-import { useApi } from '../hooks/useApi'
 import { useBalances } from '../hooks/useBalances'
 import { useNumberInput } from '../hooks/useNumberInput'
-import { useKeyring } from '../hooks/useKeyring'
 import { Account } from '../hooks/types'
 import { Modal, ModalBody, ModalFooter, ModalHeader } from './modal'
 import { ButtonPrimaryMedium, ButtonSecondarySmall } from './buttons/Buttons'
 import { AccountInfo } from './AccountInfo'
-import { RuntimeDispatchInfo } from '@polkadot/types/interfaces'
 import { TokenValue } from './TokenValue'
+import { SignTransferModal } from './transfer/SignTransferModal'
 
 interface Props {
   onClose: () => void
@@ -22,21 +20,11 @@ interface Props {
 type ModalState = 'SEND_TOKENS' | 'SIGN_TRANSACTION' | 'SENDING'
 
 export function TransferModal({ from, to, onClose }: Props) {
-  const { api } = useApi()
-  const { keyring } = useKeyring()
   const balances = useBalances([from, to])
-  const [isSending, setIsSending] = useState(false)
   const [amount, setAmount] = useNumberInput(0)
   const [step, setStep] = useState<ModalState>('SEND_TOKENS')
-  const [info, setInfo] = useState<RuntimeDispatchInfo | null>(null)
 
-  const submittableExtrinsic = api?.tx.balances.transfer(to.address, new BN(amount))
-
-  useEffect(() => {
-    submittableExtrinsic?.paymentInfo(from.address).then((info) => {
-      setInfo(info)
-    })
-  }, [api, amount])
+  const isZero = new BN(amount).lte(new BN(0))
 
   if (!balances.hasBalances) {
     return (
@@ -49,155 +37,77 @@ export function TransferModal({ from, to, onClose }: Props) {
   const transferableBalance = balances?.map[from.address]?.total
 
   const isOverBalance = new BN(amount).gt(transferableBalance)
-  const isZero = new BN(amount).lte(new BN(0))
-  const isTransferDisabled = isSending || isZero || isOverBalance
+  const isTransferDisabled = isZero || isOverBalance
   const title = step === 'SIGN_TRANSACTION' ? 'Authorize transaction' : 'Send tokens'
 
   const setHalf = () => setAmount(transferableBalance.div(new BN(2)).toString())
   const setMax = () => setAmount(transferableBalance.toString())
-  const signAndSend = async () => {
-    setIsSending(true)
 
-    if (!submittableExtrinsic) {
-      return
-    }
-
-    await submittableExtrinsic
-      .signAndSend(keyring.getPair(from.address), (result) => {
-        const { status } = result
-
-        if (status.isFinalized) {
-          onClose()
-        }
-
-        status.isFinalized
-          ? console.log(`Finalized. Block hash: ${status.asFinalized.toString()}`)
-          : console.log(`Current transaction status: ${status.type}`)
-      })
-      .catch((error) => console.log('Error', error))
-  }
-
-  return (
-    <Modal>
-      <ModalHeader onClick={onClose} title={title} />
-      <ModalBody>
-        {step === 'SEND_TOKENS' && (
-          <>
-            <Row>
-              <FormLabel>From</FormLabel>
-              <LockedAccount>
-                <AccountInfo account={from} />
-                <TransactionInfoRow>
-                  <InfoTitle>Transferable balance</InfoTitle>
-                  <InfoValue>
-                    <TokenValue value={transferableBalance} />
-                  </InfoValue>
-                </TransactionInfoRow>
-              </LockedAccount>
-            </Row>
-            <TransactionAmount>
-              <AmountInputBlock>
-                <AmountInputLabel>Number of tokens</AmountInputLabel>
-                <AmountInput value={amount} onChange={(event) => setAmount(event.target.value)} placeholder={'0'} />
-              </AmountInputBlock>
-              <AmountButtons>
-                <AmountButton onClick={setHalf}>Use half</AmountButton>
-                <AmountButton onClick={setMax}>Use max</AmountButton>
-              </AmountButtons>
-            </TransactionAmount>
-            <Row>
-              <FormLabel>Destination account</FormLabel>
-              <AccountRow>
-                <AccountInfo account={to} />
-                <TransactionInfoRow>
-                  <InfoTitle>Total balance</InfoTitle>
-                  <InfoValue>
-                    <TokenValue value={balances?.map[to.address]?.total} />
-                  </InfoValue>
-                </TransactionInfoRow>
-              </AccountRow>
-            </Row>
-          </>
-        )}
-        {step === 'SIGN_TRANSACTION' && (
-          <>
-            <Row>
-              <FormLabel>From</FormLabel>
-              <AccountRow>
-                <AccountInfo account={from} />
-                <TransactionInfoRow>
-                  <InfoTitle>Transferable balance</InfoTitle>
-                  <InfoValue>
-                    <TokenValue value={transferableBalance} />
-                  </InfoValue>
-                </TransactionInfoRow>
-              </AccountRow>
-            </Row>
-            <TransactionAmount>
-              Transferring <TokenValue value={new BN(amount)} />
-            </TransactionAmount>
-            <Row>
-              <FormLabel>Destination account</FormLabel>
-              <AccountRow>
-                <AccountInfo account={to} />
-                <TransactionInfoRow>
-                  <InfoTitle>Total balance</InfoTitle>
-                  <InfoValue>
-                    <TokenValue value={balances?.map[to.address]?.total} />
-                  </InfoValue>
-                </TransactionInfoRow>
-              </AccountRow>
-            </Row>
-          </>
-        )}
-      </ModalBody>
-      <ModalFooter>
-        {step === 'SIGN_TRANSACTION' && (
-          <>
-            <TransactionInfo>
+  if (step === 'SEND_TOKENS') {
+    return (
+      <Modal>
+        <ModalHeader onClick={onClose} title={title} />
+        <ModalBody>
+          <Row>
+            <FormLabel>From</FormLabel>
+            <LockedAccount>
+              <AccountInfo account={from} />
               <TransactionInfoRow>
-                <InfoTitle>Amount:</InfoTitle>
+                <InfoTitle>Transferable balance</InfoTitle>
                 <InfoValue>
-                  <TokenValue value={new BN(amount)} />
+                  <TokenValue value={transferableBalance} />
                 </InfoValue>
               </TransactionInfoRow>
+            </LockedAccount>
+          </Row>
+          <TransactionAmount>
+            <AmountInputBlock>
+              <AmountInputLabel>Number of tokens</AmountInputLabel>
+              <AmountInput value={amount} onChange={(event) => setAmount(event.target.value)} placeholder={'0'} />
+            </AmountInputBlock>
+            <AmountButtons>
+              <AmountButton onClick={setHalf}>Use half</AmountButton>
+              <AmountButton onClick={setMax}>Use max</AmountButton>
+            </AmountButtons>
+          </TransactionAmount>
+          <Row>
+            <FormLabel>Destination account</FormLabel>
+            <AccountRow>
+              <AccountInfo account={to} />
               <TransactionInfoRow>
-                <InfoTitle>Transaction fee:</InfoTitle>
+                <InfoTitle>Total balance</InfoTitle>
                 <InfoValue>
-                  <TokenValue value={info?.partialFee.toBn()} />
+                  <TokenValue value={balances?.map[to.address]?.total} />
                 </InfoValue>
               </TransactionInfoRow>
-            </TransactionInfo>
-          </>
-        )}
-        {step === 'SEND_TOKENS' && (
+            </AccountRow>
+          </Row>
+        </ModalBody>
+        <ModalFooter>
           <ButtonPrimaryMedium onClick={() => setStep('SIGN_TRANSACTION')} disabled={isTransferDisabled}>
             Transfer tokens
           </ButtonPrimaryMedium>
-        )}
-        {step === 'SIGN_TRANSACTION' && (
-          <ButtonPrimaryMedium onClick={signAndSend} disabled={isTransferDisabled}>
-            Sign transaction and Transfer
-          </ButtonPrimaryMedium>
-        )}
-      </ModalFooter>
-    </Modal>
-  )
+        </ModalFooter>
+      </Modal>
+    )
+  }
+
+  return <SignTransferModal onClose={onClose} from={from} amount={amount} to={to} />
 }
 
-const FormLabel = styled.div`
+export const FormLabel = styled.div`
   font-size: 14px;
   line-height: 20px;
   margin-bottom: 8px;
   font-weight: 700;
 `
-const Row = styled.div`
+export const Row = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: auto;
 `
-const AccountRow = styled.div`
+export const AccountRow = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr;
@@ -208,18 +118,17 @@ const AccountRow = styled.div`
   border-radius: ${BorderRad.s};
   background-color: ${Colors.White};
 `
-const LockedAccount = styled(AccountRow)`
+export const LockedAccount = styled(AccountRow)`
   background-color: ${Colors.Black[50]};
 `
-
-const TransactionAmount = styled.div`
+export const TransactionAmount = styled.div`
   display: grid;
   grid-template-columns: 284px auto;
   grid-template-rows: 1fr;
   grid-column-gap: 24px;
   align-items: end;
 `
-const AmountInputBlock = styled.div`
+export const AmountInputBlock = styled.div`
   display: flex;
   flex-direction: column;
 `
@@ -253,11 +162,11 @@ const AmountButtons = styled.div`
 `
 const AmountButton = styled(ButtonSecondarySmall)``
 
-const TransactionInfo = styled.div`
+export const TransactionInfo = styled.div`
   display: flex;
   flex-direction: column;
 `
-const TransactionInfoRow = styled.div`
+export const TransactionInfoRow = styled.div`
   display: grid;
   grid-template-columns: 1fr 128px;
   grid-template-rows: 1fr;
@@ -266,7 +175,7 @@ const TransactionInfoRow = styled.div`
     margin-top: 4px;
   }
 `
-const InfoTitle = styled.span`
+export const InfoTitle = styled.span`
   font-size: 10px;
   line-height: 16px;
   font-weight: 700;
@@ -274,7 +183,7 @@ const InfoTitle = styled.span`
   text-align: right;
   color: ${Colors.Black[400]};
 `
-const InfoValue = styled.span`
+export const InfoValue = styled.span`
   display: grid;
   position: relative;
   text-align: right;
