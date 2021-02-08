04739a369e034f65839fef5304be787d81fc8a1f
Maciej Gołaszewski <jodator@jodator.net>
2021-02-08 17:31:30 +0100

    Add body and footer to modal

--- packages/ui/src/modals/AddMembershipModal.tsx
+++ packages/ui/src/modals/AddMembershipModal.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { Modal, ModalHeader } from '../components/Modal'
+import { ButtonPrimaryMedium } from '../components/buttons'
+import { Modal, ModalBody, ModalFooter, ModalHeader } from '../components/Modal'
+import { Row } from './common'
 
 interface MembershipModalProps {
   onClose: () => void
@@ -10,10 +12,22 @@ type ModalState = 'Create' | 'Authorize'
 export const AddMembershipModal = ({ onClose }: MembershipModalProps) => {
   const [state] = useState<ModalState>('Create')
 
+  const onClick = () => {
+    /**/
+  }
+
   if (state === 'Create') {
     return (
       <Modal>
         <ModalHeader onClick={onClose} title="Add membership" />
+        <ModalBody>
+          <Row>Row</Row>
+        </ModalBody>
+        <ModalFooter>
+          <ButtonPrimaryMedium onClick={onClick} disabled>
+            Create a Membership
+          </ButtonPrimaryMedium>
+        </ModalFooter>
       </Modal>
     )
   }
