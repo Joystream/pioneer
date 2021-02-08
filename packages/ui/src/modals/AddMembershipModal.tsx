b9e92a810dd874c8a9a695e641a59bf1a46abaeb
Maciej Gołaszewski <jodator@jodator.net>
2021-02-08 17:28:44 +0100

    Add create step

--- packages/ui/src/modals/AddMembershipModal.tsx
+++ packages/ui/src/modals/AddMembershipModal.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal, ModalHeader } from '../components/Modal'
 
 interface MembershipModalProps {
   onClose: () => void
 }
 
+type ModalState = 'Create' | 'Authorize'
+
 export const AddMembershipModal = ({ onClose }: MembershipModalProps) => {
+  const [state] = useState<ModalState>('Create')
+
+  if (state === 'Create') {
+    return (
+      <Modal>
+        <ModalHeader onClick={onClose} title="Add membership" />
+      </Modal>
+    )
+  }
+
   return (
     <Modal>
-      <ModalHeader onClick={onClose} title="Add membership" />
+      <ModalHeader onClick={onClose} title="Authorize transaction" />
     </Modal>
   )
 }
