4ce7651896955cd10f97f138633cc585996ac223
Maciej Gołaszewski <jodator@jodator.net>
2021-02-09 16:14:14 +0100

    Add add member form fields

--- packages/ui/src/modals/AddMembershipModal.tsx
+++ packages/ui/src/modals/AddMembershipModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { ButtonPrimaryMedium } from '../components/buttons'
 import { Modal, ModalBody, ModalFooter, ModalHeader } from '../components/Modal'
+import { SelectAccount } from '../components/selects/AccountSelectTemplate/SelectAccount'
 import { Row } from './common'
 
 interface MembershipModalProps {
@@ -21,9 +22,46 @@ export const AddMembershipModal = ({ onClose }: MembershipModalProps) => {
       <Modal>
         <ModalHeader onClick={onClose} title="Add membership" />
         <ModalBody>
-          <Row>Row</Row>
+          <Row>
+            <label>I was referred by a member Yes/No</label>
+            <input type="text" value="Select Member or type a member" />
+            <p>Please fill in all the details below.</p>
+          </Row>
+
+          <Row>
+            <label>Root account (?) *</label>
+            <SelectAccount options={[]} onChange={console.log} />
+          </Row>
+
+          <Row>
+            <label>Controller account (?) *</label>
+            <SelectAccount options={[]} onChange={console.log} />
+          </Row>
+
+          <Row>
+            <label>Member Name (?) *</label>
+            <input type="text" />
+          </Row>
+
+          <Row>
+            <label>Membership handle (?) *</label>
+            <input type="text" />
+          </Row>
+
+          <Row>
+            <label>About Member *</label>
+            <input type="text" />
+          </Row>
+
+          <Row>
+            <label>Member Avatar *</label>
+            <input type="text" />
+          </Row>
         </ModalBody>
         <ModalFooter>
+          <label>
+            <input type="checkbox" />I agree to our Terms of Service and Privacy Policy.
+          </label>
           <ButtonPrimaryMedium onClick={onClick} disabled>
             Create a Membership
           </ButtonPrimaryMedium>
