86322a551c2983a2ff9b4fd367308c1c47390c26
Maciej Gołaszewski <jodator@jodator.net>
2021-02-11 16:48:12 +0100

    Add tests for modal

---
+++ packages/ui/test/modals/AddMembershipModal.test.tsx
@@ -0,0 +1,87 @@
+import { ApiRx } from '@polkadot/api'
+import { Keyring } from '@polkadot/ui-keyring/Keyring'
+import { cryptoWaitReady } from '@polkadot/util-crypto'
+import { render } from '@testing-library/react'
+import BN from 'bn.js'
+import { expect } from 'chai'
+import { set } from 'lodash'
+import React from 'react'
+import { from, of } from 'rxjs'
+import sinon from 'sinon'
+import { Account } from '../../src/hooks/types'
+import * as useAccountsModule from '../../src/hooks/useAccounts'
+import { AddMembershipModal } from '../../src/modals/AddMembershipModal'
+import { ApiContext } from '../../src/providers/api/context'
+import { UseApi } from '../../src/providers/api/provider'
+import { KeyringContext } from '../../src/providers/keyring/context'
+
+import { aliceSigner, bobSigner, mockKeyring } from '../mocks/keyring'
+
+describe('UI: AddMembershipModal', () => {
+  before(async () => {
+    await cryptoWaitReady()
+  })
+
+  const api: UseApi = {
+    api: ({} as unknown) as ApiRx,
+    isConnected: true,
+  }
+  let fromAccount: Account
+  let to: Account
+  let accounts: {
+    hasAccounts: boolean
+    allAccounts: Account[]
+  }
+  let query: any
+  let keyring: Keyring
+
+  beforeEach(() => {
+    keyring = mockKeyring()
+    fromAccount = {
+      address: aliceSigner().address,
+      name: 'alice',
+    }
+    to = {
+      address: bobSigner().address,
+      name: 'bob',
+    }
+    set(api, 'api.derive.balances.all', () =>
+      from([
+        {
+          availableBalance: new BN(1000),
+          lockedBalance: new BN(0),
+        },
+      ])
+    )
+    query = {}
+
+    set(query, 'members.membershipPrice', () => of(set({}, 'toBn', () => new BN(25))))
+    set(api, 'api.query', query)
+
+    accounts = {
+      hasAccounts: true,
+      allAccounts: [fromAccount, to],
+    }
+    sinon.stub(useAccountsModule, 'useAccounts').returns(accounts)
+  })
+
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  it('Renders a modal', () => {
+    const { getByText } = renderModal()
+
+    expect(getByText('Add membership')).to.exist
+  })
+
+  function renderModal() {
+    return render(
+      <KeyringContext.Provider value={keyring}>
+        <ApiContext.Provider value={api}>
+          <AddMembershipModal onClose={sinon.spy()} />
+        </ApiContext.Provider>
+      </KeyringContext.Provider>
+    )
+  }
+})
