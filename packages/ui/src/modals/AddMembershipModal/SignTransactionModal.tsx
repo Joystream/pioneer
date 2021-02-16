595eb93beec3c853e89c682b836a055b8dbbb98d
Maciej Gołaszewski <jodator@jodator.net>
2021-02-16 11:37:03 +0100

    Deduplicate transaction signing

--- packages/ui/src/modals/AddMembershipModal/SignTransactionModal.tsx
+++ packages/ui/src/modals/AddMembershipModal/SignTransactionModal.tsx
@@ -1,3 +1,4 @@
+import { SubmittableExtrinsic } from '@polkadot/api/types'
 import { web3FromAddress } from '@polkadot/extension-dapp'
 import { BalanceOf } from '@polkadot/types/interfaces/runtime'
 import { ISubmittableResult } from '@polkadot/types/types'
@@ -10,6 +11,7 @@ import { Help } from '../../components/Help'
 import { Modal, ModalBody, ModalFooter, ModalHeader } from '../../components/Modal'
 import { SelectAccount } from '../../components/selects/SelectAccount'
 import { Text, TokenValue } from '../../components/typography'
+import { Account } from '../../hooks/types'
 import { useApi } from '../../hooks/useApi'
 import { useKeyring } from '../../hooks/useKeyring'
 import { useObservable } from '../../hooks/useObservable'
@@ -23,27 +25,23 @@ interface SignProps {
   onSign: (transaction: Observable<ISubmittableResult>, fee: BN) => void
 }
 
-export const SignTransactionModal = ({ onClose, membershipPrice, transactionParams, onSign }: SignProps) => {
-  const { api } = useApi()
+export function useSignAndSendTransaction(
+  transfer: SubmittableExtrinsic<'rxjs'> | undefined,
+  from: Account,
+  onSign: (transaction: Observable<ISubmittableResult>, fee: BN) => void
+) {
+  const [isSending, setIsSending] = useState(false)
   const keyring = useKeyring()
-  const [from, setFrom] = useState(transactionParams.controllerAccount)
-  const transfer = api?.tx?.members?.buyMembership({
-    root_account: transactionParams.rootAccount.address,
-    controller_account: transactionParams.controllerAccount.address,
-    name: transactionParams.name,
-    handle: transactionParams.handle,
-    avatar_uri: transactionParams.avatar,
-    about: transactionParams.about,
-  })
-  const info = useObservable(transfer?.paymentInfo(from.address), [api, from])
-  const [isSending, setSending] = useState(false)
+  const { api } = useApi()
+  const paymentInfo = useObservable(transfer?.paymentInfo(from.address), [from])
+
   useEffect(() => {
-    if (!isSending || !transfer || !info) {
+    if (!isSending || !transfer || !paymentInfo) {
       return
     }
 
     const keyringPair = keyring.getPair(from.address)
-    const fee = info.partialFee.toBn()
+    const fee = paymentInfo.partialFee.toBn()
 
     if (keyringPair.meta.isInjected) {
       web3FromAddress(from.address).then(({ signer }) => {
@@ -54,7 +52,22 @@ export const SignTransactionModal = ({ onClose, membershipPrice, transactionPara
     }
   }, [api, isSending])
 
-  const transactionFee = info?.partialFee.toBn()
+  return { isSending, send: () => setIsSending(true), paymentInfo }
+}
+
+export const SignTransactionModal = ({ onClose, membershipPrice, transactionParams, onSign }: SignProps) => {
+  const { api } = useApi()
+  const [from, setFrom] = useState(transactionParams.controllerAccount)
+  const transfer = api?.tx?.members?.buyMembership({
+    root_account: transactionParams.rootAccount.address,
+    controller_account: transactionParams.controllerAccount.address,
+    name: transactionParams.name,
+    handle: transactionParams.handle,
+    avatar_uri: transactionParams.avatar,
+    about: transactionParams.about,
+  })
+
+  const { paymentInfo, isSending, send } = useSignAndSendTransaction(transfer, from, onSign)
 
   return (
     <Modal modalSize="m" modalHeight="s">
@@ -65,7 +78,7 @@ export const SignTransactionModal = ({ onClose, membershipPrice, transactionPara
           The creation of the new membership costs <TokenValue value={membershipPrice?.toBn()} />
         </Text>
         <Text>
-          Fees of <TokenValue value={transactionFee} /> will be applied to the transaction
+          Fees of <TokenValue value={paymentInfo?.partialFee.toBn()} /> will be applied to the transaction
         </Text>
         <Row>
           <Label>Sending from account</Label>
@@ -81,16 +94,11 @@ export const SignTransactionModal = ({ onClose, membershipPrice, transactionPara
           <Help helperText={'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'} />
           <InfoTitle>Transaction fee:</InfoTitle>
           <InfoValue>
-            <TokenValue value={transactionFee} />
+            <TokenValue value={paymentInfo?.partialFee.toBn()} />
           </InfoValue>
           <Help helperText={'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'} />
         </BalanceInfoNarrow>
-        <ButtonPrimaryMedium
-          onClick={() => {
-            setSending(true)
-          }}
-          disabled={isSending}
-        >
+        <ButtonPrimaryMedium onClick={send} disabled={isSending}>
           Sign and create a member
         </ButtonPrimaryMedium>
       </ModalFooter>
