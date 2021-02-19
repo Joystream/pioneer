0d1dfbdb54c308651c8b8c0d0992868be4aeb71d
Maciej Gołaszewski <jodator@jodator.net>
2021-02-19 13:20:25 +0100

    Use ApolloClient to query miragejs server

--- packages/ui/src/hooks/useMembership.ts
+++ packages/ui/src/hooks/useMembership.ts
@@ -1,7 +1,17 @@
+import { gql, useQuery } from '@apollo/client'
+
 interface UseMembership {
   count: number
 }
 
+const query = gql`
+  query GetMemberships {
+    id
+  }
+`
+
 export function useMembership(): UseMembership {
+  useQuery(query)
+
   return { count: 0 }
 }
