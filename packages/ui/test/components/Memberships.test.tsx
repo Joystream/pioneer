9a8cf5913d42b172d7240ae38d61c3bd0b4e9491
Maciej Gołaszewski <jodator@jodator.net>
2021-02-24 16:30:54 +0100

    Add tests for no members case

---
+++ packages/ui/test/components/Memberships.test.tsx
@@ -0,0 +1,39 @@
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
+import { render } from '@testing-library/react'
+import { Server } from 'miragejs/server'
+import React from 'react'
+import { CurrentMember } from '../../src/components/page/Sidebar/CurrentMember'
+import { makeServer } from '../../src/mocks/server'
+
+describe('UI: Memberships component', () => {
+  let server: Server
+
+  beforeEach(() => {
+    server = makeServer('test')
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+
+  describe('with no memberships', () => {
+    it('Displays create button', () => {
+      const { getByRole } = renderComponent()
+
+      expect(getByRole('button', { name: /create a membership/i })).toBeDefined()
+    })
+  })
+
+  function renderComponent() {
+    const link = new HttpLink({
+      uri: '/query-node',
+      fetch: (uri, options) => fetch(uri, options),
+    })
+
+    return render(
+      <ApolloProvider client={new ApolloClient({ link, cache: new InMemoryCache() })}>
+        <CurrentMember />
+      </ApolloProvider>
+    )
+  }
+})
