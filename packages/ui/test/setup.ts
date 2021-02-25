6a7322fd7dac9824835efa59ab2820708b041ecd
Maciej Gołaszewski <jodator@jodator.net>
2021-02-25 18:47:53 +0100

    Cleanup Jest config

--- packages/ui/test/setup.ts
+++ packages/ui/test/setup.ts
@@ -6,5 +6,3 @@ require.extensions['.svg'] = () => 'test-image.svg'
 require.extensions['.png'] = () => 'test-image.png'
 require.extensions['.jpg'] = () => 'test-image.jpg'
 require.extensions['.woff2'] = () => 'test-font.woff2'
-
-export default async () => undefined
