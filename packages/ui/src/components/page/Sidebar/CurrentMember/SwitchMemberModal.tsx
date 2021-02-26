2efc96327e924537644f2663a14d7642fc98b3d1
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2021-02-26 07:09:03 +0100

    🥸 Select member (#177)
    * Select member modal
    
    * Focus styles for add/select member
    
    * Use shorter imports
    
    Co-authored-by: Maciej Gołaszewski <jodator@jodator.net>

--- packages/ui/src/components/page/Sidebar/CurrentMember/SwitchMemberModal.tsx
+++ packages/ui/src/components/page/Sidebar/CurrentMember/SwitchMemberModal.tsx
@@ -1,45 +1,110 @@
 import React from 'react'
 import styled from 'styled-components'
 import { MemberFieldsFragment } from '../../../../api/queries'
-import { Colors } from '../../../../constants'
+import { AddMembershipButtonSwitch } from '../../../../components/AddMembershipButtonSwitch'
+import { MembershipsCount } from '../../../../components/MembershipCount'
+import { BorderRad, Colors, Transitions } from '../../../../constants'
 import { useMembership } from '../../../../hooks/useMembership'
-import { Row } from '../../../../modals/common'
-import { MemberInfo } from '../../../MemberInfo'
-import { Modal, ModalBody } from '../../../Modal'
-import { Badge } from '../../../typography'
+import { MemberDarkHover, MemberInfo } from '../../../MemberInfo'
+import { CloseSmallModalButton, Modal, ModalBody, ModalFooter, ModalTitle } from '../../../Modal'
+import { Notification, NotificationComponent } from '../../../Notification'
 
 interface Props {
   onClose: () => void
 }
 
 export const SwitchMemberModal = ({ onClose }: Props) => {
-  const { count, members, setActive } = useMembership()
+  const { members, setActive } = useMembership()
   const switchMember = (member: MemberFieldsFragment) => {
     setActive(member)
     onClose()
   }
 
   return (
-    <Modal modalSize="s" modalHeight="s" isDark>
-      <ModalBody>
-        <h3>
-          My memberships: <Badge>{count}</Badge>
-        </h3>
-        <div>
+    <Modal modalSize="xs" modalHeight="s" isDark>
+      <SwitchModalBody>
+        <CloseSmallModalButton onClick={onClose} />
+        <SwitchModalTitle>Select Membership</SwitchModalTitle>
+        <MembershipsCount />
+        <MembersList>
           {members.map((member) => (
-            <MemberRow key={member.handle} onClick={() => switchMember(member)}>
-              <MemberInfo member={member} />
-            </MemberRow>
+            <MemberItem key={member.handle} onClick={() => switchMember(member)}>
+              <MemberInfo member={member} isOnDark={true} />
+              <Notification />
+            </MemberItem>
           ))}
-        </div>
-      </ModalBody>
+        </MembersList>
+      </SwitchModalBody>
+      <SwitchModalFooter>
+        <AddMembershipButtonSwitch />
+      </SwitchModalFooter>
     </Modal>
   )
 }
-const MemberRow = styled(Row)`
+
+const SwitchModalBody = styled(ModalBody)`
+  position: relative;
+  padding: 16px;
+  grid-row-gap: 16px;
+
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: calc(100% - 32px);
+    height: 1px;
+    background-color: ${Colors.Black[700]};
+    transform: translateX(-50%);
+  }
+`
+
+const SwitchModalTitle = styled(ModalTitle)`
+  line-height: 40px;
+`
+
+const SwitchModalFooter = styled(ModalFooter)`
+  width: 100%;
+  height: auto;
+  padding: 16px;
+`
+
+const MembersList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  max-height: 192px;
+  overflow: hidden;
+  overflow-y: scroll;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const MemberItem = styled.li`
+  display: grid;
+  grid-template-columns: 1fr 16px;
+  grid-column-gap: 8px;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  max-height: 64px;
+  padding: 12px 16px;
+  border-radius: ${BorderRad.s};
+  transition: ${Transitions.all};
   cursor: pointer;
 
-  &:hover {
-    background: ${Colors.Black[700]};
+  &:hover,
+  &:focus {
+    outline: none;
+    background-color: ${Colors.Black[600]};
+
+    ${NotificationComponent} {
+      color: ${Colors.White};
+    }
+    ${MemberDarkHover}
   }
 `
