b0112bc52ba19a013b3b1c4762ece3d7e07b4c0d
p-sad <sadlik512@gmail.com>
2021-03-02 19:37:40 +0100

    Add descending sorting

--- packages/ui/src/utils/sortAccounts.ts
+++ packages/ui/src/utils/sortAccounts.ts
@@ -5,21 +5,26 @@ import { Balances } from '../common/types'
 export type SortKey = keyof Balances | 'name'
 
 const Comparator = {
-  name: (accountA: Account, accountB: Account) => {
+  name: (descending: boolean) => (accountA: Account, accountB: Account) => {
     const a = accountA.name || ''
     const b = accountB.name || ''
-    if (a < b) {
-      return -1
+    if (a === b) {
+      return 0
     }
-    return a > b ? 1 : 0
+    return (a < b ? -1 : 1) * (descending ? -1 : 1)
   },
-  balance: (balanceMap: AddressToBalanceMap, key: keyof Balances) => (accountA: Account, accountB: Account) => {
+  balance: (balanceMap: AddressToBalanceMap, key: keyof Balances, descending: boolean) => (
+    accountA: Account,
+    accountB: Account
+  ) => {
     const a = balanceMap[accountA.address]?.[key] || new BN(0)
     const b = balanceMap[accountB.address]?.[key] || new BN(0)
-    return a.cmp(b)
+    return a.cmp(b) * (descending ? -1 : 1)
   },
 }
 
-export function sortAccounts(accounts: Account[], balanceMap: AddressToBalanceMap, key: SortKey) {
-  return key === 'name' ? accounts.sort(Comparator.name) : accounts.sort(Comparator.balance(balanceMap, key))
+export function sortAccounts(accounts: Account[], balanceMap: AddressToBalanceMap, key: SortKey, descending = false) {
+  return key === 'name'
+    ? accounts.sort(Comparator.name(descending))
+    : accounts.sort(Comparator.balance(balanceMap, key, descending))
 }
