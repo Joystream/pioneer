e9d001bac1b6d070d711bd219dc61c337f68761a
Piotr Sadlik <sadlik512@gmail.com>
2021-03-03 16:15:07 +0100

    ✂️ Shorten address display (#189)
    * Add shortenAddress function
    
    * Add to account display
    
    * Change default length to reflect the design

--- packages/ui/src/utils/formatters.ts
+++ packages/ui/src/utils/formatters.ts
@@ -5,3 +5,11 @@ const NUMBER_SEPARATOR_REG_EXP = /\B(?=(\d{3})+(?!\d))/g
 export const formatTokenValue = (value: BN | number | undefined) => {
   return new BN(value || 0).toString().replace(NUMBER_SEPARATOR_REG_EXP, ',')
 }
+
+export function shortenAddress(address: string, length = 18) {
+  return length >= address.length
+    ? address
+    : address.substring(0, Math.ceil(length / 2)) +
+        '...' +
+        address.substring(address.length - Math.floor(length / 2), address.length)
+}
--- packages/ui/test/utils/formatters.test.ts
+++ packages/ui/test/utils/formatters.test.ts
@@ -1,6 +1,6 @@
 import { expect } from '@jest/globals'
 import BN from 'bn.js'
-import { formatTokenValue } from '../../src/utils/formatters'
+import { formatTokenValue, shortenAddress } from '../../src/utils/formatters'
 
 describe('formatters', () => {
   describe('formatTokenValue', () => {
@@ -13,6 +13,31 @@ describe('formatters', () => {
       expect(formatTokenValue(new BN('100000'))).toBe('100,000')
       expect(formatTokenValue(new BN('1000000'))).toBe('1,000,000')
       expect(formatTokenValue(new BN('12345678912345679'))).toBe('12,345,678,912,345,679')
+    })
+  })
+
+  describe('shortenAddress', () => {
+    const address = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty'
+
+    it('Shortens addresses', () => {
+      expect(shortenAddress(address)).toEqual('5FHneW46x...hjJM694ty')
+    })
+
+    it('Shortens to specified length', () => {
+      expect(shortenAddress(address, 8)).toEqual('5FHn...94ty')
+      expect(shortenAddress(address, 9)).toEqual('5FHne...94ty')
+    })
+
+    it('Empty string', () => {
+      expect(shortenAddress('')).toEqual('')
+    })
+
+    it('Specified length greater than address length', () => {
+      expect(shortenAddress(address, 99)).toEqual(address)
+    })
+
+    it('Specified length equal to address length', () => {
+      expect(shortenAddress(address, address.length)).toEqual(address)
     })
   })
 })
