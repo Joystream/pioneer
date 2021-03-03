785eb23c1ed7381131eccd66801c30b97c97a4c7
Maciej Gołaszewski <jodator@jodator.net>
2021-03-03 17:04:16 +0100

    Remove unneeded React portals

--- packages/ui/src/modals/WaitModal.tsx
+++ packages/ui/src/modals/WaitModal.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { Loader } from '../components/icons'
 import { WaitingIcon } from '../components/icons/WaitingIcon'
 import { Modal, ModalTitle, ResultModalBody, ResultTextWhite } from '../components/Modal'
@@ -9,15 +8,13 @@ interface Props {
   description: string
 }
 
-export const WaitModal = ({ title, description }: Props) =>
-  ReactDOM.createPortal(
-    <Modal modalSize="xs" modalHeight="s" isDark>
-      <ResultModalBody>
-        <Loader />
-        <WaitingIcon />
-        <ModalTitle>{title}</ModalTitle>
-        <ResultTextWhite size={2}>{description}</ResultTextWhite>
-      </ResultModalBody>
-    </Modal>,
-    document.body
-  )
+export const WaitModal = ({ title, description }: Props) => (
+  <Modal modalSize="xs" modalHeight="s" isDark>
+    <ResultModalBody>
+      <Loader />
+      <WaitingIcon />
+      <ModalTitle>{title}</ModalTitle>
+      <ResultTextWhite size={2}>{description}</ResultTextWhite>
+    </ResultModalBody>
+  </Modal>
+)
