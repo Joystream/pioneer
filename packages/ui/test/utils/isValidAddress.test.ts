417c05be1c60a1b6784fa4f3f91f38f3c226aca7
p-sad <sadlik512@gmail.com>
2021-03-05 16:56:51 +0100

    Add isValidAddress helper

---
+++ packages/ui/test/utils/isValidAddress.test.ts
@@ -0,0 +1,26 @@
+import { isValidAddress } from '../../src/components/selects/SelectAccount/helpers'
+import testKeyring from '@polkadot/keyring/testing'
+
+describe('isValidAddress', () => {
+  const keyring = testKeyring()
+
+  it('Valid: Correct address', () => {
+    const address = '5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc'
+    expect(isValidAddress(address, keyring)).toBeTruthy()
+  })
+
+  it('Invalid: Too short', () => {
+    const address = '5tyW46xGFne2UhjJM694Xgs5mUiveU4sbTyGBzmstUspZC9'
+    expect(isValidAddress(address, keyring)).toBeFalsy()
+  })
+
+  it('Invalid: Too long', () => {
+    const address = '5tyW46x1lGFne2UhjJM694Xgs5mUiveU4sbTyGBzmstUspZC9'
+    expect(isValidAddress(address, keyring)).toBeFalsy()
+  })
+
+  it('Invalid: Illegal character', () => {
+    const address = '5Hp!9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc'
+    expect(isValidAddress(address, keyring)).toBeFalsy()
+  })
+})
