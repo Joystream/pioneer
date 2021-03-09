f73ffb8e3167ad706517b8bb38dc11627584f9c7
Maciej <jodator@jodator.net>
2021-03-09 12:32:22 +0100

    🚫 Handle extension rejections (#228)
    * Refactor transaction signing flow
    
    * Fix code style
    
    * Refactor useSignAndSendTransaction
    
    * Unify onDone callback
    
    * Handle cancelled authorization
    
    * Add deps to useEffect hook

--- packages/ui/src/hooks/useSignAndSendTransaction.ts
+++ packages/ui/src/hooks/useSignAndSendTransaction.ts
@@ -1,43 +1,94 @@
+import React from 'react'
 import { SubmittableExtrinsic } from '@polkadot/api/types'
 import { web3FromAddress } from '@polkadot/extension-dapp'
+import { EventRecord } from '@polkadot/types/interfaces/system'
 import { ISubmittableResult } from '@polkadot/types/types'
 import BN from 'bn.js'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Observable } from 'rxjs'
 import { Account } from '../common/types'
 import { useApi } from './useApi'
 import { useKeyring } from './useKeyring'
 import { useObservable } from './useObservable'
-import { useToggle } from './useToggle'
 
 interface UseSignAndSendTransactionParams {
   transaction: SubmittableExtrinsic<'rxjs'> | undefined
   from: Account
-  onSign: (transaction: Observable<ISubmittableResult>, fee: BN) => void
+  onDone: (success: boolean, fee: BN) => void
 }
 
-export const useSignAndSendTransaction = ({ transaction, from, onSign }: UseSignAndSendTransactionParams) => {
-  const [isSending, toggleSending] = useToggle()
+type TransactionStatus = 'READY' | 'SIGN' | 'EXTENSION' | 'PENDING' | 'SUCCESS' | 'ERROR'
+
+const isError = (events: EventRecord[]) => events.find(({ event: { method } }) => method === 'ExtrinsicFailed')
+
+const observeTransaction = (
+  transaction: Observable<ISubmittableResult>,
+  setStatus: React.Dispatch<TransactionStatus>
+) => {
+  const statusCallback = (result: ISubmittableResult) => {
+    const { status, events } = result
+
+    console.log(`Current transaction status: ${status.type}`)
+
+    if (status.isReady) {
+      setStatus('PENDING')
+    }
+
+    if (status.isInBlock) {
+      console.log('Included at block hash', JSON.stringify(status.asInBlock))
+      console.log('Events:')
+
+      events.forEach(({ event: { data, method, section }, phase }) => {
+        console.log('\t', JSON.stringify(phase), `: ${section}.${method}`, JSON.stringify(data))
+      })
+      console.log(JSON.stringify(events))
+    }
+
+    if (!status.isFinalized) {
+      return
+    }
+
+    setStatus(isError(events) ? 'ERROR' : 'SUCCESS')
+  }
+
+  const errorHandler = () => setStatus('ERROR')
+
+  transaction.subscribe(statusCallback, errorHandler)
+}
+
+export const useSignAndSendTransaction = ({ transaction, from, onDone }: UseSignAndSendTransactionParams) => {
   const keyring = useKeyring()
   const { api } = useApi()
   const paymentInfo = useObservable(transaction?.paymentInfo(from.address), [from])
+  const [status, setStatus] = useState<TransactionStatus>('READY')
 
   useEffect(() => {
-    if (!isSending || !transaction || !paymentInfo) {
+    if (status !== 'SIGN' || !transaction || !paymentInfo) {
       return
     }
 
     const keyringPair = keyring.getPair(from.address)
-    const fee = paymentInfo.partialFee.toBn()
 
     if (keyringPair.meta.isInjected) {
+      setStatus('EXTENSION')
       web3FromAddress(from.address).then(({ signer }) => {
-        onSign(transaction.signAndSend(from.address, { signer: signer }), fee)
+        observeTransaction(transaction.signAndSend(from.address, { signer: signer }), setStatus)
       })
     } else {
-      onSign(transaction.signAndSend(keyringPair), fee)
+      setStatus('PENDING')
+      observeTransaction(transaction.signAndSend(keyringPair), setStatus)
     }
-  }, [api, isSending])
+  }, [api, status])
 
-  return { isSending, send: toggleSending, paymentInfo }
+  useEffect(() => {
+    if (status === 'SUCCESS' || status === 'ERROR') {
+      onDone(status === 'SUCCESS', paymentInfo?.partialFee.toBn() || new BN(0))
+    }
+  }, [status])
+
+  return {
+    send: () => setStatus('SIGN'),
+    paymentInfo,
+    status,
+  }
 }
--- packages/ui/src/modals/TransferModal/TransferModal.tsx
+++ packages/ui/src/modals/TransferModal/TransferModal.tsx
@@ -1,10 +1,6 @@
-import { EventRecord } from '@polkadot/types/interfaces/system'
-import { ISubmittableResult } from '@polkadot/types/types'
 import BN from 'bn.js'
-import React, { ReactElement, useEffect, useState } from 'react'
-import { Observable, Subscription } from 'rxjs'
+import React, { ReactElement, useState } from 'react'
 import { Account } from '../../common/types'
-import { WaitModal } from '../WaitModal'
 import { SignTransferModal } from './SignTransferModal'
 import { TransactionFailureModal } from './TransactionFailureModal'
 import { TransactionSuccessModal } from './TransactionSuccessModal'
@@ -17,8 +13,7 @@ interface Props {
   icon: ReactElement
 }
 
-type ModalState = 'PREPARE' | 'AUTHORIZE' | 'EXTENSION_SIGN' | 'SENDING' | 'SUCCESS' | 'ERROR'
-const isError = (events: EventRecord[]) => events.find(({ event: { method } }) => method === 'ExtrinsicFailed')
+type ModalState = 'PREPARE' | 'AUTHORIZE' | 'SUCCESS' | 'ERROR'
 
 export function TransferModal({ from, to, onClose, icon }: Props) {
   const [step, setStep] = useState<ModalState>('PREPARE')
@@ -26,16 +21,10 @@ export function TransferModal({ from, to, onClose, icon }: Props) {
   const [fee, setFee] = useState<BN>(new BN(0))
   const [transferFrom, setTransferFrom] = useState(from)
   const [transferTo, setTransferTo] = useState<Account | undefined>(to)
-  const [subscription, setSubscription] = useState<Subscription | undefined>(undefined)
+
   const isTransfer = !from && !to
   const isSend = !!from
   const title = isTransfer ? 'Transfer tokens' : isSend ? 'Send tokens' : 'Receive tokens'
-
-  useEffect(() => {
-    if (subscription) {
-      return () => subscription.unsubscribe()
-    }
-  }, [subscription])
 
   const onAccept = (amount: BN, from: Account, to: Account) => {
     setAmount(amount)
@@ -44,40 +33,9 @@ export function TransferModal({ from, to, onClose, icon }: Props) {
     setStep('AUTHORIZE')
   }
 
-  const onSign = (transaction: Observable<ISubmittableResult>, fee: BN) => {
-    if (!transferFrom) {
-      return
-    }
-
-    const statusCallback = (result: ISubmittableResult) => {
-      const { status, events } = result
-
-      console.log(`Current transaction status: ${status.type}`)
-
-      if (status.isReady) {
-        setStep('SENDING')
-      }
-
-      if (status.isInBlock) {
-        console.log('Included at block hash', JSON.stringify(status.asInBlock))
-        console.log('Events:')
-
-        events.forEach(({ event: { data, method, section }, phase }) => {
-          console.log('\t', JSON.stringify(phase), `: ${section}.${method}`, JSON.stringify(data))
-        })
-        console.log(JSON.stringify(events))
-      }
-
-      if (!status.isFinalized) {
-        return
-      }
-
-      setStep(isError(events) ? 'ERROR' : 'SUCCESS')
-    }
-
-    setStep('EXTENSION_SIGN')
+  const onDone = (result: boolean, fee: BN) => {
+    setStep(result ? 'SUCCESS' : 'ERROR')
     setFee(fee)
-    setSubscription(transaction.subscribe(statusCallback))
   }
 
   if (step === 'PREPARE' || !transferTo || !transferFrom) {
@@ -94,29 +52,7 @@ export function TransferModal({ from, to, onClose, icon }: Props) {
   }
 
   if (step === 'AUTHORIZE') {
-    return <SignTransferModal onClose={onClose} from={transferFrom} amount={amount} to={transferTo} onSign={onSign} />
-  }
-
-  if (step === 'EXTENSION_SIGN') {
-    return (
-      <WaitModal
-        title="Waiting for the extension"
-        description={'Please, sign the transaction using external signer app.'}
-        onClose={onClose}
-      />
-    )
-  }
-
-  if (step === 'SENDING') {
-    return (
-      <WaitModal
-        title="Pending transaction"
-        description={
-          'We are waiting for your transaction to be mined. It can takes Lorem ipsum deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim.'
-        }
-        onClose={onClose}
-      />
-    )
+    return <SignTransferModal onClose={onClose} from={transferFrom} amount={amount} to={transferTo} onDone={onDone} />
   }
 
   if (step === 'SUCCESS') {
