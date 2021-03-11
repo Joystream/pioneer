adf0795b994e3b2127847aa87ad8f927481ed73f
ClumsyVlad <mazur097mazur@gmail.com>
2021-03-11 11:20:39 +0100

    Help tooltip styles

--- packages/ui/src/components/Help.tsx
+++ packages/ui/src/components/Help.tsx
@@ -1,48 +1,101 @@
 import React from 'react'
 import styled from 'styled-components'
-import { BorderRad, Colors, Transitions } from '../constants'
+import { Animations, BorderRad, Colors, Transitions } from '../constants'
 import { QuestionIcon } from './icons'
+import { LinkSymbol, LinkSymbolStyle } from './icons/symbols/LinkSymbol'
 
 interface HelpNotificationProps {
   helperText: string
+  helperTitle?: string
+  helperLinkText?: string | React.ReactElement | React.ReactNode
+  helperLinkURL?: string
 }
 
-export const Help = React.memo(({ helperText }: HelpNotificationProps) => (
+export const Help = React.memo(({ helperText, helperTitle, helperLinkText, helperLinkURL }: HelpNotificationProps) => (
   <HelpComponent>
     <QuestionIcon />
-    <HelpPopup>{helperText}</HelpPopup>
+    <HelpPopup>
+      {helperTitle && <HelpPopupTitle>{helperTitle}</HelpPopupTitle>}
+      <HelperText>{helperText}</HelperText>
+      {helperLinkURL && (
+        <HelperLink href={helperLinkURL} target="_blank">
+          {helperLinkText ? helperLinkText : 'Link'}
+          <LinkSymbol />
+        </HelperLink>
+      )}
+    </HelpPopup>
   </HelpComponent>
 ))
 
-const HelpPopup = styled.p`
+const HelpPopup = styled.div`
   display: none;
+  flex-direction: column;
+  align-items: flex-start;
   position: absolute;
-  left: calc(100% + 8px);
+  top: 24px;
+  left: -16px;
   width: max-content;
   max-width: 250px;
-  padding: 12px;
-  border: 1px solid ${Colors.Black[100]};
-  background-color: ${Colors.White};
-  border-radius: ${BorderRad.s};
+  padding: 16px 24px;
+  border: 1px solid ${Colors.Black[300]};
+  background-color: ${Colors.Black[75]};
+  border-radius: ${BorderRad.m};
+  transition: ${Transitions.all};
+  visibility: hidden;
+  z-index: 10;
+  ${Animations.showHelperTooltip};
+
+  &:after {
+    content: '';
+    position: absolute;
+    left: 19px;
+    top: -4px;
+    width: 8px;
+    height: 8px;
+    background-color: ${Colors.Black[75]};
+    border: 1px solid ${Colors.Black[300]};
+    transform: rotate(45deg);
+    clip-path: polygon(100% 0, 0 0, 0 100%);
+  }
+`
+
+export const HelpPopupTitle = styled.h6`
+  color: ${Colors.Black[900]};
+  margin-bottom: 10px;
+`
+
+export const HelperText = styled.p`
   font-weight: 400;
   font-size: 10px;
   line-height: 14px;
-  color: ${Colors.Black[400]};
-  transition: ${Transitions.all};
-  visibility: hidden;
-  animation: showHelperNotificationText 0.25s ease;
-  animation-delay: 0.25s;
-  animation-fill-mode: forwards;
-  z-index: 10;
+  color: ${Colors.Black[500]};
+`
 
-  @keyframes showHelperNotificationText {
-    from {
-      opacity: 0;
-      transform: translateX(-8px);
-      visibility: hidden;
-    }
-    to {
-      visibility: visible;
+export const HelperLink = styled.a`
+  display: grid;
+  grid-auto-flow: column;
+  grid-column-gap: 8px;
+  align-items: center;
+  width: fit-content;
+  margin-top: 10px;
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: 400;
+  color: ${Colors.Black[500]};
+
+  ${LinkSymbolStyle} {
+    width: 12px;
+    height: 12px;
+  }
+
+  &:hover {
+    color: ${Colors.Blue[500]};
+
+    ${LinkSymbolStyle} {
+      .blackPart,
+      .primaryPart {
+        color: ${Colors.Blue[500]};
+      }
     }
   }
 `
@@ -56,8 +109,8 @@ export const HelpComponent = styled.button`
   width: 16px;
   height: 16px;
   border-radius: ${BorderRad.round};
-  background-color: ${Colors.Blue[50]};
-  color: ${Colors.Blue[500]};
+  background-color: ${Colors.Black[75]};
+  color: ${Colors.Black[500]};
   cursor: pointer;
   transform: translate(100%);
   transition: ${Transitions.all};
@@ -71,7 +124,7 @@ export const HelpComponent = styled.button`
 
   &:hover,
   &:focus {
-    color: ${Colors.Blue[400]};
+    color: ${Colors.Blue[500]};
 
     ${HelpPopup} {
       display: flex;
---
+++ packages/ui/src/components/icons/symbols/LinkSymbol.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Colors, Transitions } from '../../../constants'
+
+export function LinkSymbol() {
+  return (
+    <LinkSymbolStyle viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none" color="currentColor">
+      <path
+        className="blackPart"
+        d="M1.1001 3.99998L2.0001 3.09998H12.8001V4.89998H2.9001V21.1H19.1001V11.2H20.9001V22L20.0001 22.9H2.0001L1.1001 22V3.99998Z"
+        fill={Colors.Black[900]}
+      />
+      <path
+        className="primaryPart"
+        d="M15.4999 1.09998H21.9999L22.8999 1.99998V8.49998H21.0999V4.17277L10.6363 14.6364L9.36353 13.3636L19.8271 2.89998H15.4999V1.09998Z"
+        fill={Colors.Blue[500]}
+      />
+    </LinkSymbolStyle>
+  )
+}
+
+export const LinkSymbolStyle = styled.svg`
+  height: 16px;
+  width: 16px;
+  position: relative;
+
+  .blackPart {
+    fill: ${Colors.Black[900]};
+    transition: ${Transitions.all};
+  }
+  .primaryPart {
+    fill: ${Colors.Blue[500]};
+    transition: ${Transitions.all};
+  }
+`
