1df60f22277f08aca8ab201283f181e4e5e6eb44
ClumsyVlad <mazur097mazur@gmail.com>
2021-03-11 15:32:48 +0100

    Link component

---
+++ packages/ui/src/components/Link.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import { Colors, Transitions } from '../constants/styles'
+
+export const Link = styled.a`
+  color: ${Colors.Blue[400]};
+  text-decoration: underline;
+  text-underline-offset: 1px;
+  transition: ${Transitions.all};
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    color: ${Colors.Blue[500]};
+  }
+  &:active {
+    color: ${Colors.Blue[600]};
+  }
+`
