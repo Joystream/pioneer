79125c9f074b6e37bf5e20cb819f58826cd91b05
p-sad <sadlik512@gmail.com>
2021-03-15 12:36:01 +0100

    Separate abstraction layers

--- packages/ui/src/utils/sorting/sortAccounts.ts
+++ packages/ui/src/utils/sorting/sortAccounts.ts
@@ -1,29 +1,11 @@
-import BN from 'bn.js'
 import { Account, AddressToBalanceMap } from 'src/common/types'
 import { Balances } from '../../common/types'
+import { BalanceComparator, Comparator } from './Comparator'
 
 export type SortKey = keyof Balances | 'name'
 
-const applyOrder = (order: number, isDescending: boolean) => order * (isDescending ? -1 : 1)
-
-const Comparator = {
-  name: (isDescending: boolean) => (accountA: Account, accountB: Account) => {
-    const a = accountA.name || ''
-    const b = accountB.name || ''
-    return applyOrder(a.localeCompare(b), isDescending)
-  },
-  balance: (balanceMap: AddressToBalanceMap, key: keyof Balances, isDescending: boolean) => (
-    accountA: Account,
-    accountB: Account
-  ) => {
-    const a = balanceMap[accountA.address]?.[key] || new BN(0)
-    const b = balanceMap[accountB.address]?.[key] || new BN(0)
-    return applyOrder(a.cmp(b), isDescending)
-  },
-}
-
 export function sortAccounts(accounts: Account[], balanceMap: AddressToBalanceMap, key: SortKey, isDescending = false) {
   return key === 'name'
-    ? accounts.sort(Comparator.name(isDescending))
-    : accounts.sort(Comparator.balance(balanceMap, key, isDescending))
+    ? accounts.sort(Comparator<Account>(isDescending, key).string)
+    : accounts.sort(BalanceComparator(balanceMap, key, isDescending))
 }
