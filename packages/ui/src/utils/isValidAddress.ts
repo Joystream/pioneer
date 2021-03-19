b951f121bda94e41f3fe0022a1ddcd87a59bd214
p-sad <sadlik512@gmail.com>
2021-03-19 16:04:42 +0100

    Move isValidAddress to utils

---
+++ packages/ui/src/utils/isValidAddress.ts
@@ -0,0 +1,12 @@
+import { KeyringInstance } from '@polkadot/keyring/types'
+import { KeyringStruct } from '@polkadot/ui-keyring/types'
+import { Address } from '../common/types'
+
+export function isValidAddress(address: Address, keyring: KeyringInstance | KeyringStruct) {
+  try {
+    keyring.encodeAddress(keyring.decodeAddress(address))
+  } catch (e) {
+    return false
+  }
+  return true
+}
