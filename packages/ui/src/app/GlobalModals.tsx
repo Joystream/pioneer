cac142dbd7fa2504009e2a2517590a620b5b0992
Maciej Gołaszewski <jodator@jodator.net>
2021-03-31 12:03:12 +0200

    Add ModalName type

--- packages/ui/src/app/GlobalModals.tsx
+++ packages/ui/src/app/GlobalModals.tsx
@@ -3,12 +3,16 @@ import { MemberProfile } from '../components/membership/MemberProfile'
 import { useModal } from '../hooks/useModal'
 import { AddMembershipModal } from '../modals/AddMembershipModal'
 import { TransferInviteModal } from '../modals/TransferInviteModal'
+import { BuyMembershipModal, MemberModal, TransferInvitesModal } from '../providers/modal/provider'
+import { ModalName } from '../providers/modal/types'
+
+type ModalNames = ModalName<TransferInvitesModal> | ModalName<MemberModal> | ModalName<BuyMembershipModal>
 
 export const GlobalModals = () => {
   const { modal } = useModal()
 
-  switch (modal) {
-    case 'member':
+  switch (modal as ModalNames) {
+    case 'Member':
       return <MemberProfile />
     case 'BuyMembership':
       return <AddMembershipModal />
--- packages/ui/src/providers/modal/types.ts
+++ packages/ui/src/providers/modal/types.ts
@@ -9,6 +9,7 @@ export interface ModalWithDataCall<M, D> extends ModalCall<M> {
 export type AnyModalCall = ModalCall<string> | ModalWithDataCall<string, never>
 
 export type ModalCallData<Call> = Call extends ModalWithDataCall<any, infer Data> ? Data : never
+export type ModalName<Call> = Call extends ModalCall<infer Name> ? Name : never
 
 export interface UseModal<Data> {
   modal: string | null
