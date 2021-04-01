deca9996186ed9cd736bc8b4644083a9b4b24ca6
Maciej Gołaszewski <jodator@jodator.net>
2021-04-01 13:21:26 +0200

    Add working group budget check

--- packages/ui/src/modals/InviteMemberModal/InviteMemberModal.tsx
+++ packages/ui/src/modals/InviteMemberModal/InviteMemberModal.tsx
@@ -1,11 +1,14 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { Member, ModalState } from '../../common/types'
 import { useAccounts } from '../../hooks/useAccounts'
 import { useApi } from '../../hooks/useApi'
+import { useObservable } from '../../hooks/useObservable'
 import { AddMembershipFailureModal } from '../AddMembershipModal/AddMembershipFailureModal'
 import { AddMembershipSuccessModal } from '../AddMembershipModal/AddMembershipSuccessModal'
 import { SignCreateMemberModal } from '../AddMembershipModal/SignCreateMemberModal'
+import { WaitModal } from '../WaitModal'
 import { InviteFormModal } from './InviteFormModal'
+import { WorkingGroupBudgetModal } from './WorkingGroupBudgetModal'
 
 interface MembershipModalProps {
   onClose: () => void
@@ -14,12 +17,13 @@ interface MembershipModalProps {
 export function InviteMemberModal({ onClose }: MembershipModalProps) {
   const { api } = useApi()
   const { allAccounts } = useAccounts()
-  const [step, setStep] = useState<ModalState>('PREPARE')
+  const [step, setStep] = useState<ModalState>('REQUIREMENTS_CHECK')
   const [transactionParams, setParams] = useState<Member>()
   const onSubmit = (params: Member) => {
     setStep('AUTHORIZE')
     setParams(params)
   }
+  const onDone = (result: boolean) => setStep(result ? 'SUCCESS' : 'ERROR')
 
   const transaction = useMemo(
     () =>
@@ -38,12 +42,29 @@ export function InviteMemberModal({ onClose }: MembershipModalProps) {
         : undefined,
     [JSON.stringify(transactionParams)]
   )
+  const workingGroupBudget = useObservable(api?.query.membershipWorkingGroup.budget(), [])
+  const membershipPrice = useObservable(api?.query.members.membershipPrice(), [])
+
+  useEffect(() => {
+    if (step === 'REQUIREMENTS_CHECK' && workingGroupBudget && membershipPrice) {
+      const isBudgetOK = workingGroupBudget.toBn().gte(membershipPrice.toBn())
+
+      setStep(isBudgetOK ? 'PREPARE' : 'REQUIREMENTS_FAIL')
+    }
+  }, [workingGroupBudget, membershipPrice])
+
+  if (step === 'REQUIREMENTS_CHECK') {
+    return <WaitModal onClose={onClose} title="Loading..." description="" />
+  }
+
+  if (step === 'REQUIREMENTS_FAIL') {
+    return <WorkingGroupBudgetModal onClose={onClose} />
+  }
 
   if (step == 'PREPARE' || !transactionParams) {
     return <InviteFormModal onClose={onClose} onSubmit={onSubmit} />
   }
 
-  const onDone = (result: boolean) => setStep(result ? 'SUCCESS' : 'ERROR')
   const initialSigner =
     allAccounts.find((a) => a.address == transactionParams.invitor?.controllerAccount) ||
     transactionParams.controllerAccount
