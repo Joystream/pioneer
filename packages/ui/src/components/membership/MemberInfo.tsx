30baebf7fa2da37a2c58b6feaf56eb28c3ba1c44
ClumsyVlad <mazur097mazur@gmail.com>
2021-04-01 12:19:55 +0200

    Move edit member button to modal header

--- packages/ui/src/components/membership/MemberInfo.tsx
+++ packages/ui/src/components/membership/MemberInfo.tsx
@@ -19,27 +19,30 @@ interface MemberInfoContainerProps {
   member: BaseMember
   onClick?: () => void
   size?: 'm' | 'l'
+  className?: string
 }
 
 type MemberInfoProps = MemberInfoContainerProps & MemberInfoWrapProps
 
-export const MemberInfo = React.memo(({ member, onClick, isOnDark, showId, memberSize, size }: MemberInfoProps) => {
-  return (
-    <MemberInfoWrap isOnDark={isOnDark} memberSize={memberSize}>
-      <MemberPhoto>
-        <Avatar avatarUri={member.avatarUri} />
-      </MemberPhoto>
-      <MemberHandle onClick={onClick}>{member.handle}</MemberHandle>
-      <MemberIcons>
-        {member.isVerified && <MemberStatusHelp icon={<VerifiedMemberIcon />} helperText="Lorem fishy" />}
-        {false && <MemberStatusHelp icon={<FounderMemberIcon />} helperText="Lorem fishy" />}
-      </MemberIcons>
-      {!showId && (
-        <MemberRoles>
-          <MemberRoleHelp memberRole="LI" helperText="Lorem fishy" size={size} />
-        </MemberRoles>
-      )}
-      {showId && <MemberId size={3}>Worker ID: {member.id}</MemberId>}
-    </MemberInfoWrap>
-  )
-})
+export const MemberInfo = React.memo(
+  ({ member, onClick, isOnDark, showId, memberSize, size, className }: MemberInfoProps) => {
+    return (
+      <MemberInfoWrap isOnDark={isOnDark} memberSize={memberSize} className={className}>
+        <MemberPhoto>
+          <Avatar avatarUri={member.avatarUri} />
+        </MemberPhoto>
+        <MemberHandle onClick={onClick}>{member.handle}</MemberHandle>
+        <MemberIcons>
+          {member.isVerified && <MemberStatusHelp icon={<VerifiedMemberIcon />} helperText="Lorem fishy" />}
+          {false && <MemberStatusHelp icon={<FounderMemberIcon />} helperText="Lorem fishy" />}
+        </MemberIcons>
+        {!showId && (
+          <MemberRoles>
+            <MemberRoleHelp memberRole="LI" helperText="Lorem fishy" size={size} />
+          </MemberRoles>
+        )}
+        {showId && <MemberId size={3}>Worker ID: {member.id}</MemberId>}
+      </MemberInfoWrap>
+    )
+  }
+)
