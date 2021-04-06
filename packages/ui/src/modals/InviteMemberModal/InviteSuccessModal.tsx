5a14a5e63c49946cc9e6aeb4da156f5eae656424
Maciej Gołaszewski <jodator@jodator.net>
2021-04-06 10:11:24 +0200

    👨‍🔧 Fix linter

--- packages/ui/src/modals/InviteMemberModal/InviteSuccessModal.tsx
+++ packages/ui/src/modals/InviteMemberModal/InviteSuccessModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+
 import { useGetMemberQuery } from '../../api/queries'
 import { BaseMember, Member } from '../../common/types'
 import { SuccessIcon } from '../../components/icons'
