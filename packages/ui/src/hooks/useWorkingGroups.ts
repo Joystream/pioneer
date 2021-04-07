783c70f5640711895dffdc46b4c97c8bc9f3584c
Maciej Gołaszewski <jodator@jodator.net>
2021-04-07 08:49:13 +0200

    Introduce useWorkingGroups hook

---
+++ packages/ui/src/hooks/useWorkingGroups.ts
@@ -0,0 +1,26 @@
+export const useWorkingGroups = () => {
+  return {
+    isLoading: false,
+    groups: [
+      {
+        leaderAddress: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
+        groupTitle: 'Distribution',
+        groupContent:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      },
+      {
+        groupImage: 'https://s.gitcoin.co/static/v2/card/thumb.0a0be2e5841a.jpg',
+        leaderAddress: '5Grwasdaqqwb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+        groupTitle: 'Storage',
+        groupContent:
+          "👌🏻 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      },
+      {
+        leaderAddress: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+        groupTitle: 'Forum',
+        groupContent:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      },
+    ],
+  }
+}
--- packages/ui/src/pages/WorkingGroups/WorkingGroups/WorkingGroups.tsx
+++ packages/ui/src/pages/WorkingGroups/WorkingGroups/WorkingGroups.tsx
@@ -8,11 +8,18 @@ import { Tabs } from '../../../components/page/PageTabs'
 import { PageTitle } from '../../../components/page/PageTitle'
 import { SideBar } from '../../../components/page/SideBar'
 import { Breadcrumbs } from '../../../components/page/Sidebar/Breadcrumbs/Breadcrumbs'
+import { useWorkingGroups } from '../../../hooks/useWorkingGroups'
 import { GroupsContainer } from '../Components'
 import { WorkingGroupsList } from './WorkingGroupsList'
 
-export function WorkingGroups() {
+export const WorkingGroups = () => {
   const history = useHistory()
+  const { isLoading, groups } = useWorkingGroups()
+
+  if (isLoading) {
+    return null
+  }
+
   return (
     <Page>
       <SideBar />
@@ -28,29 +35,7 @@ export function WorkingGroups() {
             <PageTitle>Working Groups</PageTitle>
             <Tabs tabs={[{ inner: 'Working Groups', active: true, onClick: () => history.push('/groups') }]} />
           </PageHeader>
-          <WorkingGroupsList
-            groups={[
-              {
-                leaderAddress: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
-                groupTitle: 'Distribution',
-                groupContent:
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-              },
-              {
-                groupImage: 'https://s.gitcoin.co/static/v2/card/thumb.0a0be2e5841a.jpg',
-                leaderAddress: '5Grwasdaqqwb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-                groupTitle: 'Storage',
-                groupContent:
-                  "👌🏻 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-              },
-              {
-                leaderAddress: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-                groupTitle: 'Forum',
-                groupContent:
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-              },
-            ]}
-          />
+          <WorkingGroupsList groups={groups} />
         </GroupsContainer>
       </PageContent>
     </Page>
