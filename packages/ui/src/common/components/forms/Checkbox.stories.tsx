84d37e79e1bd1cff7bb499ff5993cb4e2fb9d2c8
ClumsyVlad <mazur097mazur@gmail.com>
2021-04-08 15:22:58 +0200

    Remove useless states from checkbox stories

--- packages/ui/src/common/components/forms/Checkbox.stories.tsx
+++ packages/ui/src/common/components/forms/Checkbox.stories.tsx
@@ -18,40 +18,12 @@ const Template: Story<CheckboxProps> = (args) => (
         <Checkbox {...args} id="id0" />
       </Row>
       <Row>
-        Enabled = false
+        Disabled
         <Checkbox {...args} id="id1" enabled={false} />
       </Row>
       <Row>
         Checked
         <Checkbox {...args} id="id2" isChecked />
-      </Row>
-      <Row>
-        Checked = true
-        <Checkbox {...args} id="id3" isChecked={true} />
-      </Row>
-      <Row>
-        Checked = false
-        <Checkbox {...args} id="id4" isChecked={false} />
-      </Row>
-      <Row>
-        Checked = undefined
-        <Checkbox {...args} id="id5" isChecked={undefined} />
-      </Row>
-      <Row>
-        Required
-        <Checkbox {...args} id="id6" isRequired />
-      </Row>
-      <Row>
-        Required = true
-        <Checkbox {...args} id="id7" isRequired={true} />
-      </Row>
-      <Row>
-        Required = false
-        <Checkbox {...args} id="id9" isRequired={false} />
-      </Row>
-      <Row>
-        Required = undefined
-        <Checkbox {...args} id="id9" isRequired={undefined} />
       </Row>
     </Column>
   </TemplateBlock>
