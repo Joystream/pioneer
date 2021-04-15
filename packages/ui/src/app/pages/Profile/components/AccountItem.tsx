4289a0695e4ad6f72c63fbaeff385f86bc8d4909
Maciej Gołaszewski <jodator@jodator.net>
2021-04-15 09:15:28 +0200

    Add ToggleableItem component

--- packages/ui/src/app/pages/Profile/components/AccountItem.tsx
+++ packages/ui/src/app/pages/Profile/components/AccountItem.tsx
@@ -6,6 +6,7 @@ import { AccountInfo } from '../../../../accounts/components/AccountInfo'
 import { TransferButton } from '../../../../accounts/components/TransferButton'
 import { useBalance } from '../../../../accounts/hooks/useBalance'
 import { Account } from '../../../../accounts/types'
+import { ToggleableItem } from '../../../../common/components/ToggleableItem'
 import { TokenValue } from '../../../../common/components/typography'
 import { Sizes } from '../../../../common/constants'
 
@@ -20,25 +21,34 @@ export const AccountItem = ({ account }: AccountItemDataProps) => {
   const isSendDisabled = !balance?.transferable || !balance.transferable.gt(new BN(0))
 
   return (
-    <AccountItemWrap key={address}>
-      <AccountInfo account={account} />
-      <AccountBalance>
-        <TokenValue value={balance?.total} />
-      </AccountBalance>
-      <AccountBalance>
-        <TokenValue value={balance?.locked} />
-      </AccountBalance>
-      <AccountBalance>
-        <TokenValue value={balance?.recoverable} />
-      </AccountBalance>
-      <AccountBalance>
-        <TokenValue value={balance?.transferable} />
-      </AccountBalance>
-      <AccountControls>
-        <TransferButton to={account} />
-        <TransferButton from={account} disabled={isSendDisabled} />
-      </AccountControls>
-    </AccountItemWrap>
+    <ToggleableItem>
+      {(isOpen) => {
+        return (
+          <div>
+            <AccountItemWrap key={address}>
+              <AccountInfo account={account} />
+              <AccountBalance>
+                <TokenValue value={balance?.total} />
+              </AccountBalance>
+              <AccountBalance>
+                <TokenValue value={balance?.locked} />
+              </AccountBalance>
+              <AccountBalance>
+                <TokenValue value={balance?.recoverable} />
+              </AccountBalance>
+              <AccountBalance>
+                <TokenValue value={balance?.transferable} />
+              </AccountBalance>
+              <AccountControls>
+                <TransferButton to={account} />
+                <TransferButton from={account} disabled={isSendDisabled} />
+              </AccountControls>
+            </AccountItemWrap>
+            {isOpen && <div>I'm Open!</div>}
+          </div>
+        )
+      }}
+    </ToggleableItem>
   )
 }
 
@@ -58,7 +68,7 @@ const AccountBalance = styled.p``
 
 const AccountControls = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 40px);
+  grid-template-columns: repeat(2, 40px);
   grid-template-rows: 40px;
   grid-column-gap: 8px;
 `
---
+++ packages/ui/src/common/components/ToggleableItem.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react'
+
+import { useToggle } from '../hooks/useToggle'
+import { isFunction } from '../utils'
+
+import { Toggle, ToggleButton } from './buttons/Toggle'
+import { ArrowDownIcon } from './icons'
+
+interface ToggleableItemProps {
+  children: ReactNode | ((isOpen: boolean) => ReactNode)
+}
+
+export const ToggleableItem = ({ children }: ToggleableItemProps) => {
+  const [isOpen, toggleOpen] = useToggle()
+
+  return (
+    <Toggle isOpen={isOpen}>
+      {isFunction(children) ? children(isOpen) : children}
+      <ToggleButton onClick={toggleOpen}>
+        <ArrowDownIcon />
+      </ToggleButton>
+    </Toggle>
+  )
+}
