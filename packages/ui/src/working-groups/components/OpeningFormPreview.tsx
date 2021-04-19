d3108d869dc33e91723088129d8f9c2b0d1a2204
Maciej Gołaszewski <jodator@jodator.net>
2021-04-19 13:29:11 +0200

    Use object destructuring

--- packages/ui/src/working-groups/components/OpeningFormPreview.tsx
+++ packages/ui/src/working-groups/components/OpeningFormPreview.tsx
@@ -9,15 +9,15 @@ import { WorkingGroupOpening } from '../types'
 
 export type OpeningFormPreviewProps = { opening: WorkingGroupOpening }
 
-export const OpeningFormPreview = (props: OpeningFormPreviewProps) => {
+export const OpeningFormPreview = ({ opening }: OpeningFormPreviewProps) => {
   const openingStart = '2021-02-09T10:28:04.155Z'
 
   return (
     <>
       <Row>
-        <BadgeViolet>{props.opening.type}</BadgeViolet>
+        <BadgeViolet>{opening.type}</BadgeViolet>
       </Row>
-      <h3>{props.opening.title}</h3>
+      <h3>{opening.title}</h3>
       <p>
         Content Curators will one day be essential for ensuring that the petabytes of media items uploaded to Joystream
         are format...
@@ -25,19 +25,19 @@ export const OpeningFormPreview = (props: OpeningFormPreviewProps) => {
       <span>Show more</span>
       <Row>
         <label>Time left</label>
-        {relativeTime(props.opening.expectedEnding)}
-        <TextMedium>{percentTimeLeft(props.opening.expectedEnding, openingStart)}%</TextMedium>
+        {relativeTime(opening.expectedEnding)}
+        <TextMedium>{percentTimeLeft(opening.expectedEnding, openingStart)}%</TextMedium>
       </Row>
       <Row>
         <label>Applicants</label>
         <TextMedium>
-          {props.opening.hiring.total} / {props.opening.hiring.current}
+          {opening.hiring.total} / {opening.hiring.current}
         </TextMedium>
       </Row>
       <Row>
         <label>Reward</label>
         <TextMedium>
-          <TokenValue value={props.opening.reward.value} /> per {props.opening.reward.interval} blocks
+          <TokenValue value={opening.reward.value} /> per {opening.reward.interval} blocks
         </TextMedium>
       </Row>
       <Row>
