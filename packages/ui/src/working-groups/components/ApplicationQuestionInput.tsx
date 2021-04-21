7908397ed938600abadf1dba85f246f374ffbe5a
Maciej Gołaszewski <jodator@jodator.net>
2021-04-21 12:55:53 +0200

    Add CKEditor to form

--- packages/ui/src/working-groups/components/ApplicationQuestionInput.tsx
+++ packages/ui/src/working-groups/components/ApplicationQuestionInput.tsx
@@ -1,6 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { CKEditor } from '@ckeditor/ckeditor5-react'
 import React from 'react'
 
-import { InputComponent, InputText, InputTextarea } from '../../common/components/forms'
+import { InputComponent, InputText } from '../../common/components/forms'
 import { ApplicationQuestionType } from '../types'
 
 interface ApplicationQuestionInputProps {
@@ -12,7 +18,15 @@ export const ApplicationQuestionInput = ({ question, type }: ApplicationQuestion
   return (
     <InputComponent label={question} required inputSize={type === 'TEXTAREA' ? 'l' : 'm'}>
       {type === 'TEXT' && <InputText />}
-      {type === 'TEXTAREA' && <InputTextarea />}
+      {type === 'TEXTAREA' && (
+        <CKEditor
+          editor={ClassicEditor}
+          onChange={(event: any, editor: any) => {
+            const data = editor.getData()
+            console.log({ event, editor, data })
+          }}
+        />
+      )}
     </InputComponent>
   )
 }
