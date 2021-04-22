4b492946be85df75fc987075b79fb7a92b560043
Maciej Gołaszewski <jodator@jodator.net>
2021-04-22 19:11:57 +0200

    Use alpha version of storybook

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,7 +1,18 @@
 module.exports = {
+  webpackFinal: (config) => {
+    config.resolve = {
+      extensions: ['.tsx', '.ts', '.js'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        path: false,
+      },
+    }
+    return config
+  },
   core: {
     builder: 'webpack5',
   },
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-actions'],
 }
