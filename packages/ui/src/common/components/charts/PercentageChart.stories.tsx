bcc14e49dd1d2ab2ffb5f968eda341f8fa4593a5
ClumsyVlad <mazur097mazur@gmail.com>
2021-04-23 13:41:27 +0200

    Percentage chart component

---
+++ packages/ui/src/common/components/charts/PercentageChart.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { BlackBlock, Column, ModalBlock, Row, TemplateBlock, WhiteBlock } from '../storybookParts/previewStyles'
+
+import { PercentageChartProps, PercentageChart } from './PercentageChart'
+
+export default {
+  title: 'Common/PercentageChart',
+  component: PercentageChart,
+} as Meta
+
+const Template: Story<PercentageChartProps> = (args) => (
+  <ModalBlock>
+    <TemplateBlock>
+      <Row>
+        <Column>
+          <WhiteBlock>
+            <PercentageChart {...args} />
+          </WhiteBlock>
+        </Column>
+        <Column>
+          <BlackBlock>
+            <PercentageChart {...args} onBlack />
+          </BlackBlock>
+        </Column>
+      </Row>
+    </TemplateBlock>
+  </ModalBlock>
+)
+
+export const PercentageChartComponent = Template.bind({})
+
+PercentageChartComponent.args = {
+  percentage: 65,
+}
