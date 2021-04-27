3de3af0073c6075d2dca54425997cf82548fb6c9
Maciej Gołaszewski <jodator@jodator.net>
2021-04-27 09:07:43 +0200

    Use camel case for SVG React component

--- packages/ui/src/common/components/page/Sidebar/LinksIcons/MembersIcon.tsx
+++ packages/ui/src/common/components/page/Sidebar/LinksIcons/MembersIcon.tsx
@@ -11,25 +11,25 @@ export const MembersIcon = () => (
     color="currentColor"
     className="nav-icon"
   >
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M20 18H15V20H20V18ZM13 16V22H20.5L22 20.5V16H13Z" fill="#3F38FF" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M20 18H15V20H20V18ZM13 16V22H20.5L22 20.5V16H13Z" fill="#3F38FF" />
     <path
       className="whitePart"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M4 6H9V4H4V6ZM11 8V2H3.5L2 3.5V8H11Z"
       fill="currentColor"
     />
     <path
       className="whitePart"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M20 4H15V12H20V4ZM13 2V14H22V2H13Z"
       fill="currentColor"
     />
     <path
       className="whitePart"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M9 12H4V20H9V12ZM2 10V22H11V10H2Z"
       fill="currentColor"
     />
