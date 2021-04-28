69cddaa654fd9d8ab7c572c7cce65a9c152705eb
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-04-28 15:59:11 +0200

    Create the useMembers hook

--- packages/ui/src/app/pages/Members/Members.tsx
+++ packages/ui/src/app/pages/Members/Members.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
+import { Loading } from '../../../common/components/Loading'
 import { PageHeader } from '../../../common/components/page/PageHeader'
 import { PageTitle } from '../../../common/components/page/PageTitle'
+import { MemberList } from '../../../memberships/components/MemberList'
+import { useMembers } from '../../../memberships/hooks/useMembers'
 import { AppPage } from '../../components/AppPage'
 
 export const Members = () => {
   const crumbs = [{ href: '#', text: 'Members' }]
+  const { members, isLoading } = useMembers()
 
   return (
     <AppPage crumbs={crumbs}>
       <PageHeader>
         <PageTitle>Members</PageTitle>
+        {isLoading ? <Loading /> : <MemberList members={members} />}
       </PageHeader>
     </AppPage>
   )
