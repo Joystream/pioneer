6ca257629cacf976dc00ff8ae918520e86ae08f7
Rafal Pawlow <pawlow.rafal@gmail.com>
2021-05-04 17:22:39 +0200

    📖 Add pagination component (#497)

---
+++ packages/ui/src/common/components/icons/ArrowRightIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Icon } from './Icon'
+
+export const ArrowRightIcon = () => {
+  return (
+    <Icon size="16" viewBox="0 0 16 16" preserveAspectRatio="xMidYMid meet" fill="none" color="currentColor">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9.3679 8.09566L4.90051 12.5631L5.90448 13.567L11.3758 8.09566L5.90448 2.62431L4.90051 3.62827L9.3679 8.09566Z"
+        fill="black"
+      />
+    </Icon>
+  )
+}
