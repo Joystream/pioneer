24d056169c5d6fddc0a7d80f4db0e5d93c61594c
Maciej Gołaszewski <jodator@jodator.net>
2021-05-04 14:33:18 +0200

    Add path resolver setting

--- packages/ui/dev/webpack.shared.js
+++ packages/ui/dev/webpack.shared.js
@@ -9,6 +9,7 @@ const resolve = {
   fallback: {
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
+    path: false,
   },
   alias: {
     '@': path.resolve(__dirname, '../src'),
