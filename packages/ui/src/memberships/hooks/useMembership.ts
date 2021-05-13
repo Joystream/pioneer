79abbbfe9c6ec66a79bf769f2da090c136f156cd
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-05-13 12:34:37 +0200

    Output basic roles info for my profile

--- packages/ui/src/memberships/hooks/useMembership.ts
+++ packages/ui/src/memberships/hooks/useMembership.ts
@@ -2,23 +2,19 @@ import { asBlock } from '@/common/types'
 
 import { MemberWithDetailsFragment, useGetMemberQuery } from '../queries'
 import { asMember, DetailedMember } from '../types'
-import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
-import { useMyWorkers } from '@/working-groups/hooks/useMyWorkers'
-import { WorkerWithDetails } from '@/working-groups/types'
 
 interface UseMember {
   member?: DetailedMember
   isLoading: boolean
 }
 
-export const asMemberWithDetails = (data: MemberWithDetailsFragment, roles: WorkerWithDetails[]): DetailedMember => {
+export const asMemberWithDetails = (data: MemberWithDetailsFragment): DetailedMember => {
   return {
     ...asMember(data),
     about: '',
     invitedBy: '',
     registeredAtBlock: asBlock(data.registeredAtBlock),
     invitees: [],
-    roles,
   }
 }
 
@@ -27,7 +23,6 @@ export function useMember(memberId?: string): UseMember {
     variables: { where: { id: memberId ?? '' } },
   }
 
-  const { workers } = useMyWorkers()
   const { data, loading, error } = useGetMemberQuery(options)
 
   if (error) {
@@ -35,7 +30,7 @@ export function useMember(memberId?: string): UseMember {
   }
 
   return {
-    member: data?.membershipByUniqueInput ? asMemberWithDetails(data.membershipByUniqueInput, workers) : undefined,
+    member: data?.membershipByUniqueInput ? asMemberWithDetails(data.membershipByUniqueInput) : undefined,
     isLoading: loading,
   }
 }
