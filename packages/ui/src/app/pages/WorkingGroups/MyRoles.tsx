380ce0918a235fba7a17a6399f4b7f1e10ab06e9
DevNikita16 <83761318+DevNikita16@users.noreply.github.com>
2021-05-13 09:31:32 +0200

    📈 Add roles dashboard (#547)
    * Add stats for my roles
    
    * Add "no roles" text
    
    * Resolve linter issues

--- packages/ui/src/app/pages/WorkingGroups/MyRoles.tsx
+++ packages/ui/src/app/pages/WorkingGroups/MyRoles.tsx
@@ -1,3 +1,4 @@
+import BN from 'bn.js'
 import React from 'react'
 
 import { AppPage } from '@/app/components/AppPage'
@@ -5,7 +6,8 @@ import { Loading } from '@/common/components/Loading'
 import { ContentWithTabs, MainPanel } from '@/common/components/page/PageContent'
 import { PageHeader } from '@/common/components/page/PageHeader'
 import { PageTitle } from '@/common/components/page/PageTitle'
-import { Label } from '@/common/components/typography'
+import { MultiTokenValueStat, Statistics, TokenValueStat } from '@/common/components/statistics'
+import { Label, TextBig } from '@/common/components/typography'
 import { RolesList } from '@/working-groups/components/RolesList'
 import { useMyWorkers } from '@/working-groups/hooks/useMyWorkers'
 
@@ -28,13 +30,31 @@ export const MyRoles = () => {
 
     return (
       <MainPanel>
+        <Statistics>
+          <MultiTokenValueStat
+            title="Total earned in the past"
+            values={[
+              {
+                label: '24 hours',
+                value: new BN(200),
+              },
+              {
+                label: 'Month',
+                value: new BN(10200000),
+              },
+            ]}
+          />
+          <TokenValueStat title="Total stake height" value={150000} />
+          <TokenValueStat title="Total owed reward" value={150000} />
+          <TokenValueStat title="Next payout in" value={150000} />
+        </Statistics>
         <ContentWithTabs>
           <Label>Current roles</Label>
-          <RolesList workers={currentRoles} />
+          {currentRoles.length ? <RolesList workers={currentRoles} /> : <TextBig>No current roles found</TextBig>}
         </ContentWithTabs>
         <ContentWithTabs>
           <Label>Past roles</Label>
-          <RolesList workers={pastRoles} />
+          {pastRoles.length ? <RolesList workers={pastRoles} /> : <TextBig>No past roles found</TextBig>}
         </ContentWithTabs>
       </MainPanel>
     )
