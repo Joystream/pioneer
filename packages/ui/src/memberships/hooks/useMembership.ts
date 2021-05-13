95d035cd52ec32775b31b0903bc5521db69359c5
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-05-13 11:58:17 +0200

    Change roles type in Member

--- packages/ui/src/memberships/hooks/useMembership.ts
+++ packages/ui/src/memberships/hooks/useMembership.ts
@@ -2,19 +2,23 @@ import { asBlock } from '@/common/types'
 
 import { MemberWithDetailsFragment, useGetMemberQuery } from '../queries'
 import { asMember, DetailedMember } from '../types'
+import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
+import { useMyWorkers } from '@/working-groups/hooks/useMyWorkers'
+import { WorkerWithDetails } from '@/working-groups/types'
 
 interface UseMember {
   member?: DetailedMember
   isLoading: boolean
 }
 
-export const asMemberWithDetails = (data: MemberWithDetailsFragment): DetailedMember => {
+export const asMemberWithDetails = (data: MemberWithDetailsFragment, roles: WorkerWithDetails[]): DetailedMember => {
   return {
     ...asMember(data),
     about: '',
     invitedBy: '',
     registeredAtBlock: asBlock(data.registeredAtBlock),
     invitees: [],
+    roles,
   }
 }
 
@@ -23,6 +27,7 @@ export function useMember(memberId?: string): UseMember {
     variables: { where: { id: memberId ?? '' } },
   }
 
+  const { workers } = useMyWorkers()
   const { data, loading, error } = useGetMemberQuery(options)
 
   if (error) {
@@ -30,7 +35,7 @@ export function useMember(memberId?: string): UseMember {
   }
 
   return {
-    member: data?.membershipByUniqueInput ? asMemberWithDetails(data.membershipByUniqueInput) : undefined,
+    member: data?.membershipByUniqueInput ? asMemberWithDetails(data.membershipByUniqueInput, workers) : undefined,
     isLoading: loading,
   }
 }
