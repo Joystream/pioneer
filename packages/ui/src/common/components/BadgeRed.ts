2f0807ea1c85416918b94aa69565ca6bb254e231
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2021-05-14 14:24:10 +0200

    🌊 Add new overflow with full max width (#573)

--- packages/ui/src/common/components/BadgeRed.ts
+++ packages/ui/src/common/components/BadgeRed.ts
@@ -22,7 +22,7 @@ export const BadgeRed = styled.span<BadgeRedProps>`
   line-height: 16px;
   font-weight: 700;
   text-transform: uppercase;
-  ${Overflow.Dots}
+  ${Overflow.FullDots}
 
   ${({ separated }) =>
     separated &&
