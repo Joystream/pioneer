db00a811ed2d5bc664e2adadac2532cb3d697923
ClumsyVlad <mazur097mazur@gmail.com>
2021-05-16 17:23:19 +0200

    New sidebar icons and link states

---
+++ packages/ui/src/common/components/page/Sidebar/LinksIcons/ForumIcon.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Icon } from '../../../icons'
+
+export const ForumIcon = () => (
+  <Icon
+    size="16"
+    viewBox="0 0 16 16"
+    preserveAspectRatio="xMidYMid meet"
+    fill="none"
+    color="currentColor"
+    className="nav-icon"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0.733276 1.3334L1.33328 0.733398H11.9999L12.5999 1.3334V9.0371L11.9999 9.6371H7.44087L4.03647 11.9067L3.10365 11.4075V9.6371H1.33328L0.733276 9.0371V1.3334ZM1.93328 1.9334V8.4371H3.70365L4.30365 9.0371V10.2864L6.92638 8.53787L7.2592 8.4371L11.3999 8.4371V1.9334H1.93328Z"
+      fill="currentColor"
+    />
+    <path fillRule="evenodd" clipRule="evenodd" d="M3.33337 3.33301H10V4.66634H3.33337V3.33301Z" fill="currentColor" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M3.33337 6H7.33337V7.33333H3.33337V6Z" fill="currentColor" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M15.2667 4.66641L14.6667 4.06641H12.0001V5.26641H14.0667V11.7701H12.3334L11.7334 12.3701V13.5245L9.34044 11.876L9.00006 11.7701H5.66672V12.9701H8.81339L11.993 15.1605L12.9334 14.6664V12.9701H14.6667L15.2667 12.3701V4.66641Z"
+      fill="currentColor"
+    />
+  </Icon>
+)
--- packages/ui/src/common/components/page/Sidebar/LinksIcons/ProposalsIcon.tsx
+++ packages/ui/src/common/components/page/Sidebar/LinksIcons/ProposalsIcon.tsx
@@ -14,29 +14,17 @@ export const ProposalsIcon = () => (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M13.3333 12.0003H9.99996V13.3337H13.3333V12.0003ZM8.66663 10.667V14.667H13.6666L14.6666 13.667V10.667H8.66663Z"
+      d="M5 0.666992H13.6667L14.3333 1.33366V12.667H5.66667L5 12.0003V0.666992ZM6.33333 2.00033V11.3337H13V2.00033H6.33333Z"
       fill="currentColor"
     />
     <path
-      className="whitePart"
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M2.66671 3.99967H6.00004V2.66634H2.66671V3.99967ZM7.33337 5.33301V1.33301H2.33337L1.33337 2.33301V5.33301H7.33337Z"
+      d="M2.33337 3.33398H4.33337V4.66732H3.66671V14.0007H10.3334V13.334H11.6667V14.6673L11 15.334H3.00004L2.33337 14.6673V3.33398Z"
       fill="currentColor"
     />
-    <path
-      className="whitePart"
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M13.3333 2.66634H9.99996V7.99967H13.3333V2.66634ZM8.66663 1.33301V9.33301H14.6666V1.33301H8.66663Z"
-      fill="currentColor"
-    />
-    <path
-      className="whitePart"
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M6.00004 8.00033H2.66671V13.3337H6.00004V8.00033ZM1.33337 6.66699V14.667H7.33337V6.66699H1.33337Z"
-      fill="currentColor"
-    />
+    <path fillRule="evenodd" clipRule="evenodd" d="M7.33337 6H12V7.33333H7.33337V6Z" fill="currentColor" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M7.33337 3.33398H12V4.66732H7.33337V3.33398Z" fill="currentColor" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M7.33337 8.66699H12V10.0003H7.33337V8.66699Z" fill="currentColor" />
   </Icon>
 )
