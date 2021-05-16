3534b8c8d01875fe2d3791e34e510dd1dd9bd723
p-sad <sadlik512@gmail.com>
2021-05-16 23:36:37 +0200

    Add tests for rendering sign step

---
+++ packages/ui/test/working-groups/modals/LeaveRoleModal.test.tsx
@@ -0,0 +1,132 @@
+import { cryptoWaitReady } from '@polkadot/util-crypto'
+import { fireEvent, render, screen } from '@testing-library/react'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { AccountsContext } from '../../../src/accounts/providers/accounts/context'
+import { UseAccounts } from '../../../src/accounts/providers/accounts/provider'
+import { ApiContext } from '../../../src/common/providers/api/context'
+import { MembershipContext } from '../../../src/memberships/providers/membership/context'
+import { MyMemberships } from '../../../src/memberships/providers/membership/provider'
+import { seedMembers } from '../../../src/mocks/data'
+import { seedWorkingGroups } from '../../../src/mocks/data/mockWorkingGroups'
+import { LeaveRoleModal } from '../../../src/working-groups/modals/LeaveRoleModal/LeaveRoleModal'
+import { WorkerWithDetails } from '../../../src/working-groups/types'
+import { alice } from '../../_mocks/keyring'
+import { getMember } from '../../_mocks/members'
+import { MockKeyringProvider, MockQueryNodeProviders } from '../../_mocks/providers'
+import { setupMockServer } from '../../_mocks/server'
+import { stubApi, stubDefaultBalances, stubTransaction } from '../../_mocks/transactions'
+
+const WORKER: WorkerWithDetails = {
+  membership: {
+    id: '',
+    controllerAccount: '',
+  },
+  group: {
+    name: 'forum',
+    id: '1',
+  },
+  isLeader: false,
+  rewardPerBlock: 100,
+  earnedTotal: 2000,
+  stake: 2000,
+  status: '',
+  id: '1',
+}
+
+describe('UI: LeaveRoleModal', () => {
+  const api = stubApi()
+
+  const useMyMemberships: MyMemberships = {
+    active: undefined,
+    members: [],
+    setActive: (member) => (useMyMemberships.active = member),
+    isLoading: false,
+  }
+
+  let useAccounts: UseAccounts
+  let transaction: any
+
+  const server = setupMockServer()
+
+  beforeAll(async () => {
+    await cryptoWaitReady()
+
+    useAccounts = {
+      hasAccounts: true,
+      allAccounts: [alice],
+    }
+  })
+
+  beforeEach(async () => {
+    seedMembers(server.server)
+    seedWorkingGroups(server.server)
+    useMyMemberships.setActive(getMember('alice'))
+    stubDefaultBalances(api)
+    transaction = stubTransaction(api, 'api.tx.forumWorkingGroup.leaveRole')
+  })
+
+  it('Renders the modal', async () => {
+    await renderModal()
+    expect(screen.findByText('Leaving a position?')).toBeDefined()
+    expect(screen.findByText('Please remember that this action is irreversible.')).toBeDefined()
+    expect(screen.getByRole('button', { name: 'Leave the position anyway' })).toBeDefined()
+  })
+
+  it("With member's controller account in keyring", async () => {
+    await renderWithValidSigner()
+    await fireEvent.click(screen.getByRole('button', { name: 'Leave the position anyway' }))
+    expect(
+      screen.findByText("The transaction can only be signed with the membership's controller account.")
+    ).toBeDefined()
+    expect(screen.getByRole('button', { name: 'Sign and leave role' })).not.toBeDisabled()
+  })
+
+  it("Without member's controller account in keyring", async () => {
+    await renderWithoutValidSigner()
+    await fireEvent.click(screen.getByRole('button', { name: 'Leave the position anyway' }))
+    expect(
+      screen.findByText("The transaction can only be signed with the membership's controller account.")
+    ).toBeDefined()
+    expect(screen.getByRole('button', { name: 'Sign and leave role' })).toBeDisabled()
+  })
+
+  function renderModal(worker = WORKER) {
+    return render(
+      <BrowserRouter>
+        <MockQueryNodeProviders>
+          <MockKeyringProvider>
+            <AccountsContext.Provider value={useAccounts}>
+              <MembershipContext.Provider value={useMyMemberships}>
+                <ApiContext.Provider value={api}>
+                  <LeaveRoleModal worker={worker} onClose={() => null} />
+                </ApiContext.Provider>
+              </MembershipContext.Provider>
+            </AccountsContext.Provider>
+          </MockKeyringProvider>
+        </MockQueryNodeProviders>
+      </BrowserRouter>
+    )
+  }
+
+  async function renderWithValidSigner() {
+    return await renderModal({
+      ...WORKER,
+      membership: {
+        id: '1',
+        controllerAccount: alice.address,
+      },
+    })
+  }
+
+  async function renderWithoutValidSigner() {
+    return await renderModal({
+      ...WORKER,
+      membership: {
+        id: '1',
+        controllerAccount: '5GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
+      },
+    })
+  }
+})
