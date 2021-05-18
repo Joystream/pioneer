361d14412711c319ddaa4bb9054bbf8ecab8e9a6
p-sad <sadlik512@gmail.com>
2021-05-18 10:12:20 +0200

    Add stat boxes to about tab

--- packages/ui/src/app/pages/WorkingGroups/WorkingGroup/AboutTab.tsx
+++ packages/ui/src/app/pages/WorkingGroups/WorkingGroup/AboutTab.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import { ContentWithSidepanel, MainPanel, SidePanel } from '../../../../common/components/page/PageContent'
+import { Statistics, TokenValueStat } from '../../../../common/components/statistics'
+import { NumericValueStat } from '../../../../common/components/statistics/NumericValueStat'
 import { useMember } from '../../../../memberships/hooks/useMembership'
 import { WorkersList } from '../../../../working-groups/components/WorkersList'
 import { useWorkers } from '../../../../working-groups/hooks/useWorkers'
@@ -16,6 +18,11 @@ export const AboutTab = ({ workingGroup }: Props) => {
   return (
     <ContentWithSidepanel>
       <MainPanel>
+        <Statistics>
+          <TokenValueStat title="Spending" helperText="Lorem ipsum..." value={workingGroup.budget} />
+          <NumericValueStat title="Total hired" value={34} />
+          <NumericValueStat title="Total fired" value={7} />
+        </Statistics>
         <h4>Welcome</h4>
         <div>{workingGroup.description}</div>
         <h4>Status</h4>
