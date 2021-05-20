97267591913de11bae4b8909a8e206e05dbead87
Piotr Sadlik <sadlik512@gmail.com>
2021-05-20 09:51:38 +0200

    🪨 Add a dashboard with stats to About Group tab (#627)
    * Add stat boxes to about tab
    
    * Change useWorkers API to be more robust
    
    * Add a mock hook for retrieving group stats

--- packages/ui/src/app/pages/WorkingGroups/WorkingGroup/AboutTab.tsx
+++ packages/ui/src/app/pages/WorkingGroups/WorkingGroup/AboutTab.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-import { ContentWithSidepanel, MainPanel, SidePanel } from '../../../../common/components/page/PageContent'
-import { useMember } from '../../../../memberships/hooks/useMembership'
-import { WorkersList } from '../../../../working-groups/components/WorkersList'
-import { useWorkers } from '../../../../working-groups/hooks/useWorkers'
-import { WorkingGroup } from '../../../../working-groups/types'
+import { ContentWithSidepanel, MainPanel, SidePanel } from '@/common/components/page/PageContent'
+import { Statistics, TokenValueStat } from '@/common/components/statistics'
+import { NumericValueStat } from '@/common/components/statistics/NumericValueStat'
+import { useMember } from '@/memberships/hooks/useMembership'
+import { WorkersList } from '@/working-groups/components/WorkersList'
+import { useGroupStatistics } from '@/working-groups/hooks/useGroupStatistics'
+import { useWorkers } from '@/working-groups/hooks/useWorkers'
+import { WorkingGroup } from '@/working-groups/types'
 
 interface Props {
   workingGroup: WorkingGroup
@@ -12,10 +15,16 @@ interface Props {
 export const AboutTab = ({ workingGroup }: Props) => {
   const { member: leader } = useMember(workingGroup.leaderId ?? '')
   const { workers } = useWorkers({ groupId: workingGroup.id ?? '' })
+  const { statistics } = useGroupStatistics(workingGroup.id)
 
   return (
     <ContentWithSidepanel>
       <MainPanel>
+        <Statistics>
+          <TokenValueStat title="Spending" helperText="Lorem ipsum..." value={statistics.spending} />
+          <NumericValueStat title="Total hired" value={statistics.totalHired ?? 'Loading...'} />
+          <NumericValueStat title="Total fired" value={statistics.totalFired ?? 'Loading...'} />
+        </Statistics>
         <h4>Welcome</h4>
         <div>{workingGroup.description}</div>
         <h4>Status</h4>
