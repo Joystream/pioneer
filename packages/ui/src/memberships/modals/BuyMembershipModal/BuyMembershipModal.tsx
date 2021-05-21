d8176f9c9112876d8bbdb6f5037c090d25962f2e
Maciej Gołaszewski <jodator@jodator.net>
2021-05-21 16:33:54 +0200

    Encode membership metadata

--- packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
+++ packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
@@ -1,3 +1,5 @@
+import { MembershipMetadata } from '@joystream/metadata-protobuf'
+import { createType } from '@joystream/types'
 import { MemberId } from '@joystream/types/common'
 import { EventRecord } from '@polkadot/types/interfaces'
 import React, { useContext, useMemo, useState } from 'react'
@@ -15,6 +17,20 @@ import { BuyMembershipFormModal, FormFields } from './BuyMembershipFormModal'
 import { BuyMembershipSignModal } from './BuyMembershipSignModal'
 import { BuyMembershipSuccessModal } from './BuyMembershipSuccessModal'
 
+export type AnyMessage<T> = T & {
+  toJSON(): Record<string, unknown>
+}
+
+export type AnyMetadataClass<T> = {
+  decode(binary: Uint8Array): AnyMessage<T>
+  encode(obj: T): { finish(): Uint8Array }
+  toObject(obj: AnyMessage<T>): Record<string, unknown>
+}
+
+const metadataToBytes = <T extends any>(metaClass: AnyMetadataClass<T>, message: T) => {
+  return createType('Bytes', '0x' + Buffer.from(metaClass.encode(message).finish()).toString('hex'))
+}
+
 export const BuyMembershipModal = () => {
   const { hideModal } = useModal()
   const onClose = hideModal
@@ -30,51 +46,50 @@ export const BuyMembershipModal = () => {
     setParams(params)
   }
 
-  const transaction = useMemo(
-    () =>
-      formData
-        ? api?.tx?.members?.buyMembership({
-            root_account: formData.rootAccount?.address,
-            controller_account: formData.controllerAccount?.address,
-            handle: formData.handle,
-            metadata: {
-              name: formData.name,
-              avatar_uri: formData.avatarUri,
-              about: formData.about,
-            },
-            referrer_id: formData.referrer?.id,
-          })
-        : undefined,
-    [JSON.stringify(formData)]
-  )
+  const transaction = useMemo(() => {
+    if (!formData || !api) {
+      return
+    }
 
-  const onDone = useMemo(
-    () =>
-      formData
-        ? (result: boolean, events: EventRecord[]) => {
-            const memberId = getEventParam<MemberId>(events, 'MemberRegistered')
-            setId(memberId)
+    return api.tx.members.buyMembership({
+      root_account: formData.rootAccount?.address,
+      controller_account: formData.controllerAccount?.address,
+      handle: formData.handle,
+      metadata: metadataToBytes(MembershipMetadata, {
+        name: formData.name,
+        about: formData.about,
+      }),
+      referrer_id: formData.referrer?.id,
+    })
+  }, [JSON.stringify(formData)])
 
-            if (server && memberId) {
-              server.schema.create('Membership', {
-                id: memberId.toString(),
-                rootAccount: formData.rootAccount?.address,
-                controllerAccount: formData.controllerAccount?.address,
-                name: formData.name,
-                handle: formData.handle,
-                avatarUri: formData.avatarUri,
-                about: formData.about,
-                isVerified: false,
-                isFoundingMember: false,
-                inviteCount: '5',
-                registeredAtBlockId: 'block-3',
-              })
-            }
-            setStep(result ? 'SUCCESS' : 'ERROR')
-          }
-        : () => null,
-    [formData]
-  )
+  const onDone = useMemo(() => {
+    if (!formData) {
+      return () => null
+    }
+
+    return (result: boolean, events: EventRecord[]) => {
+      const memberId = getEventParam<MemberId>(events, 'MemberRegistered')
+      setId(memberId)
+
+      if (server && memberId) {
+        server.schema.create('Membership', {
+          id: memberId.toString(),
+          rootAccount: formData.rootAccount?.address,
+          controllerAccount: formData.controllerAccount?.address,
+          name: formData.name,
+          handle: formData.handle,
+          avatarUri: formData.avatarUri,
+          about: formData.about,
+          isVerified: false,
+          isFoundingMember: false,
+          inviteCount: '5',
+          registeredAtBlockId: 'block-3',
+        })
+      }
+      setStep(result ? 'SUCCESS' : 'ERROR')
+    }
+  }, [JSON.stringify(formData)])
 
   if (step === 'PREPARE' || !formData) {
     return <BuyMembershipFormModal onClose={onClose} onSubmit={onSubmit} membershipPrice={membershipPrice} />
