d109f2a775001fbb0ee2a38dd38ade8e8ec9d7ab
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-05-21 17:09:40 +0200

    Complete the `<DatePicker />`

--- packages/ui/src/common/components/forms/DatePicker/DatePicker.stories.tsx
+++ packages/ui/src/common/components/forms/DatePicker/DatePicker.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, Story } from '@storybook/react'
 import { addMonths, startOfToday } from 'date-fns'
-import React, { useState } from 'react'
+import React from 'react'
 
 import { PartialDateRange } from '@/common/types/Dates'
 
@@ -12,10 +12,15 @@ export default {
 } as Meta
 
 const Template: Story = () => {
-  const [dates, setDates] = useState<PartialDateRange>()
   const today = startOfToday()
   const withinDates = { start: addMonths(today, -13), end: today }
-  return <DatePicker dates={dates} withinDates={withinDates} onDatesChange={setDates} />
+
+  const display = (dates: PartialDateRange) =>
+    setTimeout(() => {
+      alert(JSON.stringify(dates, null, 2))
+    }, 100)
+
+  return <DatePicker withinDates={withinDates} onApply={display} />
 }
 
 export const Default = Template.bind({})
--- packages/ui/src/common/components/forms/DatePicker/DatePicker.tsx
+++ packages/ui/src/common/components/forms/DatePicker/DatePicker.tsx
@@ -1,8 +1,8 @@
 import { addMonths, addWeeks, addYears, isAfter, isBefore, isEqual, startOfMonth, startOfToday } from 'date-fns'
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import styled from 'styled-components'
 
-import { ButtonSecondary } from '@/common/components/buttons'
+import { ButtonGhost, ButtonPrimary, ButtonSecondary } from '@/common/components/buttons'
 import { Shadows } from '@/common/constants'
 import { DateRange, PartialDateRange } from '@/common/types/Dates'
 import { earliest, fromRange, latest, toDDMMYY } from '@/common/utils/dates'
@@ -11,22 +11,57 @@ import { Calendar } from '../Calendar'
 import { InputComponent, InputText } from '../InputComponent'
 
 interface DatePickerProps {
-  dates: PartialDateRange
   withinDates?: PartialDateRange
-  onDatesChange: (dates: PartialDateRange) => void
+  onApply?: (value: PartialDateRange) => void
 }
-export const DatePicker = ({ dates, withinDates, onDatesChange }: DatePickerProps) => {
+
+export const DatePicker = ({ withinDates, onApply }: DatePickerProps) => {
+  const [dates, setDates] = useState<PartialDateRange>()
   const placeholder = '__/__/__'
   const { start, end } = fromRange(dates)
   const dateString = `${toDDMMYY(start) ?? placeholder} - ${toDDMMYY(end) ?? placeholder}`
 
+  const datepicker = useRef<HTMLDivElement>(null)
+  const [isOpen, toggleOpen] = useState(false)
+
+  useEffect(() => {
+    if (!isOpen) return
+
+    const closePopup = (evt: MouseEvent) => {
+      evt.stopPropagation()
+      if (!evt.target) return
+
+      const target = evt.target as Node
+      const clickedOutside = !datepicker.current?.contains(target) ?? false
+      clickedOutside && toggleOpen(false)
+    }
+
+    window.addEventListener('mousedown', closePopup)
+    return () => window.removeEventListener('mousedown', closePopup)
+  }, [datepicker, isOpen])
+
+  const clear = () => {
+    setDates(undefined)
+    onApply?.(undefined)
+  }
+  const apply = () => {
+    onApply?.(dates)
+    toggleOpen(false)
+  }
+
   return (
-    <InputComponent tight inputWidth="xs">
-      <InputText placeholder="-" value={dateString} readOnly />
-      <DatePickerPopupContainer>
-        <DatePickerCalendars value={dates} within={withinDates} onChange={onDatesChange} />
-      </DatePickerPopupContainer>
-    </InputComponent>
+    <DatePickerContainer ref={datepicker} onMouseDown={() => !isOpen && toggleOpen(true)}>
+      <InputComponent tight inputWidth="xs">
+        <InputText placeholder="-" value={dateString} readOnly />
+        {isOpen && (
+          <DatePickerPopup>
+            <DatePickerCalendars value={dates} within={withinDates} onChange={setDates} />
+            <ClearFilterBtutton onClick={clear}>Clear Filter</ClearFilterBtutton>
+            <ApplyFilterBtutton onClick={apply}>Apply Filter</ApplyFilterBtutton>
+          </DatePickerPopup>
+        )}
+      </InputComponent>
+    </DatePickerContainer>
   )
 }
 
@@ -36,11 +71,12 @@ interface DatePickerCalendarsProps {
   onChange: (value: PartialDateRange) => void
 }
 const DatePickerCalendars = ({ value, within, onChange }: DatePickerCalendarsProps) => {
+  const { start, end } = fromRange(value)
+
   const today = useMemo(() => startOfToday(), [])
   const thisMonth = useMemo(() => startOfMonth(today), [])
-  const [monthL, setMonthL] = useState(addMonths(thisMonth, -1))
-  const [monthR, setMonthR] = useState(thisMonth)
-  const { start, end } = fromRange(value)
+  const [monthL, setMonthL] = useState(start ? startOfMonth(start) : addMonths(thisMonth, -1))
+  const [monthR, setMonthR] = useState(end ? startOfMonth(end) : thisMonth)
 
   const selectionStrategy = (k1: keyof DateRange) => (day: Date) => {
     if (!value) {
@@ -75,7 +111,7 @@ const DatePickerCalendars = ({ value, within, onChange }: DatePickerCalendarsPro
   }
 
   return (
-    <DatePickerCalendarsContainer>
+    <>
       <Calendar
         selection={value}
         month={monthL}
@@ -101,26 +137,35 @@ const DatePickerCalendars = ({ value, within, onChange }: DatePickerCalendarsPro
         <ButtonSecondary onClick={setToNowFrom(addMonths, -6)}>Past 6 months</ButtonSecondary>
         <ButtonSecondary onClick={setToNowFrom(addYears, -1)}>Past year</ButtonSecondary>
       </DatePickerSideNav>
-    </DatePickerCalendarsContainer>
+    </>
   )
 }
 
-const DatePickerPopupContainer = styled.div`
-  pointer-events: none;
+const DatePickerContainer = styled.div`
+  display: inline-block;
+`
+
+const DatePickerPopup = styled.div`
   position: absolute;
   left: 0;
   top: calc(100% + 4px);
-  width: 100vw;
-`
-
-const DatePickerCalendarsContainer = styled.div`
   box-shadow: ${Shadows.light};
-  pointer-events: all;
+  padding: 16px;
+
   display: inline-grid;
   gap: 16px;
-  grid-auto-flow: column;
+  grid-template-columns: auto auto auto;
   justify-items: center;
-  padding: 16px;
+  width: 672px;
+`
+
+const ClearFilterBtutton = styled(ButtonGhost)`
+  grid-column: 2;
+  justify-self: end;
+`
+const ApplyFilterBtutton = styled(ButtonPrimary)`
+  grid-column: 3;
+  width: 100%;
 `
 
 const DatePickerSideNav = styled.div`
