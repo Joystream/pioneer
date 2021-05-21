0a114e1a03c286bc54889cafe1af952c0d51e596
DevNikita16 <83761318+DevNikita16@users.noreply.github.com>
2021-05-21 08:36:32 +0200

    ☢️ Introduce warning as global component (#639)
    * Refactor Warning to be global component
    
    * Add stories for warning component
    
    * Refactor warning props
    
    * Refactor warning component

---
+++ packages/ui/src/common/components/Warning.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import { CloseButton } from '@/common/components/buttons'
+import { Colors } from '@/common/constants'
+import { useToggle } from '@/common/hooks/useToggle'
+import { size, spacing } from '@/common/utils/styles'
+
+export interface WarningProps {
+  title: string
+  content: string
+  isClosable?: boolean
+}
+
+export const Warning = ({ title, content, isClosable }: WarningProps) => {
+  const [isOpened, toggleOpen] = useToggle(true)
+
+  if (!isOpened) {
+    return null
+  }
+
+  return (
+    <WarningBlock>
+      {isClosable !== false && <StyledCloseButton onClick={toggleOpen} />}
+      <h5>{title}</h5>
+      <p>{content}</p>
+    </WarningBlock>
+  )
+}
+
+const WarningBlock = styled.div`
+  background-color: ${Colors.Red[50]};
+  position: relative;
+  padding: ${spacing(2)};
+  color: ${Colors.Black[600]};
+
+  h5 {
+    margin-bottom: ${spacing(1)};
+  }
+`
+
+const StyledCloseButton = styled(CloseButton)`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  ${size('16px')}
+`
