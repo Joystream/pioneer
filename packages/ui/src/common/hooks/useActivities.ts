c609972b815aa48de4cd924f8641df1e419ad58b
p-sad <sadlik512@gmail.com>
2021-05-24 11:46:55 +0200

    Use Memo in useActivities to check what rerenders

--- packages/ui/src/common/hooks/useActivities.ts
+++ packages/ui/src/common/hooks/useActivities.ts
@@ -1,46 +1,51 @@
 import BN from 'bn.js'
+import { useMemo } from 'react'
 
 import { Activity } from '../types'
 
-export const useActivities = (): Activity[] => [
-  {
-    id: '1',
-    createdAt: '2021-03-09T10:28:04.155Z',
-    eventType: 'AppliedOnOpeningEvent',
-    membership: {
-      handle: 'xXproGamerDarknessXx',
-      id: '1',
-    },
-    opening: {
-      title: 'Forum Worker',
-      id: '2',
-    },
-  },
-  {
-    id: '2',
-    createdAt: '2021-05-09T10:28:04.155Z',
-    eventType: 'ApplicationWithdrawnEvent',
-    membership: {
-      handle: 'andy00',
-      id: '1',
-    },
-    opening: {
-      title: 'Forum Worker',
-      id: '2',
-    },
-  },
-  {
-    id: '3',
-    createdAt: '2021-05-19T10:28:04.155Z',
-    eventType: 'BudgetSpendingEvent',
-    amount: new BN('10000'),
-    groupName: 'Forum',
-  },
-  {
-    id: '4',
-    createdAt: '2021-05-20T10:28:04.155Z',
-    eventType: 'BudgetSetEvent',
-    groupName: 'Forum',
-    newBudget: new BN(100000),
-  },
-]
+export const useActivities = (): Activity[] =>
+  useMemo(
+    () => [
+      {
+        id: '1',
+        createdAt: '2021-03-09T10:28:04.155Z',
+        eventType: 'AppliedOnOpeningEvent',
+        membership: {
+          handle: 'xXproGamerDarknessXx',
+          id: '1',
+        },
+        opening: {
+          title: 'Forum Worker',
+          id: '2',
+        },
+      },
+      {
+        id: '2',
+        createdAt: '2021-05-09T10:28:04.155Z',
+        eventType: 'ApplicationWithdrawnEvent',
+        membership: {
+          handle: 'andy00',
+          id: '1',
+        },
+        opening: {
+          title: 'Forum Worker',
+          id: '2',
+        },
+      },
+      {
+        id: '3',
+        createdAt: '2021-05-19T10:28:04.155Z',
+        eventType: 'BudgetSpendingEvent',
+        amount: new BN('10000'),
+        groupName: 'Forum',
+      },
+      {
+        id: '4',
+        createdAt: '2021-05-20T10:28:04.155Z',
+        eventType: 'BudgetSetEvent',
+        groupName: 'Forum',
+        newBudget: new BN(100000),
+      },
+    ],
+    []
+  )
