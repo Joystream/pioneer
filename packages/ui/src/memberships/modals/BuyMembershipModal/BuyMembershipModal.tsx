6c3eb907c14d041421163f184bfacdcd4a3b157d
Maciej Gołaszewski <jodator@jodator.net>
2021-05-24 15:47:55 +0200

    Remove mock creation on buy member

--- packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
+++ packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
@@ -1,14 +1,13 @@
 import { MembershipMetadata } from '@joystream/metadata-protobuf'
 import { MemberId } from '@joystream/types/common'
 import { EventRecord } from '@polkadot/types/interfaces'
-import React, { useContext, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import { FailureModal } from '@/common/components/FailureModal'
 import { useApi } from '@/common/hooks/useApi'
 import { useModal } from '@/common/hooks/useModal'
 import { useObservable } from '@/common/hooks/useObservable'
 import { getEventParam, metadataToBytes } from '@/common/model/JoystreamNode'
-import { ServerContext } from '@/common/providers/server/context'
 import { ModalState } from '@/common/types'
 
 import { BuyMembershipFormModal, FormFields } from './BuyMembershipFormModal'
@@ -22,7 +21,6 @@ export const BuyMembershipModal = () => {
   const membershipPrice = useObservable(api?.query.members.membershipPrice(), [])
   const [step, setStep] = useState<ModalState>('PREPARE')
   const [formData, setParams] = useState<FormFields>()
-  const server = useContext(ServerContext)
   const [id, setId] = useState<MemberId>()
 
   const onSubmit = (params: FormFields) => {
@@ -35,16 +33,7 @@ export const BuyMembershipModal = () => {
       return
     }
 
-    return api.tx.members.buyMembership({
-      root_account: formData.rootAccount?.address,
-      controller_account: formData.controllerAccount?.address,
-      handle: formData.handle,
-      metadata: metadataToBytes(MembershipMetadata, {
-        name: formData.name,
-        about: formData.about,
-      }),
-      referrer_id: formData.referrer?.id,
-    })
+    return api.tx.members.buyMembership(toByMembershipTransactionParams(formData))
   }, [JSON.stringify(formData)])
 
   const onDone = useMemo(() => {
@@ -55,22 +44,6 @@ export const BuyMembershipModal = () => {
     return (result: boolean, events: EventRecord[]) => {
       const memberId = getEventParam<MemberId>(events, 'MemberRegistered')
       setId(memberId)
-
-      if (server && memberId) {
-        server.schema.create('Membership', {
-          id: memberId.toString(),
-          rootAccount: formData.rootAccount?.address,
-          controllerAccount: formData.controllerAccount?.address,
-          name: formData.name,
-          handle: formData.handle,
-          avatarUri: formData.avatarUri,
-          about: formData.about,
-          isVerified: false,
-          isFoundingMember: false,
-          inviteCount: '5',
-          registeredAtBlockId: 'block-3',
-        })
-      }
       setStep(result ? 'SUCCESS' : 'ERROR')
     }
   }, [JSON.stringify(formData)])
@@ -100,3 +73,14 @@ export const BuyMembershipModal = () => {
     <FailureModal onClose={onClose}>There was a problem with creating a membership for {formData.name}.</FailureModal>
   )
 }
+
+const toByMembershipTransactionParams = (formData: FormFields) => ({
+  root_account: formData.rootAccount?.address,
+  controller_account: formData.controllerAccount?.address,
+  handle: formData.handle,
+  metadata: metadataToBytes(MembershipMetadata, {
+    name: formData.name,
+    about: formData.about,
+  }),
+  referrer_id: formData.referrer?.id,
+})
