9034439438ec67d8cbb76e7490eb669a90d5ea5f
p-sad <sadlik512@gmail.com>
2021-05-25 15:48:21 +0200

    Add necessary providers to story

--- packages/ui/src/common/components/Activities/Activities.stories.tsx
+++ packages/ui/src/common/components/Activities/Activities.stories.tsx
@@ -1,7 +1,9 @@
 import { Meta, Story } from '@storybook/react'
 import BN from 'bn.js'
 import React from 'react'
+import { MemoryRouter } from 'react-router'
 
+import { ModalContext } from '../../providers/modal/context'
 import { TemplateBlock } from '../storybookParts/previewStyles'
 
 import { Activities, ActivitiesProps } from './Activities'
@@ -12,9 +14,20 @@ export default {
 } as Meta
 
 const Template: Story<ActivitiesProps> = (args) => (
-  <TemplateBlock>
-    <Activities {...args} />
-  </TemplateBlock>
+  <MemoryRouter>
+    <ModalContext.Provider
+      value={{
+        showModal: () => null,
+        hideModal: () => null,
+        modal: '',
+        modalData: {},
+      }}
+    >
+      <TemplateBlock>
+        <Activities {...args} />
+      </TemplateBlock>
+    </ModalContext.Provider>
+  </MemoryRouter>
 )
 
 export const Default = Template.bind({})
@@ -52,6 +65,29 @@ Default.args = {
       eventType: 'BudgetSpendingEvent',
       amount: new BN('10000'),
       groupName: 'Forum',
+    },
+    {
+      id: '4',
+      createdAt: '2021-05-20T10:28:04.155Z',
+      eventType: 'BudgetSetEvent',
+      groupName: 'Forum',
+      newBudget: new BN(100000),
+    },
+    {
+      id: '5',
+      createdAt: '2021-05-23T10:28:04.155Z',
+      eventType: 'LeaderSetEvent',
+      membership: {
+        id: '3',
+        handle: 'Kyle_1994',
+      },
+      groupName: 'storage',
+    },
+    {
+      id: '6',
+      createdAt: '2021-05-24T10:28:04.155Z',
+      eventType: 'StatusTextChangedEvent',
+      groupName: 'storage',
     },
   ],
 }
