beb02bca4c0bb31b533f568778650b6fac18005d
ClumsyVlad <mazur097mazur@gmail.com>
2021-05-25 15:08:33 +0200

    Side panel scroll fix

--- packages/ui/src/common/components/page/SidePanel.tsx
+++ packages/ui/src/common/components/page/SidePanel.tsx
@@ -1,33 +1,21 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
-interface SidePanelProps extends SidePanelStylesProps {
+interface SidePanelProps {
   className?: string
   children?: React.ReactNode
-}
-
-interface SidePanelStylesProps {
-  neighborHeight?: number
-  ownHeight?: number
   neighbor?: React.MutableRefObject<HTMLDivElement | null>
 }
 
 export const SidePanel = ({ neighbor, className, children }: SidePanelProps) => {
-  const ownRef = useRef<HTMLDivElement>(null)
   return (
-    <SidePanelStyles
-      ref={ownRef}
-      neighbor={neighbor}
-      neighborHeight={neighbor?.current?.getBoundingClientRect().height}
-      ownHeight={ownRef.current ? ownRef?.current?.getBoundingClientRect()?.top : 0}
-      className={className}
-    >
+    <SidePanelStyles neighborHeight={neighbor?.current?.getBoundingClientRect().height} className={className}>
       {children}
     </SidePanelStyles>
   )
 }
 
-export const SidePanelStyles = styled.aside<SidePanelStylesProps>`
+export const SidePanelStyles = styled.aside<{ neighborHeight?: number }>`
   display: grid;
   position: absolute;
   top: 0;
@@ -36,7 +24,7 @@ export const SidePanelStyles = styled.aside<SidePanelStylesProps>`
   width: 100%;
   max-width: 280px;
   height: fit-content;
-  max-height: max(100%, ${(props) => (props.ownHeight ? screen.height - props.ownHeight + 24 + 'px' : '0')});
+  max-height: max(564px, ${({ neighborHeight }) => neighborHeight + 'px'});
   padding-left: 24px;
   overflow: hidden;
   overflow-y: scroll;
