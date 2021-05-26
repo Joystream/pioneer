4d320ab283913fabfc9cbe9fe239671cf9e89f26
Maciej Gołaszewski <jodator@jodator.net>
2021-05-26 15:54:49 +0200

    Cleanup main index.tsx file

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,6 +4,3 @@ import ReactDOM from 'react-dom'
 import { App } from './app'
 
 ReactDOM.render(<App />, document.getElementById('app'))
-export { AppPage } from './app/components/AppPage'
-export { toMemberTransactionParams } from '@/memberships/modals/utils'
-export { useApplication } from '@/working-groups/hooks/useApplication'
