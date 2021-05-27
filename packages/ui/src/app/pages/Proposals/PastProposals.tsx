7ce15a362c18e19e7049cd78a3f7dc7f69a21fb7
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-05-27 09:58:11 +0200

    Open the past proposal tab

---
+++ packages/ui/src/app/pages/Proposals/PastProposals.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { AppPage } from '@/app/components/AppPage'
+import { ActivitiesBlock } from '@/common/components/Activities/ActivitiesBlock'
+import { Loading } from '@/common/components/Loading'
+import { ContentWithSidepanel, MainPanel, SidePanel } from '@/common/components/page/PageContent'
+import { PageHeader } from '@/common/components/page/PageHeader'
+import { PageTitle } from '@/common/components/page/PageTitle'
+import { useActivities } from '@/common/hooks/useActivities'
+import { AddProposalButton } from '@/proposals/components/AddProposalButton'
+import { NoProposals } from '@/proposals/components/NoProposals'
+import { ProposalList } from '@/proposals/components/ProposalList'
+import { usePastProposals } from '@/proposals/hooks/usePastProposals'
+
+import { ProposalsTabs } from './components/ProposalsTabs'
+
+export const PastProposals = () => {
+  const { proposals, isLoading } = usePastProposals()
+  const activities = useActivities()
+
+  return (
+    <AppPage>
+      <PageHeader>
+        <PageTitle>Proposals</PageTitle>
+        <AddProposalButton />
+        <ProposalsTabs />
+      </PageHeader>
+
+      {isLoading ? (
+        <MainPanel>
+          <Loading />
+        </MainPanel>
+      ) : proposals.length ? (
+        <ContentWithSidepanel>
+          <MainPanel>
+            <ProposalList proposals={proposals} isPast />
+          </MainPanel>
+          <SidePanel>
+            <ActivitiesBlock activities={activities} label="Proposals Activities" />
+          </SidePanel>
+        </ContentWithSidepanel>
+      ) : (
+        <MainPanel>
+          <NoProposals />
+        </MainPanel>
+      )}
+    </AppPage>
+  )
+}
