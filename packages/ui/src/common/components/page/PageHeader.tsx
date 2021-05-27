9754a3bb23989132eb009f11612ef81307c4db12
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2021-05-27 15:08:00 +0200

    🔝 Move memberships buttons to page header (#688)

--- packages/ui/src/common/components/page/PageHeader.tsx
+++ packages/ui/src/common/components/page/PageHeader.tsx
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 
+import { ButtonsGroup } from '../buttons'
+
 export const PageHeader = styled.section`
   display: flex;
+  position: relative;
   flex-wrap: wrap;
   justify-content: space-between;
   row-gap: 16px;
+
+  ${ButtonsGroup} {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
 `
