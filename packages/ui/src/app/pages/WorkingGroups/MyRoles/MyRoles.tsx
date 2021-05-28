c30c70f23dbf00ed234b733777439507e269466c
Maciej Gołaszewski <jodator@jodator.net>
2021-05-28 13:16:44 +0200

    Use extracted components in pages

--- packages/ui/src/app/pages/WorkingGroups/MyRoles/MyRoles.tsx
+++ packages/ui/src/app/pages/WorkingGroups/MyRoles/MyRoles.tsx
@@ -1,4 +1,3 @@
-import BN from 'bn.js'
 import React from 'react'
 
 import { AppPage } from '@/app/components/AppPage'
@@ -6,8 +5,10 @@ import { Loading } from '@/common/components/Loading'
 import { ContentWithTabs, MainPanel } from '@/common/components/page/PageContent'
 import { PageHeader } from '@/common/components/page/PageHeader'
 import { PageTitle } from '@/common/components/page/PageTitle'
-import { MultiTokenValueStat, Statistics, TokenValueStat } from '@/common/components/statistics'
+import { Statistics, TokenValueStat } from '@/common/components/statistics'
 import { Label, TextBig } from '@/common/components/typography'
+import { MyEarnings } from '@/working-groups/components/MyEarnings'
+import { MyStake } from '@/working-groups/components/MyStake'
 import { RolesList } from '@/working-groups/components/Roles/RolesList'
 import { useMyWorkers } from '@/working-groups/hooks/useMyWorkers'
 
@@ -18,35 +19,14 @@ export const MyRoles = () => {
 
   const displayRoles = () => {
     if (isLoading) {
-      return (
-        <MainPanel>
-          <Loading />
-        </MainPanel>
-      )
+      return <Loading />
     }
 
     const currentRoles = (workers && workers.filter((worker) => worker.status === 'WorkerStatusActive')) || []
     const pastRoles = (workers && workers.filter((worker) => worker.status !== 'WorkerStatusActive')) || []
+
     return (
-      <MainPanel>
-        <Statistics>
-          <MultiTokenValueStat
-            title="Total earned in the past"
-            values={[
-              {
-                label: '24 hours',
-                value: new BN(200),
-              },
-              {
-                label: 'Month',
-                value: new BN(10200000),
-              },
-            ]}
-          />
-          <TokenValueStat title="Total stake height" value={150000} />
-          <TokenValueStat title="Total owed reward" value={150000} />
-          <TokenValueStat title="Next payout in" value={150000} />
-        </Statistics>
+      <>
         <ContentWithTabs>
           <Label>Current roles</Label>
           {currentRoles.length ? <RolesList workers={currentRoles} /> : <TextBig>No current roles found</TextBig>}
@@ -55,7 +35,7 @@ export const MyRoles = () => {
           <Label>Past roles</Label>
           {pastRoles.length ? <RolesList workers={pastRoles} /> : <TextBig>No past roles found</TextBig>}
         </ContentWithTabs>
-      </MainPanel>
+      </>
     )
   }
 
@@ -65,7 +45,15 @@ export const MyRoles = () => {
         <PageTitle>Working Groups</PageTitle>
         <WorkingGroupsTabs />
       </PageHeader>
-      {displayRoles()}
+      <MainPanel>
+        <Statistics>
+          <MyEarnings />
+          <MyStake />
+          <TokenValueStat title="Total owed reward" value={150000} />
+          <TokenValueStat title="Next payout in" value={150000} />
+        </Statistics>
+        {displayRoles()}
+      </MainPanel>
     </AppPage>
   )
 }
