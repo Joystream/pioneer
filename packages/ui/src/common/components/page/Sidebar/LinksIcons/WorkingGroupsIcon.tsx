b62f428bc6a7313e4f018db24aa28de2c84ee468
Maciej Gołaszewski <jodator@jodator.net>
2021-06-01 18:00:48 +0200

    Fix lint errors

--- packages/ui/src/common/components/page/Sidebar/LinksIcons/WorkingGroupsIcon.tsx
+++ packages/ui/src/common/components/page/Sidebar/LinksIcons/WorkingGroupsIcon.tsx
@@ -17,12 +17,6 @@ export const WorkingGroupsIcon = () => (
       d="M0.833252 10H5.83325H6.66658V7.5H13.3333V10H19.1666V5.83333L18.3333 5H14.1666V3.33333L13.3333 2.5H6.66658L5.83325 3.33333V5H1.66659L0.833252 5.83333V10ZM6.66658 11.6667V14.1667H13.3333V11.6667H19.1666V17.5H0.833252V11.6667H6.66658Z"
       fill="currentColor"
     />
-    <rect
-      x="8.33325"
-      y="9.16663"
-      width="3.33333"
-      height="3.33333"
-      fill="currentColor"
-    />
+    <rect x="8.33325" y="9.16663" width="3.33333" height="3.33333" fill="currentColor" />
   </Icon>
 )
