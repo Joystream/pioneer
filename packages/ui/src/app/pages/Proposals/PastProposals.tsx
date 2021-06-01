5d9f0defbd913d81ced1db3f1f667f6b2780517e
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-06-01 16:19:50 +0200

    Add `<ProposalFilters />` to the past proposals tab

--- packages/ui/src/app/pages/Proposals/PastProposals.tsx
+++ packages/ui/src/app/pages/Proposals/PastProposals.tsx
@@ -1,34 +1,44 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 import { AppPage } from '@/app/components/AppPage'
 import { ActivitiesBlock } from '@/common/components/Activities/ActivitiesBlock'
+import { FilterPageHeader } from '@/common/components/forms/FilterBox'
 import { Loading } from '@/common/components/Loading'
 import { ContentWithSidepanel, MainPanel } from '@/common/components/page/PageContent'
-import { PageHeader } from '@/common/components/page/PageHeader'
-import { PageTitle } from '@/common/components/page/PageTitle'
 import { SidePanel } from '@/common/components/page/SidePanel'
 import { useActivities } from '@/common/hooks/useActivities'
 import { AddProposalButton } from '@/proposals/components/AddProposalButton'
+import { ProposalEmptyFilter, ProposalFilters } from '@/proposals/components/ProposalFilters'
 import { ProposalList } from '@/proposals/components/ProposalList'
 import { usePastProposals } from '@/proposals/hooks/usePastProposals'
 
 import { ProposalsTabs } from './components/ProposalsTabs'
 
 export const PastProposals = () => {
-  const { proposals, isLoading } = usePastProposals()
+  const searchSlot = useRef<HTMLDivElement>(null)
+  const [filters, setFilters] = useState(ProposalEmptyFilter)
+
+  const { proposals, types, stages, proposers, isLoading } = usePastProposals({ filters })
   const activities = useActivities()
+
   const sideNeighborRef = useRef<HTMLDivElement>(null)
 
   return (
     <AppPage>
-      <PageHeader>
-        <PageTitle>Proposals</PageTitle>
+      <FilterPageHeader ref={searchSlot} title="Proposals">
         <AddProposalButton />
         <ProposalsTabs />
-      </PageHeader>
+      </FilterPageHeader>
 
       <ContentWithSidepanel>
         <MainPanel ref={sideNeighborRef}>
+          <ProposalFilters
+            searchSlot={searchSlot}
+            types={types}
+            stages={stages}
+            proposers={proposers}
+            onApply={setFilters}
+          />
           {isLoading ? <Loading /> : <ProposalList proposals={proposals} isPast />}
         </MainPanel>
         <SidePanel neighbor={sideNeighborRef}>
--- packages/ui/src/proposals/hooks/usePastProposals.ts
+++ packages/ui/src/proposals/hooks/usePastProposals.ts
@@ -1,16 +1,30 @@
 import { error } from '@/common/logger'
-import { mockPastProposals } from '@/mocks/data/mockProposals'
+import { Member } from '@/memberships/types'
+import { mockMembers } from '@/mocks/data'
+import { mockPastProposals, proposalStages, proposalTypes } from '@/mocks/data/mockProposals'
 import { useMockDelay } from '@/mocks/hooks/useMockDelay'
+import { ProposalFiltersState } from '@/proposals/components/ProposalFilters'
 
-export const usePastProposals = () => {
-  const { loading, data, error: err } = useMockDelay({ proposals: mockPastProposals })
+interface UsePastProposalsProps {
+  filters: ProposalFiltersState
+}
+
+export const usePastProposals = ({ filters }: UsePastProposalsProps) => {
+  const { loading, data, error: err } = useMockDelay({ proposals: mockPastProposals, filters })
 
   if (err) {
     error(err)
   }
 
+  const stages = data ? proposalStages : []
+  const types = data ? proposalTypes : []
+  const proposers = data ? ((mockMembers.slice(0, 10) as unknown) as Member[]) : []
+
   return {
     isLoading: loading,
     proposals: data?.proposals ?? [],
+    types,
+    stages,
+    proposers,
   }
 }
