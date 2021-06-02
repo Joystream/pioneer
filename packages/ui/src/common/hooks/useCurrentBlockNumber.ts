216654424da556c11f34edbb07121483978d1050
p-sad <sadlik512@gmail.com>
2021-06-02 14:45:36 +0200

    Add useCurrentBlockNumber hook

---
+++ packages/ui/src/common/hooks/useCurrentBlockNumber.ts
@@ -0,0 +1,7 @@
+import { useApi } from "./useApi";
+import { useObservable } from "./useObservable";
+
+export function useCurrentBlockNumber() {
+  const { api } = useApi()
+  return useObservable(api?.rpc.chain.getBlock(), [api])?.block.header.number.toBn()
+}
