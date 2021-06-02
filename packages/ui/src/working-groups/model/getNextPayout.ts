1dd175f01cb2749f5c7e1bff01a60c5628b6c45f
p-sad <sadlik512@gmail.com>
2021-06-02 18:33:52 +0200

    Improve readability

--- packages/ui/src/working-groups/model/getNextPayout.ts
+++ packages/ui/src/working-groups/model/getNextPayout.ts
@@ -5,16 +5,13 @@ import { GroupRewardPeriods, isKnownGroupName, Worker } from '../types'
 export function getNextPayout(workers: Pick<Worker, 'group'>[], blockNumber: BN) {
   const blocksUntilNext = (interval: BN) => interval.sub(blockNumber.mod(interval))
 
-  const nextPayoutPerGroup = workers
-    .map((worker) => worker.group.name)
-    .filter(distinct)
+  const userGroups = [...new Set(workers.map((worker) => worker.group.name))]
+  const nextPayoutPerGroup = userGroups
     .filter(isKnownGroupName)
     .map((name) => GroupRewardPeriods[name])
     .map(blocksUntilNext)
 
-  return nextPayoutPerGroup.length
-    ? nextPayoutPerGroup.reduce((closest, time) => (closest = BN.min(closest, time)))
-    : undefined
+  if (nextPayoutPerGroup.length) {
+    return nextPayoutPerGroup.reduce((closest, time) => (closest = BN.min(closest, time)))
+  }
 }
-
-const distinct = (name: string, index: number, all: string[]) => index === all.indexOf(name)
