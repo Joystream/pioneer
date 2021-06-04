b376f9f4f4174cd9c51eecd01759c739e085e52b
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-06-04 20:16:59 +0200

    Implement the `<ProposerSelect />` component

---
+++ packages/ui/src/proposals/components/ProposalFilters/SelectProposer.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, Story } from '@storybook/react'
+import React, { useState } from 'react'
+
+import { SelectMember } from '@/memberships/components/SelectMember'
+import { MembershipContextProvider } from '@/memberships/providers/membership/provider'
+import { Member } from '@/memberships/types'
+import { MockApolloProvider } from '@/mocks/components/storybook/MockApolloProvider'
+
+import { SelectProposer } from './SelectProposer'
+
+export default {
+  title: 'Proposals/SelectProposer',
+  component: SelectProposer,
+  argTypes: {
+    onChange: { action: 'changed' },
+  },
+} as Meta
+
+export const Default: Story = ({ onChange }) => {
+  const [value, setValue] = useState<Member | null>(null)
+
+  return (
+    <MockApolloProvider>
+      <MembershipContextProvider>
+        <SelectMember onChange={onChange} /> {/* TODO remove */}
+        <SelectProposer
+          value={value}
+          onChange={(value) => {
+            setValue(value)
+            onChange(value)
+          }}
+        />
+      </MembershipContextProvider>
+    </MockApolloProvider>
+  )
+}
