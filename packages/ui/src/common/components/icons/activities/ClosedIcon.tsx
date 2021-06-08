be2f20c9b070eb77fe046fdc4b3b6d95086c397e
Pawel <Pawel@Tests-MacBook-Air.local>
2021-06-08 09:56:26 +0200

    Update solid icon

--- packages/ui/src/common/components/icons/activities/ClosedIcon.tsx
+++ packages/ui/src/common/components/icons/activities/ClosedIcon.tsx
@@ -12,7 +12,7 @@ export const ClosedIcon = ({ className }: StyledIcon) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M13.5774 5.24403L9.99994 8.82145L6.42253 5.24403L5.24402 6.42255L8.82153 10.0001L5.24414 13.5774L6.42266 14.756L10.0001 11.1785L13.5775 14.756L14.756 13.5774L11.1785 9.99993L14.7559 6.42255L13.5774 5.24403Z"
-      fill="currentColor"
+      fill="white"
     />
   </Icon>
 )
