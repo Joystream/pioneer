e20c220964968f36659678ab90d2835817aed061
Maciej Gołaszewski <jodator@jodator.net>
2021-06-09 15:55:29 +0200

    Instantiate MirageJS server before Apollo client

--- packages/ui/src/app/providers/QueryNodeProvider.tsx
+++ packages/ui/src/app/providers/QueryNodeProvider.tsx
@@ -9,27 +9,27 @@ interface Props {
   children: ReactNode
 }
 
-const endpointMap: Record<NetworkType, string> = {
+const ENDPOINTS: Record<NetworkType, string> = {
   local: 'http://localhost:8081/graphql',
   'olympia-testnet': 'https://olympia-dev.joystream.app/query/server/graphql',
 }
 
-const getQueryNodeUri = (network: NetworkType) => endpointMap[network]
-
 export const QueryNodeProvider = ({ children }: Props) => {
   const [network] = useNetwork()
-  const client = new ApolloClient({
-    uri: getQueryNodeUri(network),
-    cache: new InMemoryCache(),
-  })
 
   if (network === 'olympia-testnet') {
-    return <ApolloProvider client={client}>{children}</ApolloProvider>
+    return <ApolloProvider client={getApolloClient(network)}>{children}</ApolloProvider>
   }
 
   return (
     <ServerContextProvider value={makeServer()}>
-      <ApolloProvider client={client}>{children}</ApolloProvider>
+      <ApolloProvider client={getApolloClient(network)}>{children}</ApolloProvider>
     </ServerContextProvider>
   )
 }
+
+const getApolloClient = (network: 'local' | 'olympia-testnet') =>
+  new ApolloClient({
+    uri: ENDPOINTS[network],
+    cache: new InMemoryCache(),
+  })
