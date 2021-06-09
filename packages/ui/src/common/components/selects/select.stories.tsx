5539ddc0af8d64e5bac6257d13be571aacc5b57d
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-06-09 18:39:36 +0200

    Add the new select components to Storybook

--- packages/ui/src/common/components/selects/select.stories.tsx
+++ packages/ui/src/common/components/selects/select.stories.tsx
@@ -1,30 +1,54 @@
 import { Meta, Story } from '@storybook/react'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
-import { Column, ModalBlock, Row } from '../storybookParts/previewStyles'
+import { ModalBlock, Row, TemplateBlock } from '../storybookParts/previewStyles'
 
-import { SimpleSelect, FilterSelect } from '.'
+import { SimpleSelect, FilterSelect, MultiSelect } from '.'
 
 export default {
   title: 'Common/Forms/Select',
-  subcomponents: { SimpleSelect, FilterSelect },
+  subcomponents: { SimpleSelect, FilterSelect, MultiSelect },
+  argTypes: {
+    onApply: { action: 'Apply' },
+  },
 } as Meta
 
-const Template: Story<{ values: string[] }> = ({ values }) => {
+interface Props {
+  optionsString: string
+  onApply: (value: string[]) => void
+}
+
+const Template: Story<Props> = ({ optionsString = '', onApply }) => {
+  const options = (optionsString || '').split(' ')
   const [value, setValue] = useState<string | null>(null)
+
+  // Search Select
+  const [search, setSearch] = useState('')
+  const result = useMemo(() => {
+    const pattern = RegExp(search)
+    return options.filter((option) => pattern.test(option))
+  }, [search, options])
+
+  // Multi Select
+  const [multi, setMulti] = useState<string[]>([])
+
   return (
     <ModalBlock>
-      <Row>
-        <Column>
-          <SimpleSelect title="Simple Select" values={values} value={value} onChange={setValue} />
-        </Column>
-        <Column>
-          <FilterSelect title="Filter Select" values={values} value={value} onChange={setValue} />
-        </Column>
-      </Row>
+      <TemplateBlock>
+        <Row>
+          <SimpleSelect title="Simple Select" options={options} value={value} onChange={setValue} />
+          <FilterSelect title="Filter Select" options={options} value={value} onChange={setValue} />
+        </Row>
+        <Row>
+          <SimpleSelect title="Search Select" options={result} value={value} onChange={setValue} onSearch={setSearch} />
+          <MultiSelect title="Multi Select" options={options} value={multi} onChange={setMulti} onApply={onApply} />
+        </Row>
+      </TemplateBlock>
     </ModalBlock>
   )
 }
 
 export const Default = Template
-Default.args = { values: ['sunt', 'irure', 'cillum', 'labore'] }
+Default.args = {
+  optionsString: 'sunt irure cillum labore pariatur commodo tempor excepteur',
+}
