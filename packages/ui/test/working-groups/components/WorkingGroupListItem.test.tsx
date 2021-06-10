1352661eb902f15243edbbc9ad05ab6259ae522d
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-06-10 10:25:09 +0200

    Remove junk

--- packages/ui/test/working-groups/components/WorkingGroupListItem.test.tsx
+++ packages/ui/test/working-groups/components/WorkingGroupListItem.test.tsx
@@ -1,26 +1,13 @@
 import { cryptoWaitReady } from '@polkadot/util-crypto'
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
-import { startOfToday, subDays } from 'date-fns'
 import React from 'react'
 import { MemoryRouter } from 'react-router'
 
-import { AccountsContext } from '@/accounts/providers/accounts/context'
-import { UseAccounts } from '@/accounts/providers/accounts/provider'
-import { MembershipContext } from '@/memberships/providers/membership/context'
-import { MyMemberships } from '@/memberships/providers/membership/provider'
 import { seedMembers, seedOpening, seedOpeningStatuses } from '@/mocks/data'
-import { seedApplications } from '@/mocks/data/mockApplications'
-import { seedOpenings } from '@/mocks/data/mockOpenings'
-import { seedRewardPaidEvent } from '@/mocks/data/mockRewardPaidEvents'
 import { seedWorkingGroups } from '@/mocks/data/mockWorkingGroups'
-import { seedWorkers } from '@/mocks/data/seedWorkers'
-import { MyEarningsStat } from '@/working-groups/components/MyEarningsStat'
 import { WorkingGroupListItem } from '@/working-groups/components/WorkingGroupListItem'
-import { WorkingGroupFieldsFragment } from '@/working-groups/queries'
-import { asWorkingGroup, WorkingGroup } from '@/working-groups/types'
+import { WorkingGroup } from '@/working-groups/types'
 
-import { alice, bob } from '../../_mocks/keyring'
-import { getMember } from '../../_mocks/members'
 import { MockKeyringProvider, MockQueryNodeProviders } from '../../_mocks/providers'
 import { setupMockServer } from '../../_mocks/server'
 import { OPENING_DATA } from '../../_mocks/server/seeds'
@@ -44,7 +31,6 @@ describe('WorkingGroupListItem', () => {
   it('Loading', () => {
     renderElement(group)
 
-    expect(1).toBe(1)
     expect(screen.getByText('-')).toBeDefined()
   })
 
