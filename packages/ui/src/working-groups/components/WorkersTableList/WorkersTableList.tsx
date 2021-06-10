e6cf017c00ca8dbdeed60c189c7c998a29868bac
ClumsyVlad <mazur097mazur@gmail.com>
2021-06-10 14:57:27 +0200

    Fix workers fail

--- packages/ui/src/working-groups/components/WorkersTableList/WorkersTableList.tsx
+++ packages/ui/src/working-groups/components/WorkersTableList/WorkersTableList.tsx
@@ -6,16 +6,15 @@ import { Member } from '@/memberships/types'
 import { WorkersTableListRow } from './WorkersTableListRow'
 
 export interface WorkersTableListProps {
-  leader?: Member
   workers?: Member[]
   past?: boolean
 }
 
-export const WorkersTableList = ({ leader, workers, past }: WorkersTableListProps) => (
+export const WorkersTableList = ({workers, past }: WorkersTableListProps) => (
   <>
     {workers && (
       <List>
-        {workers.map((worker, leader) => (
+        {workers.map((worker) => (
           <ListItem key={worker.id}>
             <WorkersTableListRow worker={worker} past={past} />
           </ListItem>
