39bc85c5e22e1c956d8282c452ecfe6b1bb127aa
ClumsyVlad <mazur097mazur@gmail.com>
2021-06-10 12:05:53 +0200

    Get back before create new component

--- packages/ui/src/working-groups/components/Worker.tsx
+++ packages/ui/src/working-groups/components/Worker.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { ButtonBareGhost, ButtonGhost } from '@/common/components/buttons'
+import { ButtonGhost } from '@/common/components/buttons'
 import { FileIcon } from '@/common/components/icons/FileIcon'
-import { Colors, Transitions } from '@/common/constants'
 import { MemberInfo } from '@/memberships/components'
 import { Member } from '@/memberships/types'
 
@@ -15,22 +14,15 @@ interface WorkerProps {
 
 export const Worker = ({ member, isLeader, past }: WorkerProps) => (
   <WorkerWrap past={past}>
-    <MemberInfo member={member} isLeader={isLeader} showId />
-    <ButtonBareGhost square size="small">
+    <MemberInfo member={member} isLeader={isLeader} />
+    <ButtonGhost square size="small">
       <FileIcon />
-    </ButtonBareGhost>
+    </ButtonGhost>
   </WorkerWrap>
 )
 
 const WorkerWrap = styled.div<{ past?: boolean }>`
   display: grid;
-  grid-template-columns: 1fr 40px;
-  grid-column-gap: 24px;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 94px;
-  padding: 16px;
-  background-color: ${({ past }) => (past ? Colors.Black[50] : Colors.White)};
-  transition: ${Transitions.all};
+  grid-template-columns: 1fr 32px;
+  grid-column-gap: 8px;
 `
--- packages/ui/src/working-groups/components/WorkersList.tsx
+++ packages/ui/src/working-groups/components/WorkersList.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { CountBadge } from '@/common/components/CountBadge'
-import { List, ListItem } from '@/common/components/List'
 import { ContentWithTabs, RowGapBlock } from '@/common/components/page/PageContent'
 import { Label } from '@/common/components/typography'
 import { Member } from '@/memberships/types'
@@ -16,33 +15,25 @@ export interface WorkersListProps {
 
 export const WorkersList = ({ leader, workers, past }: WorkersListProps) => {
   return (
-    <List as="div">
-      <RowGapBlock gap={36} align="none">
-        {leader && (
-          <RowGapBlock gap={16} align="none">
-            <Label>Leader</Label>
-            <List>
-              <ListItem>
-                <Worker member={leader} isLeader={true} past={past} />
-              </ListItem>
-            </List>
-          </RowGapBlock>
+    <RowGapBlock gap={36}>
+      {leader && (
+        <ContentWithTabs>
+          <Label>Leader</Label>
+          <Worker member={leader} isLeader={true} past={past} />
+        </ContentWithTabs>
+      )}
+      <ContentWithTabs>
+        <Label>
+          Workers <CountBadge count={workers?.length ?? 0} />{' '}
+        </Label>
+        {workers && (
+          <ContentWithTabs>
+            {workers.map((member) => (
+              <Worker key={member.handle} member={member} past={past} />
+            ))}
+          </ContentWithTabs>
         )}
-        <RowGapBlock gap={16} align="none">
-          <Label>
-            Workers <CountBadge count={workers?.length ?? 0} />{' '}
-          </Label>
-          {workers && (
-            <List>
-              {workers.map((member) => (
-                <ListItem key={member.handle}>
-                  <Worker member={member} past={past} />
-                </ListItem>
-              ))}
-            </List>
-          )}
-        </RowGapBlock>
-      </RowGapBlock>
-    </List>
+      </ContentWithTabs>
+    </RowGapBlock>
   )
 }
