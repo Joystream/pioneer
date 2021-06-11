863e0ef036054877226869d01ab81bc1d823993a
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2021-06-11 14:49:11 +0200

    📚 Inputs with smaller height prop (#814)
    * Inputs with smaller height prop
    
    * Add Datepicker calendar icon

--- packages/ui/src/common/components/forms/DatePicker/DatePicker.tsx
+++ packages/ui/src/common/components/forms/DatePicker/DatePicker.tsx
@@ -7,6 +7,7 @@ import { Colors, Shadows } from '@/common/constants'
 import { DateRange, PartialDateRange } from '@/common/types/Dates'
 import { earliest, fromRange, latest, toDDMMYY } from '@/common/utils/dates'
 
+import { CalendarIcon } from '../../icons/CalendarIcon'
 import { Calendar } from '../Calendar'
 import { CALENDAR_WRAP_SIZE } from '../Calendar/components'
 import { FilterLabel } from '../FilterBox'
@@ -18,9 +19,20 @@ interface DatePickerProps extends ControlProps<PartialDateRange> {
   withinDates?: PartialDateRange
   onApply?: () => void
   onClear?: () => void
+  inputSize?: 'xs' | 's' | 'm' | 'l' | 'auto' | undefined
+  inputWidth?: 'auto' | 's' | 'xs' | undefined
 }
 
-export const DatePicker = ({ title, value, withinDates, onApply, onClear, onChange }: DatePickerProps) => {
+export const DatePicker = ({
+  title,
+  value,
+  withinDates,
+  onApply,
+  onClear,
+  onChange,
+  inputSize,
+  inputWidth,
+}: DatePickerProps) => {
   const placeholder = '__/__/__'
   const { start, end } = fromRange(value)
   const dateString = `${toDDMMYY(start) ?? placeholder} - ${toDDMMYY(end) ?? placeholder}`
@@ -52,7 +64,7 @@ export const DatePicker = ({ title, value, withinDates, onApply, onClear, onChan
   return (
     <DatePickerContainer ref={container} onMouseDown={() => !isOpen && toggleOpen(true)}>
       {title && <FilterLabel>{title}</FilterLabel>}
-      <DatePickerInput tight inputWidth="xs">
+      <DatePickerInput tight inputWidth={inputWidth} inputSize={inputSize} icon={<CalendarIcon />} iconRight>
         <InputText placeholder="-" value={dateString} readOnly />
         {isOpen && (
           <DatePickerPopup>
