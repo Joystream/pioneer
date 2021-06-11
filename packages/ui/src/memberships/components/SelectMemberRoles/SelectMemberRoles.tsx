757913d20a981d438ecb0f9719a996536ce1a1bf
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-06-11 17:17:29 +0200

    Reuse `<MemberRoles />` to render selected roles

--- packages/ui/src/memberships/components/SelectMemberRoles/SelectMemberRoles.tsx
+++ packages/ui/src/memberships/components/SelectMemberRoles/SelectMemberRoles.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
 
 import { ControlProps } from '@/common/components/forms'
-import { MultiSelect, MultiSelected, Selected } from '@/common/components/selects'
+import { MultiSelect, Selected } from '@/common/components/selects'
 import { TextMedium } from '@/common/components/typography'
-import { intersperse } from '@/common/utils'
-import { MemberRoleHelp } from '@/memberships/components/MemberRoles'
-import { memberRoleAbbreviation, memberRoleTitle } from '@/memberships/helpers'
+import { MemberRoles } from '@/memberships/components/MemberRoles'
+import { memberRoleTitle } from '@/memberships/helpers'
 import { useAllMemberRoles } from '@/memberships/hooks/useAllMemberRoles'
 import { MemberRole } from '@/memberships/types'
 
@@ -23,23 +21,11 @@ export const SelectMemberRoles = ({ value, ...props }: Props) => {
     roles.length > 0 && setOptions(roles)
   }, [JSON.stringify(roles)])
 
-  const renderSelected = (value: MemberRole[]) => {
-    if (!value.length) {
-      return (
-        <Selected>
-          <TextMedium lighter>Select</TextMedium>
-        </Selected>
-      )
-    } else {
-      const roleHelpers = value.map((role, key) => (
-        <MemberRoleHelp key={key} size="l">
-          {memberRoleAbbreviation(role)}
-        </MemberRoleHelp>
-      ))
-      return <SelectedRoles>{intersperse(roleHelpers, ' ')}</SelectedRoles>
-    }
-  }
-
+  const renderSelected = (roles: MemberRole[]) => (
+    <Selected>
+      {roles.length ? <MemberRoles roles={roles} size="l" max={3} /> : <TextMedium lighter>Select</TextMedium>}
+    </Selected>
+  )
   return (
     <MultiSelect
       title="Roles"
@@ -51,10 +37,3 @@ export const SelectMemberRoles = ({ value, ...props }: Props) => {
     />
   )
 }
-
-const SelectedRoles = styled(MultiSelected)`
-  word-spacing: 4px;
-  ${MemberRoleHelp} {
-    display: inline-flex;
-  }
-`
