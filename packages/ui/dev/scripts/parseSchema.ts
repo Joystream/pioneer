b0fb441350dff435b6b6e61660acfecd2f992d0e
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-06-13 15:52:14 +0200

    Add script to parse queiries

---
+++ packages/ui/dev/scripts/parseSchema.ts
@@ -0,0 +1,12 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
+
+const parse = async () => {
+  const schemaPath = join(__dirname, '..', '..', 'src', 'common', 'api', 'schemas', 'schema.graphql')
+  let schemaContent = await readFileSync(schemaPath, 'utf-8')
+
+  schemaContent = schemaContent.replace(/\b(implements)\b(.*?),((\s*).*?,)?/g, '$1$2 &$3')
+  await writeFileSync(schemaPath, schemaContent, { encoding: 'utf-8' })
+}
+
+parse()
