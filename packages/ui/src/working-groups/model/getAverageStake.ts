1e76353902e474625a0bfa49cc9c579d4bcc59f0
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-06-14 18:01:56 +0200

    Reduce BNs instead of numbers

--- packages/ui/src/working-groups/model/getAverageStake.ts
+++ packages/ui/src/working-groups/model/getAverageStake.ts
@@ -3,7 +3,7 @@ import BN from 'bn.js'
 import { WorkerFieldsFragment } from '@/working-groups/queries'
 
 export const getAverageStake = (workers: Pick<WorkerFieldsFragment, 'stake'>[]): BN => {
-  const totalStake = workers.reduce((a, b) => a + b.stake, 0)
+  const totalStake = workers.reduce((a, b) => a.add(new BN(b.stake)), new BN(0))
 
-  return new BN(Math.round(totalStake / workers.length))
+  return totalStake.divn(workers.length)
 }
