18f6e0570b3312c0949d4a692c6b1cd28f536063
p-sad <sadlik512@gmail.com>
2021-06-15 11:07:58 +0200

    Re-add a "no openings found" message

--- packages/ui/src/working-groups/components/OpeningsList/OpeningsList.tsx
+++ packages/ui/src/working-groups/components/OpeningsList/OpeningsList.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { List, ListItem } from '@/common/components/List'
-import { Loading } from '@/common/components/Loading'
 import { UpcomingWorkingGroupOpening, WorkingGroupOpening } from '@/working-groups/types'
 
 import { OpeningsListRow } from './OpeningsListRow'
@@ -9,13 +8,10 @@ import { OpeningsListRow } from './OpeningsListRow'
 export interface OpeningsListProps {
   openings?: WorkingGroupOpening[] | UpcomingWorkingGroupOpening[]
   past?: boolean
-  isLoading?: boolean
 }
 
-export const OpeningsList = ({ openings, past, isLoading }: OpeningsListProps) => {
-  return isLoading ? (
-    <Loading />
-  ) : (
+export const OpeningsList = ({ openings, past }: OpeningsListProps) => {
+  return (
     <List>
       {openings?.map((opening: any) => (
         <ListItem key={opening.id}>
--- packages/ui/src/working-groups/components/OpeningsList/OpeningsPagination.tsx
+++ packages/ui/src/working-groups/components/OpeningsList/OpeningsPagination.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 
+import { Loading } from '@/common/components/Loading'
 import { Pagination } from '@/common/components/Pagination'
+import { TextBig } from '@/common/components/typography'
 import { OpeningsList } from '@/working-groups/components/OpeningsList/OpeningsList'
 import { useOpenings, UseOpeningsParams } from '@/working-groups/hooks/useOpenings'
 
@@ -9,9 +11,13 @@ type Props = Omit<UseOpeningsParams, 'page'>
 export const OpeningsPagination = (props: Props) => {
   const [page, setPage] = useState(1)
   const { isLoading, openings, pageCount } = useOpenings({ ...props, page })
+
+  if (isLoading) return <Loading />
+  if (openings.length === 0) return <TextBig>No openings found</TextBig>
+
   return (
     <>
-      <OpeningsList isLoading={isLoading} openings={openings} />
+      <OpeningsList openings={openings} />
       <Pagination page={page} pageCount={pageCount} handlePageChange={setPage} />
     </>
   )
