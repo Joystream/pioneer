3c2590bbe2dd3db5a5946c81b52a9cbc922ee004
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-06-16 13:17:47 +0200

    Check active member and funds for add proposal

--- packages/ui/src/proposals/modals/AddNewProposal/AddNewProposalModal.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/AddNewProposalModal.tsx
@@ -1,26 +1,64 @@
 import { ApiRx } from '@polkadot/api'
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
+import { useTransactionFee } from '@/accounts/hooks/useTransactionFee'
+import { InsufficientFundsModal } from '@/accounts/modals/InsufficientFundsModal'
 import { FailureModal } from '@/common/components/FailureModal'
+import { useApi } from '@/common/hooks/useApi'
 import { useModal } from '@/common/hooks/useModal'
+import { ModalState } from '@/common/types'
 import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 import { SwitchMemberModalCall } from '@/memberships/modals/SwitchMemberModal'
 import { AddNewProposalModalCall } from '@/proposals/modals/AddNewProposal/index'
+import { ProposalDetails } from '@/proposals/types'
 
-export type OpeningParams = Exclude<
-  Parameters<ApiRx['tx']['membershipWorkingGroup']['applyOnOpening']>[0],
+export type NewProposalParams = Exclude<
+  Parameters<ApiRx['tx']['proposalsCodex']['createProposal']>[0],
   string | Uint8Array
 >
 
 export const AddNewProposalModal = () => {
-  const { active: membership } = useMyMemberships()
+  const { api } = useApi()
+  const { active: member } = useMyMemberships()
   const { hideModal, showModal } = useModal<AddNewProposalModalCall>()
+  const [state, setState] = useState<ModalState>('REQUIREMENTS_CHECK')
+  const [type, setType] = useState<ProposalDetails>('signal')
+
+  const [txParams, setTxParams] = useState<NewProposalParams>({
+    member_id: member?.id,
+    title: '',
+    description: '',
+    staking_account_id: member?.controllerAccount,
+  })
+
+  const transaction = useMemo(() => {
+    if (member && txParams && api) {
+      return api.tx.proposalsCodex.createProposal(txParams, { Signal: '' })
+    }
+  }, [api, JSON.stringify(txParams)])
+  const feeInfo = useTransactionFee(member?.controllerAccount, transaction)
 
   useEffect(() => {
-    if (!membership) {
-      showModal<SwitchMemberModalCall>({ modal: 'SwitchMember' })
+    if (!member) {
+      return showModal<SwitchMemberModalCall>({ modal: 'SwitchMember' })
     }
-  }, [membership?.id])
+
+    if (feeInfo && feeInfo.canAfford) {
+      return setState('PREPARE')
+    }
+
+    setState('REQUIREMENTS_FAIL')
+  }, [state, member?.id, JSON.stringify(feeInfo)])
+
+  if (!member || !feeInfo) {
+    return null
+  }
+
+  if (state === 'REQUIREMENTS_FAIL') {
+    return (
+      <InsufficientFundsModal onClose={hideModal} address={member.controllerAccount} amount={feeInfo.transactionFee} />
+    )
+  }
 
   return <FailureModal onClose={hideModal}>There was a problem with applying for an opening.</FailureModal>
 }
