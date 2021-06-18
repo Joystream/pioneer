d564d74d70ece0d4a8c5832872e39a8930ae7fe0
Maciej Gołaszewski <jodator@jodator.net>
2021-06-18 12:00:36 +0200

    Extract apply for role machine

---
+++ packages/ui/src/working-groups/modals/ApplyForRoleModal/machine.ts
@@ -0,0 +1,56 @@
+import { EventRecord } from '@polkadot/types/interfaces/system'
+import { assign, createMachine } from 'xstate'
+
+import { isError } from '../../../common/hooks/useSignAndSendTransaction'
+import { transactionConfig } from '../../../common/model/machines'
+
+export const applyForRoleMachine = createMachine<{ transactionEvents: EventRecord[] }>(
+  {
+    initial: 'requirementsVerification',
+    context: { transactionEvents: [] },
+    states: {
+      requirementsVerification: {
+        on: {
+          FAIL: 'requirementsFailed',
+          PASS: 'steps',
+        },
+      },
+      requirementsFailed: { type: 'final' },
+      steps: {
+        initial: 'prepare',
+        states: {
+          prepare: {
+            on: { VALID: 'transaction' },
+          },
+          transaction: {
+            invoke: {
+              id: 'transaction',
+              src: createMachine(transactionConfig),
+              onDone: [
+                {
+                  target: 'error',
+                  actions: ['assignTransactionEvents'],
+                  cond: (context, event) => isError(event.data.events),
+                },
+                {
+                  target: 'success',
+                  cond: (context, event) => !isError(event.data.events),
+                  actions: ['assignTransactionEvents'],
+                },
+              ],
+            },
+          },
+          success: { type: 'final' },
+          error: { type: 'final' },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      assignTransactionEvents: assign({
+        transactionEvents: (context, event) => event.data.events,
+      }),
+    },
+  }
+)
