9ec060e24a4fc4242810606e40625f3170776b27
Maciej Gołaszewski <jodator@jodator.net>
2021-06-18 12:00:36 +0200

    Extract apply for role machine

--- packages/ui/src/working-groups/modals/ApplyForRoleModal/ApplyForRoleModal.tsx
+++ packages/ui/src/working-groups/modals/ApplyForRoleModal/ApplyForRoleModal.tsx
@@ -1,11 +1,9 @@
 import { ApplicationMetadata } from '@joystream/metadata-protobuf'
 import { ApplicationId } from '@joystream/types/working-group'
 import { ApiRx } from '@polkadot/api'
-import { EventRecord } from '@polkadot/types/interfaces/system'
 import { useMachine } from '@xstate/react'
 import BN from 'bn.js'
 import React, { useEffect, useMemo, useState } from 'react'
-import { assign, createMachine } from 'xstate'
 
 import { useHasRequiredStake } from '@/accounts/hooks/useHasRequiredStake'
 import { useTransactionFee } from '@/accounts/hooks/useTransactionFee'
@@ -16,7 +14,6 @@ import { FailureModal } from '@/common/components/FailureModal'
 import { BN_ZERO } from '@/common/constants'
 import { useApi } from '@/common/hooks/useApi'
 import { useModal } from '@/common/hooks/useModal'
-import { isError } from '@/common/hooks/useSignAndSendTransaction'
 import { getEventParam, metadataToBytes } from '@/common/model/JoystreamNode'
 import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 import { SwitchMemberModalCall } from '@/memberships/modals/SwitchMemberModal'
@@ -24,11 +21,10 @@ import { ApplyForRoleModalCall } from '@/working-groups/modals/ApplyForRoleModal
 import { StakeStepForm } from '@/working-groups/modals/ApplyForRoleModal/StakeStep'
 import { getGroup } from '@/working-groups/model/getGroup'
 
-import { transactionConfig } from '../../../common/model/machines'
-
 import { ApplyForRolePrepareModal } from './ApplyForRolePrepareModal'
 import { ApplyForRoleSignModal } from './ApplyForRoleSignModal'
 import { ApplyForRoleSuccessModal } from './ApplyForRoleSuccessModal'
+import { applyForRoleMachine } from './machine'
 
 export type OpeningParams = Exclude<
   Parameters<ApiRx['tx']['membershipWorkingGroup']['applyOnOpening']>[0],
@@ -39,61 +35,9 @@ export const ApplyForRoleModal = () => {
   const { api } = useApi()
   const { active } = useMyMemberships()
   const { hideModal, modalData, showModal } = useModal<ApplyForRoleModalCall>()
+  const [state, send] = useMachine(applyForRoleMachine)
+
   const opening = modalData.opening
-
-  const [state, send] = useMachine(
-    createMachine<{ transactionEvents: EventRecord[] }>(
-      {
-        initial: 'requirementsVerification',
-        context: { transactionEvents: [] },
-        states: {
-          requirementsVerification: {
-            on: {
-              FAIL: 'requirementsFailed',
-              PASS: 'steps',
-            },
-          },
-          requirementsFailed: { type: 'final' },
-          steps: {
-            initial: 'prepare',
-            states: {
-              prepare: {
-                on: { VALID: 'transaction' },
-              },
-              transaction: {
-                invoke: {
-                  id: 'transaction',
-                  src: createMachine(transactionConfig),
-                  onDone: [
-                    {
-                      target: 'error',
-                      actions: ['assignTransactionEvents'],
-                      cond: (context, event) => isError(event.data.events),
-                    },
-                    {
-                      target: 'success',
-                      cond: (context, event) => !isError(event.data.events),
-                      actions: ['assignTransactionEvents'],
-                    },
-                  ],
-                },
-              },
-              success: { type: 'final' },
-              error: { type: 'final' },
-            },
-          },
-        },
-      },
-      {
-        actions: {
-          assignTransactionEvents: assign({
-            transactionEvents: (context, event) => event.data.events,
-          }),
-        },
-      }
-    )
-  )
-
   const [txParams, setTxParams] = useState<OpeningParams>({
     member_id: active?.id,
     opening_id: opening.runtimeId,
