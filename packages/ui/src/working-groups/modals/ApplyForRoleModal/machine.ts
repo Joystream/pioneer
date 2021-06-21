9c957ba5603f667773d6376248e94eda5f5d833e
Maciej Gołaszewski <jodator@jodator.net>
2021-06-21 15:46:13 +0200

    Remove machine context and add event type

--- packages/ui/src/working-groups/modals/ApplyForRoleModal/machine.ts
+++ packages/ui/src/working-groups/modals/ApplyForRoleModal/machine.ts
@@ -1,46 +1,38 @@
-import { EventRecord } from '@polkadot/types/interfaces/system'
-import { assign, createMachine } from 'xstate'
+import { createMachine } from 'xstate'
 
-interface ApplyForRoleContext {
-  transactionEvents: EventRecord[]
-}
+type ApplyForRoleEvent =
+  | { type: 'FAIL' }
+  | { type: 'PASS' }
+  | { type: 'VALID' }
+  | { type: 'SUCCESS' }
+  | { type: 'ERROR' }
 
-export const applyForRoleMachine = createMachine<ApplyForRoleContext>(
-  {
-    initial: 'requirementsVerification',
-    context: { transactionEvents: [] },
-    states: {
-      requirementsVerification: {
-        on: {
-          FAIL: 'requirementsFailed',
-          PASS: 'stake',
-        },
+export const applyForRoleMachine = createMachine<Record<string, never>, ApplyForRoleEvent>({
+  initial: 'requirementsVerification',
+  states: {
+    requirementsVerification: {
+      on: {
+        FAIL: 'requirementsFailed',
+        PASS: 'stake',
       },
-      requirementsFailed: { type: 'final' },
-      stake: {
-        meta: { isStep: true, stepTitle: 'Stake' },
-        on: { VALID: 'form' },
-      },
-      form: {
-        meta: { isStep: true, stepTitle: 'Form' },
-        on: { VALID: 'transaction' },
-      },
-      transaction: {
-        meta: { isStep: true, stepTitle: 'Submit application' },
-        on: {
-          SUCCESS: 'success',
-          ERROR: 'error',
-        },
-      },
-      success: { type: 'final' },
-      error: { type: 'final' },
     },
+    requirementsFailed: { type: 'final' },
+    stake: {
+      meta: { isStep: true, stepTitle: 'Stake' },
+      on: { VALID: 'form' },
+    },
+    form: {
+      meta: { isStep: true, stepTitle: 'Form' },
+      on: { VALID: 'transaction' },
+    },
+    transaction: {
+      meta: { isStep: true, stepTitle: 'Submit application' },
+      on: {
+        SUCCESS: 'success',
+        ERROR: 'error',
+      },
+    },
+    success: { type: 'final' },
+    error: { type: 'final' },
   },
-  {
-    actions: {
-      assignTransactionEvents: assign({
-        transactionEvents: (context, event) => event.data.events,
-      }),
-    },
-  }
-)
+})
