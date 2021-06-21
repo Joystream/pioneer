d7f52d349c18718a3f2df6c4371f09f4a64c82be
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-06-21 19:08:31 +0200

    Display all proposal types

--- packages/ui/src/proposals/modals/AddNewProposal/components/TypeSelection.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/components/TypeSelection.tsx
@@ -1,24 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import { Info } from '@/common/components/Info'
-import { TextMedium } from '@/common/components/typography'
+import { AccountItemWrap } from '@/app/pages/Profile/components/AccountItem'
+import { List, ListItem } from '@/common/components/List'
+import { Row } from '@/common/components/Modal'
+import { RowGapBlock } from '@/common/components/page/PageContent'
+import { TextBig, TextMedium } from '@/common/components/typography'
+import { Sizes } from '@/common/constants'
+import { camelCaseToText } from '@/common/helpers'
+import { MemberItemWrap } from '@/memberships/components/MemberListItem/Fileds'
+import { proposalDescriptions } from '@/proposals/model/proposalDescriptions'
 
 export const TypeSelection = () => {
   return (
-    <Info
-      content={
-        <>
-          <TextMedium margin="s">
-            - you may get rejected amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-            officia consequat duis enim velit mollit
-          </TextMedium>
-          <TextMedium margin="s">- you may lose a rejection fee from any required stake.</TextMedium>
-          <TextMedium margin="s">
-            - you may get outright slashed on top of the rejection, depending on what type of proposal this is (not true
-            for all proposal types), which means you will lose the entire stake put up.
-          </TextMedium>
-        </>
-      }
-    />
+    <RowGapBlock gap={24}>
+      <Row>
+        <RowGapBlock gap={8}>
+          <h4>Proposal type</h4>
+          <TextMedium lighter>Please choose proposal type</TextMedium>
+        </RowGapBlock>
+      </Row>
+      <Row>
+        <List>
+          {Object.entries(proposalDescriptions).map(([type, description]) => (
+            <ListItem key={type}>
+              <TypeItemWrap>
+                <TextBig dark bold>
+                  {camelCaseToText(type)}
+                </TextBig>
+                <TextMedium light>{description}</TextMedium>
+              </TypeItemWrap>
+            </ListItem>
+          ))}
+        </List>
+      </Row>
+    </RowGapBlock>
   )
 }
+
+export const TypeItemWrap = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  align-items: center;
+  width: 100%;
+  min-height: 100px;
+  padding: 8px 16px;
+  cursor: pointer;
+`
