f64be85cf2cb51a4c4ef4c061b301a4cef67218e
Maciej Gołaszewski <jodator@jodator.net>
2021-06-23 15:49:59 +0200

    Add buy member machine

--- packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
+++ packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
@@ -1,74 +1,69 @@
 import { MemberId } from '@joystream/types/common'
 import { EventRecord } from '@polkadot/types/interfaces'
-import React, { useMemo, useState } from 'react'
+import { useMachine } from '@xstate/react'
+import React from 'react'
 
 import { FailureModal } from '@/common/components/FailureModal'
 import { useApi } from '@/common/hooks/useApi'
 import { useModal } from '@/common/hooks/useModal'
 import { useObservable } from '@/common/hooks/useObservable'
 import { getEventParam } from '@/common/model/JoystreamNode'
-import { ModalState } from '@/common/types'
 import { toMemberTransactionParams } from '@/memberships/modals/utils'
 
 import { BuyMembershipFormModal, MemberFormFields } from './BuyMembershipFormModal'
 import { BuyMembershipSignModal } from './BuyMembershipSignModal'
 import { BuyMembershipSuccessModal } from './BuyMembershipSuccessModal'
+import { buyMembershipMachine } from './machine'
 
 export const BuyMembershipModal = () => {
   const { hideModal } = useModal()
   const { api } = useApi()
   const membershipPrice = useObservable(api?.query.members.membershipPrice(), [])
-  const [step, setStep] = useState<ModalState>('PREPARE')
-  const [formData, setParams] = useState<MemberFormFields>()
-  const [id, setId] = useState<MemberId>()
+  const [state, send] = useMachine(buyMembershipMachine)
 
-  const onSubmit = (params: MemberFormFields) => {
-    setStep('AUTHORIZE')
-    setParams(params)
-  }
+  if (state.matches('prepare')) {
+    const onSubmit = (params: MemberFormFields) => send({ type: 'DONE', form: params })
 
-  const transaction = useMemo(() => {
-    if (!formData || !api) {
-      return
-    }
-
-    return api.tx.members.buyMembership(toMemberTransactionParams(formData))
-  }, [JSON.stringify(formData)])
-
-  const onDone = useMemo(() => {
-    if (!formData) {
-      return () => null
-    }
-
-    return (result: boolean, events: EventRecord[]) => {
-      const memberId = getEventParam<MemberId>(events, 'MemberRegistered')
-      setId(memberId)
-      setStep(result ? 'SUCCESS' : 'ERROR')
-    }
-  }, [JSON.stringify(formData)])
-
-  if (step === 'PREPARE' || !formData) {
     return <BuyMembershipFormModal onClose={hideModal} onSubmit={onSubmit} membershipPrice={membershipPrice} />
   }
 
-  if (step === 'AUTHORIZE') {
+  if (state.matches('transaction')) {
+    const transaction = api?.tx.members.buyMembership(toMemberTransactionParams(state.context.form))
+    const { form } = state.context
+
+    const onDone = (result: boolean, events: EventRecord[]) => {
+      if (result) {
+        const memberId = getEventParam<MemberId>(events, 'MemberRegistered')
+        send('SUCCESS', { memberId: memberId })
+      } else {
+        send('ERROR')
+      }
+    }
+
     return (
       <BuyMembershipSignModal
         onClose={hideModal}
         membershipPrice={membershipPrice}
-        formData={formData}
+        formData={form}
         onDone={onDone}
         transaction={transaction}
-        initialSigner={formData.controllerAccount}
+        initialSigner={form.controllerAccount}
       />
     )
   }
 
-  if (step === 'SUCCESS' && id) {
-    return <BuyMembershipSuccessModal onClose={hideModal} member={formData} memberId={id.toString()} />
+  if (state.matches('success')) {
+    const { form, memberId } = state.context
+    return <BuyMembershipSuccessModal onClose={hideModal} member={form} memberId={memberId.toString()} />
   }
 
-  return (
-    <FailureModal onClose={hideModal}>There was a problem with creating a membership for {formData.name}.</FailureModal>
-  )
+  if (state.matches('error')) {
+    return (
+      <FailureModal onClose={hideModal}>
+        There was a problem with creating a membership for {state.context.form.name}.
+      </FailureModal>
+    )
+  }
+
+  return null
 }
