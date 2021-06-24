b15beec72fa49bb28d4fe2dd4879448216e6001a
Maciej Gołaszewski <jodator@jodator.net>
2021-06-24 20:06:51 +0200

    Remove unused types

--- packages/ui/src/proposals/modals/AddNewProposal/index.ts
+++ packages/ui/src/proposals/modals/AddNewProposal/index.ts
@@ -1,7 +1,5 @@
 import { ModalCall } from '@/common/providers/modal/types'
-import { ModalState } from '@/common/types'
 
 export type AddNewProposalModalCall = ModalCall<'AddNewProposalModal'>
-export type AddProposalModalState = ModalState | 'WARNING'
 
 export * from './AddNewProposalModal'
