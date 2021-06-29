cb582c115420a3db3191c7414baad3aee182b88d
p-sad <sadlik512@gmail.com>
2021-06-29 17:06:39 +0200

    Show application events in member notifications

--- packages/ui/src/memberships/hooks/useMemberNotifications.ts
+++ packages/ui/src/memberships/hooks/useMemberNotifications.ts
@@ -1,20 +1,31 @@
 import { useMemo } from 'react'
 
+import { useMyApplicationIds } from '@/working-groups/hooks/useMyApplicationIds'
 import { useMyRoleIds } from '@/working-groups/hooks/useMyRoleIds'
 import { useGetMemberRoleEventsQuery } from '@/working-groups/queries'
-import { asStakeChangedActivity, asStakeSlashedActivity } from '@/working-groups/types/WorkingGroupActivity'
+import {
+  asApplicationWithdrawnActivity,
+  asAppliedOnOpeningActivity,
+  asStakeChangedActivity,
+  asStakeSlashedActivity,
+} from '@/working-groups/types/WorkingGroupActivity'
 
 export const useMemberNotifications = () => {
   const { workerIds } = useMyRoleIds()
-  const { loading, data } = useGetMemberRoleEventsQuery({ variables: { worker_in: workerIds } })
+  const { applicationIds } = useMyApplicationIds()
+  const { loading, data } = useGetMemberRoleEventsQuery({
+    variables: { worker_in: workerIds, application_in: applicationIds },
+  })
   const activities = useMemo(
     () =>
       data
         ? [
+            ...data.appliedOnOpeningEvents.map(asAppliedOnOpeningActivity),
+            ...data.applicationWithdrawnEvents.map(asApplicationWithdrawnActivity),
             ...data.stakeDecreasedEvents.map(asStakeChangedActivity),
             ...data.stakeIncreasedEvents.map(asStakeChangedActivity),
             ...data.stakeSlashedEvents.map(asStakeSlashedActivity),
-          ]
+          ].sort((a, b) => b.createdAt.localeCompare(a.createdAt))
         : [],
     [loading, data, workerIds.length]
   )
