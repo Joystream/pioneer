6c75421fdaad0ec1c1f235396cde7fc0a514391e
Maciej Gołaszewski <jodator@jodator.net>
2021-06-30 14:48:31 +0200

    Add members accounts CLI

---
+++ packages/ui/dev/mocks/addMemberAccounts.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import { ApiPromise } from '@polkadot/api'
+
+import memberData from '../../src/mocks/data/raw/members.json'
+
+import { ALICE } from './data/addresses'
+import { getApi, signAndSend } from './lib/api'
+
+async function addMembers(api: ApiPromise) {
+  const members = memberData
+
+  const createMembers = members.map((member) => {
+    return api?.tx.members.buyMembership({
+      handle: member.handle,
+      metadata: {
+        name: member.metadata.name,
+        about: member.metadata.about,
+      },
+      root_account: member.rootAccount,
+      controller_account: member.controllerAccount,
+    })
+  })
+
+  const tx = api.tx.utility.batch(createMembers)
+
+  await signAndSend(tx, ALICE)
+}
+
+const main = async () => {
+  const api = await getApi()
+
+  await addMembers(api)
+
+  await api.disconnect()
+}
+
+main()
