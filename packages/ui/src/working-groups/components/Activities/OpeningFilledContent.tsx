3ef6ee67626c40ffba2b8ea5d9efc0bd42b7d478
p-sad <sadlik512@gmail.com>
2021-07-01 17:27:22 +0200

    Separate Lead/non-Lead text contents

--- packages/ui/src/working-groups/components/Activities/OpeningFilledContent.tsx
+++ packages/ui/src/working-groups/components/Activities/OpeningFilledContent.tsx
@@ -7,10 +7,22 @@ import { OpeningFilledActivity } from '@/working-groups/types'
 
 export const OpeningFilledContent: ActivityContentComponent<OpeningFilledActivity> = ({ activity }) => {
   const { opening, hiredMembers } = activity
-  return (
+  return opening.type == 'LEADER' ? (
     <>
-      Opening <ActivityRouterLink to={`/working-groups/openings/${opening.id}`}>{opening.title}</ActivityRouterLink>
-      {opening.type == 'LEADER' && ' for a Lead'} has been filled.
+      Opening <ActivityRouterLink to={`/working-groups/openings/${opening.id}`}>{opening.title}</ActivityRouterLink> for
+      a Lead has been filled.
+      {hiredMembers.map((member) => (
+        <span key={member.handle}>
+          <br />
+          <MemberModalLink call={{ modal: 'Member', data: { id: member.id } }}>{member.handle}</MemberModalLink> has
+          been hired.
+        </span>
+      ))}
+    </>
+  ) : (
+    <>
+      Opening <ActivityRouterLink to={`/working-groups/openings/${opening.id}`}>{opening.title}</ActivityRouterLink> has
+      been filled.
       {hiredMembers.map((member) => (
         <span key={member.handle}>
           <br />
