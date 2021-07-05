4c9ac85b750d1dbec22e0a169dbaa51b8bce8156
p-sad <sadlik512@gmail.com>
2021-07-05 12:01:48 +0200

    Fix path in now moved saveFile

--- packages/ui/dev/scripts/helpers/saveFile.ts
+++ packages/ui/dev/scripts/helpers/saveFile.ts
@@ -2,6 +2,6 @@ import path from "path";
 import fs from "fs";
 
 export const saveFile = (name: string, contents: any) => {
-    const pathName = path.join(__dirname, '..', '..', 'src', 'mocks', 'data', 'raw', name + '.json')
+    const pathName = path.join(__dirname, '..', '..', '..', 'src', 'mocks', 'data', 'raw', name + '.json')
     fs.writeFileSync(pathName, JSON.stringify(contents, null, 2) + '\n')
 }
