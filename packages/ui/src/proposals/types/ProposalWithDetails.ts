5c042c5dc565322a2af0bc3f24f90ed5f6655985
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-07-06 18:34:29 +0200

    Implement the ProposalHistory section

--- packages/ui/src/proposals/types/ProposalWithDetails.ts
+++ packages/ui/src/proposals/types/ProposalWithDetails.ts
@@ -4,13 +4,22 @@ import { Member } from '@/memberships/types'
 import { ProposalWithDetailsFieldsFragment } from '@/proposals/queries'
 
 import { getMember } from '../../../test/_mocks/members'
+import { typenameToProposalStatus } from '../model/proposalStatus'
 
-import { asProposal, Proposal } from './proposals'
+import { asProposal, Proposal, ProposalStatus } from './proposals'
+
+interface ProposalStatusUpdates {
+  status: ProposalStatus
+  inBlock: Block
+}
 
 export interface ProposalWithDetails extends Proposal {
   votes: ProposalVote[]
   statusSetAtBlock: Block
+  statusSetAtTime: string
   rationale: string
+  createdInBlock: Block
+  proposalStatusUpdates: ProposalStatusUpdates[]
   exactExecutionBlock?: Block
 }
 
@@ -19,6 +28,12 @@ export const asProposalWithDetails = (fields: ProposalWithDetailsFieldsFragment)
   votes: fields.votes.map(asProposalVote),
   rationale: fields.description,
   statusSetAtBlock: asBlock(),
+  statusSetAtTime: fields.statusSetAtTime,
+  createdInBlock: asBlock(),
+  proposalStatusUpdates: fields.proposalStatusUpdates.map(({ newStatus }) => ({
+    inBlock: asBlock(),
+    status: typenameToProposalStatus(newStatus.__typename),
+  })),
 })
 
 export interface ProposalVote {
