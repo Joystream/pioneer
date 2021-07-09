9718868c5ab4af7501eaa5b280a6c1d5c6e98fa8
p-sad <sadlik512@gmail.com>
2021-07-09 15:37:11 +0200

    Use the correct component for token value

--- packages/ui/src/working-groups/components/Activities/WorkerRewardAccountUpdatedContent.tsx
+++ packages/ui/src/working-groups/components/Activities/WorkerRewardAccountUpdatedContent.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 import { WorkerRewardAccountUpdatedEvent } from '@/common/api/queries'
 import { ActivityContentComponent } from '@/common/components/Activities/ActivityContent'
 
-export const WorkerRewardAccountUpdatedContent: ActivityContentComponent<WorkerRewardAccountUpdatedEvent> = ({
-  activity,
-}) => {
+export const WorkerRewardAccountUpdatedContent: ActivityContentComponent<WorkerRewardAccountUpdatedEvent> = () => {
   return <>Your reward account has been updated.</>
 }
--- packages/ui/src/working-groups/components/Activities/WorkerRewardAmountUpdatedContent.tsx
+++ packages/ui/src/working-groups/components/Activities/WorkerRewardAmountUpdatedContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { ActivityContentComponent } from '@/common/components/Activities/ActivityContent'
-import { TokenValueStat } from '@/common/components/statistics'
+import { TokenValue } from '@/common/components/typography'
 import { WorkerRewardAmountUpdatedActivity } from '@/working-groups/types'
 
 export const WorkerRewardAmountUpdatedContent: ActivityContentComponent<WorkerRewardAmountUpdatedActivity> = ({
@@ -10,7 +10,7 @@ export const WorkerRewardAmountUpdatedContent: ActivityContentComponent<WorkerRe
   const { newAmount } = activity
   return (
     <>
-      Your reward amount has been updated to <TokenValueStat value={newAmount} />.
+      Your reward amount has been updated to <TokenValue value={newAmount} />.
     </>
   )
 }
