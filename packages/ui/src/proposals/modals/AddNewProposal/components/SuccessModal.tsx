b52a7b9b9f7f05246dcd8a8cf37209f64447bef9
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-07-14 16:54:56 +0300

    Proposal sign & send transaction

---
+++ packages/ui/src/proposals/modals/AddNewProposal/components/SuccessModal.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { ButtonGhost } from '@/common/components/buttons'
+import { AlertSymbol } from '@/common/components/icons/symbols'
+import { Info } from '@/common/components/Info'
+import { Modal, ModalBody, ModalFooter, ModalHeader } from '@/common/components/Modal'
+import { TextMedium } from '@/common/components/typography'
+import { camelCaseToText } from '@/common/helpers'
+import { ProposalType } from '@/proposals/types'
+
+interface SuccessModalProps {
+  onClose: () => void
+  proposalType: ProposalType
+  proposalTitle: string
+}
+
+export const SuccessModal = ({ onClose, proposalType, proposalTitle }: SuccessModalProps) => {
+  return (
+    <Modal modalSize="m" modalHeight="s" onClose={onClose}>
+      <ModalHeader onClick={onClose} title="Success" icon={<AlertSymbol />} />
+      <ModalBody>
+        <Info
+          title="Created"
+          content={
+            <TextMedium light>
+              You have just successfully created {camelCaseToText(proposalType)} proposal “{proposalTitle}”.
+            </TextMedium>
+          }
+        />
+      </ModalBody>
+      <ModalFooter>
+        <ButtonGhost size="medium">See my Proposal</ButtonGhost>
+      </ModalFooter>
+    </Modal>
+  )
+}
