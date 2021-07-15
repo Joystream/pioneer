8b6dafb50003559bcf2118f28a2dd1415da87c6d
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-07-15 15:30:45 +0300

    Storybook for SuccessModal

--- packages/ui/src/proposals/modals/AddNewProposal/components/SuccessModal.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/components/SuccessModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { ButtonGhost } from '@/common/components/buttons'
-import { AlertSymbol } from '@/common/components/icons/symbols'
+import { AlertSymbol, SuccessSymbol } from '@/common/components/icons/symbols'
 import { Info } from '@/common/components/Info'
 import { Modal, ModalBody, ModalFooter, ModalHeader } from '@/common/components/Modal'
 import { TextMedium } from '@/common/components/typography'
@@ -17,10 +17,9 @@ interface SuccessModalProps {
 export const SuccessModal = ({ onClose, proposalType, proposalTitle }: SuccessModalProps) => {
   return (
     <Modal modalSize="m" modalHeight="s" onClose={onClose}>
-      <ModalHeader onClick={onClose} title="Success" icon={<AlertSymbol />} />
+      <ModalHeader onClick={onClose} title="Success" icon={<SuccessSymbol />} />
       <ModalBody>
         <Info
-          title="Created"
           content={
             <TextMedium light>
               You have just successfully created {camelCaseToText(proposalType)} proposal “{proposalTitle}”.
