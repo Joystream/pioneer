4b36d150dea98ad6c40da33cf20916644db1ccef
p-sad <sadlik512@gmail.com>
2021-07-15 14:58:09 +0200

    Add Description preview to Lead Opeing proposal

--- packages/ui/src/proposals/components/ProposalPreview/CreateLeadOpeningDetailsComponent.tsx
+++ packages/ui/src/proposals/components/ProposalPreview/CreateLeadOpeningDetailsComponent.tsx
@@ -1,6 +1,18 @@
+import { styled } from '@storybook/theming'
 import BN from 'bn.js'
-import React from 'react'
+import React, { useState } from 'react'
 
+import { CloseButton } from '@/common/components/buttons'
+import { Link } from '@/common/components/Link'
+import { MarkdownPreview } from '@/common/components/MarkdownPreview'
+import {
+  SidePane,
+  SidePaneBody,
+  SidePaneGlass,
+  SidePaneHeader,
+  SidePanelTop,
+  SidePaneTitle,
+} from '@/common/components/SidePane'
 import { StatisticItem, Statistics } from '@/common/components/statistics'
 import { TextBig, TokenValue } from '@/common/components/typography'
 import { capitalizeFirstLetter } from '@/common/helpers'
@@ -9,16 +21,18 @@ import { GroupRewardPeriods, isKnownGroupName } from '@/working-groups/types'
 
 import { ProposalPropertiesContent } from './ProposalProperties'
 
-interface Props {
+interface DetailsComponentProps {
   details: CreateLeadOpeningDetails
 }
 
 export const CreateLeadOpeningDetailsComponent: ProposalPropertiesContent<'createWorkingGroupLeadOpening'> = ({
   details,
-}: Props) => {
+}: DetailsComponentProps) => {
   const name = details.group?.name ?? ''
   const rewardPeriod = isKnownGroupName(name) ? GroupRewardPeriods[name] : new BN(1)
   const payoutAmount = rewardPeriod.mul(details.rewardPerBlock)
+  const [isDescriptionVisible, setDescriptionVisible] = useState(false)
+  const description = details.openingDescription ?? ''
   return (
     <>
       <Statistics>
@@ -41,9 +55,56 @@ export const CreateLeadOpeningDetailsComponent: ProposalPropertiesContent<'creat
           </TextBig>
         </StatisticItem>
         <StatisticItem title="Description">
-          <TextBig>Opening Description</TextBig>
+          <TextBig>
+            <Link
+              dark
+              onClick={(evt) => {
+                evt.preventDefault()
+                setDescriptionVisible(true)
+              }}
+            >
+              Opening Description
+            </Link>
+          </TextBig>
         </StatisticItem>
       </Statistics>
+      {isDescriptionVisible && (
+        <OpeningDescriptionPreview description={description} onClose={() => setDescriptionVisible(false)} />
+      )}
     </>
   )
 }
+
+interface DescriptionPreviewProps {
+  onClose: () => void
+  description: string
+}
+
+const OpeningDescriptionPreview = ({ onClose, description }: DescriptionPreviewProps) => {
+  const onBackgroundClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === e.currentTarget) {
+      onClose()
+    }
+  }
+  return (
+    <SidePaneGlass onClick={onBackgroundClick}>
+      <SidePane topSize="s">
+        <SidePaneHeader>
+          <SidePanelTop>
+            <SidePaneTitle>Opening Description</SidePaneTitle>
+            <CloseButton onClick={onClose} />
+          </SidePanelTop>
+        </SidePaneHeader>
+        <SidePaneBody>
+          <DescriptionContainer>
+            <MarkdownPreview markdown={description} />
+          </DescriptionContainer>
+        </SidePaneBody>
+      </SidePane>
+    </SidePaneGlass>
+  )
+}
+
+const DescriptionContainer = styled.div`
+  padding: 12px 24px;
+`
