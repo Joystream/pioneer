114f84c2e7101fd39f00155112d84d415e9aba9e
ClumsyVlad <mazur097mazur@gmail.com>
2021-07-15 13:41:07 +0200

    Align lock icons

--- packages/ui/src/common/components/icons/locks/EnvelopeIcon.tsx
+++ packages/ui/src/common/components/icons/locks/EnvelopeIcon.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Colors } from '@/common/constants'
+
 import { Icon } from '../Icon'
 
 interface EnvelopeIconProps {
@@ -9,7 +11,7 @@ interface EnvelopeIconProps {
 export const EnvelopeIcon = React.memo(({ className }: EnvelopeIconProps) => (
   <Icon
     size="16"
-    viewBox="0 0 16 16"
+    viewBox="0 0 24 24"
     preserveAspectRatio="xMidYMid meet"
     fill="none"
     color="currentColor"
@@ -18,20 +20,20 @@ export const EnvelopeIcon = React.memo(({ className }: EnvelopeIconProps) => (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M8.3567 0.85083H7.64343L5.18671 2.66667H7.20558L8.00006 2.07944L8.79455 2.66667H10.8134L8.3567 0.85083ZM12.6667 4.03646V5.52867L13.6373 6.2461L8.00006 10.1677L2.36283 6.2461L3.33333 5.52876V4.03655L0.733398 5.95825V14.6667L1.3334 15.2667H14.6667L15.2667 14.6667V5.95825L12.6667 4.03646ZM1.9334 14.0667V7.40916L8.00006 11.6295L14.0667 7.40916V14.0667H1.9334Z"
-      fill="black"
+      d="M12.535 1.27625H11.4651L7.78007 4H10.8084L12.0001 3.11916L13.1918 4H16.2201L12.535 1.27625ZM19 6.05469V8.293L20.456 9.36914L12.0001 15.2515L3.54424 9.36914L5 8.29315V6.05483L1.1001 8.93737V22L2.0001 22.9H22.0001L22.9001 22V8.93737L19 6.05469ZM2.9001 21.1V11.1137L12.0001 17.4442L21.1001 11.1137V21.1H2.9001Z"
+      fill={Colors.Black[900]}
     />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M3.7334 3.06665H12.2667V8.66665H11.0667V4.26665H4.9334V8.66665H3.7334V3.06665Z"
-      fill="black"
+      d="M5.6001 4.59998H18.4001V13H16.6001V6.39998H7.4001V13H5.6001V4.59998Z"
+      fill={Colors.Black[900]}
     />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M6 5.06665V6.26665H10L9.99958 5.06665H6ZM6 6.73332V7.93332H9.33333L9.33283 6.73332H6Z"
-      fill="#3F38FF"
+      d="M9 7.59998V9.39998H15L14.9994 7.59998H9ZM9 10.1V11.9H14L13.9993 10.1H9Z"
+      fill={Colors.Blue[500]}
     />
   </Icon>
 ))
