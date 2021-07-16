d4eef43ad4a0b6595391ee601fca2c93704972d0
Maciej Gołaszewski <jodator@jodator.net>
2021-07-16 09:44:31 +0200

    Introduce toBeBN() Jest matcher

--- packages/ui/test/setup.ts
+++ packages/ui/test/setup.ts
@@ -1,1 +1,34 @@
 import '@testing-library/jest-dom'
+import BN from 'bn.js'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      toBeBN(expected: BN): R
+    }
+  }
+}
+
+expect.extend({
+  toBeBN: (received: any, expected: BN) => {
+    if (!BN.isBN(received)) {
+      return {
+        pass: false,
+        message: () => 'The result is not BN',
+      }
+    }
+
+    if (!received.eq(expected)) {
+      return {
+        pass: false,
+        message: () => `Expected ${received.toString()} to equal ${expected.toString()}`,
+      }
+    }
+
+    return {
+      pass: true,
+      message: () => 'OK',
+    }
+  },
+})
