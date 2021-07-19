d86c5d49cf25b193b274883893007fa52ff78ace
Maciej Gołaszewski <jodator@jodator.net>
2021-07-19 13:05:38 +0200

    Add react-dropzone

--- packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/RuntimeUpgrade.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/RuntimeUpgrade.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useDropzone } from 'react-dropzone'
 import styled from 'styled-components'
 
-import { InputComponent } from '@/common/components/forms'
+import { Label } from '@/common/components/forms'
 import { Row } from '@/common/components/Modal'
 import { RowGapBlock } from '@/common/components/page/PageContent'
-import { TextMedium } from '@/common/components/typography'
+import { TextMedium, TextSmall } from '@/common/components/typography'
 import { Colors } from '@/common/constants'
 
 export interface RuntimeUpgradeParameters {
@@ -16,6 +17,8 @@ interface RuntimeUpgradeProps extends RuntimeUpgradeParameters {
 }
 
 export const RuntimeUpgrade = ({ blob, setBlob }: RuntimeUpgradeProps) => {
+  const { isDragActive, getRootProps } = useDropzone()
+
   return (
     <RowGapBlock gap={24}>
       <Row>
@@ -26,15 +29,21 @@ export const RuntimeUpgrade = ({ blob, setBlob }: RuntimeUpgradeProps) => {
       </Row>
       <Row>
         <RowGapBlock gap={20}>
-          <InputComponent label="Amount" tight units="JOY" required>
-            <DropZone onChange={() => setBlob('')}>{blob}</DropZone>
-          </InputComponent>
+          <Label isRequired>Blob</Label>
+          <TextSmall>Please upload the raw WebAssembly object to be used as the new runtime.</TextSmall>
+          <DropZone {...getRootProps()} isDragActive={isDragActive}>
+            Drop your file here or browse
+          </DropZone>
+          <TextSmall>Maximum upload file size is 3 MB</TextSmall>
         </RowGapBlock>
       </Row>
     </RowGapBlock>
   )
 }
 
-const DropZone = styled.div`
-  border: 1px dotted ${Colors.Black['200']};
+const DropZone = styled.div<{ isDragActive: boolean }>`
+  border: 1px dotted;
+  border-color: ${({ isDragActive }) => (isDragActive ? Colors.Blue['200'] : Colors.Black['200'])};
+  padding: 1em;
+  text-align: center;
 `
