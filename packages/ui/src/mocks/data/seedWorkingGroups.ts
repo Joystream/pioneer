d391d6796b9e7ea793124b35f933829315dd5c2b
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-07-20 13:01:05 +0300

    Seed leaders

--- packages/ui/src/mocks/data/seedWorkingGroups.ts
+++ packages/ui/src/mocks/data/seedWorkingGroups.ts
@@ -20,10 +20,19 @@ const seedWorkingGroup = (group: RawWorkingGroupMock, server: any) => {
   const groupData = {
     ...group,
     workers: null,
+    leaderId: null,
     metadata: server.schema.create('WorkingGroupMetadata', group.metadata),
   }
 
   return server.schema.create('WorkingGroup', groupData)
 }
 
+const updateWorkingGroup = (data: RawWorkingGroupMock, server: any) => {
+  const leaderId = data.leaderId
+  const group = server.schema.find('WorkingGroup', data.id)
+
+  return group.update({ leaderId })
+}
+
 export const seedWorkingGroups = (server: any) => mockWorkingGroups.map((group) => seedWorkingGroup(group, server))
+export const updateWorkingGroups = (server: any) => mockWorkingGroups.map((group) => updateWorkingGroup(group, server))
