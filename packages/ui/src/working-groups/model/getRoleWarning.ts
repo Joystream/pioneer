e6655029a462e9222e43be3d199f9dd8754e8ce4
Maciej <jodator@jodator.net>
2021-07-23 14:55:13 +0200

    Add brackets

--- packages/ui/src/working-groups/model/getRoleWarning.ts
+++ packages/ui/src/working-groups/model/getRoleWarning.ts
@@ -1,13 +1,18 @@
 import { WorkerStatusTypename } from '../types'
 
 export function getRoleWarning(status: WorkerStatusTypename) {
-  if (status === 'WorkerStatusActive') return undefined
-  if (status === 'WorkerStatusLeaving')
+  if (status === 'WorkerStatusActive') {
+    return undefined
+  }
+
+  if (status === 'WorkerStatusLeaving') {
     return {
       title: 'Unstaking period',
       content: 'This role is in the unstaking period.',
       isClosable: false,
     }
+  }
+
   return {
     title: 'Role Ended',
     content: 'We are sorry, but this role has already ended.',
