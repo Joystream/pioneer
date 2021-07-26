a4678e5a77c1bfa2be07b31dc43017fcc7e196c4
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-07-26 10:28:00 +0200

    🔧 Fix the Opening statistic mismatch (#1104)
    * Fix the Opening statistic mismatch
    
    * Add a story for DurationStatistics
    
    * Display "best fit format" in `DurationStatistics`
    
    * Fix the footer

--- packages/ui/src/common/components/statistics/DurationStatistics.tsx
+++ packages/ui/src/common/components/statistics/DurationStatistics.tsx
@@ -1,32 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Colors, Fonts } from '@/common/constants'
-import { dateFromNow } from '@/common/model/formatters'
+import { AN_HOUR, A_DAY, A_MINUTE, A_WEEK, Colors, Fonts } from '@/common/constants'
+import { splitDuration } from '@/common/model/formatters'
+import { intersperse } from '@/common/utils'
 import { spacing } from '@/common/utils/styles'
 
 import { StatisticItem, StatisticItemProps } from './StatisticItem'
+
+const format = splitDuration([
+  [A_WEEK, 'w'],
+  [A_DAY, 'd'],
+  [AN_HOUR, 'h'],
+  [A_MINUTE, 'min'],
+])
 
 export interface DurationStatisticsProps extends StatisticItemProps {
   value: string
 }
 
 export const DurationStatistics = (props: DurationStatisticsProps) => {
-  const { days, minutes } = dateFromNow(props.value)
+  const duration = Date.parse(props.value) - Date.now()
   return (
     <StatisticItem {...props}>
-      <Days unit="d">
-        {days} <Unit>d</Unit>
-      </Days>
-      :
-      <Days unit="min">
-        {minutes} <Unit>min</Unit>
-      </Days>
+      {duration > A_MINUTE ? (
+        intersperse(
+          format(duration)
+            .flatMap(([amount, unit]) => (amount ? <Period key={unit} amount={amount} unit={unit} /> : []))
+            .slice(0, 2),
+          <Separator />
+        )
+      ) : (
+        <Days>None</Days>
+      )}
     </StatisticItem>
   )
 }
 
-const Days = styled.div<{ unit: string }>`
+const Period = ({ amount, unit }: { amount: number; unit: string }) => (
+  <Days unit={unit}>
+    {amount} <Unit>{unit}</Unit>
+  </Days>
+)
+
+const Days = styled.div<{ unit?: string }>`
   display: inline-grid;
   position: relative;
   grid-auto-flow: column;
@@ -37,11 +54,6 @@ const Days = styled.div<{ unit: string }>`
   color: ${Colors.Black[900]};
   font-family: ${Fonts.Grotesk};
   font-size: 20px;
-  margin-right: ${spacing(1)};
-
-  &:last-child {
-    margin-left: ${spacing(1)};
-  }
 `
 
 const Unit = styled.span`
@@ -51,4 +63,9 @@ const Unit = styled.span`
   font-weight: 400;
   color: ${Colors.Black[400]};
   font-family: ${Fonts.Grotesk};
+`
+
+const Separator = styled((props) => <span {...props}>:</span>)`
+  display: inline-block;
+  margin: ${spacing(1)};
 `
--- packages/ui/src/common/model/formatters.ts
+++ packages/ui/src/common/model/formatters.ts
@@ -1,8 +1,6 @@
 import BN from 'bn.js'
-import { parseISO, differenceInCalendarDays, differenceInMinutes } from 'date-fns'
 
 import { AN_HOUR, A_DAY, A_MINUTE, A_SECOND, SECONDS_PER_BLOCK } from '../constants'
-import { Reducer } from '../types/helpers'
 import { isNumber } from '../utils'
 
 const NUMBER_SEPARATOR_REG_EXP = /\B(?=(\d{3})+(?!\d))/g
@@ -31,27 +29,17 @@ const DefaultDateFormatter = Intl.DateTimeFormat('en', {
 
 export const formatDateString = (timestamp: string) => DefaultDateFormatter.format(new Date(timestamp))
 
-export const dateFromNow = (date: string) => ({
-  days: differenceInCalendarDays(parseISO(date), new Date()),
-  minutes: differenceInMinutes(parseISO(date), new Date()),
-})
-
 type TimeUnit = [number, Intl.RelativeTimeFormatUnit]
 const defaultDurationUnits: TimeUnit[] = [
   [A_DAY, 'day'],
   [AN_HOUR, 'hour'],
   [A_MINUTE, 'minute'],
 ]
-export const durationFormater = (units = defaultDurationUnits) => (duration: number | BN) => {
-  const total = Math.abs(isNumber(duration) ? duration : duration.toNumber())
-  const [result] = units.reduce(durationReducer, [[], total])
-  return result.length ? result.join(' ') : 'none'
-}
-const durationReducer: Reducer<[string[], number], TimeUnit> = ([result, remaining], [unitValue, unitName]) => {
-  const amount = Math.floor(remaining / unitValue)
-  const formated = amount ? [formatDurationUnit(amount, unitName)] : []
-  return [[...result, ...formated], remaining - amount * unitValue]
-}
+export const durationFormater = (units = defaultDurationUnits) => (duration: number | BN) =>
+  splitDuration(units)(Math.abs(isNumber(duration) ? duration : duration.toNumber()))
+    .flatMap(([duration, unit]) => (duration ? formatDurationUnit(duration, unit) : []))
+    .join(' ') || 'none'
+
 const formatDurationUnit = (duration: number, unit: Intl.RelativeTimeFormatUnit) =>
   duration.toLocaleString('en', { style: 'unit', unit, unitDisplay: 'long' })
 
@@ -61,3 +49,10 @@ export const formatBlocksToDuration = durationFormater([
   [AN_HOUR / MILISECOND_PER_BLOCK, 'hour'],
   [A_MINUTE / MILISECOND_PER_BLOCK, 'minute'],
 ])
+
+export const splitDuration = <T extends [number, any]>(units: T[]) => (duration: number, result: T[] = []): T[] => {
+  if (!units.length) return result
+  const [[unitValue, unitName], ...submultiples] = units
+  const amount = Math.floor(duration / unitValue)
+  return splitDuration(submultiples)(duration - amount * unitValue, [...result, [amount, unitName] as T])
+}
