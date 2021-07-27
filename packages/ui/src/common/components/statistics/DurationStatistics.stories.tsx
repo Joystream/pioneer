9360cd5c0e27ae6635c030f6b7f6c5e4971ac4a2
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-07-27 13:27:00 +0200

    🔨 Fix Upcoming Opening statistic mismatch (#1111)
    * Add a from property to `DurationStatistics`
    
    * Fix Upcoming Opening statistic mismatch
    
    * Fix Upcoming Opening footer
    
    * Fix typo
    
    Co-authored-by: Piotr Sadlik <sadlik512@gmail.com>
    
    Co-authored-by: Maciej <jodator@jodator.net>
    Co-authored-by: Piotr Sadlik <sadlik512@gmail.com>

--- packages/ui/src/common/components/statistics/DurationStatistics.stories.tsx
+++ packages/ui/src/common/components/statistics/DurationStatistics.stories.tsx
@@ -8,20 +8,26 @@ import { DurationStatistics, DurationStatisticsProps, Statistics } from '.'
 export default {
   title: 'Common/Statistics/DurationStatistics',
   component: DurationStatistics,
-  argTypes: { value: { control: 'date' } },
+  argTypes: { value: { control: 'date' }, from: { control: 'date' } },
+  parameters: { controls: { exclude: ['className', 'title'] } },
 } as Meta
 
-const Template: Story<DurationStatisticsProps> = ({ value, ...props }) => (
+const Template: Story<DurationStatisticsProps> = ({ value, from, ...props }) => (
   <Statistics>
-    <DurationStatistics {...props} value={new Date(value).toISOString()} />
+    <DurationStatistics {...props} title="Time until value" value={new Date(value).toISOString()} />
+    <DurationStatistics
+      {...props}
+      title="Time difference"
+      value={new Date(value).toISOString()}
+      from={from && new Date(from).toISOString()}
+    />
   </Statistics>
 )
 
 export const Default = Template.bind({})
 Default.args = {
   value: new Date(Date.now() + A_DAY + 10 * A_MINUTE).toISOString(),
-
-  title: 'Statistic title',
+  from: new Date(Date.now() + A_DAY).toISOString(),
   tooltipText: 'Text to help',
   tooltipTitle: 'Title to help',
   tooltipLinkText: 'More info',
