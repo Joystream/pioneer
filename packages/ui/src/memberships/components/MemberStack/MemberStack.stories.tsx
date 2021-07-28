e7dbbb4226d2e27d8747e1bccbc58415ae38d2a7
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-07-28 15:55:42 +0200

    Implement a MemberStack component to show moderators

---
+++ packages/ui/src/memberships/components/MemberStack/MemberStack.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { repeat } from '@/common/utils'
+
+import { getMember } from '../../../../test/_mocks/members'
+
+import { MemberStack } from './MemberStack'
+
+export default {
+  title: 'Member/MemberStack',
+  component: MemberStack,
+  argTypes: {
+    memberCount: { control: { type: 'range', min: 0, max: 20 } },
+    max: { control: { type: 'range', min: -1, max: 20 } },
+  },
+} as Meta
+
+const alice = getMember('alice')
+
+interface Props {
+  memberCount: number
+  max: number
+}
+const Template: Story<Props> = ({ memberCount, max }) => (
+  <MemberStack members={repeat(() => alice, memberCount)} max={max > 0 ? max : undefined} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  memberCount: 14,
+  max: 4,
+}
---
+++ packages/ui/src/memberships/components/MemberStack/MemberStack.tsx
@@ -0,0 +1,68 @@
+import React, { memo } from 'react'
+import styled from 'styled-components'
+
+import { DefaultTooltip, Tooltip, TooltipContainer } from '@/common/components/Tooltip'
+import { TextInlineSmall } from '@/common/components/typography'
+import { Colors } from '@/common/constants'
+import { spacing } from '@/common/utils/styles'
+import { MemberPhoto } from '@/memberships/components'
+import { MemberAvatar } from '@/memberships/components/Avatar'
+import { Member } from '@/memberships/types'
+
+interface MemberStackProps {
+  members: Member[]
+  max?: number
+}
+export const MemberStack = memo(({ members, max = 5 }: MemberStackProps) => {
+  const hasExtra = members.length > max
+  const toDisplay = hasExtra ? members.slice(0, max - 1) : members
+  const remaining = +hasExtra && members.length - max + 1
+
+  return (
+    <MemberStackStyles>
+      {toDisplay.map(({ id, handle, avatar }) => (
+        <Tooltip key={id} tooltipTitle={handle} tooltipText={`Worker ID: ${id}`}>
+          <MemberAvatar avatarUri={avatar} />
+        </Tooltip>
+      ))}
+      {hasExtra && (
+        <Tooltip tooltipText={`And ${remaining} more`}>
+          <HiddenMember>
+            <TextInlineSmall bold>+{remaining}</TextInlineSmall>
+          </HiddenMember>
+        </Tooltip>
+      )}
+    </MemberStackStyles>
+  )
+})
+
+const HiddenMember = styled(DefaultTooltip)`
+  background-color: ${Colors.Blue[50]};
+  outline: 1px solid ${Colors.Black[200]};
+  height: 26px;
+  width: 26px;
+
+  ${TextInlineSmall} {
+    font-size: 10px;
+  }
+`
+const outlineWidth = '2px'
+const MemberStackStyles = styled.div`
+  display: flex;
+
+  ${TooltipContainer} {
+    margin-right: ${spacing(-1)};
+    &:hover {
+      margin-right: ${outlineWidth};
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  ${MemberPhoto} {
+    cursor: pointer;
+    outline: ${outlineWidth} solid ${Colors.White};
+    height: 26px;
+    width: 26px;
+  }
+`
