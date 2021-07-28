1ebc88a810fb4e460992159395f3de7d6bd61ba0
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-07-28 15:18:48 +0300

    Remove unused from tests

--- packages/ui/test/forum/pages/ForumThread.test.tsx
+++ packages/ui/test/forum/pages/ForumThread.test.tsx
@@ -1,4 +1,4 @@
-import { prettyDOM, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { MemoryRouter } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
