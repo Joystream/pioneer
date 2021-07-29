6991f1c28d1c373734d1cba9dde7bd334438fe53
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-07-29 15:20:46 +0300

    Generate more posts for thread

--- packages/ui/dev/scripts/generators/forum/generateForumThreads.ts
+++ packages/ui/dev/scripts/generators/forum/generateForumThreads.ts
@@ -4,20 +4,28 @@ import { RawForumCategoryMock, RawForumPostMock, RawForumThreadMock } from '@/mo
 
 import { randomFromRange, randomMember } from '../utils'
 
+let nextThreadId = 0
+let nextPostId = 0
+
+export const generateForumPost = (threadId: string, authorId: string): RawForumPostMock => ({
+  id: String(nextPostId++),
+  threadId,
+  authorId,
+  text: faker.lorem.words(randomFromRange(10, 100)),
+})
+
 export const generateForumThreads = (
   forumCategories: Pick<RawForumCategoryMock, 'id'>[]
 ): {
   forumThreads: RawForumThreadMock[]
   forumPosts: RawForumPostMock[]
 } => {
-  let nextId = 0
-
   const forumThreads = forumCategories
     .map(({ id }) => {
       return [...new Array(randomFromRange(3, 10))].map(() => ({
-        id: String(nextId++),
+        id: String(nextThreadId++),
         categoryId: id,
-        isSticky: !(nextId % 5),
+        isSticky: faker.datatype.boolean(),
         title: faker.lorem.words(randomFromRange(4, 8)),
         authorId: randomMember().id,
         createdInEvent: {
@@ -27,14 +35,12 @@ export const generateForumThreads = (
     })
     .flatMap((a) => a)
 
-  nextId = 0
-
-  const forumPosts = forumThreads.map(({ id, authorId }: RawForumThreadMock) => ({
-    id: String(nextId++),
-    threadId: id,
-    authorId: authorId,
-    text: faker.lorem.words(randomFromRange(10, 100)),
-  }))
+  const forumPosts = forumThreads
+    .map(({ id, authorId }: RawForumThreadMock) => {
+      const posts = [...new Array(randomFromRange(3, 10))].map(() => generateForumPost(id, randomMember().id))
+      return [generateForumPost(id, authorId), ...posts]
+    })
+    .flatMap((a) => a)
 
   return { forumThreads, forumPosts }
 }
