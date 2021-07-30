1e26dab3349e482656e3574bcb743d88d270e7d1
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-07-30 16:57:51 +0200

    Fix linting error

--- packages/ui/src/forum/components/category/storybook-helpers.ts
+++ packages/ui/src/forum/components/category/storybook-helpers.ts
@@ -5,14 +5,14 @@ import { asBlock } from '@/common/types'
 import { ForumModerator, ForumPost, ForumThread } from '@/forum/types'
 import { getMember } from '@/mocks/helpers'
 
-export const asModerator =
-  (hasHandle = true, hasAvatar = false) =>
-  (index: number): ForumModerator => ({
+export const asModerator = (hasHandle = true, hasAvatar = false) => {
+  return (index: number): ForumModerator => ({
     id: String(index),
     membershipId: '0',
     handle: hasHandle ? faker.name.firstName() : undefined,
     avatar: hasAvatar ? faker.image.avatar() : undefined,
   })
+}
 
 export const asPost = (text: string): ForumPost | undefined => {
   if (text)
