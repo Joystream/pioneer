329db2c5e6a999fe41657b3f648fa4ba02f19246
Maciej Gołaszewski <jodator@jodator.net>
2021-08-02 16:28:22 +0200

    Prepare base story

--- packages/ui/src/common/components/MultiTransactionModal.stories.tsx
+++ packages/ui/src/common/components/MultiTransactionModal.stories.tsx
@@ -1,7 +1,17 @@
 import { Meta, Story } from '@storybook/react'
-import React, { ReactNode } from 'react'
-import { interpret } from 'xstate'
+import { useActor } from '@xstate/react'
+import React, { ReactNode, useState } from 'react'
+import { ActorRef, interpret } from 'xstate'
 
+import { useBalance } from '@/accounts/hooks/useBalance'
+import { useMyAccounts } from '@/accounts/hooks/useMyAccounts'
+import { accountOrNamed } from '@/accounts/model/accountOrNamed'
+import { useApi } from '@/common/hooks/useApi'
+import { useSignAndSendTransaction } from '@/common/hooks/useSignAndSendTransaction'
+import { multiTransaction } from '@/common/model/machines/multiTransaction'
+import { ApiContext } from '@/common/providers/api/context'
+
+import { stubApi } from '../../../test/_mocks/transactions'
 import { transactionMachine } from '../model/machines'
 
 import { ButtonPrimary } from './buttons'
@@ -14,54 +24,75 @@ export default {
   component: TransactionModal,
 } as Meta
 
-const Template: Story<{ children: ReactNode; state: string }> = ({ children, state }) => {
-  const service = interpret(transactionMachine)
+interface Props {
+  children: ReactNode
+  service: ActorRef<any>
+}
+
+const MultiTransactionModal = ({ children, service }: Props) => {
+  const [state] = useActor(service)
+
+  const { type, transaction, signer } = state.context.transactions[0]
+  const { paymentInfo, sign, isReady } = useSignAndSendTransaction({ transaction, signer, service })
+
+  if (state.matches('transactions')) {
+    return (
+      <TransactionModal service={service} onClose={() => undefined}>
+        {children}
+      </TransactionModal>
+    )
+  }
+
+  return null
+}
+
+const api = stubApi()
+
+const Template: Story<{ children: ReactNode; state: string }> = ({ children }) => {
+  const { api } = useApi()
+
+  const service = interpret(multiTransaction, {
+    context: {
+      transactions: [
+        {
+          type: 'stake',
+          transaction: api?.tx.members.addStakingAccountCandidate('0'),
+          signer: 'ALICE', // TODO
+        },
+        {
+          type: 'addProposal',
+          transaction: api?.tx.proposalsCodex.createProposal({}, { Signal: 'Choo, choo!' }),
+          signer: 'BOB', // TODO
+        },
+      ],
+    },
+  })
 
   service.start()
 
-  if (state === 'extension') {
-    service.send('SIGN')
-    service.send('SIGN_EXTERNAL')
-  }
-
-  if (state === 'pending') {
-    service.send('SIGN')
-    service.send('SIGN_EXTERNAL')
-    service.send('SIGNED')
-  }
-
   return (
-    <TransactionModal service={service} onClose={() => undefined}>
-      {children}
-    </TransactionModal>
+    <div>
+      <h3>Modal:</h3>
+      <MultiTransactionModal service={service}>{children}</MultiTransactionModal>
+      <h3>Actions:?</h3>
+    </div>
   )
 }
 
 const StubBody = () => {
   return (
-    <>
+    <ApiContext.Provider value={api}>
       <ModalBody>
         <TextMedium>Transaction preview here.</TextMedium>
       </ModalBody>
       <ModalFooter>
         <ButtonPrimary size="medium">Sign and send</ButtonPrimary>
       </ModalFooter>
-    </>
+    </ApiContext.Provider>
   )
 }
 
 export const Default = Template.bind({})
 Default.args = {
   children: <StubBody />,
-  state: 'start',
-}
-
-export const Extension = Template.bind({})
-Extension.args = {
-  state: 'extension',
-}
-
-export const Pending = Template.bind({})
-Pending.args = {
-  state: 'pending',
 }
