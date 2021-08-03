f502b4511da93f9c7b16b23d31b3ebb106c9a4d6
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-08-03 19:03:49 +0200

    Add `<ForumCategoryList />` in `<ForumCategories />`

--- packages/ui/src/app/pages/Forum/ForumCategories.tsx
+++ packages/ui/src/app/pages/Forum/ForumCategories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { PageLayout } from '@/app/components/PageLayout'
 import { Loading } from '@/common/components/Loading'
-import { RouterLink } from '@/common/components/RouterLink'
+import { ForumCategoryList } from '@/forum/components/category'
 import { useForumCategories } from '@/forum/hooks/useForumCategories'
 
 export const ForumCategories = () => {
@@ -11,20 +11,7 @@ export const ForumCategories = () => {
   return (
     <PageLayout
       header={<h2>Categories</h2>}
-      main={
-        <div>
-          {isLoading && <Loading />}
-          {!isLoading &&
-            forumCategories.length &&
-            forumCategories.map((category) => {
-              return (
-                <div key={category.id}>
-                  {category.id} | <RouterLink to={`/forum/category/${category.id}`}> {category.title}</RouterLink>
-                </div>
-              )
-            })}
-        </div>
-      }
+      main={isLoading ? <Loading /> : <ForumCategoryList categories={forumCategories} />}
     />
   )
 }
