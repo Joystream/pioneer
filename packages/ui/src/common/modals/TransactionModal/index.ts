55d091107b0cca3c5c4b3e39c73e064aac014b4a
Maciej Gołaszewski <jodator@jodator.net>
2021-08-03 16:15:07 +0200

    Fix exports

--- packages/ui/src/common/modals/TransactionModal/index.ts
+++ packages/ui/src/common/modals/TransactionModal/index.ts
@@ -1,4 +1,2 @@
 export * from './TransactionModal'
 export * from './types'
-export { MultiTransactionModalHeader } from '@/common/modals/TransactionModal/MultiTransactionModalHeader'
-export { MultiTransactionModalHeaderParams } from '@/common/modals/TransactionModal/MultiTransactionModalHeader'
