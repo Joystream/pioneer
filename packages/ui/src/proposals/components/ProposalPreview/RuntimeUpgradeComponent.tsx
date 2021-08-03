a4415b45c3efbb1ce57a50f00e2aaad97610906a
Piotr Sadlik <sadlik512@gmail.com>
2021-08-03 15:16:12 +0200

    🦔 Change runtime download to inline component (#1159)
    * Change runtime download to inline component
    
    * Use lazy query

--- packages/ui/src/proposals/components/ProposalPreview/RuntimeUpgradeComponent.tsx
+++ packages/ui/src/proposals/components/ProposalPreview/RuntimeUpgradeComponent.tsx
@@ -1,10 +1,7 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo } from 'react'
 
 import { ArrowRightIcon, FileIcon } from '@/common/components/icons'
-import { StyledLink } from '@/common/components/Link'
-import { Loading } from '@/common/components/Loading'
-import { Modal, ModalBody, ModalHeader } from '@/common/components/Modal'
-import { Statistics } from '@/common/components/statistics'
+import { StatisticItem, Statistics } from '@/common/components/statistics'
 import { StatisticButton } from '@/common/components/statistics/StatisticButton'
 import { TextInlineBig } from '@/common/components/typography'
 import { useRuntimeBytecode } from '@/proposals/hooks/useRuntimeBytecode'
@@ -18,57 +15,57 @@ interface RuntimeUpgradeProps {
 }
 
 export const RuntimeUpgradeComponent: ProposalPropertiesContent<'runtimeUpgrade'> = ({
-  details,
+  details: { newBytecodeId },
 }: RuntimeUpgradeProps) => {
-  const [downloadOpen, setDownloadOpen] = useState(false)
+  const { state, runtimeBytecode, fetch } = useRuntimeBytecode(newBytecodeId)
+
+  const downloadHref = useMemo(() => {
+    if (!runtimeBytecode) {
+      return ''
+    }
+    const base64string = Buffer.from(runtimeBytecode.bytecode.replace(/^0x/, ''), 'hex').toString('base64')
+    return `data:application/octet-stream;base64,${base64string}`
+  }, [runtimeBytecode?.bytecode])
+
+  useEffect(() => {
+    if (state === 'loaded') {
+      downloadFile(newBytecodeId, downloadHref)
+    }
+  }, [state])
+
+  const onFileClick = () => {
+    if (state !== 'loaded') {
+      fetch()
+    } else {
+      downloadFile(newBytecodeId, downloadHref)
+    }
+  }
+
   return (
-    <>
-      <Statistics>
-        <StatisticButton
-          title="Blob"
-          onClick={() => {
-            setDownloadOpen(true)
-          }}
-          icon={<ArrowRightIcon />}
-        >
+    <Statistics>
+      {state !== 'loading' && (
+        <StatisticButton title="Blob" onClick={onFileClick} icon={<ArrowRightIcon />}>
           <FileIcon />
           <TextInlineBig bold value>
             File Preview
           </TextInlineBig>
         </StatisticButton>
-      </Statistics>
-      {details.newBytecodeId && downloadOpen && (
-        <RuntimeDownloadModal id={details.newBytecodeId} onClose={() => setDownloadOpen(false)} />
       )}
-    </>
+      {state === 'loading' && (
+        <StatisticItem title="Blob">
+          <FileIcon />
+          <TextInlineBig bold value>
+            Downloading file...
+          </TextInlineBig>
+        </StatisticItem>
+      )}
+    </Statistics>
   )
 }
 
-interface RuntimeDownloadProps {
-  id: string
-  onClose: () => void
-}
-
-const RuntimeDownloadModal = ({ id, onClose }: RuntimeDownloadProps) => {
-  const { isLoading, runtimeBytecode } = useRuntimeBytecode(id)
-  const downloadHref = useMemo(() => {
-    if (!runtimeBytecode) return ''
-    const base64string = Buffer.from(runtimeBytecode.bytecode.replace(/^0x/, ''), 'hex').toString('base64')
-    return `data:application/octet-stream;base64,${base64string}`
-  }, [runtimeBytecode?.bytecode])
-
-  return (
-    <Modal modalSize="xs" onClose={onClose}>
-      <ModalHeader icon={<FileIcon />} onClick={onClose} title="Download Bytecode" />
-      <ModalBody>
-        {isLoading || !downloadHref ? (
-          <Loading />
-        ) : (
-          <StyledLink href={downloadHref} download={`bytecode_${id}.wasm`}>
-            Download the file
-          </StyledLink>
-        )}
-      </ModalBody>
-    </Modal>
-  )
+function downloadFile(id: string | undefined, downloadHref: string) {
+  const a = document.createElement('a')
+  a.download = `bytecode_${id}.wasm`
+  a.href = downloadHref
+  a.click()
 }
--- packages/ui/src/proposals/hooks/useRuntimeBytecode.ts
+++ packages/ui/src/proposals/hooks/useRuntimeBytecode.ts
@@ -1,9 +1,20 @@
-import { useGetRuntimeWasmBytecodeQuery } from '../queries'
+import { useGetRuntimeWasmBytecodeLazyQuery } from '../queries'
 import { asRuntimeBytecode } from '../types'
 
-export const useRuntimeBytecode = (id: string) => {
-  const where = { id }
-  const { data, loading } = useGetRuntimeWasmBytecodeQuery({ variables: { where } })
+export const useRuntimeBytecode = (id?: string) => {
+  const where = { id: id ?? '' }
+  const [fetch, { data, loading, called }] = useGetRuntimeWasmBytecodeLazyQuery({ variables: { where } })
   const runtimeBytecode = asRuntimeBytecode(data?.runtime)
-  return { isLoading: loading, runtimeBytecode }
+  return {
+    state: getState(loading, called),
+    runtimeBytecode,
+    fetch,
+  }
 }
+
+function getState(loading: boolean, called: boolean): State {
+  if (!called) return 'init'
+  return loading ? 'loading' : 'loaded'
+}
+
+type State = 'init' | 'loading' | 'loaded'
