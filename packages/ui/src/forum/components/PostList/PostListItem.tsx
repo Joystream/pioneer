c2404fd65626f050d6dcc7ecefcc6cc8ac3226dc
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-08-05 14:20:37 +0300

    Remove console.log

--- packages/ui/src/forum/components/PostList/PostListItem.tsx
+++ packages/ui/src/forum/components/PostList/PostListItem.tsx
@@ -125,6 +125,11 @@ const ReplyBadge = styled.div`
 
 const EditionTime = styled(TextInlineSmall).attrs({ lighter: true, italic: true })`
   float: right;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `
 
 export const ForumPostStyles = styled.div<Pick<PostProps, 'isSelected'>>`
