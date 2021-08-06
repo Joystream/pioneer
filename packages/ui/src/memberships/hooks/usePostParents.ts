6e09de55d35f52f83fd85732f5e736a066463c65
p-sad <sadlik512@gmail.com>
2021-08-06 17:23:49 +0200

    Cast threadId and categoryId to correct types

--- packages/ui/src/memberships/hooks/usePostParents.ts
+++ packages/ui/src/memberships/hooks/usePostParents.ts
@@ -1,17 +1,20 @@
+import { ApiRx } from '@polkadot/api'
 import { useMemo } from 'react'
 
+import { useApi } from '@/common/hooks/useApi'
 import { ForumPostParentsFragment, useGetForumPostParentsQuery } from '@/forum/queries'
 
 export const usePostParents = (id: string) => {
   const { data, loading } = useGetForumPostParentsQuery({ variables: { where: { id } } })
-  const { threadId, categoryId } = useMemo(() => asPostParents(data?.forumPostByUniqueInput), [data, loading])
+  const { api } = useApi()
+  const { threadId, categoryId } = useMemo(() => asPostParents(data?.forumPostByUniqueInput, api), [data, loading])
   return { threadId, categoryId, parentsLoading: loading }
 }
 
-const asPostParents = (fields: ForumPostParentsFragment | null | undefined) =>
-  fields
+const asPostParents = (fields: ForumPostParentsFragment | null | undefined, api: ApiRx | undefined) =>
+  fields && api
     ? {
-        threadId: fields.thread.id,
-        categoryId: fields.thread.category.id,
+        threadId: api.createType('ThreadId', fields.thread.id),
+        categoryId: api.createType('CategoryId', fields.thread.category.id),
       }
     : {}
