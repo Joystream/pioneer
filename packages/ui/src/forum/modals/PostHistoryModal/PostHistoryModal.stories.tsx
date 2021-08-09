732f7055f4d945d97670063feb29e354c6b83024
p-sad <sadlik512@gmail.com>
2021-08-09 13:36:59 +0200

    Fix storybook

--- packages/ui/src/forum/modals/PostHistoryModal/PostHistoryModal.stories.tsx
+++ packages/ui/src/forum/modals/PostHistoryModal/PostHistoryModal.stories.tsx
@@ -1,19 +1,9 @@
 import { Meta, Story } from '@storybook/react'
-import BN from 'bn.js'
-import React, { useState } from 'react'
-import { interpret } from 'xstate'
+import React from 'react'
 
-import { StepDescriptionColumn, Stepper, StepperBody, StepperModalBody } from '@/common/components/StepperModal'
-import { getSteps } from '@/common/model/machines/getSteps'
 import { ModalContext } from '@/common/providers/modal/context'
 import { PostHistoryModal } from '@/forum/modals/PostHistoryModal/PostHistoryModal'
 import { MockApolloProvider } from '@/mocks/components/storybook/MockApolloProvider'
-import { getMockAsOpening } from '@/mocks/data'
-import { StepperProposalWrapper } from '@/proposals/modals/AddNewProposal'
-import { Constants } from '@/proposals/modals/AddNewProposal/components/Constants'
-import { DecreaseWorkingGroupLeadStake } from '@/proposals/modals/AddNewProposal/components/SpecificParameters/DecreaseWorkingGroupLeadStake'
-import { applyForRoleMachine } from '@/working-groups/modals/ApplyForRoleModal'
-import { ApplyForRoleSuccessModal } from '@/working-groups/modals/ApplyForRoleModal/ApplyForRoleSuccessModal'
 
 export default {
   title: 'Forum/PostHistoryModal',
@@ -22,7 +12,7 @@ export default {
 
 const Template: Story = () => {
   return (
-    <MockApolloProvider members forum>
+    <MockApolloProvider workingGroups workers members forum>
       <ModalContext.Provider
         value={{
           modalData: {
@@ -32,7 +22,9 @@ const Template: Story = () => {
           showModal: () => undefined,
           modal: 'PostHistoryModal',
         }}
-      />
+      >
+        <PostHistoryModal />
+      </ModalContext.Provider>
     </MockApolloProvider>
   )
 }
