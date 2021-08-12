5c3bfce13251654c3c01924e69270b11f2949c6a
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-08-12 09:47:36 +0200

    Add `<ThreadList />` to the category page

--- packages/ui/src/app/pages/Forum/ForumCategory.tsx
+++ packages/ui/src/app/pages/Forum/ForumCategory.tsx
@@ -10,11 +10,11 @@ import { Loading } from '@/common/components/Loading'
 import { RowGapBlock } from '@/common/components/page/PageContent'
 import { PageTitle } from '@/common/components/page/PageTitle'
 import { PreviousPage } from '@/common/components/page/PreviousPage'
-import { RouterLink } from '@/common/components/RouterLink'
 import { Label } from '@/common/components/typography'
 import { useModal } from '@/common/hooks/useModal'
 import { ForumCategoryList } from '@/forum/components/category'
 import { ThreadFilters } from '@/forum/components/threads/ThreadFilters'
+import { ThreadList } from '@/forum/components/threads/ThreadList'
 import { useForumCategory } from '@/forum/hooks/useForumCategory'
 import { useForumCategoryThreads } from '@/forum/hooks/useForumCategoryThreads'
 import { MemberStack, moderatorsSumary } from '@/memberships/components/MemberStack'
@@ -68,14 +68,7 @@ export const ForumCategory = () => {
                   Threads
                 </ItemCount>
               </ThreadFilters>
-              {threads.length > 0 &&
-                threads.map((thread) => (
-                  <div key={thread.id}>
-                    <RouterLink to={'/forum/thread/' + thread.id}>
-                      {thread.id} | {thread.isSticky ? '📌' : ''} {thread.title}
-                    </RouterLink>
-                  </div>
-                ))}
+              {threads.length > 0 && <ThreadList threads={threads} />}
             </RowGapBlock>
           )}
         </RowGapBlock>
