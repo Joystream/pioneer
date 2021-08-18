e8abc6aeac7f39a7694a95c0d0d6de0be6f7fd27
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-08-18 14:03:40 +0200

    Test `useForumCategories`

---
+++ packages/ui/test/forum/hooks/useForumCategories.test.ts
@@ -0,0 +1,49 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { ActiveStatus, ArchivedStatus, useForumCategories } from '@/forum/hooks/useForumCategories'
+import { useGetForumCategoriesQuery } from '@/forum/queries'
+
+type Props = Parameters<typeof useForumCategories>
+
+const renderUseForumCategoryThreads = (...props: Props) =>
+  renderHook((props: Props) => useForumCategories(...props), { initialProps: props })
+
+jest.mock('../../../src/forum/queries', () => ({
+  useGetForumCategoriesQuery: jest.fn(() => ({})),
+}))
+const mockedQueryHook = useGetForumCategoriesQuery as jest.Mock
+
+describe('useForumCategories', () => {
+  afterEach(() => {
+    mockedQueryHook.mockClear()
+  })
+
+  it('Default', () => {
+    const { rerender } = renderUseForumCategoryThreads({})
+
+    expect(mockedQueryHook).toBeCalledWith({
+      variables: {
+        where: { status_json: { isTypeOf_eq: ActiveStatus } },
+      },
+    })
+
+    rerender([{ isRoot: true }])
+
+    expect(mockedQueryHook).toBeCalledWith({
+      variables: {
+        where: { parent_eq: null, status_json: { isTypeOf_eq: ActiveStatus } },
+      },
+    })
+
+    rerender([{ isArchive: true }])
+
+    expect(mockedQueryHook).toBeCalledWith({
+      variables: {
+        where: {
+          status_json: { isTypeOf_eq: ArchivedStatus },
+          OR: [{ parent_eq: null }, { parent: { status_json: { isTypeOf_eq: ActiveStatus } } }],
+        },
+      },
+    })
+  })
+})
