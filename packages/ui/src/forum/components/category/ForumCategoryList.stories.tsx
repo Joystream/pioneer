e189b107b902bcddb0830bcdac1468cf1c842cd3
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-08-18 11:35:26 +0200

    Add `isArchive` to the stories

--- packages/ui/src/forum/components/category/ForumCategoryList.stories.tsx
+++ packages/ui/src/forum/components/category/ForumCategoryList.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, Story } from '@storybook/react'
 import React, { useEffect, useState } from 'react'
 import { MemoryRouter } from 'react-router'
 
+import { Loading } from '@/common/components/Loading'
 import { repeat } from '@/common/utils'
 import { asStorybookModerator } from '@/forum/helpers/storybook'
 import { asForumCategory, ForumCategory } from '@/forum/types'
@@ -16,12 +17,12 @@ export default {
 
 const defaultModerators = repeat(asStorybookModerator(false), 14)
 
-const Template: Story = () => {
+const Template: Story = ({ isArchive }) => {
   const [categories, setCategories] = useState<ForumCategory[]>([])
 
   useEffect(() => {
     import('@/mocks/data/raw/forumCategories.json').then((rawCategories) => {
-      const categories = rawCategories.default.slice(0, 10).map((rawCategory) => {
+      const categories = rawCategories.default.slice(0, 5).map((rawCategory) => {
         const category = asForumCategory({ ...rawCategory, moderators: [] })
         const subcategories = 'Lorem ipsum, Dolor, Name, Name'
           .split(', ')
@@ -33,26 +34,16 @@ const Template: Story = () => {
     })
   }, [])
 
-  useEffect(() => {
-    if (categories.length) {
-      const timeout = setTimeout(() => {
-        const moderators = [...repeat(asStorybookModerator(true), 4), ...defaultModerators.slice(4)]
-        setCategories(categories.map((category) => ({ ...category, moderators })))
-      }, 2000)
-      return () => {
-        clearTimeout(timeout)
-      }
-    }
-  }, [categories.length])
-
   return (
     <MockApolloProvider members workers forum>
       <MemoryRouter>
-        <ForumCategoryList categories={categories} />
+        {!categories.length ? <Loading /> : <ForumCategoryList categories={categories} isArchive={isArchive} />}
       </MemoryRouter>
     </MockApolloProvider>
   )
 }
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  isArchive: false,
+}
