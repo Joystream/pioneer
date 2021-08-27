8febda8e5be24096d434b829aa41b11bc23f6aeb
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-08-27 17:51:28 +0200

    Fix the `ForumCategoryWithDetails` type

--- packages/ui/src/forum/types/ForumCategoryWithDetails.ts
+++ packages/ui/src/forum/types/ForumCategoryWithDetails.ts
@@ -2,11 +2,11 @@ import { ForumCategoryWithDetailsFieldsFragment } from '@/forum/queries'
 
 import { asBaseForumCategory, asForumCategory, ForumCategory } from './ForumCategory'
 
-export interface ForumCategoryWithDetails extends ForumCategory {
+export interface ForumCategoryWithDetails extends Omit<ForumCategory, 'subcategories'> {
   subcategories: ForumCategory[]
 }
 
 export const asCategoryWithDetails = (fields: ForumCategoryWithDetailsFieldsFragment): ForumCategoryWithDetails => ({
   ...asBaseForumCategory(fields),
-  subcategories: fields.forumcategoryparent?.map((x) => asForumCategory(x)) ?? [],
+  subcategories: fields.forumcategoryparent?.map(asForumCategory) ?? [],
 })
