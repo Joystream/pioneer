1e1c86686e9f68ed23c69768c3a97ca7d3fecf2e
p-sad <sadlik512@gmail.com>
2021-08-30 01:53:29 +0200

    Add a Latest Threads page

--- packages/ui/src/app/pages/Forum/Forum.tsx
+++ packages/ui/src/app/pages/Forum/Forum.tsx
@@ -7,6 +7,8 @@ import { ForumCategory } from '@/app/pages/Forum/ForumCategory'
 import { ForumThread } from '@/app/pages/Forum/ForumThread'
 import { ForumRoutes } from '@/forum/constant'
 
+import { LatestThreads } from './LatestThreads'
+
 export const Forum = () => {
   return (
     <Switch>
@@ -14,6 +16,7 @@ export const Forum = () => {
       <Route path={`${ForumRoutes.category}/:id/:type?`} exact component={ForumCategory} />
       <Route path={ForumRoutes.archived} exact component={ForumArchived} />
       <Route path={`${ForumRoutes.thread}/:id`} exact component={ForumThread} />
+      <Route path={ForumRoutes.latestThreads} exact component={LatestThreads} />
       <Redirect from="/forum/*" to={ForumRoutes.forum} />
     </Switch>
   )
