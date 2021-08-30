94e996e3c9041c7f5f6a637064c42f35266740da
p-sad <sadlik512@gmail.com>
2021-08-30 16:03:25 +0200

    Add an empty status for proposals search

--- packages/ui/src/proposals/components/ProposalList/ProposalList.tsx
+++ packages/ui/src/proposals/components/ProposalList/ProposalList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { List } from '@/common/components/List'
 import { ListHeader } from '@/common/components/List/ListHeader'
+import { NotFoundText } from '@/common/components/NotFoundText'
 import { RowGapBlock } from '@/common/components/page/PageContent'
 import { ProposalColLayout, ProposalsListHeaders } from '@/proposals/constants'
 import { Proposal } from '@/proposals/types'
@@ -13,18 +14,21 @@ export interface ProposalListProps {
   isPast?: boolean
 }
 
-export const ProposalList = ({ proposals, isPast }: ProposalListProps) => (
-  <RowGapBlock gap={4}>
-    <ProposalsListHeaders $colLayout={ProposalColLayout}>
-      <ListHeader />
-      <ListHeader>Stage</ListHeader>
-      <ListHeader>Proposer</ListHeader>
-      <ListHeader />
-    </ProposalsListHeaders>
-    <List as="div">
-      {proposals.map((proposal) => (
-        <ProposalListItem key={proposal.id} proposal={proposal} isPast={isPast} />
-      ))}
-    </List>
-  </RowGapBlock>
-)
+export const ProposalList = ({ proposals, isPast }: ProposalListProps) =>
+  proposals.length ? (
+    <RowGapBlock gap={4}>
+      <ProposalsListHeaders $colLayout={ProposalColLayout}>
+        <ListHeader />
+        <ListHeader>Stage</ListHeader>
+        <ListHeader>Proposer</ListHeader>
+        <ListHeader />
+      </ProposalsListHeaders>
+      <List as="div">
+        {proposals.map((proposal) => (
+          <ProposalListItem key={proposal.id} proposal={proposal} isPast={isPast} />
+        ))}
+      </List>
+    </RowGapBlock>
+  ) : (
+    <NotFoundText>No proposals matching search criteria</NotFoundText>
+  )
