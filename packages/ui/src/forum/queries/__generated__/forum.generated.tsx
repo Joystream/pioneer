e774fa29b32fa70e4492007f7878cfc21004819d
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-09-01 17:21:49 +0200

    Search forum posts by text

--- packages/ui/src/forum/queries/__generated__/forum.generated.tsx
+++ packages/ui/src/forum/queries/__generated__/forum.generated.tsx
@@ -292,6 +292,23 @@ export type GetForumPostParentsQuery = {
   forumPostByUniqueInput?: Types.Maybe<{ __typename: 'ForumPost' } & ForumPostParentsFragment>
 }
 
+export type SearchForumPostQueryVariables = Types.Exact<{
+  where: Types.ForumPostWhereInput
+  orderBy?: Types.Maybe<Array<Types.ForumPostOrderByInput> | Types.ForumPostOrderByInput>
+  offset?: Types.Maybe<Types.Scalars['Int']>
+  limit?: Types.Maybe<Types.Scalars['Int']>
+}>
+
+export type SearchForumPostQuery = {
+  __typename: 'Query'
+  forumPosts: Array<{
+    __typename: 'ForumPost'
+    id: string
+    text: string
+    thread: { __typename: 'ForumThread'; id: string; title: string; categoryId: string }
+  }>
+}
+
 export const ForumModeratorFieldsFragmentDoc = gql`
   fragment ForumModeratorFields on Worker {
     id
@@ -1110,3 +1127,51 @@ export type GetForumPostParentsQueryResult = Apollo.QueryResult<
   GetForumPostParentsQuery,
   GetForumPostParentsQueryVariables
 >
+export const SearchForumPostDocument = gql`
+  query SearchForumPost($where: ForumPostWhereInput!, $orderBy: [ForumPostOrderByInput!], $offset: Int, $limit: Int) {
+    forumPosts(where: $where, orderBy: $orderBy, offset: $offset, limit: $limit) {
+      id
+      text
+      thread {
+        id
+        title
+        categoryId
+      }
+    }
+  }
+`
+
+/**
+ * __useSearchForumPostQuery__
+ *
+ * To run a query within a React component, call `useSearchForumPostQuery` and pass it any options that fit your needs.
+ * When your component renders, `useSearchForumPostQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useSearchForumPostQuery({
+ *   variables: {
+ *      where: // value for 'where'
+ *      orderBy: // value for 'orderBy'
+ *      offset: // value for 'offset'
+ *      limit: // value for 'limit'
+ *   },
+ * });
+ */
+export function useSearchForumPostQuery(
+  baseOptions: Apollo.QueryHookOptions<SearchForumPostQuery, SearchForumPostQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<SearchForumPostQuery, SearchForumPostQueryVariables>(SearchForumPostDocument, options)
+}
+export function useSearchForumPostLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<SearchForumPostQuery, SearchForumPostQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<SearchForumPostQuery, SearchForumPostQueryVariables>(SearchForumPostDocument, options)
+}
+export type SearchForumPostQueryHookResult = ReturnType<typeof useSearchForumPostQuery>
+export type SearchForumPostLazyQueryHookResult = ReturnType<typeof useSearchForumPostLazyQuery>
+export type SearchForumPostQueryResult = Apollo.QueryResult<SearchForumPostQuery, SearchForumPostQueryVariables>
