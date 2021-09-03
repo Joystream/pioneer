9d2ecd9420aaf55d96b333aa44affebd2c266287
ClumsyVlad <mazur097mazur@gmail.com>
2021-09-03 12:20:13 +0200

    Remove voter column from threads

--- packages/ui/src/forum/constant/styles.ts
+++ packages/ui/src/forum/constant/styles.ts
@@ -1,3 +1,3 @@
 export const categoriesColLayout = (isArchived: boolean) =>
   isArchived ? '356px 77px 276px 212px' : '356px 77px 172px 144px 104px'
-export const ThreadsColLayout = '356px 40px 32px 148px 156px 212px'
+export const ThreadsColLayout = '356px 40px 148px 156px 212px'
