e763a89797495aa9b386a99eb966fdfa2dff109e
p-sad <sadlik512@gmail.com>
2021-09-06 17:08:02 +0200

    Only match titles against headers in test

--- packages/ui/test/forum/components/WatchlistThreadsBrowser.test.tsx
+++ packages/ui/test/forum/components/WatchlistThreadsBrowser.test.tsx
@@ -14,6 +14,9 @@ import { setupMockServer } from '../../_mocks/server'
 describe('UI: WatchlistThreadsBrowser', () => {
   const server = setupMockServer({ noCleanupAfterEach: true })
 
+  const matchHeader = (regex: RegExp) => (content: string, element: Element | null) =>
+    element?.tagName.toLowerCase() == 'h5' && regex.test(content)
+
   beforeAll(() => {
     seedMembers(server.server, 2)
     mockCategories.map((category) => seedForumCategory(category, server.server))
@@ -32,19 +35,19 @@ describe('UI: WatchlistThreadsBrowser', () => {
   it('Two watchlisted threads', async () => {
     window.localStorage.setItem(FORUM_WATCHLIST, JSON.stringify(['0', '1']))
     renderComponent()
-    expect(await screen.findByText(/test thread/i)).toBeDefined()
-    expect(await screen.findByText(/nested thread 1/i)).toBeDefined()
+    expect(await screen.findByText(matchHeader(/test thread/i))).toBeDefined()
+    expect(await screen.findByText(matchHeader(/nested thread 1/i))).toBeDefined()
   })
 
   it('Pagination', async () => {
     window.localStorage.setItem(FORUM_WATCHLIST, JSON.stringify(['0', '1', '2']))
     renderComponent()
-    expect(await screen.findByText(/test thread/i)).toBeDefined()
-    expect(await screen.findByText(/nested thread 1/i)).toBeDefined()
+    expect(await screen.findByText(matchHeader(/test thread/i))).toBeDefined()
+    expect(await screen.findByText(matchHeader(/nested thread 1/i))).toBeDefined()
     act(() => {
       fireEvent.click(screen.getByTitle('Browse next'))
     })
-    expect(await screen.findByText(/nested thread 2/i)).toBeDefined()
+    expect(await screen.findByText(matchHeader(/nested thread 2/i))).toBeDefined()
   })
 
   const renderComponent = () =>
