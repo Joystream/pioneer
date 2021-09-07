bd2ae5749dbd73b7e15034d57246eae1bd9cc51b
Maciej Gołaszewski <jodator@jodator.net>
2021-09-07 14:48:35 +0200

    Update selectAccount test util

--- packages/ui/test/_helpers/selectAccount.ts
+++ packages/ui/test/_helpers/selectAccount.ts
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react'
+import { fireEvent, screen, waitFor } from '@testing-library/react'
 
 export async function selectAccount(label: string, name: string) {
   const labelElement = await screen.findByText(new RegExp(`^${label}$`, 'i'))
@@ -11,9 +11,12 @@ export async function selectAccount(label: string, name: string) {
   const toggle = parentElement.querySelector('.ui-toggle')
   toggle && fireEvent.click(toggle)
 
-  const accountTitles = parentElement.querySelectorAll('ul > li')
-  const found = accountTitles && Array.from(accountTitles).find((li) => li.textContent?.match(name))
+  let found: any
+  await waitFor(() => {
+    const accountTitles = parentElement.querySelectorAll('ul > li')
+    found = accountTitles && Array.from(accountTitles).find((li) => li.textContent?.match(name))
 
-  expect(found).toBeDefined()
+    expect(found).toBeDefined()
+  })
   found && fireEvent.click(found)
 }
