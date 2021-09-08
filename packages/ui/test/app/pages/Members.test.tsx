3bab3b136265fcb7055cf9f3d4ccefd743548030
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-09-08 14:00:40 +0200

    Add tests for isVerified and isFoundingMember filters

--- packages/ui/test/app/pages/Members.test.tsx
+++ packages/ui/test/app/pages/Members.test.tsx
@@ -1,5 +1,5 @@
 import { cryptoWaitReady } from '@polkadot/util-crypto'
-import { render, screen } from '@testing-library/react'
+import { act, findAllByRole, fireEvent, render, screen } from '@testing-library/react'
 import { createMemoryHistory } from 'history'
 import React from 'react'
 import { Route, Router, Switch } from 'react-router-dom'
@@ -7,7 +7,7 @@ import { Route, Router, Switch } from 'react-router-dom'
 import { GlobalModals } from '@/app/GlobalModals'
 import { Members } from '@/app/pages/Members/Members'
 import { ModalContextProvider } from '@/common/providers/modal/provider'
-import { seedMembers } from '@/mocks/data'
+import { seedMembers, mockMembers } from '@/mocks/data'
 import { seedApplications } from '@/mocks/data/seedApplications'
 import { seedOpenings } from '@/mocks/data/seedOpenings'
 import { seedWorkers } from '@/mocks/data/seedWorkers'
@@ -30,9 +30,46 @@ describe('Members', () => {
   })
 
   it('Renders the page', async () => {
+    const membersCount = mockMembers.length
+
     renderPage()
+
     expect(await screen.findByText(/all members/i)).toBeDefined()
+    expect(await screen.findByText(membersCount)).toBeDefined()
     expect(await screen.findByText(/alice/i)).toBeDefined()
+  })
+
+  it('Filter: only verified', async () => {
+    const membersCount = mockMembers.filter((member) => member.isVerified).length
+
+    renderPage()
+
+    await clickIsVerifiedMember()
+
+    expect(await screen.findByText(/all members/i)).toBeDefined()
+    expect(await screen.findByText(membersCount)).toBeDefined()
+  })
+
+  it('Filter: only founder', async () => {
+    const membersCount = mockMembers.filter((member) => member.isFoundingMember).length
+
+    renderPage()
+    await clickIsFoundingMember()
+
+    expect(await screen.findByText(/all members/i)).toBeDefined()
+    expect(await screen.findByText(membersCount)).toBeDefined()
+  })
+
+  it('Filter: founder and verified', async () => {
+    const membersCount = mockMembers.filter((member) => member.isFoundingMember && member.isVerified).length
+
+    renderPage()
+
+    await clickIsFoundingMember()
+    await clickIsVerifiedMember()
+
+    expect(await screen.findByText(/all members/i)).toBeDefined()
+    expect(await screen.findByText(membersCount)).toBeDefined()
   })
 
   it('Renders the page with Member Profile', async () => {
@@ -43,6 +80,20 @@ describe('Members', () => {
     const idElement = await screen.findByText(/member id/i)
     expect(idElement.parentNode?.textContent).toMatch(/^Member ID1$/)
   })
+
+  async function clickIsVerifiedMember() {
+    const isVerifiedMemberCheckbox = (await screen.findByText(/member type/i)).parentElement?.children[1]
+    act(() => {
+      fireEvent.click(isVerifiedMemberCheckbox as Element)
+    })
+  }
+
+  async function clickIsFoundingMember() {
+    const isFoundingMemberCheckbox = (await screen.findByText(/member type/i)).parentElement?.children[2]
+    act(() => {
+      fireEvent.click(isFoundingMemberCheckbox as Element)
+    })
+  }
 
   function renderPage(path = '/members/') {
     const history = createMemoryHistory()
