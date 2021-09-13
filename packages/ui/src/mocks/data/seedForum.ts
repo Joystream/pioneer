a56f7479ea35f9c8a174eb55a00b53970306cce7
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-09-13 12:19:49 +0200

    Add `isVisible` to the mocked posts

--- packages/ui/src/mocks/data/seedForum.ts
+++ packages/ui/src/mocks/data/seedForum.ts
@@ -1,3 +1,4 @@
+import { PostStatusTypename, visiblePostStatuses } from '@/forum/types'
 import { BlockFieldsMock } from '@/mocks/data/common'
 
 import rawForumCategories from './raw/forumCategories.json'
@@ -97,6 +98,7 @@ export function seedForumPost(data: RawForumPostMock, server: any) {
     createdAt: data.postAddedEvent.createdAt,
     updatedAt: sortedEdits.length ? sortedEdits[0].createdAt : null,
     status: server.schema.create(data.status),
+    isVisible: visiblePostStatuses.includes(data.status as PostStatusTypename),
   })
 }
 
