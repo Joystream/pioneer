b1324c3f947907545638663d5b2fb538d43b7960
DevNikita16 <83761318+DevNikita16@users.noreply.github.com>
2021-09-13 14:46:26 +0200

    🙈 Hide pagination component when there's only one page (#1457)
    * Hide pagination component when there's only one page
    
    * Refactor pagination

--- packages/ui/src/app/pages/Forum/ForumMyThreads.tsx
+++ packages/ui/src/app/pages/Forum/ForumMyThreads.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import { PageLayout } from '@/app/components/PageLayout'
 import { RowGapBlock } from '@/common/components/page/PageContent'
@@ -17,13 +17,9 @@ export const ForumMyThreads = () => {
   const displayThreads = () => {
     return (
       <RowGapBlock gap={24}>
-        {!isLoading && !!pageCount && pageCount > 1 && (
-          <Pagination pageCount={pageCount} handlePageChange={setPage} page={page} />
-        )}
+        <Pagination pageCount={pageCount} handlePageChange={setPage} page={page} />
         <ThreadList threads={threads} onSort={() => null} isLoading={isLoading} />
-        {!isLoading && !!pageCount && pageCount > 1 && (
-          <Pagination pageCount={pageCount} handlePageChange={setPage} page={page} />
-        )}
+        <Pagination pageCount={pageCount} handlePageChange={setPage} page={page} />
       </RowGapBlock>
     )
   }
