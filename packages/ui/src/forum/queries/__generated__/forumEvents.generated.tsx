8e63741f6c29514935f7ce359031ba2d24a80fe9
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-09-15 11:35:50 +0200

    Update queries to fetch edits in proper order

--- packages/ui/src/forum/queries/__generated__/forumEvents.generated.tsx
+++ packages/ui/src/forum/queries/__generated__/forumEvents.generated.tsx
@@ -1,84 +1,66 @@
-import * as Types from '../../../common/api/queries/__generated__/baseTypes.generated'
+import * as Types from '../../../common/api/queries/__generated__/baseTypes.generated';
 
-import { gql } from '@apollo/client'
-import * as Apollo from '@apollo/client'
-const defaultOptions = {}
-export type PostAddedEventFieldsFragment = {
-  __typename: 'PostAddedEvent'
-  id: string
-  createdAt: any
-  post: {
-    __typename: 'ForumPost'
-    id: string
-    thread: { __typename: 'ForumThread'; id: string }
-    author: { __typename: 'Membership'; id: string; handle: string }
-  }
-}
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions =  {}
+export type PostAddedEventFieldsFragment = { __typename: 'PostAddedEvent', id: string, createdAt: any, post: { __typename: 'ForumPost', id: string, thread: { __typename: 'ForumThread', id: string }, author: { __typename: 'Membership', id: string, handle: string } } };
 
-export type PostTextUpdatedEventFieldsFragment = {
-  __typename: 'PostTextUpdatedEvent'
-  id: string
-  createdAt: any
-  post: {
-    __typename: 'ForumPost'
-    id: string
-    thread: { __typename: 'ForumThread'; id: string }
-    author: { __typename: 'Membership'; id: string; handle: string }
-  }
-}
+export type PostTextUpdatedEventFieldsFragment = { __typename: 'PostTextUpdatedEvent', id: string, createdAt: any, post: { __typename: 'ForumPost', id: string, thread: { __typename: 'ForumThread', id: string }, author: { __typename: 'Membership', id: string, handle: string } } };
 
-export type GetForumEventsQueryVariables = Types.Exact<{ [key: string]: never }>
+export type GetForumEventsQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
-export type GetForumEventsQuery = {
-  __typename: 'Query'
-  postAddedEvents: Array<{ __typename: 'PostAddedEvent' } & PostAddedEventFieldsFragment>
-  postTextUpdatedEvents: Array<{ __typename: 'PostTextUpdatedEvent' } & PostTextUpdatedEventFieldsFragment>
-}
+
+export type GetForumEventsQuery = { __typename: 'Query', postAddedEvents: Array<(
+    { __typename: 'PostAddedEvent' }
+    & PostAddedEventFieldsFragment
+  )>, postTextUpdatedEvents: Array<(
+    { __typename: 'PostTextUpdatedEvent' }
+    & PostTextUpdatedEventFieldsFragment
+  )> };
 
 export const PostAddedEventFieldsFragmentDoc = gql`
-  fragment PostAddedEventFields on PostAddedEvent {
+    fragment PostAddedEventFields on PostAddedEvent {
+  id
+  createdAt
+  post {
     id
-    createdAt
-    post {
+    thread {
       id
-      thread {
-        id
-      }
-      author {
-        id
-        handle
-      }
+    }
+    author {
+      id
+      handle
     }
   }
-`
+}
+    `;
 export const PostTextUpdatedEventFieldsFragmentDoc = gql`
-  fragment PostTextUpdatedEventFields on PostTextUpdatedEvent {
+    fragment PostTextUpdatedEventFields on PostTextUpdatedEvent {
+  id
+  createdAt
+  post {
     id
-    createdAt
-    post {
+    thread {
       id
-      thread {
-        id
-      }
-      author {
-        id
-        handle
-      }
+    }
+    author {
+      id
+      handle
     }
   }
-`
+}
+    `;
 export const GetForumEventsDocument = gql`
-  query GetForumEvents {
-    postAddedEvents(orderBy: createdAt_DESC, limit: 10) {
-      ...PostAddedEventFields
-    }
-    postTextUpdatedEvents(orderBy: createdAt_DESC, limit: 10) {
-      ...PostTextUpdatedEventFields
-    }
+    query GetForumEvents {
+  postAddedEvents(orderBy: createdAt_DESC, limit: 10) {
+    ...PostAddedEventFields
   }
-  ${PostAddedEventFieldsFragmentDoc}
-  ${PostTextUpdatedEventFieldsFragmentDoc}
-`
+  postTextUpdatedEvents(orderBy: createdAt_DESC, limit: 10) {
+    ...PostTextUpdatedEventFields
+  }
+}
+    ${PostAddedEventFieldsFragmentDoc}
+${PostTextUpdatedEventFieldsFragmentDoc}`;
 
 /**
  * __useGetForumEventsQuery__
@@ -95,18 +77,14 @@ export const GetForumEventsDocument = gql`
  *   },
  * });
  */
-export function useGetForumEventsQuery(
-  baseOptions?: Apollo.QueryHookOptions<GetForumEventsQuery, GetForumEventsQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useQuery<GetForumEventsQuery, GetForumEventsQueryVariables>(GetForumEventsDocument, options)
-}
-export function useGetForumEventsLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<GetForumEventsQuery, GetForumEventsQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useLazyQuery<GetForumEventsQuery, GetForumEventsQueryVariables>(GetForumEventsDocument, options)
-}
-export type GetForumEventsQueryHookResult = ReturnType<typeof useGetForumEventsQuery>
-export type GetForumEventsLazyQueryHookResult = ReturnType<typeof useGetForumEventsLazyQuery>
-export type GetForumEventsQueryResult = Apollo.QueryResult<GetForumEventsQuery, GetForumEventsQueryVariables>
+export function useGetForumEventsQuery(baseOptions?: Apollo.QueryHookOptions<GetForumEventsQuery, GetForumEventsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetForumEventsQuery, GetForumEventsQueryVariables>(GetForumEventsDocument, options);
+      }
+export function useGetForumEventsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetForumEventsQuery, GetForumEventsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetForumEventsQuery, GetForumEventsQueryVariables>(GetForumEventsDocument, options);
+        }
+export type GetForumEventsQueryHookResult = ReturnType<typeof useGetForumEventsQuery>;
+export type GetForumEventsLazyQueryHookResult = ReturnType<typeof useGetForumEventsLazyQuery>;
+export type GetForumEventsQueryResult = Apollo.QueryResult<GetForumEventsQuery, GetForumEventsQueryVariables>;
