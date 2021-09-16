d4d315b62350e18f0cdad02beb0213f390547ed8
ClumsyVlad <mazur097mazur@gmail.com>
2021-09-16 15:33:44 +0200

    Update loading component

--- packages/ui/src/common/components/Loading.tsx
+++ packages/ui/src/common/components/Loading.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { Colors } from '../constants'
+
+import { TextInlineSmall } from './typography'
 
 interface Props {
   text?: string
 }
 
-export const Loading = ({ text }: Props) => <h3>{text ?? 'Loading...'}</h3>
+export const Loading = ({ text }: Props) => {
+  return (
+    <LoadingWrapper>
+      <LoadingText italic>{text ?? 'Loading...'}</LoadingText>
+    </LoadingWrapper>
+  )
+}
+
+const LoadingText = styled(TextInlineSmall)`
+  color: ${Colors.Blue[300]};
+`
+
+const LoadingWrapper = styled.div`
+  display: flex;
+  width: fit-content;
+  height: fit-content;
+  align-items: center;
+  column-gap: 4px;
+`
