5c84a0d54e6469fc887637dd803290a8f4611963
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-09-22 13:17:35 +0200

    Hide election tab when election is inactive along with test

--- packages/ui/src/app/pages/Council/Election.tsx
+++ packages/ui/src/app/pages/Council/Election.tsx
@@ -5,10 +5,13 @@ import { ButtonsGroup, CopyButtonTemplate } from '@/common/components/buttons'
 import { LinkIcon } from '@/common/components/icons'
 import { MainPanel } from '@/common/components/page/PageContent'
 import { PageTitle } from '@/common/components/page/PageTitle'
+import { useElectionState } from '@/council/hooks/useElectionState'
 
 import { CouncilTabs } from './components/CouncilTabs'
 
 export const Election = () => {
+  const electionState = useElectionState()
+
   const header = (
     <PageHeaderWrapper>
       <PageHeaderRow>
--- packages/ui/src/app/pages/Council/components/CouncilTabs.tsx
+++ packages/ui/src/app/pages/Council/components/CouncilTabs.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
-import { usePageTabs } from '@/app/hooks/usePageTabs'
+import { TabsDefinition, usePageTabs } from '@/app/hooks/usePageTabs'
 import { Tabs } from '@/common/components/Tabs'
 import { CouncilRoutes } from '@/council/constants'
+import { useElectionState } from '@/council/hooks/useElectionState'
 
 export const CouncilTabs = () => {
-  const tabs = usePageTabs([
-    ['Election', CouncilRoutes.currentElection],
+  const electionState = useElectionState()
+
+  const pages: TabsDefinition[] = [
     ['Council', CouncilRoutes.council],
     ['Past Councils', CouncilRoutes.pastCouncils],
     ['Past Votes', CouncilRoutes.pastVotes],
     ['Past Elections', CouncilRoutes.pastElections],
-  ])
+  ]
+
+  if (electionState !== 'inactive') {
+    pages.unshift(['Election', CouncilRoutes.currentElection])
+  }
+
+  const tabs = usePageTabs(pages)
 
   return <Tabs tabs={tabs} />
 }
