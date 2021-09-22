c6c2d81df2cab7038346278d93c29dd0b45baf7d
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-09-22 13:17:07 +0200

    Introduce election state hook

---
+++ packages/ui/src/council/hooks/useElectionState.ts
@@ -0,0 +1,24 @@
+import { useApi } from '@/common/hooks/useApi'
+import { useObservable } from '@/common/hooks/useObservable'
+
+type ElectionState = 'announcing' | 'voting' | 'revealing' | 'inactive'
+
+export const useElectionState = (): ElectionState => {
+  const { api } = useApi()
+  const electionStage = useObservable(api?.query.referendum.stage(), [api])
+  const councilStage = useObservable(api?.query.council.stage(), [api])
+
+  if (councilStage?.stage.isIdle) {
+    return 'inactive'
+  }
+
+  if (councilStage?.stage.isAnnouncing) {
+    return 'announcing'
+  }
+
+  if (electionStage?.isVoting) {
+    return 'voting'
+  }
+
+  return 'revealing'
+}
