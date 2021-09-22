943ecedd61b048e1dbfb85b9cc4850191a3f1c63
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-09-22 15:27:39 +0200

    Display no candidates

---
+++ packages/ui/src/council/components/election/announcing/AnnounceCandidacyButton.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+import { ButtonPrimary } from '@/common/components/buttons'
+
+export const AnnounceCandidacyButton = () => <ButtonPrimary size="medium">Announce Candidacy</ButtonPrimary>
--- packages/ui/src/council/components/election/announcing/NoCandidates.tsx
+++ packages/ui/src/council/components/election/announcing/NoCandidates.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 import { NoData } from '@/common/components/NoData'
 import { TextMedium } from '@/common/components/typography'
+import { AnnounceCandidacyButton } from '@/council/components/election/announcing/AnnounceCandidacyButton'
 
-import { AddProposalButton } from '../AddProposalButton'
-
-export const NoProposals = () => (
+export const NoCandidates = () => (
   <NoData>
-    <h3>There are no current proposals yet</h3>
+    <h3>There are no candidates yet</h3>
     <TextMedium>
       Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit
       mollit. Exercitation veniam consequat sunt nostrud amet.
     </TextMedium>
-    <AddProposalButton />
+    <AnnounceCandidacyButton />
   </NoData>
 )
