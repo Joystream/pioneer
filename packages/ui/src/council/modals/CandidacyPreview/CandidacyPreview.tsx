0702fb5bec936886a4bdda5e3275c3303e8155fd
Piotr Sadlik <sadlik512@gmail.com>
2021-09-29 08:41:21 +0200

    🍦 Candidacy preview modal skeleton (#1554)
    * Refactor MemberProfile as a reusable component
    
    * Add a dummy candidacy modal
    
    * Restyle the thread button

--- packages/ui/src/council/modals/CandidacyPreview/CandidacyPreview.tsx
+++ packages/ui/src/council/modals/CandidacyPreview/CandidacyPreview.tsx
@@ -2,24 +2,24 @@ import React, { useState } from 'react'
 
 import { Loading } from '@/common/components/Loading'
 import { useModal } from '@/common/hooks/useModal'
+import { MemberDetails, MemberModalCall } from '@/memberships/components/MemberProfile'
+import { MemberAccounts } from '@/memberships/components/MemberProfile/MemberAccounts'
+import { MemberModal } from '@/memberships/components/MemberProfile/MemberModal'
+import { MemberSideRoles } from '@/memberships/components/MemberProfile/MemberRoles'
+import { useMember } from '@/memberships/hooks/useMembership'
 
-import { useMember } from '../../hooks/useMembership'
+import { CandidacyDetails } from './CandidacyDetails'
 
-import { MemberAccounts } from './MemberAccounts'
-import { MemberDetails } from './MemberDetails'
-import { MemberModal } from './MemberModal'
-import { MemberSideRoles } from './MemberRoles'
-import { MemberModalCall } from './types'
+type ProfileTabs = 'CANDIDACY' | 'DETAILS' | 'ACCOUNTS' | 'ROLES'
 
-type ProfileTabs = 'DETAILS' | 'ACCOUNTS' | 'ROLES'
-
-export const MemberProfile = React.memo(() => {
-  const [activeTab, setActiveTab] = useState<ProfileTabs>('DETAILS')
+export const CandidacyPreview = React.memo(() => {
+  const [activeTab, setActiveTab] = useState<ProfileTabs>('CANDIDACY')
   const { modalData } = useModal<MemberModalCall>()
   const { isLoading, member } = useMember(modalData.id)
   return (
     <MemberModal
       tabs={[
+        { title: 'Candidacy', active: activeTab === 'CANDIDACY', onClick: () => setActiveTab('CANDIDACY') },
         { title: 'Member details', active: activeTab === 'DETAILS', onClick: () => setActiveTab('DETAILS') },
         { title: 'Accounts', active: activeTab === 'ACCOUNTS', onClick: () => setActiveTab('ACCOUNTS') },
         { title: 'Roles', active: activeTab === 'ROLES', onClick: () => setActiveTab('ROLES') },
@@ -32,6 +32,7 @@ export const MemberProfile = React.memo(() => {
         <Loading />
       ) : (
         <>
+          {activeTab === 'CANDIDACY' && <CandidacyDetails />}
           {activeTab === 'DETAILS' && <MemberDetails member={member} />}
           {activeTab === 'ACCOUNTS' && <MemberAccounts member={member} />}
           {activeTab === 'ROLES' && <MemberSideRoles member={member} />}
