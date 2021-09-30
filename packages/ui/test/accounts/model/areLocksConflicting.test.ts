b676e503a7cf2d8d6ae21ccdfb2e1e825a0d82ca
Maciej Gołaszewski <jodator@jodator.net>
2021-09-30 14:41:03 +0200

    Add tests for areLocksConflicting()

---
+++ packages/ui/test/accounts/model/areLocksConflicting.test.ts
@@ -0,0 +1,16 @@
+const areLocksConflicting = () => false
+
+describe('areLocksConflicting', () => {
+  it('Staking Candidate', () => {
+    expect(areLocksConflicting()).toBe(false)
+  })
+  it('Invitation')
+  it('Voting')
+  it('Council')
+  it('Council Candidate')
+  it('Councilor')
+  it('Validation')
+  it('Nomination')
+  it('Proposals')
+  it('Worker')
+})
