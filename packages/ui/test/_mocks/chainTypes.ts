720cb54a4cdebe33d7be86fb504611dadfb40d2b
Maciej Gołaszewski <jodator@jodator.net>
2021-09-30 14:40:32 +0200

    Extract lock identifier mock

--- packages/ui/test/_mocks/chainTypes.ts
+++ packages/ui/test/_mocks/chainTypes.ts
@@ -17,9 +17,11 @@ export const createBalance = (value: number) => {
   return createType('Balance', new BN(value))
 }
 
+const creteLockIdentifier = (type: number) => createType('LockIdentifier', new Uint8Array(new Array(8).fill(type)))
+
 export const createBalanceLock = (amount: number, type = 9) =>
   createType('BalanceLock', {
-    id: createType('LockIdentifier', new Uint8Array(new Array(8).fill(type))),
+    id: creteLockIdentifier(type),
     amount: createBalance(amount),
     reasons: createType('Reasons', 'all'),
   })
