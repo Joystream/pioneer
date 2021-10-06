053369f822a3ea3c07c410d883e41d58c0c9a1c0
DevNikita16 <83761318+DevNikita16@users.noreply.github.com>
2021-10-06 14:08:15 +0200

    📭 Handle title and bullet points for announce candidacy (#1586)
    * Initial form for title and bullet points
    
    * Updated modal machine
    
    * Validate bullet points and title
    
    * Handle tests
    
    * Refactor validation using Yup

--- packages/ui/src/council/modals/AnnounceCandidacy/AnnounceCandidacyModal.tsx
+++ packages/ui/src/council/modals/AnnounceCandidacy/AnnounceCandidacyModal.tsx
@@ -19,6 +19,7 @@ import { useCouncilConstants } from '@/council/hooks/useCouncilConstants'
 import { AnnounceCandidacyConstantsWrapper } from '@/council/modals/AnnounceCandidacy/components/AnnounceCandidacyConstantsWrapper'
 import { RewardAccountStep } from '@/council/modals/AnnounceCandidacy/components/RewardAccountStep'
 import { StakingStep } from '@/council/modals/AnnounceCandidacy/components/StakingStep'
+import { TitleAndBulletPointsStep } from '@/council/modals/AnnounceCandidacy/components/TitleAndBulletPointsStep'
 import { announceCandidacyMachine } from '@/council/modals/AnnounceCandidacy/machine'
 import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 import { SwitchMemberModalCall } from '@/memberships/modals/SwitchMemberModal'
@@ -73,6 +74,12 @@ export const AnnounceCandidacyModal = () => {
     if (state.matches('staking') && state.context.stakingAccount && state.context.stakingAmount) {
       setValidNext(true)
     } else if (state.matches('rewardAccount') && state.context.rewardAccount) {
+      setValidNext(true)
+    } else if (
+      state.matches('candidateProfile.titleAndBulletPoints') &&
+      state.context.title &&
+      state.context.bulletPoints.length
+    ) {
       setValidNext(true)
     } else {
       setValidNext(false)
@@ -130,6 +137,14 @@ export const AnnounceCandidacyModal = () => {
               <RewardAccountStep
                 account={state.context.rewardAccount}
                 setAccount={(account) => send('SET_ACCOUNT', { account })}
+              />
+            )}
+            {state.matches('candidateProfile.titleAndBulletPoints') && (
+              <TitleAndBulletPointsStep
+                title={state.context.title}
+                setTitle={(title) => send('SET_TITLE', { title })}
+                bulletPoints={state.context.bulletPoints}
+                setBulletPoints={(bulletPoints) => send('SET_BULLET_POINTS', { bulletPoints })}
               />
             )}
           </StepperBody>
---
+++ packages/ui/src/council/modals/AnnounceCandidacy/components/TitleAndBulletPointsStep.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from 'react'
+import * as Yup from 'yup'
+
+import { InputComponent, InputText } from '@/common/components/forms'
+import { getErrorMessage, hasError } from '@/common/components/forms/FieldError'
+import { Row } from '@/common/components/Modal'
+import { RowGapBlock } from '@/common/components/page/PageContent'
+import { useForm } from '@/common/hooks/useForm'
+
+interface TitleAndBulletPointsStepProps {
+  title?: string
+  bulletPoints: string[]
+  setTitle: (title?: string) => void
+  setBulletPoints: (bulletPoints: string[]) => void
+}
+
+interface FormFields {
+  title?: string
+  bulletPoint1?: string
+  bulletPoint2?: string
+  bulletPoint3?: string
+}
+
+const FormSchema = Yup.object().shape({
+  title: Yup.string().trim().max(60, 'Maximum length is 60 symbols.'),
+  bulletPoint1: Yup.string().trim().max(120, 'Maximum length is 120 symbols.'),
+  bulletPoint2: Yup.string().trim().max(120, 'Maximum length is 120 symbols.'),
+  bulletPoint3: Yup.string().trim().max(120, 'Maximum length is 120 symbols.'),
+})
+
+const getBulletPoints = (fields: FormFields) => {
+  return Object.entries(fields)
+    .filter(([field, value]) => field.startsWith('bulletPoint') && value)
+    .map(([, value]) => value)
+}
+
+export const TitleAndBulletPointsStep = ({
+  title,
+  bulletPoints,
+  setTitle,
+  setBulletPoints,
+}: TitleAndBulletPointsStepProps) => {
+  const formInitializer: FormFields = {
+    title,
+    bulletPoint1: bulletPoints[0] || '',
+    bulletPoint2: bulletPoints[1] || '',
+    bulletPoint3: bulletPoints[2] || '',
+  }
+  const { fields, changeField, validation } = useForm<FormFields>(formInitializer, FormSchema)
+
+  useEffect(() => {
+    setTitle(validation.isValid ? fields.title : undefined)
+    setBulletPoints(validation.isValid ? getBulletPoints(fields) : [])
+  }, [validation.isValid, JSON.stringify(fields)])
+
+  return (
+    <RowGapBlock gap={24}>
+      <Row>
+        <RowGapBlock gap={8}>
+          <h4>Candidate profile</h4>
+        </RowGapBlock>
+      </Row>
+      <Row>
+        <RowGapBlock gap={20}>
+          <InputComponent
+            label="Profile title"
+            required
+            inputSize="s"
+            validation={hasError('title', validation.errors) ? 'invalid' : undefined}
+            message={hasError('title', validation.errors) ? getErrorMessage('title', validation.errors) : undefined}
+          >
+            <InputText id="title" value={fields.title} onChange={(event) => changeField('title', event.target.value)} />
+          </InputComponent>
+          {[1, 2, 3].map((index) => {
+            const fieldName = ('bulletPoint' + index) as keyof FormFields
+
+            return (
+              <InputComponent
+                key={index}
+                inputSize="s"
+                label={index === 1 ? 'Bullet points' : undefined}
+                required={index === 1}
+                validation={hasError(fieldName, validation.errors) ? 'invalid' : undefined}
+                message={
+                  hasError(fieldName, validation.errors) ? getErrorMessage(fieldName, validation.errors) : undefined
+                }
+              >
+                <InputText
+                  id={fieldName}
+                  value={fields[fieldName]}
+                  onChange={(event) => changeField(fieldName, event.target.value)}
+                />
+              </InputComponent>
+            )
+          })}
+        </RowGapBlock>
+      </Row>
+    </RowGapBlock>
+  )
+}
