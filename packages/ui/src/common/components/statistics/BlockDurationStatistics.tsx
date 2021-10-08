c7d7576d312c9d301a8bfb42d6d82ed9a532bd6a
ClumsyVlad <mazur097mazur@gmail.com>
2021-10-08 13:34:09 +0200

    Update duration statistic styles

--- packages/ui/src/common/components/statistics/BlockDurationStatistics.tsx
+++ packages/ui/src/common/components/statistics/BlockDurationStatistics.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { BlockIcon, BlockIconStyles } from '@/common/components/icons'
-import { TextSmall } from '@/common/components/typography'
+import { TextInlineSmall } from '@/common/components/typography'
 import { DurationValue } from '@/common/components/typography/DurationValue'
 import { AN_HOUR, A_DAY, A_MINUTE, A_WEEK, Fonts } from '@/common/constants'
 import { plural } from '@/common/helpers'
 import { formatTokenValue, MILISECOND_PER_BLOCK, splitDuration } from '@/common/model/formatters'
 import { isDefined, toNumber } from '@/common/utils'
+
+import { ColumnGapBlock } from '../page/PageContent'
 
 import { MultiStatisticItem } from './MultiValueStat'
 import { StatisticItem, StatisticItemProps, StatisticItemSpacedContent } from './StatisticItem'
@@ -36,25 +38,36 @@ export const BlockDurationStatistics = (props: BlockDurationStatisticsProps) => 
   const duration = toNumber(props.value)
 
   return (
-    <MultiStatisticItem {...props}>
+    <BlockDurationStatisticItem {...props}>
       <ItemRow>
         <DurationValue value={duration > A_MINUTE / MILISECOND_PER_BLOCK ? format(duration) : []} />
       </ItemRow>
 
       <ItemRow>
-        <NumberOfBlocks lighter>
-          <BlockIcon /> {formatTokenValue(duration)} block{plural(props.value)}
-        </NumberOfBlocks>
+        <BlocksInfo gap={8}>
+          <BlockIcon />
+          <NumberOfBlocks lighter>
+            {formatTokenValue(duration)} block{plural(props.value)}
+          </NumberOfBlocks>
+        </BlocksInfo>
       </ItemRow>
-    </MultiStatisticItem>
+    </BlockDurationStatisticItem>
   )
 }
+
+const BlocksInfo = styled(ColumnGapBlock)`
+  margin-top: 2px;
+`
+
+const BlockDurationStatisticItem = styled(MultiStatisticItem)`
+  padding: 12px 16px 16px;
+`
 
 const ItemRow = styled(StatisticItemSpacedContent)`
   justify-content: start;
 `
 
-const NumberOfBlocks = styled(TextSmall)`
+const NumberOfBlocks = styled(TextInlineSmall)`
   font-family: ${Fonts.Grotesk};
   ${BlockIconStyles} {
     vertical-align: middle;
