dfd25845dc6f5d4973b6093f461b774fed8da502
Nikita Olizarenko <dev.nikita16@gmail.com>
2021-10-08 11:22:19 +0200

    Remove unused import

--- packages/ui/src/council/modals/AnnounceCandidacy/components/PreviewButtons.tsx
+++ packages/ui/src/council/modals/AnnounceCandidacy/components/PreviewButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import { ButtonGhost } from '@/common/components/buttons'
 import { WatchIcon } from '@/common/components/icons'
