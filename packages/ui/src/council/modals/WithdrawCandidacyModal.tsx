6c8417d29c6b1d0479b4f58d9f0aa510f3676d51
Maciej Gołaszewski <jodator@jodator.net>
2021-10-11 14:28:28 +0200

    Add WithdrawCandidacyModal

---
+++ packages/ui/src/council/modals/WithdrawCandidacyModal.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const WithdrawCandidacyModal = () => <div>Please remember that this action is irreversible</div>
---
+++ packages/ui/test/council/modals/WithdrawCandidacyModal.test.tsx
@@ -0,0 +1,97 @@
+import { cryptoWaitReady } from '@polkadot/util-crypto'
+import { configure, render, screen } from '@testing-library/react'
+import React from 'react'
+import { MemoryRouter } from 'react-router'
+
+import { AccountsContext } from '@/accounts/providers/accounts/context'
+import { UseAccounts } from '@/accounts/providers/accounts/provider'
+import { BalancesContextProvider } from '@/accounts/providers/balances/provider'
+import { CKEditorProps } from '@/common/components/CKEditor'
+import { ApiContext } from '@/common/providers/api/context'
+import { ModalContext } from '@/common/providers/modal/context'
+import { UseModal } from '@/common/providers/modal/types'
+import { WithdrawCandidacyModal } from '@/council/modals/WithdrawCandidacyModal'
+import { MembershipContext } from '@/memberships/providers/membership/context'
+import { MyMemberships } from '@/memberships/providers/membership/provider'
+import { seedMembers } from '@/mocks/data'
+
+import { mockCKEditor } from '../../_mocks/components/CKEditor'
+import { alice, bob } from '../../_mocks/keyring'
+import { getMember } from '../../_mocks/members'
+import { MockKeyringProvider, MockQueryNodeProviders } from '../../_mocks/providers'
+import { setupMockServer } from '../../_mocks/server'
+import { stubApi, stubCouncilConstants, stubDefaultBalances, stubTransaction } from '../../_mocks/transactions'
+
+configure({ testIdAttribute: 'id' })
+
+jest.mock('@/common/components/CKEditor', () => ({
+  CKEditor: (props: CKEditorProps) => mockCKEditor(props),
+}))
+
+describe('UI: Withdraw Candidacy Modal', () => {
+  const api = stubApi()
+  const useModal: UseModal<any> = {
+    hideModal: jest.fn(),
+    showModal: jest.fn(),
+    modal: null,
+    modalData: undefined,
+  }
+  const useMyMemberships: MyMemberships = {
+    active: undefined,
+    members: [],
+    setActive: (member) => (useMyMemberships.active = member),
+    isLoading: false,
+    hasMembers: true,
+  }
+
+  let useAccounts: UseAccounts
+
+  const server = setupMockServer({ noCleanupAfterEach: true })
+
+  beforeAll(async () => {
+    await cryptoWaitReady()
+    seedMembers(server.server, 2)
+
+    useAccounts = {
+      hasAccounts: true,
+      allAccounts: [alice, bob],
+    }
+  })
+
+  beforeEach(async () => {
+    useMyMemberships.members = [getMember('alice'), getMember('bob')]
+    useMyMemberships.setActive(getMember('alice'))
+
+    stubDefaultBalances(api)
+    stubCouncilConstants(api)
+    stubTransaction(api, 'api.tx.council.withdrawCandidacy', 25)
+  })
+
+  it('Warning', async () => {
+    renderModal()
+
+    expect(await screen.findByText('Please remember that this action is irreversible')).toBeDefined()
+  })
+
+  function renderModal() {
+    return render(
+      <MemoryRouter>
+        <ModalContext.Provider value={useModal}>
+          <MockQueryNodeProviders>
+            <MockKeyringProvider>
+              <AccountsContext.Provider value={useAccounts}>
+                <ApiContext.Provider value={api}>
+                  <BalancesContextProvider>
+                    <MembershipContext.Provider value={useMyMemberships}>
+                      <WithdrawCandidacyModal />
+                    </MembershipContext.Provider>
+                  </BalancesContextProvider>
+                </ApiContext.Provider>
+              </AccountsContext.Provider>
+            </MockKeyringProvider>
+          </MockQueryNodeProviders>
+        </ModalContext.Provider>
+      </MemoryRouter>
+    )
+  }
+})
