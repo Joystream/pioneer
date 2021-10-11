0057cbb353771f5e119d918dda7d9106097a27c6
p-sad <sadlik512@gmail.com>
2021-10-11 17:44:13 +0200

    Add separate switches for modules

---
+++ packages/ui/src/app/constants/routes.ts
@@ -0,0 +1,8 @@
+export const ProfileRoutes = {
+  profile: '/profile',
+  memberships: '/profile/memberships',
+}
+
+export const settingsRoute = '/settings'
+
+export const membersRoute = '/members/:id?'
---
+++ packages/ui/src/working-groups/WorkingGroupsModule.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, Switch } from 'react-router'
+
+import { MyApplications } from '@/app/pages/WorkingGroups/MyApplications'
+import { MyRole } from '@/app/pages/WorkingGroups/MyRoles/MyRole'
+import { MyRoles } from '@/app/pages/WorkingGroups/MyRoles/MyRoles'
+import { UpcomingOpening } from '@/app/pages/WorkingGroups/UpcomingOpening'
+import { WorkingGroup } from '@/app/pages/WorkingGroups/WorkingGroup'
+import { WorkingGroups } from '@/app/pages/WorkingGroups/WorkingGroups'
+import { WorkingGroupOpening } from '@/app/pages/WorkingGroups/WorkingGroupsOpening'
+import { WorkingGroupsOpenings } from '@/app/pages/WorkingGroups/WorkingGroupsOpenings'
+
+import { WorkingGroupsRoutes } from './constants'
+
+export const WorkingGroupsModule = () => (
+  <Switch>
+    <Route exact path={WorkingGroupsRoutes.groups} component={WorkingGroups} />
+    <Route exact path={`${WorkingGroupsRoutes.upcomingOpenings}/:id`} component={UpcomingOpening} />
+    <Route exact path={`${WorkingGroupsRoutes.openings}/:id`} component={WorkingGroupOpening} />
+    <Route exact path={WorkingGroupsRoutes.openings} component={WorkingGroupsOpenings} />
+    <Route exact path={WorkingGroupsRoutes.myApplications} component={MyApplications} />
+    <Route exact path={WorkingGroupsRoutes.myRoles} component={MyRoles} />
+    <Route exact path={`${WorkingGroupsRoutes.myRoles}/:id`} component={MyRole} />
+    <Route exact path={`${WorkingGroupsRoutes.groups}/:name`} component={WorkingGroup} />
+  </Switch>
+)
