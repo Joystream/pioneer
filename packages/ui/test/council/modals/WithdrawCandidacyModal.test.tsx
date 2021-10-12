6a1dba9c759d1b6f84a47567d6c502268bac333a
Maciej Gołaszewski <jodator@jodator.net>
2021-10-12 12:01:06 +0200

    Align tests to changed texts

--- packages/ui/test/council/modals/WithdrawCandidacyModal.test.tsx
+++ packages/ui/test/council/modals/WithdrawCandidacyModal.test.tsx
@@ -92,7 +92,7 @@ describe('UI: Withdraw Candidacy Modal', () => {
     fireEvent.click(await getButton('Withdraw Candidacy'))
     fireEvent.click(await getButton('Sign and send'))
 
-    expect(await screen.findByText(/^You have successfully withdraw your candidacy/i)).toBeDefined()
+    expect(await screen.findByText(/^You have successfully withdrawn your candidacy/i)).toBeDefined()
   })
 
   it('Transaction error', async () => {
@@ -102,7 +102,7 @@ describe('UI: Withdraw Candidacy Modal', () => {
     fireEvent.click(await getButton('Withdraw Candidacy'))
     fireEvent.click(await getButton('Sign and send'))
 
-    expect(await screen.findByText(/^There was a problem with withdrawing you candidacy/i)).toBeDefined()
+    expect(await screen.findByText(/^There was a problem with withdrawing your candidacy/i)).toBeDefined()
   })
 
   function renderModal() {
