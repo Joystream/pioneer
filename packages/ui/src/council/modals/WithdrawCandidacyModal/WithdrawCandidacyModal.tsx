39f6d8e710e95090705049f954ce048cab4d8fdd
Maciej Gołaszewski <jodator@jodator.net>
2021-10-18 12:06:46 +0200

    Pass member using modal hook

--- packages/ui/src/council/modals/WithdrawCandidacyModal/WithdrawCandidacyModal.tsx
+++ packages/ui/src/council/modals/WithdrawCandidacyModal/WithdrawCandidacyModal.tsx
@@ -4,31 +4,33 @@ import React, { useCallback } from 'react'
 import { FailureModal } from '@/common/components/FailureModal'
 import { Modal, ModalBody, ModalHeader } from '@/common/components/Modal'
 import { TextMedium } from '@/common/components/typography'
-import { useModal } from '@/common/hooks/useModal'
 import { WithdrawSignModal } from '@/council/modals/WithdrawCandidacyModal/WithdrawSignModal'
 import { WithdrawWarningModal } from '@/council/modals/WithdrawCandidacyModal/WithdrawWarningModal'
-import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
+import { Member } from '@/memberships/types'
 
 import { machine } from './machine'
 
-export const WithdrawCandidacyModal = () => {
-  const { active } = useMyMemberships()
-  const { hideModal } = useModal()
+interface Props {
+  onClose: () => void
+  member: Member
+}
+
+export const WithdrawCandidacyModal = ({ onClose, member }: Props) => {
   const [state, send] = useMachine(machine)
   const onNext = useCallback(() => send('NEXT'), [send])
 
   if (state.matches('warning')) {
-    return <WithdrawWarningModal onNext={onNext} onClose={hideModal} />
+    return <WithdrawWarningModal onNext={onNext} onClose={onClose} />
   }
 
-  if (state.matches('transaction') && active) {
-    return <WithdrawSignModal onClose={hideModal} service={state.children.transaction} member={active} />
+  if (state.matches('transaction')) {
+    return <WithdrawSignModal onClose={onClose} service={state.children.transaction} member={member} />
   }
 
   if (state.matches('success')) {
     return (
-      <Modal onClose={hideModal} modalSize="m">
-        <ModalHeader onClick={hideModal} title="Success!" />
+      <Modal onClose={onClose} modalSize="m">
+        <ModalHeader onClick={onClose} title="Success!" />
         <ModalBody>
           <TextMedium>You have successfully withdrawn your candidacy.</TextMedium>
         </ModalBody>
@@ -37,7 +39,7 @@ export const WithdrawCandidacyModal = () => {
   }
 
   if (state.matches('error')) {
-    return <FailureModal onClose={hideModal}>There was a problem with withdrawing your candidacy.</FailureModal>
+    return <FailureModal onClose={onClose}>There was a problem with withdrawing your candidacy.</FailureModal>
   }
 
   return null
