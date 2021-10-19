e0d36c520b9d7e19895c016f15bfd083e50f1210
p-sad <sadlik512@gmail.com>
2021-10-19 11:13:45 +0200

    Move logic to hook

--- packages/ui/src/council/components/election/CandidateVote/ElectionVotes.tsx
+++ packages/ui/src/council/components/election/CandidateVote/ElectionVotes.tsx
@@ -1,12 +1,10 @@
-import BN from 'bn.js'
-import React, { useMemo } from 'react'
+import React from 'react'
 
-import { useMyAccounts } from '@/accounts/hooks/useMyAccounts'
 import { Loading } from '@/common/components/Loading'
 import { NoData } from '@/common/components/NoData'
 import { BN_ZERO } from '@/common/constants'
 import { useElectionVotes } from '@/council/hooks/useElectionVotes'
-import { Election, ElectionCandidate } from '@/council/types/Election'
+import { Election } from '@/council/types/Election'
 
 import { CandidateVoteList } from './CandidateVoteList'
 
@@ -14,42 +12,14 @@ interface Props {
   election: Election
 }
 
-interface CandidateStats {
-  candidate: ElectionCandidate
-  votesNumber: number
-  totalStake: BN
-  ownStake: BN
-}
-
 export const ElectionVotes = ({ election }: Props) => {
-  const { votes, isLoading } = useElectionVotes(election.cycleId)
-  const { allAccounts } = useMyAccounts()
-  const votesPerCandidate = useMemo(() => {
-    const candidateStats: Record<string, CandidateStats> = {}
-    election.candidates.forEach(
-      (candidate) =>
-        (candidateStats[candidate.member.id] = { candidate, votesNumber: 0, totalStake: BN_ZERO, ownStake: BN_ZERO })
-    )
-    votes?.forEach((vote) => {
-      const candidate = vote.voteFor && candidateStats[vote.voteFor.id]
-      if (candidate) {
-        candidate.votesNumber += 1
-        candidate.totalStake = candidate.totalStake.add(vote.stake)
-        if (allAccounts.find((account) => account.address === vote.castBy)) {
-          candidate.ownStake = candidate.ownStake.add(vote.stake)
-        }
-      }
-    })
-    return Object.values(candidateStats).sort((a, b) => b.totalStake.sub(a.totalStake).toNumber())
-  }, [votes?.length])
-
-  const sumOfStakes = useMemo(() => votes?.reduce((acc, vote) => acc.add(vote.stake), BN_ZERO), [votes])
+  const { votesPerCandidate, sumOfStakes, isLoading } = useElectionVotes(election)
 
   if (isLoading) {
     return <Loading />
   }
 
-  if (!votes) {
+  if (!votesPerCandidate) {
     return <NoData>No votes have been cast yet.</NoData>
   }
 
