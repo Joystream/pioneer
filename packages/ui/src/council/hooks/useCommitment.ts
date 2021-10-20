5aa281562683f836d88de571285a5e654327641a
p-sad <sadlik512@gmail.com>
2021-10-20 14:44:08 +0200

    Add calculateCommitment function

--- packages/ui/src/council/hooks/useCommitment.ts
+++ packages/ui/src/council/hooks/useCommitment.ts
@@ -1,9 +1,10 @@
-import { stringToU8a } from '@polkadot/util'
-import { blake2AsHex, randomAsHex } from '@polkadot/util-crypto'
+import { randomAsHex } from '@polkadot/util-crypto'
 import { useEffect, useMemo, useState } from 'react'
 
 import { useLocalStorage } from '@/common/hooks/useLocalStorage'
 import { useCandidate } from '@/council/hooks/useCandidate'
+
+import { calculateCommitment } from '../model/calculateCommitment'
 
 interface VotingAttempt {
   salt: string
@@ -20,15 +21,12 @@ export const useCommitment = (accountId: string, candidateId: string) => {
     const salt = randomAsHex()
 
     const optionId = candidate.member.id
-    const cycleId = String(candidate.cycleId)
-
-    // See https://github.com/Joystream/joystream/blob/db3885858a7812377a19390968bdbf65221f0270/tests/integration-tests/src/fixtures/council/ElectCouncilFixture.ts#L62
-    const payload = Buffer.concat([accountId, optionId, salt, cycleId].map(stringToU8a))
+    const cycleId = candidate.cycleId
 
     return {
       key: `vote:${cycleId}:${accountId}`,
       value: { salt, optionId },
-      commitment: blake2AsHex(payload),
+      commitment: calculateCommitment(accountId, optionId, salt, cycleId),
     }
   }, [accountId, candidate?.id])
 
---
+++ packages/ui/src/council/model/calculateCommitment.ts
@@ -0,0 +1,16 @@
+import { createType } from '@joystream/types'
+import { blake2AsHex } from '@polkadot/util-crypto'
+
+export function calculateCommitment(accountId: string, optionId: string, salt: string, cycleId: number) {
+  // See https://github.com/Joystream/joystream/blob/db3885858a7812377a19390968bdbf65221f0270/tests/integration-tests/src/fixtures/council/ElectCouncilFixture.ts#L62
+  // Should mirror the runtime implementation:
+  // https://github.com/Joystream/joystream/blob/db3885858a7812377a19390968bdbf65221f0270/runtime-modules/referendum/src/lib.rs#L638
+
+  const accountPayload = createType('AccountId', accountId).toU8a()
+  const optionPayload = createType('u64', Number.parseInt(optionId)).toU8a()
+  const saltPayload = createType('Bytes', salt).toU8a()
+  const cyclePayload = createType('u64', cycleId).toU8a()
+
+  const payload = Buffer.concat([accountPayload, optionPayload, saltPayload, cyclePayload])
+  return blake2AsHex(payload)
+}
