5eff07647e38790b6824e7d9f7e126c0032f9c7c
Rafal <rafalploszka@live.com>
2021-10-20 18:37:35 +0200

    add useIsCouncilMember hook and test

---
+++ packages/ui/test/membership/hooks/useIsCouncilMember.test.tsx
@@ -0,0 +1,37 @@
+import { ApiContext } from "@/common/providers/api/context"
+import { useIsCouncilMember } from "@/memberships/hooks/useIsCouncilMember"
+import { Member } from "@/memberships/types"
+import { createType } from "@joystream/types"
+import { renderHook } from "@testing-library/react-hooks"
+import { getMember } from "../../_mocks/members"
+import { stubApi, stubQuery } from "../../_mocks/transactions"
+import React from 'react'
+
+describe('useIsCouncilMember', () => {
+    const api = stubApi()
+    stubQuery(api, 'council.councilMembers', [{ membership_id: createType('u64', 0) }])
+    const alice = getMember('alice')
+    const bob = getMember('bob')
+
+    it('No member provided', () => {
+        const { result } = render()
+        expect(result.current).toBeUndefined()
+    })
+
+    it('Member is council member', () => {
+      const { result } = render(alice)
+      expect(result.current).toBeTruthy()
+    })
+
+    it('Member is not council member', () => {
+      const { result } = render(bob)
+      expect(result.current).toStrictEqual(false)
+    })
+
+    const render = (member?: Member) => {
+      return renderHook(() => useIsCouncilMember(member), {
+        wrapper: ({ children }) => <ApiContext.Provider value={api}>{children}</ApiContext.Provider>,
+      })
+  }
+
+})
