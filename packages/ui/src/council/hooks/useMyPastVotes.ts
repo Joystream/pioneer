0528b2344e8c8fca97a6782939b05f038d8d4e0b
p-sad <sadlik512@gmail.com>
2021-10-20 10:42:31 +0200

    Simplify hook

--- packages/ui/src/council/hooks/useMyPastVotes.ts
+++ packages/ui/src/council/hooks/useMyPastVotes.ts
@@ -1,31 +1,20 @@
-import { useEffect } from 'react'
-
 import { useMyAccounts } from '@/accounts/hooks/useMyAccounts'
-import { isDefined } from '@/common/utils'
 
-import { useGetCouncilVotesLazyQuery } from '../queries'
+import { useGetCouncilVotesQuery } from '../queries'
 import { asVote } from '../types/Vote'
-
-import { useCurrentElection } from './useCurrentElection'
 
 export const useMyPastVotes = () => {
   const { allAccounts } = useMyAccounts()
-  const { isLoading: electionLoading, election } = useCurrentElection()
-  const [fetch, { data, loading }] = useGetCouncilVotesLazyQuery()
-  useEffect(() => {
-    if (isDefined(election)) {
-      fetch({
-        variables: {
-          where: {
-            castBy_in: allAccounts.map((account) => account.address),
-            electionRound: { isFinished_eq: true },
-          },
-        },
-      })
-    }
-  }, [election?.cycleId])
+  const { data, loading } = useGetCouncilVotesQuery({
+    variables: {
+      where: {
+        castBy_in: allAccounts.map((account) => account.address),
+        electionRound: { isFinished_eq: true },
+      },
+    },
+  })
   return {
     votes: data?.castVotes.map(asVote),
-    isLoading: loading || electionLoading,
+    isLoading: loading,
   }
 }
