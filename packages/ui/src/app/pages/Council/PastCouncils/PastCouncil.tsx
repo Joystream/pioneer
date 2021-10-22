3bd4e8fbf4e65e3b221115cd7d35a3ab15e3ee9e
DevNikita16 <83761318+DevNikita16@users.noreply.github.com>
2021-10-22 15:07:04 +0200

    💊 Display base past council page (#1670)
    * Fetch past council from query node
    
    * Display base past council page

---
+++ packages/ui/src/app/pages/Council/PastCouncils/PastCouncil.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+
+import { PageHeaderRow, PageHeaderWrapper, PageLayout } from '@/app/components/PageLayout'
+import { BadgesRow, BadgeStatus } from '@/common/components/BadgeStatus'
+import { BlockTime } from '@/common/components/BlockTime'
+import { ButtonsGroup, CopyButtonTemplate } from '@/common/components/buttons'
+import { LinkIcon } from '@/common/components/icons'
+import { Loading } from '@/common/components/Loading'
+import { MainPanel, RowGapBlock } from '@/common/components/page/PageContent'
+import { PageTitle } from '@/common/components/page/PageTitle'
+import { PreviousPage } from '@/common/components/page/PreviousPage'
+import { getUrl } from '@/common/utils/getUrl'
+import { PastCouncilTabs } from '@/council/components/pastCouncil/PastCouncilTabs/PastCouncilTabs'
+import { CouncilRoutes } from '@/council/constants'
+import { usePastCouncil } from '@/council/hooks/usePastCouncil'
+
+export const PastCouncil = () => {
+  const history = useHistory()
+
+  const { id } = useParams<{ id: string }>()
+  const { isLoading, council } = usePastCouncil(id)
+
+  useEffect(() => {
+    if (!isLoading && !council) {
+      history.replace('/404')
+    }
+  }, [isLoading, council])
+
+  const displayHeader = () => {
+    if (isLoading || !council) {
+      return null
+    }
+
+    return (
+      <PageHeaderWrapper>
+        <PageHeaderRow showOverflow>
+          <PreviousPage showOverflow>
+            <PageTitle>Council #{council.id}</PageTitle>
+          </PreviousPage>
+          <ButtonsGroup>
+            <CopyButtonTemplate
+              size="medium"
+              textToCopy={getUrl({ route: CouncilRoutes.pastCouncil, params: { id: council.id } })}
+              icon={<LinkIcon />}
+            >
+              Copy link
+            </CopyButtonTemplate>
+          </ButtonsGroup>
+        </PageHeaderRow>
+        <RowGapBlock>
+          <BadgesRow space={8}>
+            <BadgeStatus inverted size="l">
+              Past Council
+            </BadgeStatus>
+            <BlockTime
+              block={{
+                network: 'OLYMPIA',
+                timestamp: new Date().toString(),
+                number: council.endedAtBlock,
+              }}
+              lessInfo
+              dateLabel="Term ended at"
+              layout="row"
+            />
+          </BadgesRow>
+        </RowGapBlock>
+      </PageHeaderWrapper>
+    )
+  }
+
+  const displayMain = () => {
+    return (
+      <MainPanel>
+        {isLoading && <Loading />}
+        {!isLoading && council && <PastCouncilTabs council={council} />}
+      </MainPanel>
+    )
+  }
+
+  return <PageLayout header={displayHeader()} main={displayMain()} />
+}
