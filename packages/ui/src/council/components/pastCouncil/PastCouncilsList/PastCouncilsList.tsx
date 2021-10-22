6989f2d968a706886856b3835cfdb31b09ce5418
ClumsyVlad <mazur097mazur@gmail.com>
2021-10-22 14:16:12 +0200

    Fix past council list + rebase

--- packages/ui/src/council/components/pastCouncil/PastCouncilsList/PastCouncilsList.tsx
+++ packages/ui/src/council/components/pastCouncil/PastCouncilsList/PastCouncilsList.tsx
@@ -3,14 +3,18 @@ import React from 'react'
 import { generatePath } from 'react-router'
 
 import { BlockTime } from '@/common/components/BlockTime'
-import { List, TableListItem } from '@/common/components/List'
+import { List } from '@/common/components/List'
+import { ListHeader } from '@/common/components/List/ListHeader'
 import { Loading } from '@/common/components/Loading'
+import { RowGapBlock } from '@/common/components/page/PageContent'
 import { GhostRouterLink } from '@/common/components/RouterLink'
 import { TokenValue } from '@/common/components/typography'
 import { NotFoundText } from '@/common/components/typography/NotFoundText'
 import { CouncilRoutes } from '@/council/constants'
 import { PastCouncil } from '@/council/types/PastCouncil'
 import { CountInfo, Info } from '@/memberships/components/MemberListItem/Fileds'
+
+import { PastCouncilColLayout, PastCouncilListHeaders, PastCouncilTableListItem } from './styles'
 
 interface Props {
   isLoading: boolean
@@ -27,28 +31,65 @@ export const PastCouncilsList = ({ councils, isLoading }: Props) => {
   }
 
   return (
-    <List>
-      {councils.map((council) => (
-        <TableListItem
-          $colLayout={'50px 1fr 1fr 1fr 1fr 1fr'}
-          as={GhostRouterLink}
-          to={generatePath(CouncilRoutes.pastCouncil, { id: council.id })}
-        >
-          <Info>#{council.id}</Info>
-          <BlockTime
-            block={{
-              network: 'OLYMPIA',
-              timestamp: new Date().toString(),
-              number: council.endedAtBlock,
-            }}
-            lessInfo
-          />
-          <TokenValue value={new BN(0)} />
-          <TokenValue value={new BN(0)} />
-          <CountInfo count={0} />
-          <CountInfo count={0} />
-        </TableListItem>
-      ))}
-    </List>
+    <>
+      {/* <List>
+        {councils.map((council) => (
+          <TableListItem
+            $colLayout={'50px 1fr 1fr 1fr 1fr 1fr'}
+            as={GhostRouterLink}
+            to={generatePath(CouncilRoutes.pastCouncil, { id: council.id })}
+          >
+            <Info>#{council.id}</Info>
+            <BlockTime
+              block={{
+                network: 'OLYMPIA',
+                timestamp: new Date().toString(),
+                number: council.endedAtBlock,
+              }}
+              lessInfo
+            />
+            <TokenValue value={new BN(0)} />
+            <TokenValue value={new BN(0)} />
+            <CountInfo count={0} />
+            <CountInfo count={0} />
+          </TableListItem>
+        ))}
+      </List> */}
+      <RowGapBlock gap={4}>
+        <PastCouncilListHeaders $colLayout={PastCouncilColLayout}>
+          <ListHeader>Term</ListHeader>
+          <ListHeader>Term Ended</ListHeader>
+          <ListHeader>Total Spent</ListHeader>
+          <ListHeader>Spent on Proposals</ListHeader>
+          <ListHeader>Proposals approved</ListHeader>
+          <ListHeader>Proposals Rejected</ListHeader>
+        </PastCouncilListHeaders>
+        <List>
+          {councils.map((council) => (
+            <PastCouncilTableListItem
+              $colLayout={PastCouncilColLayout}
+              $isPast
+              as={GhostRouterLink}
+              to={generatePath(CouncilRoutes.pastCouncil, { id: council.id })}
+            >
+              <Info>#{council.id}</Info>
+              <BlockTime
+                block={{
+                  network: 'OLYMPIA',
+                  timestamp: new Date().toString(),
+                  number: council.endedAtBlock,
+                }}
+                layout="reverse-start"
+                lessInfo
+              />
+              <TokenValue value={new BN(0)} />
+              <TokenValue value={new BN(0)} />
+              <CountInfo count={0} />
+              <CountInfo count={0} />
+            </PastCouncilTableListItem>
+          ))}
+        </List>
+      </RowGapBlock>
+    </>
   )
 }
