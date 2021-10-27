39d2b4428b7e9157f0a5df555c1ff99b8548659d
Maciej Gołaszewski <jodator@jodator.net>
2021-10-27 17:39:11 +0200

    Use sort headers in WorkersHistory

--- packages/ui/src/app/pages/WorkingGroups/WorkingGroup/components/WorkersHistory.tsx
+++ packages/ui/src/app/pages/WorkingGroups/WorkingGroup/components/WorkersHistory.tsx
@@ -1,33 +1,25 @@
-import React, { ReactNode, useState } from 'react'
+import React, { useState } from 'react'
 
+import { WorkerOrderByInput } from '@/common/api/queries'
 import { ListHeader, ListHeaders } from '@/common/components/List/ListHeader'
+import { SortHeader } from '@/common/components/List/SortHeader'
 import { Loading } from '@/common/components/Loading'
 import { RowGapBlock } from '@/common/components/page/PageContent'
 import { Pagination } from '@/common/components/Pagination'
-import { HeaderText, SortIconDown, SortIconUp } from '@/common/components/SortedListHeaders'
 import { TextBig } from '@/common/components/typography'
-import { SortOrder } from '@/common/hooks/useSort'
+import { useSort } from '@/common/hooks/useSort'
 import { PastWorkersList } from '@/working-groups/components/WorkersTableList/PastWorkersList'
-import { usePastWorkersPagination, WorkersOrderKey } from '@/working-groups/hooks/usePastWorkersPagination'
+import { usePastWorkersPagination } from '@/working-groups/hooks/usePastWorkersPagination'
 
 export const WorkersHistory = ({ groupId }: { groupId: string | undefined }) => {
   const [page, setPage] = useState(1)
-  const [order, setOrder] = useState<SortOrder<WorkersOrderKey>>({ orderKey: 'DateFinished', isDescending: true })
+  const { order, getSortProps } = useSort<WorkerOrderByInput>('updatedAt')
 
   const { loadingWorkers, loadingCount, workers, pageCount } = usePastWorkersPagination({
     groupId,
     page,
-    isDescending: order.isDescending,
-    orderKey: order.orderKey,
+    order,
   })
-
-  const sort = (sortKey: WorkersOrderKey) => {
-    if (sortKey === order.orderKey) {
-      setOrder({ orderKey: sortKey, isDescending: !order.isDescending })
-    } else {
-      setOrder({ orderKey: sortKey, isDescending: true })
-    }
-  }
 
   if (loadingWorkers && loadingCount) {
     return <Loading />
@@ -42,12 +34,8 @@ export const WorkersHistory = ({ groupId }: { groupId: string | undefined }) => 
       <RowGapBlock gap={4}>
         <ListHeaders $colLayout={pastWorkersColLayout}>
           <ListHeader>Worker</ListHeader>
-          <SortHeader order={order} sort={sort} sortKey="DateStarted">
-            Date Started
-          </SortHeader>
-          <SortHeader order={order} sort={sort} sortKey="DateFinished">
-            Date Finished
-          </SortHeader>
+          <SortHeader {...getSortProps('createdAt')}>Date Started</SortHeader>
+          <SortHeader {...getSortProps('updatedAt')}>Date Finished</SortHeader>
         </ListHeaders>
         {loadingWorkers ? <Loading /> : <PastWorkersList workers={workers} />}
       </RowGapBlock>
@@ -55,20 +43,5 @@ export const WorkersHistory = ({ groupId }: { groupId: string | undefined }) => 
     </>
   )
 }
+
 const pastWorkersColLayout = '1fr 1fr 1fr'
-
-interface SortHeaderProps {
-  sortKey: WorkersOrderKey
-  order: SortOrder<WorkersOrderKey>
-  children: ReactNode
-  sort: (sortKey: WorkersOrderKey) => void
-}
-
-const SortHeader = ({ sortKey, order, children, sort }: SortHeaderProps) => (
-  <ListHeader onClick={() => sort(sortKey)}>
-    <HeaderText>
-      {children}
-      {order.orderKey === sortKey && (order.isDescending ? <SortIconDown /> : <SortIconUp />)}
-    </HeaderText>
-  </ListHeader>
-)
