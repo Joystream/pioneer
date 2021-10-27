5bd22c42596c8cec9d7d7795845bd4bd88b56bd9
Radoslaw Wyszynski <radoslaw.wyszynski@4soft.co>
2021-10-27 11:10:33 +0200

    Initial tests on past council members list

--- packages/ui/test/council/pages/PastCouncil.test.tsx
+++ packages/ui/test/council/pages/PastCouncil.test.tsx
@@ -7,9 +7,17 @@ import { PastCouncil } from '@/app/pages/Council/PastCouncils/PastCouncil'
 import { NotFound } from '@/app/pages/NotFound'
 import { ApiContext } from '@/common/providers/api/context'
 import { CouncilRoutes } from '@/council/constants'
-import { seedCouncilMember, seedElectedCouncil, seedEvent, seedMembers } from '@/mocks/data'
+import {
+  seedCouncilMember,
+  seedElectedCouncil,
+  seedEvent,
+  seedMembers,
+  seedProposal,
+  seedWorkingGroups,
+} from '@/mocks/data'
 
 import { getCouncilor } from '../../_mocks/council'
+import { testProposals } from '../../_mocks/proposals'
 import { MockKeyringProvider, MockQueryNodeProviders } from '../../_mocks/providers'
 import { setupMockServer } from '../../_mocks/server'
 import { stubApi } from '../../_mocks/transactions'
@@ -30,6 +38,14 @@ describe('UI: Past Council page', () => {
       },
       mockServer.server
     )
+    seedCouncilMember(
+      { ...getCouncilor({ electedInCouncilId: '1', memberId: '0' }), unpaidReward: 10, accumulatedReward: 15 },
+      mockServer.server
+    )
+    seedCouncilMember(
+      { ...getCouncilor({ electedInCouncilId: '1', memberId: '1' }), unpaidReward: 20, accumulatedReward: 40 },
+      mockServer.server
+    )
   })
 
   describe('Council found', () => {
@@ -45,14 +61,6 @@ describe('UI: Past Council page', () => {
 
     describe('Stats', () => {
       beforeEach(async () => {
-        seedCouncilMember(
-          { ...getCouncilor({ electedInCouncilId: '1', memberId: '0' }), unpaidReward: 10, accumulatedReward: 15 },
-          mockServer.server
-        )
-        seedCouncilMember(
-          { ...getCouncilor({ electedInCouncilId: '1', memberId: '1' }), unpaidReward: 20, accumulatedReward: 40 },
-          mockServer.server
-        )
         seedEvent(
           {
             id: '0',
@@ -99,6 +107,51 @@ describe('UI: Past Council page', () => {
         const { getByText } = await renderComponent()
 
         expect(getByText(/^Total spent on proposals$/i).parentElement?.nextSibling?.textContent).toBe('0')
+      })
+    })
+
+    describe('Council Members List', () => {
+      beforeEach(() => {
+        seedWorkingGroups(mockServer.server)
+        seedProposal({ ...testProposals[0] }, mockServer.server)
+        seedEvent(
+          {
+            id: '1',
+            inBlock: 6,
+            createdAt: '2021-10-07T11:47:39.042Z',
+            network: 'OLYMPIA',
+            amount: 200,
+          },
+          'BudgetSpendingEvent',
+          mockServer.server
+        )
+      })
+
+      it('Renders table', async () => {
+        const { queryByText } = await renderComponent()
+
+        expect(queryByText(/^Council Members$/i)).not.toBeNull()
+        expect(queryByText(/^Proposals approved$/i)).not.toBeNull()
+        expect(queryByText(/^Proposals rejected$/i)).not.toBeNull()
+        expect(queryByText(/^Proposals slashed$/i)).not.toBeNull()
+        expect(queryByText(/^Proposals abstained$/i)).not.toBeNull()
+      })
+
+      it('Check approved ', async () => {
+        seedEvent(
+          {
+            id: '0-0',
+            voterId: '0',
+            inBlock: 40456,
+            createdAt: '2021-06-16T17:21:12.161Z',
+            network: 'OLYMPIA',
+            proposalId: '0',
+            voteKind: 'REJECT',
+          },
+          'ProposalVotedEvent',
+          mockServer.server
+        )
+        expect(0).toBe(0)
       })
     })
   })
