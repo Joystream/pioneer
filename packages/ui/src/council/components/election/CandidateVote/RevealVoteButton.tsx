9c9b6970ac2a02d38feb557c87966da10ea09f99
p-sad <sadlik512@gmail.com>
2021-10-28 13:29:04 +0200

    Display modal on clicking button
    Co-authored-by: Rafał Płoszka <RafalPloszka@users.noreply.github.com>

---
+++ packages/ui/src/council/components/election/CandidateVote/RevealVoteButton.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback } from 'react'
+
+import { ButtonPrimary } from '@/common/components/buttons'
+import { useModal } from '@/common/hooks/useModal'
+import { MyCastVote } from '@/council/hooks/useCommitment'
+import { RevealVoteModalCall } from '@/council/modals/RevealVote'
+
+interface Props {
+  myVotes: MyCastVote[]
+  voteForHandle: string
+}
+
+export const RevealVoteButton = ({ myVotes, voteForHandle }: Props) => {
+  const { showModal } = useModal()
+  const unrevealedVotes = myVotes.filter((vote) => vote.isRevealed)
+  const onClick = useCallback(() => {
+    showModal<RevealVoteModalCall>({
+      modal: 'RevealVote',
+      data: { vote: unrevealedVotes[0], voteForHandle },
+    })
+  }, [unrevealedVotes.length])
+  return (
+    <ButtonPrimary size="medium" onClick={onClick}>
+      Reveal
+    </ButtonPrimary>
+  )
+}
