7417572cf5596cd341c6486b6b93e1368c965a0a
Maciej <jodator@jodator.net>
2021-10-28 16:36:38 +0200

    📑 Add usePagination hook (#1691)

--- packages/ui/src/council/hooks/usePastCouncils.ts
+++ packages/ui/src/council/hooks/usePastCouncils.ts
@@ -1,19 +1,27 @@
 import { ElectedCouncilOrderByInput } from '@/common/api/queries'
-import { toQueryOrderByInput, SortOrder } from '@/common/hooks/useSort'
-import { useGetPastCouncilsQuery } from '@/council/queries'
+import { usePagination } from '@/common/hooks/usePagination'
+import { SortOrder, toQueryOrderByInput } from '@/common/hooks/useSort'
+import { useGetPastCouncilsCountQuery, useGetPastCouncilsQuery } from '@/council/queries'
 import { asPastCouncil } from '@/council/types/PastCouncil'
 
 interface UsePastCouncilsProps {
-  page?: number
   order: SortOrder<ElectedCouncilOrderByInput>
+  perPage?: number
 }
 
-export const usePastCouncils = ({ order }: UsePastCouncilsProps) => {
+export const usePastCouncils = ({ order, perPage = 10 }: UsePastCouncilsProps) => {
   const orderBy = toQueryOrderByInput<ElectedCouncilOrderByInput>(order)
+  const { data: dataCount } = useGetPastCouncilsCountQuery()
+  const totalCountPerPage = dataCount?.electedCouncilsConnection.totalCount
+  const { offset, pagination } = usePagination(perPage, totalCountPerPage ?? 0, [order])
 
   const { loading, data } = useGetPastCouncilsQuery({
-    variables: { orderBy: orderBy ?? [] },
+    variables: { orderBy: orderBy ?? [], limit: perPage, offset: offset },
   })
 
-  return { isLoading: loading, councils: data?.electedCouncils.map(asPastCouncil) }
+  return {
+    isLoading: loading,
+    councils: data?.electedCouncils.map(asPastCouncil),
+    pagination: pagination,
+  }
 }
