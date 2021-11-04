84b74bfc1dcc4d0b15a80d15568c55e262f2361b
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-11-04 17:10:52 +0100

    Fix both Voting and Revealing stage votes count

--- packages/ui/src/council/components/election/revealing/RevealingStage.tsx
+++ packages/ui/src/council/components/election/revealing/RevealingStage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import { Loading } from '@/common/components/Loading'
 import { useElectionVotes } from '@/council/hooks/useElectionVotes'
+import { useMyCurrentVotesCount } from '@/council/hooks/useMyCurrentVotesCount'
 import { Election } from '@/council/types/Election'
 
 import { CandidateCardList } from '../CandidateCard/CandidateCardList'
@@ -15,6 +16,8 @@ interface Props {
 
 export const RevealingStage = ({ election, isLoading }: Props) => {
   const [tab, setTab] = useState<RevealingStageTab>('results')
+  const { votesTotal } = useMyCurrentVotesCount()
+
   const { votesPerCandidate, sumOfStakes: totalStake, isLoading: votesLoading } = useElectionVotes(election)
   const myVotesTotal = votesPerCandidate.filter((vote) => vote.myVotes.length).length
 
@@ -26,7 +29,7 @@ export const RevealingStage = ({ election, isLoading }: Props) => {
     <>
       <ElectionTabs
         stage="revealing"
-        myVotes={myVotesTotal}
+        myVotes={myVotesTotal && votesTotal}
         tab={tab}
         onSetTab={(tab) => setTab(tab as RevealingStageTab)}
       />
