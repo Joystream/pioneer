47bb830d7b39b29ff92a838813b5a96c98e1f467
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-11-04 10:53:19 +0100

    📓 Document the chain specs generation (#1717)
    * Don't track the chain-spec file
    
    * Document the chain specs generation
    
    * Document building the chain spec without docker

--- packages/ui/dev/chain-spec/index.ts
+++ packages/ui/dev/chain-spec/index.ts
@@ -1,17 +1,16 @@
-import fs from 'fs'
+import { readFileSync, writeFileSync } from 'fs'
 
 import { DeepMerger } from '@apollo/client/utilities'
 import yargs from 'yargs'
 
 import configs from './configs'
-import spec from './data/chain-spec.json'
 
 const handlerFor = (stage: 'announcing' | 'voting' | 'revealing') => () => {
   const merger = new DeepMerger()
-  const newSpec = merger.merge(spec, configs[stage])
   const pathName = `${__dirname}/data/chain-spec.json`
-  const content = JSON.stringify(newSpec, null, 2) + '\n'
-  fs.writeFileSync(pathName, content)
+  const oldSpec = JSON.parse(readFileSync(pathName, 'utf8'))
+  const newSpec = merger.merge(oldSpec, configs[stage])
+  writeFileSync(pathName, `${JSON.stringify(newSpec, null, 2)}\n`)
 }
 
 yargs(process.argv.slice(2))
