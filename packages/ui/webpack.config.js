af04f0d82bfa651d9a36631a44631c45064974a5
Maciej Gołaszewski <jodator@jodator.net>
2021-11-05 16:33:36 +0100

    Add optimization split chunks

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -78,6 +78,30 @@ module.exports = (env, argv) => {
       globalObject: 'this',
       pathinfo: false,
     },
+    optimization: {
+      runtimeChunk: true,
+      splitChunks: {
+        chunks: 'async',
+        minSize: 20000,
+        minRemainingSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
     stats: 'minimal',
     devServer: {
       historyApiFallback: true,
