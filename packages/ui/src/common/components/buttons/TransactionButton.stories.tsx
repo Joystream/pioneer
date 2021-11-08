15560eddac2c1d2b2668e389795492542183d96e
p-sad <sadlik512@gmail.com>
2021-11-08 14:45:58 +0100

    Add a storybook entry

---
+++ packages/ui/src/common/components/buttons/TransactionButton.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { TransactionContext, UseTransaction } from '@/common/providers/transaction/context'
+
+import { Row, TemplateBlock } from '../storybookParts/previewStyles'
+
+import { TransactionButton } from './TransactionButton'
+
+export default {
+  title: 'Common/Buttons/TransactionButton',
+  component: TransactionButton,
+  argTypes: {
+    isTransactionPending: { options: [true, false] },
+  },
+} as Meta
+
+const Template: Story<UseTransaction> = (args) => (
+  <TransactionContext.Provider value={{ ...args, setPending: () => undefined }}>
+    <TemplateBlock>
+      <Row>
+        <TransactionButton style="primary" size="large">
+          Button Primary
+        </TransactionButton>
+        <TransactionButton style="secondary" size="large">
+          Button Secondary
+        </TransactionButton>
+        <TransactionButton style="ghost" size="large">
+          Button Ghost
+        </TransactionButton>
+      </Row>
+    </TemplateBlock>
+  </TransactionContext.Provider>
+)
+
+export const Buttons = Template.bind({})
+
+Buttons.args = {
+  isTransactionPending: false,
+  setPending: () => undefined,
+}
