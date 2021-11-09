b51049e850e6ce012de1678d9309e0886b14d644
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-11-09 18:04:04 +0100

    Set custom stage durations

--- packages/ui/dev/chain-spec/index.ts
+++ packages/ui/dev/chain-spec/index.ts
@@ -3,6 +3,8 @@ import { readFileSync, writeFileSync } from 'fs'
 import { DeepMerger } from '@apollo/client/utilities'
 import yargs from 'yargs'
 
+import { capitalizeFirstLetter } from '../../src/common/helpers'
+import { isDefined } from '../../src/common/utils'
 import rawCandidates from '../../src/mocks/data/raw/candidates.json'
 import rawMembers from '../../src/mocks/data/raw/members.json'
 
@@ -34,20 +36,43 @@ const candidates = rawCandidates
     },
   ])
 
-const handlerFor = (stage: 'announcing' | 'voting' | 'revealing') => () => {
+const DefaultDurations = { voting: 5, revealing: 7 }
+
+type ReferendumStage<T extends any> = T extends Record<'Voting', infer U> | Record<'Revealing', infer V>
+  ? { [k in 'Voting' | 'Revealing']?: U & V }
+  : never
+const asReferendumStage = <T>(x: T) => (x as unknown) as ReferendumStage<T>
+
+const handlerFor = (stage: 'announcing' | 'voting' | 'revealing') => (args: any) => {
   const merger = new DeepMerger()
   const pathName = `${__dirname}/data/chain-spec.json`
   const oldSpec = JSON.parse(readFileSync(pathName, 'utf8'))
   const config = configs[stage]
 
+  const councilStageConf = config.genesis.runtime.council.stage.stage
+  const referendumStageConf = asReferendumStage(config.genesis.runtime.referendumInstance1.stage)
+
+  // Reset stages
+  oldSpec.genesis.runtime.council.stage.stage = {}
+  oldSpec.genesis.runtime.referendumInstance1.stage = {}
+
   // Set members
   oldSpec.genesis.runtime.membership.members = members
 
-  // Set candidates (on Voting and Revealing stage)
-  const councilStageConf = config.genesis.runtime.council.stage.stage
-  if (councilStageConf.Election) {
-    oldSpec.genesis.runtime.council.candidates = candidates
-    councilStageConf.Election.candidates_count = candidates.length
+  if (stage === 'voting' || stage === 'revealing') {
+    // Set candidates (on Voting and Revealing stage)
+    if (councilStageConf.Election) {
+      oldSpec.genesis.runtime.council.candidates = candidates
+      councilStageConf.Election.candidates_count = candidates.length
+    }
+
+    // Set stage length
+    const duration = args.duration as number | undefined
+    if (isDefined(duration)) {
+      const key = capitalizeFirstLetter(stage)
+      const ref = referendumStageConf[key]
+      if (ref) ref.started = duration - DefaultDurations[stage]
+    }
   }
 
   const newSpec = merger.merge(oldSpec, config)
@@ -58,6 +83,10 @@ yargs(process.argv.slice(2))
   .usage('yarn set-chain-spec <command>')
   .scriptName('')
   .command('announcing', 'Make the node start at the Announcing stage', handlerFor('announcing'))
-  .command('voting', 'Make the node start at the Voting stage', handlerFor('voting'))
-  .command('revealing', 'Make the node start at the Revealing stage', handlerFor('revealing'))
+  .command('voting', 'Make the node start at the Voting stage', builder, handlerFor('voting'))
+  .command('revealing', 'Make the node start at the Revealing stage', builder, handlerFor('revealing'))
   .demandCommand().argv
+
+function builder(argv: yargs.Argv<unknown>) {
+  argv.options({ d: { type: 'number', alias: 'duration' } })
+}
