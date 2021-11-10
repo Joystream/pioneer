899f4299d0f9404794d7ca9cb0ed21d2eb6ae78b
Maciej <jodator@jodator.net>
2021-11-10 13:58:28 +0100

    ⚡️ TS improvements (#1746)
    * Add transpile only to cli commands
    
    * Add experimental ts configuration
    
    * Tune up jest config

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/en/configuration.html
  */
 
-export default {
+module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -63,7 +63,7 @@ export default {
   // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  // maxWorkers: "50%",
+  maxWorkers: '50%',
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
