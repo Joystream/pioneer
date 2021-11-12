a7ccf1ccee2fbe995db424b37c2f59856baa88bb
mazya <36150652+ClumsyVlad@users.noreply.github.com>
2021-11-12 15:07:55 +0000

    🐒 Update sidebar items & dropdown (#1762)
    Co-authored-by: Vladyslav Mazurenko <vladyslav@TPL005-VM-MBP17.local>

---
+++ packages/ui/src/common/components/page/Sidebar/LinksIcons/BandwidthIcon.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import { Colors } from '@/common/constants'
+
+import { Icon } from '../../../icons'
+
+export const BandwidthIcon = () => (
+  <Icon
+    size="20"
+    viewBox="0 0 20 20"
+    preserveAspectRatio="xMidYMid meet"
+    fill="none"
+    color="currentColor"
+    className="nav-icon"
+  >
+    <rect x="6.31226" y="6.31226" width="7.37553" height="7.37551" fill="currentColor" />
+    <ellipse cx="10.0575" cy="10.0575" rx="0.670502" ry="0.670502" fill={Colors.Black[900]} />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.99998 0L12.1504 2.15037L11.2021 3.0986L10.6705 2.56696V7.98849H9.32948V2.56697L8.79784 3.0986L7.84961 2.15037L9.99998 0Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M10.6706 6.31226V7.98851H9.32959V6.31226H10.6706Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.99998 20L12.1504 17.8496L11.2021 16.9014L10.6705 17.433V12.0115H9.32948V17.433L8.79784 16.9014L7.84961 17.8496L9.99998 20Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M20 9.99998L17.8496 12.1503L16.9014 11.2021L17.433 10.6705L12.0115 10.6705L12.0115 9.32948L17.433 9.32948L16.9014 8.79784L17.8496 7.84961L20 9.99998Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.39958e-08 9.99998L2.15037 12.1503L3.09861 11.2021L2.56697 10.6705L7.98851 10.6705L7.98851 9.32948L2.56697 9.32948L3.09861 8.79784L2.15037 7.84961L9.39958e-08 9.99998Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M10.6706 12.0115V13.6877H9.32959V12.0115H10.6706Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7.98853 10.6705L6.31227 10.6705L6.31227 9.32947L7.98853 9.32947L7.98853 10.6705Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13.6877 10.6705L12.0115 10.6705L12.0115 9.32947L13.6877 9.32947L13.6877 10.6705Z"
+      fill="currentColor"
+    />
+  </Icon>
+)
