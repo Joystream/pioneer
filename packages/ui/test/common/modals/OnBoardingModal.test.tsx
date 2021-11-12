3c7b1d3a5fbca2442f0f11175fa136050f968cf3
Radoslaw Wyszynski <radoslaw.wyszynski@4soft.co>
2021-11-12 12:38:26 +0100

    Onboarding Modal: Full test

--- packages/ui/test/common/modals/OnBoardingModal.test.tsx
+++ packages/ui/test/common/modals/OnBoardingModal.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
+import BN from 'bn.js'
 import React from 'react'
 
 import { UseAccounts } from '@/accounts/providers/accounts/provider'
@@ -21,10 +22,10 @@ const mockUseMyAccounts: UseAccounts = {
 const mockOnBoardingStatus: UseOnBoardingStatus = {
   status: 'installPlugin',
   isLoading: false,
-  setFreeTokens: undefined,
+  setFreeTokens: jest.fn(),
 }
 
-const mockMyBalances: AddressToBalanceMap = {}
+let mockMyBalances: AddressToBalanceMap = {}
 
 jest.mock('@/accounts/hooks/useMyAccounts', () => ({
   useMyAccounts: () => mockUseMyAccounts,
@@ -48,6 +49,8 @@ describe('UI: OnBoardingModal', () => {
     modalData: undefined,
   }
 
+  afterEach(cleanup)
+
   it('Do not render', () => {
     mockOnBoardingStatus.isLoading = true
 
@@ -60,6 +63,7 @@ describe('UI: OnBoardingModal', () => {
     beforeAll(() => {
       mockOnBoardingStatus.isLoading = false
     })
+
     it('Stepper matches', () => {
       const { getByText } = renderModal()
 
@@ -78,6 +82,141 @@ describe('UI: OnBoardingModal', () => {
       pluginButton.click()
 
       expect(windowSpy).toBeCalledWith('https://polkadot.js.org/extension/', '_blank')
+    })
+  })
+
+  describe('Status: addAccount', () => {
+    beforeAll(() => {
+      mockOnBoardingStatus.status = 'addAccount'
+    })
+
+    it('Stepper matches', () => {
+      const { getByText } = renderModal()
+
+      const accountCircle = getStepperStepCircle('Connect a Polkadot account', getByText)
+
+      expect(accountCircle).toHaveStyle(`background-color: ${Colors.Blue[500]}`)
+    })
+
+    describe('Add account step', () => {
+      it('Create account instructions', () => {
+        const { queryByText } = renderModal()
+
+        expect(queryByText('Create an account')).toBeDefined()
+      })
+    })
+
+    describe('Pick account step', () => {
+      beforeAll(() => {
+        mockUseMyAccounts.hasAccounts = true
+        mockUseMyAccounts.allAccounts = [
+          {
+            address: '123',
+            name: 'Alice',
+          },
+          {
+            address: '321',
+            name: 'Bob',
+          },
+        ]
+        mockMyBalances = {
+          '123': {
+            total: new BN(10),
+            locked: new BN(0),
+            recoverable: new BN(0),
+            transferable: new BN(0),
+            locks: [],
+          },
+          '321': {
+            total: new BN(0),
+            locked: new BN(0),
+            recoverable: new BN(0),
+            transferable: new BN(0),
+            locks: [],
+          },
+        }
+      })
+
+      it('Shows correct screen', () => {
+        const { queryByText } = renderModal()
+
+        expect(queryByText('Connect accounts')).toBeDefined()
+      })
+
+      it('Shows accounts with balance', () => {
+        const { getByText } = renderModal()
+
+        const alice = getByText('Alice')
+        const bob = getByText('Bob')
+
+        expect(alice).toBeDefined()
+        expect(bob).toBeDefined()
+
+        const aliceBalance = alice?.parentElement?.parentElement?.children?.item(1)?.textContent
+        const bobBalance = bob?.parentElement?.parentElement?.children?.item(1)?.textContent
+
+        expect(aliceBalance).toBe('10')
+        expect(bobBalance).toBe('0')
+      })
+
+      it('Proceed to next step', () => {
+        const { getByText } = renderModal()
+
+        getByText('Alice').click()
+        getByText('Connect Account').click()
+
+        expect(mockOnBoardingStatus.setFreeTokens).toBeCalledWith('123')
+      })
+    })
+  })
+
+  describe('Status: getFreeTokens', () => {
+    beforeAll(() => {
+      mockOnBoardingStatus.status = 'getFreeTokens'
+    })
+
+    it('Step matches', () => {
+      const { getByText } = renderModal()
+
+      const tokenCircle = getStepperStepCircle('Get FREE tokens', getByText)
+
+      expect(tokenCircle).toHaveStyle(`background-color: ${Colors.Blue[500]}`)
+    })
+
+    it('Shows correct screen', () => {
+      const { queryByText } = renderModal()
+
+      expect(queryByText('One Last Thing')).toBeDefined()
+      expect(queryByText('Continue And Get Free Tokens')).toBeDefined()
+    })
+
+    it('Proceed to next step', () => {
+      const { getByText } = renderModal()
+
+      getByText(/Continue And Get Tokens/i)?.click()
+
+      expect(mockOnBoardingStatus.setFreeTokens).toBeCalledWith('redeemed')
+    })
+  })
+
+  describe('Status: createMembership', () => {
+    beforeAll(() => {
+      mockOnBoardingStatus.status = 'createMembership'
+    })
+
+    it('Step matches', () => {
+      const { getByText } = renderModal()
+
+      const membershipCircle = getStepperStepCircle('Create membership', getByText)
+
+      expect(membershipCircle).toHaveStyle(`background-color: ${Colors.Blue[500]}`)
+    })
+
+    it('Shows correct screen', () => {
+      const { queryByText } = renderModal()
+
+      expect(queryByText(/Root account/i)).toBeDefined()
+      expect(queryByText(/Create A Membership/)).toBeDefined()
     })
   })
 
