d8dfa757730b859e7b97300b3da4eba8aadd5aa9
p-sad <sadlik512@gmail.com>
2021-11-15 14:59:12 +0100

    Validate max active proposals count

--- packages/ui/src/proposals/components/AddProposalButton.tsx
+++ packages/ui/src/proposals/components/AddProposalButton.tsx
@@ -2,7 +2,9 @@ import React, { useCallback } from 'react'
 
 import { TransactionButton } from '@/common/components/buttons/TransactionButton'
 import { PlusIcon } from '@/common/components/icons/PlusIcon'
+import { useApi } from '@/common/hooks/useApi'
 import { useModal } from '@/common/hooks/useModal'
+import { useObservable } from '@/common/hooks/useObservable'
 import { AddNewProposalModalCall } from '@/proposals/modals/AddNewProposal'
 
 export const AddProposalButton = () => {
@@ -13,8 +15,18 @@ export const AddProposalButton = () => {
     })
   }, [])
 
+  const { api } = useApi()
+  const maxProposals = api?.consts.proposalsEngine.maxActiveProposalLimit
+  const currentProposals = useObservable(api?.query.proposalsEngine.activeProposalCount(), [api?.isConnected])
+  const areProposalSlotsAvailable = api && maxProposals && currentProposals?.lt(maxProposals)
+
   return (
-    <TransactionButton style="primary" size="medium" onClick={addNewProposalModal}>
+    <TransactionButton
+      style="primary"
+      size="medium"
+      onClick={addNewProposalModal}
+      disabled={!areProposalSlotsAvailable}
+    >
       <PlusIcon />
       Add new proposal
     </TransactionButton>
