4127c2adeaaa67b1f0a98586d4f9d1c336bb623f
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-11-15 12:47:10 +0100

    Add a `DownloadButtonGhost` component

---
+++ packages/ui/src/common/components/buttons/DownloadButton.stories.tsx
@@ -0,0 +1,60 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import { MemoryRouter } from 'react-router'
+
+import { ArrowInsideIcon } from '../icons'
+import { Row, TemplateBlock } from '../storybookParts/previewStyles'
+
+import { DownloadLink, DownloadButtonGhost, DownloadLinkProps } from './DownloadButtons'
+
+export default {
+  title: 'Common/Buttons/DownloadButtons',
+  components: [DownloadLink, DownloadButtonGhost],
+} as Meta
+
+const Template: Story<DownloadLinkProps> = (args) => (
+  <MemoryRouter>
+    <TemplateBlock>
+      <Row>
+        <DownloadButtonGhost {...args} size="large">
+          <ArrowInsideIcon />
+          Large download button
+        </DownloadButtonGhost>
+
+        <DownloadButtonGhost {...args} size="large" $square>
+          <ArrowInsideIcon />
+        </DownloadButtonGhost>
+
+        <DownloadButtonGhost {...args} size="medium">
+          <ArrowInsideIcon />
+          Medium download button
+        </DownloadButtonGhost>
+
+        <DownloadButtonGhost {...args} size="medium" $square>
+          <ArrowInsideIcon />
+        </DownloadButtonGhost>
+
+        <DownloadButtonGhost {...args} size="small">
+          <ArrowInsideIcon />
+          Small download button
+        </DownloadButtonGhost>
+
+        <DownloadButtonGhost {...args} size="small" $square>
+          <ArrowInsideIcon />
+        </DownloadButtonGhost>
+      </Row>
+      <Row>
+        <DownloadLink {...args}>
+          <ArrowInsideIcon /> Not styled Download link
+        </DownloadLink>
+      </Row>
+    </TemplateBlock>
+  </MemoryRouter>
+)
+
+export const Defaults = Template.bind({})
+
+Defaults.args = {
+  name: 'lorem ipsum.json',
+  parts: [JSON.stringify({ foo: 'Mollit elit cupidatat velit adipisicing anim aliquip quis deserunt.' })],
+}
