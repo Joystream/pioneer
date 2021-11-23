712a0903f1d014837f149e832b52dca0e16e4202
RafalPloszka <rafalploszka@live.com>
2021-11-24 00:51:58 +0100

    add tests for terms pages

--- packages/ui/src/app/pages/Terms/PrivacyPolicy.tsx
+++ packages/ui/src/app/pages/Terms/PrivacyPolicy.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-import { PageLayout } from '@/app/components/PageLayout'
+import { PageHeaderWrapper, PageLayout } from '@/app/components/PageLayout'
 import { MarkdownPreview } from '@/common/components/MarkdownPreview'
+import { PageTitle } from '@/common/components/page/PageTitle'
 
 import Privacy from './md/Privacy.md'
 
 export const PrivacyPolicy = () => {
-  return <PageLayout main={<MarkdownPreview markdown={Privacy} />} />
+  return (
+    <PageLayout
+      header={
+        <PageHeaderWrapper>
+          <PageTitle>Privacy Policy</PageTitle>
+        </PageHeaderWrapper>
+      }
+      main={<MarkdownPreview markdown={Privacy} />}
+    />
+  )
 }
--- packages/ui/src/app/pages/Terms/TermsOfService.tsx
+++ packages/ui/src/app/pages/Terms/TermsOfService.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-import { PageLayout } from '@/app/components/PageLayout'
+import { PageHeaderWrapper, PageLayout } from '@/app/components/PageLayout'
 import { MarkdownPreview } from '@/common/components/MarkdownPreview'
+import { PageTitle } from '@/common/components/page/PageTitle'
 
 import ToS from './md/ToS.md'
 
 export const TermsOfService = () => {
-  return <PageLayout main={<MarkdownPreview markdown={ToS} />} />
+  return (
+    <PageLayout
+      header={
+        <PageHeaderWrapper>
+          <PageTitle>Terms of Service</PageTitle>
+        </PageHeaderWrapper>
+      }
+      main={<MarkdownPreview markdown={ToS} />}
+    />
+  )
 }
