22da17f258af81d98819c218dc5b480f25fb3e49
p-sad <sadlik512@gmail.com>
2021-11-23 12:43:46 +0100

    Fix links in activities

--- packages/ui/src/council/components/Activities/AnnouncingPeriodStartedContent.tsx
+++ packages/ui/src/council/components/Activities/AnnouncingPeriodStartedContent.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { ActivityContentComponent } from '@/common/components/Activities/ActivityContent'
 import { ActivityRouterLink } from '@/common/components/Activities/ActivityRouterLink'
-import { CouncilRoutes } from '@/council/constants'
+import { ElectionRoutes } from '@/council/constants'
 import { AnnouncingPeriodStartedActivity } from '@/council/types/CouncilActivities'
 
 export const AnnouncingPeriodStartedContent: ActivityContentComponent<AnnouncingPeriodStartedActivity> = () => (
-  <ActivityRouterLink to={`${CouncilRoutes.currentElection}`}>Announcing period has been started </ActivityRouterLink>
+  <ActivityRouterLink to={`${ElectionRoutes.currentElection}`}>Announcing period has been started </ActivityRouterLink>
 )
