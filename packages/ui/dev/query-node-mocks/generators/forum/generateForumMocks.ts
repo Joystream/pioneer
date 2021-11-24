8afd3b778173c3c0aae2f359a1bbe47c06c2038f
DevNikita16 <83761318+DevNikita16@users.noreply.github.com>
2021-11-24 14:32:30 +0100

    🍄 Update query node mocks generators (#1815)

--- packages/ui/dev/query-node-mocks/generators/forum/generateForumMocks.ts
+++ packages/ui/dev/query-node-mocks/generators/forum/generateForumMocks.ts
@@ -1,19 +1,32 @@
 import { saveFile } from '../../helpers/saveFile'
+import { MemberMock } from '../generateMembers'
+import { WorkerMock } from '../generateWorkers'
 
 import { generateCategories } from './generateCategories'
 import { generateForumThreads } from './generateForumThreads'
 
-export const generateForum = () => {
-  const forumCategories = generateCategories(3)
-  const { forumThreads, forumPosts } = generateForumThreads(forumCategories)
 
-  const forumMocks = { forumCategories, forumThreads, forumPosts }
+export interface MocksForForum {
+  members: MemberMock[]
+  workers: WorkerMock[]
+}
 
-  Object.entries(forumMocks).forEach(([fileName, contents]) => saveFile(fileName, contents))
+export const generateForum = (mocks?: MocksForForum) => {
+  if (!mocks) {
+    mocks = {
+      members: require('../../../../src/mocks/data/raw/members.json'),
+      workers: require('../../../../src/mocks/data/raw/workers.json')
+    }
+  }
+
+  const forumCategories = generateCategories(3)(mocks)
+  const { forumThreads, forumPosts } = generateForumThreads(forumCategories)(mocks)
+
+  return { forumCategories, forumThreads, forumPosts }
 }
 
 export const forumModule = {
   command: 'forum',
   describe: 'Generate forum from other mocks',
-  handler: generateForum,
+  handler: () => Object.entries(generateForum()).forEach(([fileName, contents]) => saveFile(fileName, contents))
 }
