36b91d2e2148735cbe3d2996f91ecfa0e23669da
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-11-25 13:22:32 +0100

    Fix the chain spec generation

--- packages/ui/dev/chain-spec/configs/revealing.ts
+++ packages/ui/dev/chain-spec/configs/revealing.ts
@@ -1,24 +1,21 @@
 export default {
-  genesis: {
-    runtime: {
-      council: {
-        stage: {
-          stage: {
-            Election: {
-              candidates_count: 0,
-            },
-          },
+  council: {
+    stage: {
+      stage: {
+        Election: {
+          candidates_count: 0,
         },
       },
-      referendumInstance1: {
-        stage: {
-          Revealing: {
-            started: 0,
-            winning_target_count: 3,
-            current_cycle_id: 4,
-            intermediate_winners: [],
-          },
-        },
+      changed_at: 0,
+    },
+  },
+  referendumInstance1: {
+    stage: {
+      Revealing: {
+        started: 0,
+        winning_target_count: 3,
+        current_cycle_id: 4,
+        intermediate_winners: [],
       },
     },
   },
