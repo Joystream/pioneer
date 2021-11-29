49c185fd90f335c4976f7f11381d27241874035b
Michał Krupka <75028807+krupka4soft@users.noreply.github.com>
2021-11-29 13:42:54 +0100

    🏄‍♂️ Added .env variables to override Olympia-Testnet addresses (#1845)

---
+++ packages/ui/src/app/config.ts
@@ -0,0 +1,6 @@
+export const OLYMPIA_TESTNET_NODE_SOCKET =
+  process.env.REACT_APP_OLYMPIA_TESTNET_NODE_SOCKET || 'wss://olympia-dev.joystream.app/rpc'
+export const OLYMPIA_TESTNET_QUERY_NODE =
+  process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE || 'https://olympia-dev.joystream.app/query/server/graphql'
+export const OLYMPIA_TESTNET_QUERY_NODE_SOCKET =
+  process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE_SOCKET || 'wss://olympia-dev.joystream.app/query/server/graphql'
