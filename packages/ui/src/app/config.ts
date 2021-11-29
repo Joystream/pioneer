4fcd98176d308555510771a20e6bcffb7214fa03
Nikita <dev.nikita16@gmail.com>
2021-11-29 14:42:08 +0100

    Add REACT_APP prefix to env variable

--- packages/ui/src/app/config.ts
+++ packages/ui/src/app/config.ts
@@ -4,4 +4,4 @@ export const OLYMPIA_TESTNET_QUERY_NODE =
   process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE || 'https://olympia-dev.joystream.app/query/server/graphql'
 export const OLYMPIA_TESTNET_QUERY_NODE_SOCKET =
   process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE_SOCKET || 'wss://olympia-dev.joystream.app/query/server/graphql'
-export const MEMBERSHIP_FAUCET_URL = process.env.MEMBERSHIP_FAUCET_URL || 'http://localhost:4000/register'
+export const MEMBERSHIP_FAUCET_URL = process.env.REACT_APP_MEMBERSHIP_FAUCET_URL || 'http://localhost:4000/register'
