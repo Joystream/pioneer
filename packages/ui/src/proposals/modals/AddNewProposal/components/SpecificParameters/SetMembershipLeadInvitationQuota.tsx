59fd438686b10a9e0b90965315ae07c40aa52c75
tomekstepien <tomasz.stepien@4soft.co>
2021-12-01 14:24:38 +0100

    component created

---
+++ packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/SetMembershipLeadInvitationQuota.tsx
@@ -0,0 +1,60 @@
+import BN from 'bn.js'
+import React, { useEffect } from 'react'
+
+import { InputComponent, InputNumber } from '@/common/components/forms'
+import { Row } from '@/common/components/Modal'
+import { RowGapBlock } from '@/common/components/page/PageContent'
+import { TextMedium } from '@/common/components/typography'
+import { cleanInputValue, useNumberInput } from '@/common/hooks/useNumberInput'
+import { formatTokenValue } from '@/common/model/formatters'
+import { useWorkingGroup } from '@/working-groups/hooks/useWorkingGroup'
+
+export interface SetMembershipLeadInvitationParameters {
+  amount?: BN
+}
+
+interface Props extends SetMembershipLeadInvitationParameters {
+  setAmount: (amount: BN) => void
+}
+
+const MAX_VALUE = Math.pow(2, 32) - 1
+
+export const SetMembershipLeadInvitationQuota = ({ amount: initialAmount, setAmount: setQuota }: Props) => {
+  const [amount, setAmount] = useNumberInput(0, initialAmount)
+  const { isLoading, group } = useWorkingGroup({ name: 'membershipWorkingGroup' })
+
+  useEffect(() => {
+    setQuota(new BN(amount))
+  }, [amount])
+
+  const setInvitationQuota = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = cleanInputValue(event.target.value)
+    if (Number(value) < MAX_VALUE) {
+      setAmount(value)
+    }
+  }
+
+  return (
+    <RowGapBlock gap={24}>
+      <Row>
+        <RowGapBlock gap={8}>
+          <h4>Specific parameters</h4>
+          <TextMedium lighter>Set Membership Lead Invitation Quota</TextMedium>
+        </RowGapBlock>
+      </Row>
+      <Row>
+        <RowGapBlock gap={20}>
+          <InputComponent label="Amount" tight units="JOY" required disabled={isLoading || !group?.leadId}>
+            <InputNumber
+              id="amount-input"
+              value={formatTokenValue(new BN(amount))}
+              placeholder="0"
+              onChange={setInvitationQuota}
+              disabled={isLoading || !group?.leadId}
+            />
+          </InputComponent>
+        </RowGapBlock>
+      </Row>
+    </RowGapBlock>
+  )
+}
