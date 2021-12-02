e58097928a8100816c9841364f8f7795a436525c
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-12-02 16:05:44 +0100

    Add a helper command to calculate a commitment

--- packages/ui/dev/helpers/index.ts
+++ packages/ui/dev/helpers/index.ts
@@ -1,9 +1,11 @@
 import yargs from 'yargs'
 
 import { setChainSpecModule } from './chain-spec'
+import { commitmentModule } from './commitment'
 
 yargs(process.argv.slice(2))
   .usage('yarn helpers <command>')
   .scriptName('')
   .command(setChainSpecModule)
+  .command(commitmentModule)
   .demandCommand().argv
