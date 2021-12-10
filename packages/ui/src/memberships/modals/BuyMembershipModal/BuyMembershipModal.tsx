046cb802e37b333a39dcd97f01a2c57382f306eb
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2021-12-10 17:38:45 +0100

    🎉  Solution to flashing onboarding top bar on app load (#1962)
    * Solution to flashing onboarding topbar on app load
    
    * Fix linter
    
    * Update useMyMemberships test
    
    Co-authored-by: Maciej Gołaszewski <jodator@jodator.net>

--- packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
+++ packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipModal.tsx
@@ -5,6 +5,7 @@ import { FailureModal } from '@/common/components/FailureModal'
 import { useApi } from '@/common/hooks/useApi'
 import { useModal } from '@/common/hooks/useModal'
 import { useObservable } from '@/common/hooks/useObservable'
+import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 import { toMemberTransactionParams } from '@/memberships/modals/utils'
 
 import { BuyMembershipFormModal, MemberFormFields } from './BuyMembershipFormModal'
@@ -15,6 +16,7 @@ import { buyMembershipMachine } from './machine'
 export const BuyMembershipModal = () => {
   const { hideModal } = useModal()
   const { api, connectionState } = useApi()
+  const { refetch: refetchMemberships } = useMyMemberships()
   const membershipPrice = useObservable(api?.query.members.membershipPrice(), [connectionState])
   const [state, send] = useMachine(buyMembershipMachine)
 
@@ -43,6 +45,7 @@ export const BuyMembershipModal = () => {
 
   if (state.matches('success')) {
     const { form, memberId } = state.context
+    refetchMemberships?.()
     return <BuyMembershipSuccessModal onClose={hideModal} member={form} memberId={memberId?.toString()} />
   }
 
