8e1651dfc099c26c391767a7d5dd45c6907b88f5
WRadoslaw <r.wyszynski00@gmail.com>
2021-12-16 13:39:39 +0100

    Story

---
+++ packages/ui/src/bounty/components/TopContributors/TopContributors.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { TopContributors } from '@/bounty/components/TopContributors/TopContributors'
+import members from '@/mocks/data/raw/members.json'
+
+export default {
+  title: 'Bounty/TopContributors',
+  component: TopContributors,
+} as Meta
+
+const Template: Story = (args) => <TopContributors contributors={[]} {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  contributors: members.slice(0, 20),
+}
