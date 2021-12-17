e725ad37f0c3888df4cd50508a6ad9da63bfa21b
WRadoslaw <r.wyszynski00@gmail.com>
2021-12-17 10:49:00 +0100

    Machine

---
+++ packages/ui/src/bounty/modals/AddBountyModal/machine.ts
@@ -0,0 +1,259 @@
+import { EventRecord } from '@polkadot/types/interfaces/system'
+import { assign, createMachine, State, Typestate } from 'xstate'
+import { StateSchema } from 'xstate/lib/types'
+
+import { Member } from '@/memberships/types'
+
+export type FundingPeriodType = 'perpetual' | 'limited'
+export type WorkingPeriodType = 'open' | 'closed'
+
+interface GeneralParametersContext {
+  creator: Member
+  title: string
+  coverPhotoLink: string
+  description: string
+}
+
+interface FundingPeriodDetailsContext extends GeneralParametersContext {
+  cherry: number
+  fundingPeriodType: FundingPeriodType
+  fundingPeriodLength?: number
+  fundingMinimalRange?: number
+  fundingMaximalRange: number
+}
+
+interface WorkingPeriodDetailsContext extends FundingPeriodDetailsContext {
+  workingPeriodType: WorkingPeriodType
+  workingPeriodLength: number
+  workingPeriodWhitelist?: Member[]
+  workingPeriodStakeAllowance: boolean
+  workingPeriodStake?: number
+}
+
+interface JudgingPeriodDetailsContext extends WorkingPeriodDetailsContext {
+  judgingPeriodLength: number
+  oracle: Member
+}
+
+interface ForumThreadDetailsContext extends JudgingPeriodDetailsContext {
+  forumThreadTopic: string
+  forumThreadDescription: string
+}
+
+interface TransactionContext extends JudgingPeriodDetailsContext {
+  transactionEvents?: EventRecord[]
+  bountyId?: number
+}
+
+export type AddBountyContext = Partial<
+  GeneralParametersContext &
+    ForumThreadDetailsContext &
+    WorkingPeriodDetailsContext &
+    JudgingPeriodDetailsContext &
+    TransactionContext
+>
+
+export enum AddBountyStates {
+  generalParameters = 'generalParameters',
+  fundingPeriodDetails = 'fundingPeriodDetails',
+  workingPeriodDetails = 'workingPeriodDetails',
+  judgingPeriodDetails = 'judgingPeriodDetails',
+  forumThreadDetails = 'forumThreadDetails',
+  beforeTransaction = 'beforeTransaction',
+  transaction = 'transaction',
+  success = 'success',
+  error = 'error',
+}
+
+export type AddBountyState =
+  | { value: AddBountyStates.generalParameters; context: GeneralParametersContext }
+  | { value: AddBountyStates.fundingPeriodDetails; context: FundingPeriodDetailsContext }
+  | { value: AddBountyStates.workingPeriodDetails; context: WorkingPeriodDetailsContext }
+  | { value: AddBountyStates.judgingPeriodDetails; context: JudgingPeriodDetailsContext }
+  | { value: AddBountyStates.forumThreadDetails; context: ForumThreadDetailsContext }
+  | { value: AddBountyStates.beforeTransaction; context: Required<AddBountyContext> }
+  // | { value: 'bindStakingAccount'; context: Required<AddBountyContext> }
+  | { value: AddBountyStates.transaction; context: Required<AddBountyContext> }
+  | { value: AddBountyStates.success; context: Required<AddBountyContext> }
+  | { value: AddBountyStates.error; context: AddBountyContext }
+
+type SetCreatorEvent = { type: 'SET_CREATOR'; creator: Member }
+type SetBountyTitleEvent = { type: 'SET_BOUNTY_TITLE'; title: string }
+type SetCoverPhotoEvent = { type: 'SET_COVER_PHOTO'; coverPhotoLink: string }
+type SetBountyDescriptionEvent = { type: 'SET_BOUNTY_DESCRIPTION'; description: string }
+type SetCherryEvent = { type: 'SET_CHERRY'; cherry: number }
+type SetFundingPeriodTypeEvent = { type: 'SET_FUNDING_PERIOD_TYPE'; fundingPeriodType: FundingPeriodType }
+type SetFundingPeriodLengthEvent = { type: 'SET_FUNDING_PERIOD_LENGTH'; fundingPeriodLength: number }
+type SetFundingMinimalRangeEvent = { type: 'SET_FUNDING_MINIMAL_RANGE'; fundingMinimalRange: number }
+type SetFundingMaximalRangeEvent = { type: 'SET_FUNDING_MAXIMAL_RANGE'; fundingMaximalRange: number }
+type SetWorkingPeriodTypeEvent = { type: 'SET_WORKING_PERIOD_TYPE'; workingPeriodType: WorkingPeriodType }
+type SetWorkingPeriodLengthEvent = { type: 'SET_WORKING_PERIOD_LENGTH'; workingPeriodLength: number }
+type SetAllowWorkingPeriodEvent = { type: 'SET_ALLOW_WORKING_PERIOD_STAKE'; workingPeriodStakeAllowance: boolean }
+type SetWorkingPeriodStakeEvent = { type: 'SET_WORKING_PERIOD_STAKE'; workingPeriodStake: number }
+type SetWorkingPeriodWhitelistEvent = { type: 'SET_WORKING_PERIOD_WHITELIST'; workingPeriodWhitelist: Member[] }
+type SetJudgingPeriodLengthEvent = { type: 'SET_JUDGING_PERIOD_LENGTH'; judgingPeriodLength: number }
+type SetOracleEvent = { type: 'SET_ORACLE'; oracle: Member }
+type SetForumThreadTopicEvent = { type: 'SET_FORUM_THREAD_TOPIC'; forumThreadTopic: string }
+type SetForumThreadDescriptionEvent = { type: 'SET_FORUM_THREAD_DESCRIPTION'; forumThreadDescription: string }
+
+export type AddBountyEvent =
+  | SetCreatorEvent
+  | SetBountyTitleEvent
+  | SetCoverPhotoEvent
+  | SetBountyDescriptionEvent
+  | SetCherryEvent
+  | SetFundingPeriodTypeEvent
+  | SetFundingPeriodLengthEvent
+  | SetFundingMinimalRangeEvent
+  | SetFundingMaximalRangeEvent
+  | SetWorkingPeriodTypeEvent
+  | SetWorkingPeriodLengthEvent
+  | SetAllowWorkingPeriodEvent
+  | SetWorkingPeriodStakeEvent
+  | SetWorkingPeriodWhitelistEvent
+  | SetJudgingPeriodLengthEvent
+  | SetOracleEvent
+  | SetForumThreadTopicEvent
+  | SetForumThreadDescriptionEvent
+  | { type: 'NEXT' }
+  | { type: 'FAIL' }
+
+export type AddBountyModalMachineState = State<
+  AddBountyContext,
+  AddBountyEvent,
+  StateSchema<AddBountyContext>,
+  Typestate<AddBountyContext>
+>
+
+export const addBountyMachine = createMachine<AddBountyContext, AddBountyEvent, AddBountyState>({
+  initial: AddBountyStates.generalParameters,
+  context: {
+    fundingPeriodType: 'perpetual',
+    workingPeriodType: 'open',
+    workingPeriodStakeAllowance: true,
+  },
+  states: {
+    [AddBountyStates.generalParameters]: {
+      on: {
+        NEXT: AddBountyStates.fundingPeriodDetails,
+        SET_CREATOR: {
+          actions: assign({
+            creator: (context, event) => (event as SetCreatorEvent).creator,
+          }),
+        },
+        SET_BOUNTY_TITLE: {
+          actions: assign({
+            title: (context, event) => (event as SetBountyTitleEvent).title,
+          }),
+        },
+        SET_COVER_PHOTO: {
+          actions: assign({
+            coverPhotoLink: (context, event) => (event as SetCoverPhotoEvent).coverPhotoLink,
+          }),
+        },
+        SET_BOUNTY_DESCRIPTION: {
+          actions: assign({
+            description: (context, event) => (event as SetBountyDescriptionEvent).description,
+          }),
+        },
+      },
+      meta: { isStep: true, stepTitle: 'General Parameters' },
+    },
+    [AddBountyStates.fundingPeriodDetails]: {
+      on: {
+        NEXT: AddBountyStates.workingPeriodDetails,
+        SET_CHERRY: {
+          actions: assign({
+            cherry: (context, event) => (event as SetCherryEvent).cherry,
+          }),
+        },
+        SET_FUNDING_PERIOD_TYPE: {
+          actions: assign({
+            fundingPeriodType: (context, event) => (event as SetFundingPeriodTypeEvent).fundingPeriodType,
+          }),
+        },
+        SET_FUNDING_PERIOD_LENGTH: {
+          actions: assign({
+            fundingPeriodLength: (context, event) => (event as SetFundingPeriodLengthEvent).fundingPeriodLength,
+          }),
+        },
+        SET_FUNDING_MINIMAL_RANGE: {
+          actions: assign({
+            fundingMinimalRange: (context, event) => (event as SetFundingMinimalRangeEvent).fundingMinimalRange,
+          }),
+        },
+        SET_FUNDING_MAXIMAL_RANGE: {
+          actions: assign({
+            fundingMaximalRange: (context, event) => (event as SetFundingMaximalRangeEvent).fundingMaximalRange,
+          }),
+        },
+      },
+      meta: { isStep: true, stepTitle: 'Funding Period Details' },
+    },
+    [AddBountyStates.workingPeriodDetails]: {
+      on: {
+        NEXT: AddBountyStates.judgingPeriodDetails,
+        SET_WORKING_PERIOD_TYPE: {
+          actions: assign({
+            workingPeriodType: (context, event) => (event as SetWorkingPeriodTypeEvent).workingPeriodType,
+          }),
+        },
+        SET_WORKING_PERIOD_LENGTH: {
+          actions: assign({
+            workingPeriodLength: (context, event) => (event as SetWorkingPeriodLengthEvent).workingPeriodLength,
+          }),
+        },
+        SET_WORKING_PERIOD_WHITELIST: {
+          actions: assign({
+            workingPeriodWhitelist: (context, event) =>
+              (event as SetWorkingPeriodWhitelistEvent).workingPeriodWhitelist,
+          }),
+        },
+        SET_ALLOW_WORKING_PERIOD_STAKE: {
+          actions: assign({
+            workingPeriodStakeAllowance: (context, event) =>
+              (event as SetAllowWorkingPeriodEvent).workingPeriodStakeAllowance,
+          }),
+        },
+        SET_WORKING_PERIOD_STAKE: {
+          actions: assign({
+            workingPeriodStake: (context, event) => (event as SetWorkingPeriodStakeEvent).workingPeriodStake,
+          }),
+        },
+      },
+      meta: { isStep: true, stepTitle: 'Working Period Details' },
+    },
+    [AddBountyStates.judgingPeriodDetails]: {
+      on: {
+        NEXT: AddBountyStates.forumThreadDetails,
+        SET_JUDGING_PERIOD_LENGTH: {
+          actions: assign({
+            judgingPeriodLength: (context, event) => (event as SetJudgingPeriodLengthEvent).judgingPeriodLength,
+          }),
+        },
+        SET_ORACLE: {
+          actions: assign({
+            oracle: (context, event) => (event as SetOracleEvent).oracle,
+          }),
+        },
+      },
+      meta: { isStep: true, stepTitle: 'Judging Period Details' },
+    },
+    [AddBountyStates.forumThreadDetails]: {
+      on: {
+        NEXT: AddBountyStates.beforeTransaction,
+        SET_FORUM_THREAD_TOPIC: {
+          actions: assign({
+            forumThreadTopic: (context, event) => (event as SetForumThreadTopicEvent).forumThreadTopic,
+          }),
+        },
+        SET_FORUM_THREAD_DESCRIPTION: {
+          actions: assign({
+            forumThreadDescription: (context, event) =>
+              (event as SetForumThreadDescriptionEvent).forumThreadDescription,
+          }),
+        },
+      },
+    },
+  },
+})
