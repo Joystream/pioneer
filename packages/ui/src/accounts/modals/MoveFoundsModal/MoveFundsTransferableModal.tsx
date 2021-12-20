71f2e506e65e5af4f455f43acd964cc2965785bc
RafalPloszka <rafalploszka@live.com>
2021-12-20 11:52:15 +0100

    Add memoization + improve components structure

--- packages/ui/src/accounts/modals/MoveFoundsModal/MoveFundsTransferableModal.tsx
+++ packages/ui/src/accounts/modals/MoveFoundsModal/MoveFundsTransferableModal.tsx
@@ -1,26 +1,15 @@
 import React from 'react'
 
-import { AccountInfo } from '@/accounts/components/AccountInfo'
-import { lockIcon } from '@/accounts/components/AccountLocks'
-import { useMyAccounts } from '@/accounts/hooks/useMyAccounts'
-import { conflictingLocks } from '@/accounts/model/lockTypes'
-import { Account, AddressToBalanceMap, LockType } from '@/accounts/types'
+import { AddressToBalanceMap, LockType } from '@/accounts/types'
 import { ButtonPrimary } from '@/common/components/buttons'
 import { Info } from '@/common/components/Info'
-import {
-  BalanceInfoInRow,
-  InfoTitle,
-  InfoValue,
-  Modal,
-  ModalFooter,
-  ModalHeader,
-  ItemWrapper,
-} from '@/common/components/Modal'
+import { Modal, ModalFooter, ModalHeader } from '@/common/components/Modal'
 import { RowGapBlock } from '@/common/components/page/PageContent'
 import { TextMedium, TokenValue } from '@/common/components/typography'
 import { Address } from '@/common/types'
 
-import { AccountLocksWrapper, InlineLockIconWrapper, MemberRow, ModalBody } from './styles'
+import { MemberRowsList } from './MemberRowsList'
+import { ModalBody } from './styles'
 
 export interface MoveFoundsTransferableModalProps {
   onClose: () => void
@@ -39,17 +28,9 @@ export const MoveFundsTransferableModal = ({
   balances,
   lock,
 }: MoveFoundsTransferableModalProps) => {
-  const { allAccounts } = useMyAccounts()
-
   if (!accounts || !accounts.length) {
     return null
   }
-
-  const accountsWithIncompatibleLocks = Object.entries(balances)
-    .map(([address, balances]) => ({ address: address, locks: conflictingLocks(lock ?? 'Proposals', balances.locks) }))
-    .filter((el) => el.locks.length)
-
-  const addressesWithIncompatibleLocks = accountsWithIncompatibleLocks.map((account) => account.address)
 
   return (
     <Modal modalSize="m" modalHeight="s" onClose={onClose}>
@@ -63,31 +44,7 @@ export const MoveFundsTransferableModal = ({
           <RowGapBlock gap={4}>
             <TextMedium bold>Accounts with transferable balances:</TextMedium>
             <RowGapBlock gap={16}>
-              <ItemWrapper>
-                {accounts.map((address) => (
-                  <MemberRow key={address}>
-                    <AccountInfo account={allAccounts.find((account) => account.address === address) as Account} />
-                    <BalanceInfoInRow>
-                      <InfoTitle>
-                        Transferable balance
-                        {addressesWithIncompatibleLocks.includes(address) && (
-                          <AccountLocksWrapper>
-                            Locks:
-                            {accountsWithIncompatibleLocks
-                              .find((el) => el.address === address)
-                              ?.locks.map((lock) => (
-                                <InlineLockIconWrapper title={lock.toString()}>{lockIcon(lock)}</InlineLockIconWrapper>
-                              ))}
-                          </AccountLocksWrapper>
-                        )}
-                      </InfoTitle>
-                      <InfoValue>
-                        <TokenValue value={balances[address] && balances[address].transferable} />
-                      </InfoValue>
-                    </BalanceInfoInRow>
-                  </MemberRow>
-                ))}
-              </ItemWrapper>
+              <MemberRowsList balances={balances} accounts={accounts} lock={lock} />
               <Info title="Info">
                 <TextMedium light>
                   You can combine balances in one of your account or transfer tokens into new account.
