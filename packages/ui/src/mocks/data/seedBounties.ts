99600410d7ea674bcfdfce4a355c00843f30bf11
Theophile Sandoz <theophile.sandoz@gmail.com>
2021-12-21 12:07:14 +0100

    Seed bounties on the mirage js server

--- packages/ui/src/mocks/data/seedBounties.ts
+++ packages/ui/src/mocks/data/seedBounties.ts
@@ -1,3 +1,9 @@
+import { seedOverridableEntities } from '../helpers/seedEntities'
+
+import rawBounties from './raw/bounties.json'
+import rawContributions from './raw/bountyContributions.json'
+import rawEntries from './raw/bountyEntries.json'
+
 export interface RawBountyMock {
   id: string
   title: string
@@ -32,3 +38,34 @@ export interface RawBountyEntryMock {
   works: string[]
   status: { type: string; reward?: string }
 }
+
+const seedFundingType = ({ type, ...data }: RawBountyMock['fundingType'], server: any) =>
+  server.schema.create(`BountyFunding${type}`, data)
+
+const seedContractType = ({ type, ...data }: RawBountyMock['contractType'], server: any) =>
+  server.schema.create(`BountyContract${type}`, data)
+
+export const seedBounty = ({ fundingType, contractType, ...data }: RawBountyMock, server: any) =>
+  server.schema.create('Bounty', {
+    ...data,
+    fundingType: seedFundingType(fundingType, server),
+    contractType: seedContractType(contractType, server),
+  })
+
+export const seedBounties = seedOverridableEntities<RawBountyMock>(rawBounties, seedBounty)
+
+export const seedBountyContribution = (data: RawBountyContributionMock, server: any) =>
+  server.schema.create('BountyContribution', data)
+
+export const seedBountyContributions = seedOverridableEntities<RawBountyContributionMock>(
+  rawContributions,
+  seedBountyContribution
+)
+
+const seedEntryStatus = ({ type, ...data }: RawBountyEntryMock['status'], server: any) =>
+  server.schema.create(`BountyEntryStatus${type}`, data)
+
+export const seedBountyEntry = ({ status, ...data }: RawBountyEntryMock, server: any) =>
+  server.schema.create('BountyEntry', { ...data, status: seedEntryStatus(status, server) })
+
+export const seedBountyEntries = seedOverridableEntities(rawEntries, seedBountyEntry)
