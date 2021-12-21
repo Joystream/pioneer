bad2ddf5197c0c085d4efe55587d67ba3a30a618
WRadoslaw <r.wyszynski00@gmail.com>
2021-12-21 10:37:43 +0100

    Working Period Details

--- packages/ui/src/bounty/modals/AddBountyModal/components/WorkingDetailsStep.tsx
+++ packages/ui/src/bounty/modals/AddBountyModal/components/WorkingDetailsStep.tsx
@@ -1,5 +1,186 @@
+import BN from 'bn.js'
 import React from 'react'
+import styled from 'styled-components'
 
-export const WorkingDetailsStep = () => {
-  return <div />
+import {
+  FundingPeriodDetailsContext,
+  WorkingPeriodDetailsContext,
+  WorkingPeriodType,
+} from '@/bounty/modals/AddBountyModal/machine'
+import { CloseButton } from '@/common/components/buttons'
+import { InlineToggleWrap, InputComponent, InputNumber, Label, ToggleCheckbox } from '@/common/components/forms'
+import { Row } from '@/common/components/Modal'
+import { RowGapBlock } from '@/common/components/page/PageContent'
+import { Tooltip, TooltipDefault } from '@/common/components/Tooltip'
+import { TextHuge, TextMedium } from '@/common/components/typography'
+import { Colors } from '@/common/constants'
+import { inBlocksDate } from '@/common/model/inBlocksDate'
+import { MemberInfo } from '@/memberships/components'
+import { SelectMember } from '@/memberships/components/SelectMember'
+import { Member } from '@/memberships/types'
+
+interface Props extends Omit<WorkingPeriodDetailsContext, keyof FundingPeriodDetailsContext> {
+  setWorkingPeriodLength: (workingPeriodLength: number) => void
+  setWorkingPeriodWhitelist: (members: Member[]) => void
+  setWorkingPeriodType: (type: WorkingPeriodType) => void
+  setWorkingPeriodStake: (stake: BN) => void
+  setWorkingPeriodStakeAllowance: (allowance: boolean) => void
 }
+
+export const WorkingDetailsStep = ({
+  workingPeriodLength,
+  workingPeriodWhitelist,
+  workingPeriodType,
+  workingPeriodStake,
+  workingPeriodStakeAllowance,
+  setWorkingPeriodLength,
+  setWorkingPeriodStake,
+  setWorkingPeriodStakeAllowance,
+  setWorkingPeriodType,
+  setWorkingPeriodWhitelist,
+}: Props) => {
+  const onMemberAdd = (member: Member) => {
+    setWorkingPeriodWhitelist([...workingPeriodWhitelist, member])
+  }
+
+  const whitelistFilter = (member: Member): boolean =>
+    !workingPeriodWhitelist.some((oldMember) => member.id === oldMember.id)
+
+  const removeMemberFromWhitelist = (member: Member) => () => {
+    setWorkingPeriodWhitelist(workingPeriodWhitelist.filter((mapMember) => mapMember.id !== member.id))
+  }
+
+  return (
+    <RowGapBlock gap={24}>
+      <Row>
+        <TextHuge bold>Working Period Details</TextHuge>
+      </Row>
+
+      <Row>
+        <InlineToggleWrap>
+          <Label>Bounty type:</Label>
+          <ToggleCheckbox
+            checked={workingPeriodType === 'open'}
+            onChange={(isSet) => (isSet ? setWorkingPeriodType('open') : setWorkingPeriodType('closed'))}
+            falseLabel={
+              <CheckBoxLabelWrapper>
+                Closed{' '}
+                <Tooltip tooltipText="Lorem ipsum...">
+                  <TooltipDefault />
+                </Tooltip>
+              </CheckBoxLabelWrapper>
+            }
+            trueLabel={
+              <CheckBoxLabelWrapper>
+                Open
+                <Tooltip tooltipText="Lorem ipsum...">
+                  <TooltipDefault />
+                </Tooltip>
+              </CheckBoxLabelWrapper>
+            }
+          />
+        </InlineToggleWrap>
+      </Row>
+
+      {workingPeriodType === 'closed' && (
+        <RowGapBlock gap={10}>
+          <TextMedium bold>Whitelist</TextMedium>
+          <TextMedium>The upper bound for whitelist is 15.</TextMedium>
+          <InputComponent tooltipText="Lorem ipsum dolor sit amet consectetur, adipisicing elit." inputSize="l">
+            <SelectMember filter={whitelistFilter} onChange={onMemberAdd} />
+          </InputComponent>
+          <WhitelistWrapper>
+            {workingPeriodWhitelist?.map((member) => (
+              <MemberWrapper>
+                <MemberInfo member={member} />
+                <CloseButton onClick={removeMemberFromWhitelist(member)} />
+              </MemberWrapper>
+            ))}
+          </WhitelistWrapper>
+        </RowGapBlock>
+      )}
+
+      <Row>
+        <InputComponent
+          label="Working period length"
+          required
+          units="blocks"
+          inputSize="s"
+          tight
+          message={workingPeriodLength ? `≈ ${inBlocksDate(new BN(workingPeriodLength))}` : ''}
+        >
+          <InputNumber
+            placeholder="0"
+            id="periodLength"
+            value={workingPeriodLength?.toString()}
+            onChange={(event) => setWorkingPeriodLength(Number(event.target.value))}
+          />
+        </InputComponent>
+      </Row>
+
+      <Row>
+        <InlineToggleWrap>
+          <Label>Stake </Label>
+          <ToggleCheckbox
+            checked={workingPeriodStakeAllowance}
+            onChange={(isSet) => setWorkingPeriodStakeAllowance(isSet)}
+            trueLabel={
+              <CheckBoxLabelWrapper>
+                Yes{' '}
+                <Tooltip tooltipText="Lorem ipsum...">
+                  <TooltipDefault />
+                </Tooltip>
+              </CheckBoxLabelWrapper>
+            }
+            falseLabel={
+              <CheckBoxLabelWrapper>
+                No{' '}
+                <Tooltip tooltipText="Lorem ipsum...">
+                  <TooltipDefault />
+                </Tooltip>
+              </CheckBoxLabelWrapper>
+            }
+          />
+        </InlineToggleWrap>
+      </Row>
+
+      {workingPeriodStakeAllowance && (
+        <Row>
+          <InputComponent units="JOY" inputSize="s" tight>
+            <InputNumber
+              id="periodStake"
+              value={workingPeriodStake?.toString()}
+              placeholder="0"
+              onChange={(event) => setWorkingPeriodStake(new BN(event.target.value))}
+            />
+          </InputComponent>
+        </Row>
+      )}
+    </RowGapBlock>
+  )
+}
+
+const CheckBoxLabelWrapper = styled.div`
+  display: flex;
+  column-gap: 4px;
+  align-items: center;
+`
+
+const WhitelistWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 15px;
+  row-gap: 10px;
+`
+
+const MemberWrapper = styled.div`
+  display: flex;
+  column-gap: 5px;
+  height: min-content;
+  padding: 10px;
+  max-width: 180px;
+  min-width: 180px;
+  align-items: center;
+  border: 1px solid ${Colors.Black[300]};
+  background-color: ${Colors.White};
+`
