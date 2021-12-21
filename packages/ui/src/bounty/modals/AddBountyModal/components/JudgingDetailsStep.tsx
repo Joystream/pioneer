679b075ec0fde93f2039f3ff702ea27a7fc07ff9
WRadoslaw <r.wyszynski00@gmail.com>
2021-12-21 10:37:59 +0100

    Judging details

--- packages/ui/src/bounty/modals/AddBountyModal/components/JudgingDetailsStep.tsx
+++ packages/ui/src/bounty/modals/AddBountyModal/components/JudgingDetailsStep.tsx
@@ -1,5 +1,54 @@
-import React from 'react'
+import BN from 'bn.js'
+import React, { useCallback } from 'react'
 
-export const JudgingDetailsStep = () => {
-  return <div />
+import { JudgingPeriodDetailsContext, WorkingPeriodDetailsContext } from '@/bounty/modals/AddBountyModal/machine'
+import { InputComponent, InputNumber } from '@/common/components/forms'
+import { Row } from '@/common/components/Modal'
+import { RowGapBlock } from '@/common/components/page/PageContent'
+import { TextHuge } from '@/common/components/typography'
+import { inBlocksDate } from '@/common/model/inBlocksDate'
+import { SelectMember } from '@/memberships/components/SelectMember'
+import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
+import { Member } from '@/memberships/types'
+
+interface Props extends Omit<JudgingPeriodDetailsContext, keyof WorkingPeriodDetailsContext> {
+  setJudgingPeriodLength: (length: number) => void
+  setOracle: (oracle: Member) => void
+}
+
+export const JudgingDetailsStep = ({ judgingPeriodLength, oracle, setOracle, setJudgingPeriodLength }: Props) => {
+  const { active } = useMyMemberships()
+
+  const oracleFilter = useCallback((member) => member.id !== active?.id, [active])
+
+  return (
+    <RowGapBlock gap={24}>
+      <Row>
+        <TextHuge bold>Judging Period Details</TextHuge>
+      </Row>
+
+      <Row>
+        <InputComponent
+          label="Judging period length"
+          required
+          inputSize="s"
+          tight
+          units="blocks"
+          message={judgingPeriodLength ? `≈ ${inBlocksDate(new BN(judgingPeriodLength))}` : ''}
+        >
+          <InputNumber
+            placeholder="0"
+            onChange={(event) => setJudgingPeriodLength(Number(event.target.value))}
+            value={judgingPeriodLength?.toString()}
+          />
+        </InputComponent>
+      </Row>
+
+      <Row>
+        <InputComponent label="Oracle" required inputSize="l">
+          <SelectMember onChange={(newOracle) => setOracle(newOracle)} selected={oracle} filter={oracleFilter} />
+        </InputComponent>
+      </Row>
+    </RowGapBlock>
+  )
 }
