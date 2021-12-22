b1c2138591c198c6bf368650991fb8c5c51c3705
RafalPloszka <rafalploszka@live.com>
2021-12-22 10:49:31 +0100

    Add test for BountyActorsList

---
+++ packages/ui/test/bounty/components/BountyActorsList.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import BN from 'bn.js'
+
+import { BountyActorsList } from '@/bounty/components/BountyActorsList/BountyActorsList'
+import { getMember } from '../../_mocks/members'
+
+describe('BountyActorsList', () => {
+  it('should display proper title and amount for contributors', () => {
+    const title = 'CONTRIBUTORS'
+    const contributorsList = [
+      { actor: getMember('alice'), amount: new BN(1000) },
+    ]
+    render(<BountyActorsList title={title} elements={contributorsList} />)
+    expect(screen.queryByText('CONTRIBUTORS')).toBeDefined()
+    expect(screen.queryByText('1,000 JOY')).toBeDefined()
+  })
+  it('should display proper title and count for entrants', () => {
+    const title = 'ENTRANTS'
+    const entrantsList = [
+      { actor: getMember('alice'), count: 3 },
+    ]
+    render(<BountyActorsList title={title} elements={entrantsList} />)
+    expect(screen.queryByText('ENTRANTS')).toBeDefined()
+    expect(screen.queryByText('3')).toBeDefined()
+  })
+  it('should display proper title and label for withdrawn', () => {
+    const title = 'WITHDRAWN'
+    const withdrawnList = [
+      { actor: getMember('alice') },
+    ]
+    render(<BountyActorsList title={title} elements={withdrawnList} />)
+    expect(screen.queryByText('WITHDRAWN')).toBeDefined()
+    expect(screen.queryByText('alice')).toBeDefined()
+  })
+})
