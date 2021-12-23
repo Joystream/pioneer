7f30a6def72721cfe96e4130c97f80c544b8e45d
RafalPloszka <rafalploszka@live.com>
2021-12-23 15:12:37 +0100

    Add hook for getting bounties

---
+++ packages/ui/src/bounty/hooks/useBounties.ts
@@ -0,0 +1,27 @@
+import { BountyOrderByInput } from '@/common/api/queries'
+import { usePagination } from '@/common/hooks/usePagination'
+import { SortOrder, toQueryOrderByInput } from '@/common/hooks/useSort'
+import { useGetBountiesCountQuery, useGetBountiesQuery } from '@/bounty/queries'
+import { asBounty } from '../types/casts'
+
+interface UseBountiesProps {
+  order: SortOrder<BountyOrderByInput>
+  perPage?: number
+}
+
+export const useBounties = ({ order, perPage = 10 }: UseBountiesProps) => {
+  const orderBy = toQueryOrderByInput<BountyOrderByInput>(order)
+  const { data: dataCount } = useGetBountiesCountQuery()
+  const totalCountPerPage = dataCount?.bountiesConnection.totalCount
+  const { offset, pagination } = usePagination(perPage, totalCountPerPage ?? 0, [order])
+
+  const { loading, data } = useGetBountiesQuery({
+    variables: { orderBy: orderBy ?? [], limit: perPage, offset: offset },
+  })
+
+  return {
+    isLoading: loading,
+    bounties: data?.bounties.map(asBounty) ?? [],
+    pagination: pagination,
+  }
+}
