1d54a4516b2221d3af10ce1967c8fc4b219818d5
RafalPloszka <rafalploszka@live.com>
2021-12-31 11:04:42 +0100

    Fix BountyListItem test

--- packages/ui/test/bounty/components/BountyListItem.test.tsx
+++ packages/ui/test/bounty/components/BountyListItem.test.tsx
@@ -1,74 +1,98 @@
-// import { render, screen } from '@testing-library/react'
-// import React from 'react'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import BN from 'bn.js'
 
-// import { BountyListItem } from '@/bounty/components/BountyListItem/BountyListItem'
-// import { BountyPeriod } from '@/bounty/types/Bounty'
-// import { seedMembers } from '@/mocks/data'
+import { BountyListItem } from '@/bounty/components/BountyListItem/BountyListItem'
+import { BountyStage } from '@/bounty/types/Bounty'
+import { seedMembers } from '@/mocks/data'
 
-// import { MockApolloProvider } from '../../_mocks/providers'
-// import { setupMockServer } from '../../_mocks/server'
+import { MockApolloProvider } from '../../_mocks/providers'
+import { setupMockServer } from '../../_mocks/server'
+import { getMember } from '../../_mocks/members'
 
-// describe('UI: BountyListItem', () => {
-//   const server = setupMockServer()
-//   const props = {
-//     title: 'Title',
-//     creator: 'Apetor',
-//     date: new Date(),
-//     imageUrl: 'test/img',
-//   }
+describe('UI: BountyListItem', () => {
+  const server = setupMockServer()
+  const props = {
+    id: '1',
+    createdAt: '2021-12-31',
+    title: 'Title',
+    cherry: new BN(1010),
+    entrantStake: new BN(10000),
+    creator: getMember('alice'),
+    oracle: getMember('bob'),
+    fundingType: {
+      minAmount: new BN(10000),
+      maxAmount: new BN(12000),
+      maxPeriod: new BN(2000),
+    },
+    workPeriod: new BN(1000),
+    judgingPeriod: new BN(1000),
+    totalFunding: new BN(2000),
+    entries: [
+      { worker: getMember('alice') },
+      { worker: getMember('bob') },
+      { worker: getMember('alice') },
+      { worker: getMember('bob'), winner: true },
+    ],
+  }
 
-//   beforeAll(() => {
-//     seedMembers(server.server, 4)
-//   })
+  beforeAll(() => {
+    seedMembers(server.server, 4)
+  })
 
-//   it('Renders props', () => {
-//     renderItem('funding')
+  it('Renders props', () => {
+    renderItem('funding')
 
-//     checkTypeLayout([props.title, props.creator])
-//     expect(screen.getByRole('img')).toHaveAttribute('src', props.imageUrl)
-//   })
+    checkTypeLayout([props.title, props.creator.handle])
+  })
 
-//   it('Period: Funding', async () => {
-//     renderItem('funding')
+  it('Period: Funding', async () => {
+    renderItem('funding')
 
-//     checkTypeLayout(['FUNDING PERIOD', 'Funded', 'Maximal range', 'Minimum range', 'Cherry'])
-//   })
+    checkTypeLayout(['FUNDING PERIOD', 'Funded', 'Maximal range', 'Minimum range', 'Cherry'])
+  })
 
-//   it('Period: Working', async () => {
-//     renderItem('working')
+  it('Period: Working', async () => {
+    renderItem('workSubmission')
 
-//     checkTypeLayout(['WORKING PERIOD', 'Bounty', 'Entries', 'Submitted work', 'Stake'])
-//   })
+    checkTypeLayout(['WORKING PERIOD', 'Bounty', 'Entries', 'Submitted work', 'Stake'])
+  })
 
-//   it('Period: Judgment', async () => {
-//     renderItem('judgement')
+  it('Period: Judgment', async () => {
+    renderItem('judgment')
 
-//     checkTypeLayout(['JUDGEMENT PERIOD', 'Entries', 'Submitted work', 'Withdrawn work'])
-//   })
+    checkTypeLayout(['JUDGEMENT PERIOD', 'Entries', 'Submitted work', 'Withdrawn work'])
+  })
 
-//   it('Period: Withdrawal', async () => {
-//     renderItem('withdrawal')
+  it('Period: Withdrawal after success', async () => {
+    renderItem('successful')
 
-//     checkTypeLayout(['WITHDRAWAL PERIOD', 'Entries', 'Unwithdrawn funds'])
-//   })
+    checkTypeLayout(['WITHDRAWAL PERIOD', 'Entries', 'Unwithdrawn funds'])
+  })
 
-//   it('Period: Expired', async () => {
-//     renderItem('expired')
+  it('Period: Withdrawal after failure', async () => {
+    renderItem('failed')
 
-//     checkTypeLayout(['EXPIRED FUNDING PERIOD', 'Winners', 'Entries', 'Unwithdrawn funds'])
-//     expect(screen.queryByText('DURATION')).toBeNull()
-//   })
+    checkTypeLayout(['WITHDRAWAL PERIOD', 'Entries', 'Unwithdrawn funds'])
+  })
 
-//   const checkTypeLayout = (titles: string[]) => {
-//     for (const title of titles) {
-//       expect(screen.queryByText(title)).toBeDefined()
-//     }
-//   }
+  it('Period: Expired', async () => {
+    renderItem('expired')
 
-//   const renderItem = (type: BountyPeriod) =>
-//     render(
-//       <MockApolloProvider>
-//         <BountyListItem period={type} {...props} />
-//       </MockApolloProvider>
-//     )
-// })
+    checkTypeLayout(['EXPIRED FUNDING PERIOD', 'Winners', 'Entries', 'Unwithdrawn funds'])
+    expect(screen.queryByText('DURATION')).toBeNull()
+  })
+
+  const checkTypeLayout = (titles: string[]) => {
+    for (const title of titles) {
+      expect(screen.queryByText(title)).toBeDefined()
+    }
+  }
+
+  const renderItem = (type: BountyStage) =>
+    render(
+      <MockApolloProvider>
+        <BountyListItem stage={type} {...props} />
+      </MockApolloProvider>
+    )
+})
