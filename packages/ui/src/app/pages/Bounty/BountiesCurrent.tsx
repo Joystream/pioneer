502bb49c3a80bb79cf6ac81638d6a631255e307d
Qcza <qcza12@gmail.com>
2022-01-03 10:23:22 +0100

    Add useBounty hook

--- packages/ui/src/app/pages/Bounty/BountiesCurrent.tsx
+++ packages/ui/src/app/pages/Bounty/BountiesCurrent.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { BountiesLayout } from '@/app/pages/Bounty/components/BountiesLayout'
 import { BountiesMain } from '@/app/pages/Bounty/components/BountiesMain'
+import { useBounty } from '@/bounty/hooks/useBounty';
 
 export const BountiesCurrent = () => {
+  const { bounty } = useBounty('1');
+
+  useEffect(() => {
+    console.log(bounty);
+  }, [bounty])
+
   return (
     <BountiesLayout>
       <BountiesMain />
