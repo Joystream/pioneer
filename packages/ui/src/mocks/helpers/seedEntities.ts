aff751b00d0905458385f5b04e3c86b2332e9e95
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-01-11 19:25:39 +0100

    🤿 Fix helpers to seed bounties in tests (#2076)
    * Remove undefined values when seeding entities
    
    * Fix helpers to seed bounties in tests

--- packages/ui/src/mocks/helpers/seedEntities.ts
+++ packages/ui/src/mocks/helpers/seedEntities.ts
@@ -1,6 +1,9 @@
+import { definedValues } from '../../common/utils'
+
 export const seedOverridableEntities =
   <T>(rawEntities: T[], seedEntity: (server: any, entity: T) => any) =>
   (server: any, overrides?: Partial<T>[]): void => {
-    const entities = overrides?.map((override, index) => ({ ...rawEntities[index], ...override })) ?? rawEntities
+    const entities =
+      overrides?.map((override, index) => definedValues({ ...rawEntities[index], ...override })) ?? rawEntities
     entities.forEach((entity) => seedEntity(entity, server))
   }
