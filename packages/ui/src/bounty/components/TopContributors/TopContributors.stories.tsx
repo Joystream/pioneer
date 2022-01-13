68799cf06307527e8065af8c8b19c6df3c527745
RafalPloszka <rafalploszka@live.com>
2022-01-13 09:35:00 +0100

    Fix story

--- packages/ui/src/bounty/components/TopContributors/TopContributors.stories.tsx
+++ packages/ui/src/bounty/components/TopContributors/TopContributors.stories.tsx
@@ -3,17 +3,18 @@ import React from 'react'
 
 import { TopContributors } from '@/bounty/components/TopContributors/TopContributors'
 import members from '@/mocks/data/raw/members.json'
+import BN from 'bn.js'
 
 export default {
   title: 'Bounty/TopContributors',
   component: TopContributors,
 } as Meta
 
-const Template: Story = (args) => <TopContributors contributors={[]} {...args} />
+const Template: Story = (args) => <TopContributors contributions={args.contributors} />
 
 export const Default = Template.bind({})
 Default.args = {
-  contributors: members.slice(0, 20),
+  contributors: members.slice(0, 20).map((member) => ({ contributor: member, amount: new BN(10000) })),
 }
 
 export const NoContributors = Template.bind({})
