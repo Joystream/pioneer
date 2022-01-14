dbc3f500a12cdda4969a8cf3137171a5fe138f1c
WRadoslaw <r.wyszynski00@gmail.com>
2022-01-14 11:14:53 +0100

    Filters on tabs

--- packages/ui/src/app/pages/Bounty/BountiesMyContributions.tsx
+++ packages/ui/src/app/pages/Bounty/BountiesMyContributions.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 import { BountiesLayout } from '@/app/pages/Bounty/components/BountiesLayout'
+import { QueryExtraFilter } from '@/bounty/hooks/useBounties'
+import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 
 export const BountiesMyContributions = () => {
-  return <BountiesLayout />
+  const { active } = useMyMemberships()
+
+  const extraFilter: QueryExtraFilter<string> = {
+    path: 'contributions_some.worker.id_eq',
+    value: active?.id || '',
+  }
+
+  return <BountiesLayout extraFilter={extraFilter} />
 }
