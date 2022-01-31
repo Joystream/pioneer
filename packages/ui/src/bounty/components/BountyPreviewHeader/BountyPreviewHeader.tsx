3a6fc22c887b5de264533e26686052ff790a45b1
WRadoslaw <r.wyszynski00@gmail.com>
2022-01-31 13:25:45 +0100

    Button implementation

--- packages/ui/src/bounty/components/BountyPreviewHeader/BountyPreviewHeader.tsx
+++ packages/ui/src/bounty/components/BountyPreviewHeader/BountyPreviewHeader.tsx
@@ -11,11 +11,12 @@ import {
   SubmitWorkButton,
   WithdrawStakeButton,
   WithdrawWorkEntryButton,
+  SubmitJudgementButton,
 } from '@/bounty/components/modalsButtons'
 import { Bounty, isBountyEntryStatusWinner, isFundingLimited, WorkEntry } from '@/bounty/types/Bounty'
 import { BadgesRow } from '@/common/components/BadgeStatus/BadgesRow'
 import { BadgeStatus } from '@/common/components/BadgeStatus/BadgeStatus'
-import { ButtonGhost, ButtonPrimary } from '@/common/components/buttons'
+import { ButtonGhost } from '@/common/components/buttons'
 import { BellIcon } from '@/common/components/icons/BellIcon'
 import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 import { Member } from '@/memberships/types'
@@ -52,7 +53,7 @@ export const BountyPreviewHeader = React.memo(({ bounty, badgeNames }: Props) =>
       default:
         return null
     }
-  }, [bounty])
+  }, [bounty, activeMember])
 
   const badges = useMemo(
     () => (
@@ -132,8 +133,7 @@ const JudgingStageButtons = React.memo(({ bounty, activeMember, t }: BountyHeade
       <ButtonGhost size="large">
         <BellIcon /> {t('common:buttons.notifyAboutChanges')}
       </ButtonGhost>
-      {/* TODO: https://github.com/Joystream/pioneer/issues/1938 */}
-      {isOracle && <ButtonPrimary size="large">{t('buttons.submitJudgement')}</ButtonPrimary>}
+      {isOracle && <SubmitJudgementButton bounty={bounty} />}
     </>
   )
 })
