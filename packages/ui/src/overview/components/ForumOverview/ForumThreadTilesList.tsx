abf5ef667b359698b7e0b4ee3d7d2ba0cd6b6dfd
RafalPloszka <rafalploszka@live.com>
2022-02-01 10:45:56 +0100

    ThreadTilesList style fixes

--- packages/ui/src/overview/components/ForumOverview/ForumThreadTilesList.tsx
+++ packages/ui/src/overview/components/ForumOverview/ForumThreadTilesList.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
-import { TextBig, TextSmall } from '@/common/components/typography'
-import { BorderRad, Shadows } from '@/common/constants'
 import { CountBadge } from '@/common/components/CountBadge'
-import { ForumThread } from '@/forum/types'
 import { HorizontalScroller } from '@/common/components/HorizontalScroller/HorizontalScroller'
+import { AnswerIcon } from '@/common/components/icons/AnswerIcon'
 import { Loading } from '@/common/components/Loading'
+import { Badge, TextBig, TextExtraSmall } from '@/common/components/typography'
+import { BorderRad, Colors, Shadows, Transitions } from '@/common/constants'
+import { ForumThread } from '@/forum/types'
 
 interface TileProps {
   title: string
@@ -14,12 +16,20 @@ interface TileProps {
 }
 
 const ForumThreadTile = React.memo(({ title, answersCount }: TileProps) => {
+  const { t } = useTranslation('overview')
   return (
     <Wrapper>
-      <Label bold black value>
+      <Title bold black value>
         {title}
-      </Label>
-      <CountBadge count={answersCount} />
+      </Title>
+      <LabelsWrapper>
+        <NewBadge>{t('forum.new')}</NewBadge>
+        <StyledAnswerIcon />
+        <AnswersLabel bold lighter value>
+          {t('forum.answers')}
+        </AnswersLabel>
+        <CountBadge count={answersCount} />
+      </LabelsWrapper>
     </Wrapper>
   )
 })
@@ -29,21 +39,46 @@ interface ListProps {
 }
 
 export const ForumThreadTilesList = React.memo(({ threads }: ListProps) => {
-  const tiles = threads.map((thread) => <ForumThreadTile title={thread.title} answersCount={thread.visiblePostsCount} />)
-  return (!tiles ? <Loading /> : <Scroller items={tiles} />)
+  const tiles = threads.map((thread) => (
+    <ForumThreadTile title={thread.title} answersCount={thread.visiblePostsCount} />
+  ))
+  return !tiles ? <Loading /> : <Scroller items={tiles} />
 })
 
 const Wrapper = styled.div`
   width: 215px;
-  height: 145px;
-  padding: 16px;
+  height: 115px;
+  padding: 24px 16px;
   border-radius: ${BorderRad.m};
   box-shadow: ${Shadows.light};
 `
 
-const Label = styled(TextBig)`
-  margin-top: 20px;
+const Title = styled(TextBig)`
+  max-width: 100%;
   white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  transition: ${Transitions.all};
+`
+
+const LabelsWrapper = styled.div`
+  display: flex;
+  margin-top: 20px;
+`
+
+const NewBadge = styled(Badge)`
+  background-color: ${Colors.Blue[200]};
+  text-transform: uppercase;
+  margin-right: 16px;
+`
+
+const StyledAnswerIcon = styled(AnswerIcon)`
+  color: ${Colors.Black[300]};
+`
+
+const AnswersLabel = styled(TextExtraSmall)`
+  text-transform: uppercase;
+  margin: 0 8px;
 `
 
 const Scroller = styled(HorizontalScroller)`
