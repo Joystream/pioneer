0851f0200bf1e38a47201ac7dd8bf82b3a734f87
WRadoslaw <r.wyszynski00@gmail.com>
2022-02-02 13:04:58 +0100

    Working groups icons

---
+++ packages/ui/src/common/components/icons/workingGroup/Forum.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Icon } from '../Icon'
+
+export const Forum = () => (
+  <Icon size="108" viewBox="0 0 108 108" fill="none" color="currentColor">
+    <rect x="0.5" y="0.5" width="107" height="107" rx="1.5" fill="#F9FAFC" stroke="#E8EDF6" />
+    <path d="M44 53.9256V29H60H88.021V53.9256H85L75.8506 59.5V53.9256H44Z" fill="#5D6B80" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M53 42H73V44H53V42Z" fill="white" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M53 38H80V40H53V38Z" fill="white" />
+    <path d="M64.021 73.595V49H48.021H20V73.595H23.021L32.1704 79.1695V73.595H64.021Z" fill="#3F38FF" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M44 49V53.9256H64.021V49H48.021H44Z" fill="black" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M28.021 62H48.021V64H28.021V62Z" fill="white" />
+  </Icon>
+)
---
+++ packages/ui/src/common/components/icons/workingGroup/Storage.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { Icon } from '../Icon'
+
+export const Storage = () => (
+  <Icon size="108" viewBox="0 0 108 108" preserveAspectRatio="xMidYMid meet" fill="none" color="currentColor">
+    <rect x="0.5" y="0.5" width="107" height="107" rx="1.5" fill="#F9FAFC" stroke="#E8EDF6" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M53 37H73V39H53V37Z" fill="white" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M53 33H80V35H53V33Z" fill="white" />
+    <path d="M64 68V44H44.0131H24V68H24.2129H27.7738H64Z" fill="#3F38FF" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M43 77V66.5H45V77H43Z" fill="#3F38FF" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M66 76L66 48L68 48L68 76L66 76Z" fill="#5D6B80" />
+    <path d="M87 49V25H67.0131H47V49H47.2129H50.7738H87Z" fill="#5D6B80" />
+    <circle cx="74" cy="37" r="2" fill="white" />
+    <circle cx="80" cy="37" r="2" fill="white" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M47 44V49H64V44H50.4143H47Z" fill="black" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M32 53H28V55H32V53ZM32 57H28V59H32V57ZM38 57H34V59H38V57ZM40 57H44V59H40V57ZM34 53H38V55H34V53ZM44 53H40V55H44V53Z"
+      fill="white"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M55 34H51V36H55V34ZM55 38H51V40H55V38ZM61 38H57V40H61V38ZM63 38H67V40H63V38ZM57 34H61V36H57V34ZM67 34H63V36H67V34Z"
+      fill="white"
+    />
+    <circle cx="51" cy="56" r="2" fill="white" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M90 78H19V76H90V78Z" fill="black" />
+    <circle cx="44" cy="77" r="2" fill="#3F38FF" />
+    <circle cx="67" cy="77" r="2" fill="#5D6B80" />
+    <circle cx="57" cy="56" r="2" fill="white" />
+  </Icon>
+)
