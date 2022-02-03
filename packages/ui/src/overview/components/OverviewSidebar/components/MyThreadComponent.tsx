0d912a0919e47b9ef551318975d575f63259a3b1
WRadoslaw <r.wyszynski00@gmail.com>
2022-02-03 12:59:15 +0100

    MyThreadTile

---
+++ packages/ui/src/overview/components/OverviewSidebar/components/MyThreadComponent.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { BadgeStatus } from '@/common/components/BadgeStatus'
+import { Counter } from '@/common/components/HorizontalScroller/HorizontalScroller'
+import { ColumnGapBlock } from '@/common/components/page/PageContent'
+import { ForumIcon } from '@/common/components/page/Sidebar/LinksIcons'
+import { TextBig, TextSmall } from '@/common/components/typography'
+import { Colors, Shadows } from '@/common/constants'
+import { OverviewSidebarThread } from '@/overview/types/Overview'
+
+export const MyThreadComponent = ({ title, numberOfPosts }: OverviewSidebarThread) => {
+  return (
+    <Tile>
+      <TextBig bold value truncate>
+        {title}
+      </TextBig>
+      <StyledColumnBlock gap={5}>
+        <BadgeStatus>NEW</BadgeStatus>
+        <ForumIcon />
+        <TextSmall light>ANSWERS</TextSmall>
+        <Counter>{numberOfPosts}</Counter>
+      </StyledColumnBlock>
+    </Tile>
+  )
+}
+
+const Tile = styled.div`
+  min-width: 216px;
+  max-width: 216px;
+  padding: 20px;
+  height: 114px;
+  box-shadow: ${Shadows.light};
+  border-radius: 4px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const StyledColumnBlock = styled(ColumnGapBlock)`
+  align-items: center;
+  > *:first-child {
+    margin-right: 5px;
+  }
+
+  svg {
+    color: ${Colors.Black[400]};
+    width: 15px;
+  }
+`
