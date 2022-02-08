49382ca4da1eb260cfec575bcfeda7b9f33678b5
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-02-08 14:23:10 +0100

    Update hard coded endpoints

--- packages/ui/src/app/config/network.ts
+++ packages/ui/src/app/config/network.ts
@@ -1,12 +1,13 @@
 export type NetworkType = 'local' | 'local-mocks' | 'olympia-testnet'
 
 const OLYMPIA_TESTNET_NODE_SOCKET =
-  process.env.REACT_APP_OLYMPIA_TESTNET_NODE_SOCKET || 'wss://olympia-dev.joystream.app/rpc'
+  process.env.REACT_APP_OLYMPIA_TESTNET_NODE_SOCKET || 'wss://54.210.109.171.nip.io/ws-rpc'
 const OLYMPIA_TESTNET_QUERY_NODE =
-  process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE || 'https://olympia-dev.joystream.app/query/server/graphql'
+  process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE || 'https://54.210.109.171.nip.io/query-node/server/graphql'
 const OLYMPIA_TESTNET_QUERY_NODE_SOCKET =
-  process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE_SOCKET || 'wss://olympia-dev.joystream.app/query/server/graphql'
-const MEMBERSHIP_FAUCET_URL = process.env.REACT_APP_MEMBERSHIP_FAUCET_URL || 'http://localhost:4000/register'
+  process.env.REACT_APP_OLYMPIA_TESTNET_QUERY_NODE_SOCKET || 'wss://54.210.109.171.nip.io/query-node/server/graphql'
+const MEMBERSHIP_FAUCET_URL =
+  process.env.REACT_APP_MEMBERSHIP_FAUCET_URL || 'https://54.210.109.171.nip.io/member-faucet/register'
 
 export const QUERY_NODE_ENDPOINT_SUBSCRIPTION: Record<NetworkType, string> = {
   local: 'ws://localhost:8081/graphql',
