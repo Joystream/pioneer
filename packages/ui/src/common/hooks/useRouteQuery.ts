e09a5860c6572079fdacf534f8b1db1c325dd0da
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-02-11 18:23:24 +0100

    Fix the `useRouterQuery` hook

--- packages/ui/src/common/hooks/useRouteQuery.ts
+++ packages/ui/src/common/hooks/useRouteQuery.ts
@@ -1,5 +1,7 @@
+import { useMemo } from 'react'
 import { useLocation } from 'react-router'
 
 export const useRouteQuery = () => {
-  return new URLSearchParams(useLocation().search)
+  const { search } = useLocation()
+  return useMemo(() => new URLSearchParams(search), [search])
 }
