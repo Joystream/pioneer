deae286b21e9c3174a2466859bee947477ce0399
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-02-18 09:50:00 +0100

    🪲 Update `@joystream/types` (#2247)

--- packages/types/augment/all/types.ts
+++ packages/types/augment/all/types.ts
@@ -240,12 +240,6 @@ export interface ChannelCreationParameters extends Struct {
 /** @name ChannelId */
 export interface ChannelId extends u64 {}
 
-/** @name ChannelMigrationConfig */
-export interface ChannelMigrationConfig extends Struct {
-  readonly current_id: ChannelId;
-  readonly final_id: ChannelId;
-}
-
 /** @name ChannelOwner */
 export interface ChannelOwner extends Enum {
   readonly isMember: boolean;
@@ -578,9 +572,6 @@ export interface Membership extends Struct {
   readonly verified: bool;
   readonly invites: u32;
 }
-
-/** @name MemoText */
-export interface MemoText extends Text {}
 
 /** @name ModeratorId */
 export interface ModeratorId extends u64 {}
@@ -1106,12 +1097,6 @@ export interface VideoCreationParameters extends Struct {
 
 /** @name VideoId */
 export interface VideoId extends u64 {}
-
-/** @name VideoMigrationConfig */
-export interface VideoMigrationConfig extends Struct {
-  readonly current_id: VideoId;
-  readonly final_id: VideoId;
-}
 
 /** @name VideoPost */
 export interface VideoPost extends Struct {
