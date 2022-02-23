42018dcaa4cf5b8b3a307b158474d92735208e90
tomekstepien <tomasz.stepien@4soft.co>
2022-02-23 12:38:15 +0100

    tutorial movies hidden

--- packages/ui/src/app/components/PageHeaderWithHint.tsx
+++ packages/ui/src/app/components/PageHeaderWithHint.tsx
@@ -13,50 +13,58 @@ interface PageHeaderWithHintProps extends PageHeaderProps {
 }
 
 export const PageHeaderWithHint = ({ title, hintType, buttons, tabs }: PageHeaderWithHintProps) => {
-  const [hintFinished, setHintFinished] = useLocalStorage<boolean>(hintType + '-hint')
-  const [experiencedCloseTooltip, setExperiencedCloseTooltip] = useLocalStorage<boolean>('experience-close-tooltip')
-  const [showCloseTooltip, toggleCloseTooltip] = useToggle(false)
-  const [isHintVisible, toggleHint] = useToggle(!hintFinished)
+  //TODO has to be uncomment when movies will be ready
 
-  useEffect(() => {
-    if (!isHintVisible && !hintFinished) {
-      setHintFinished(true)
-    }
-  }, [isHintVisible])
+  // const [hintFinished, setHintFinished] = useLocalStorage<boolean>(hintType + '-hint')
+  // const [experiencedCloseTooltip, setExperiencedCloseTooltip] = useLocalStorage<boolean>('experience-close-tooltip')
+  // const [showCloseTooltip, toggleCloseTooltip] = useToggle(false)
+  // const [isHintVisible, toggleHint] = useToggle(!hintFinished)
 
-  const closeHint = useCallback(() => {
-    toggleHint()
+  // useEffect(() => {
+  //   if (!isHintVisible && !hintFinished) {
+  //     setHintFinished(true)
+  //   }
+  // }, [isHintVisible])
 
-    if (!experiencedCloseTooltip) {
-      toggleCloseTooltip()
-      setExperiencedCloseTooltip(true)
-    }
-  }, [experiencedCloseTooltip])
+  // const closeHint = useCallback(() => {
+  //   toggleHint()
+  //
+  //   if (!experiencedCloseTooltip) {
+  //     toggleCloseTooltip()
+  //     setExperiencedCloseTooltip(true)
+  //   }
+  // }, [experiencedCloseTooltip])
 
-  const compiledButtons = useMemo(
-    () => (
-      <>
-        {buttons}
-        <HintButton
-          isActive={isHintVisible}
-          onClick={toggleHint}
-          tooltip={showCloseTooltip ? 'Click here to see video again' : undefined}
-          onTooltipClose={() => toggleCloseTooltip()}
-          isOnTop={showCloseTooltip}
-        />
-      </>
-    ),
-    [isHintVisible, showCloseTooltip]
+  // const compiledButtons = useMemo(
+  //   () => (
+  //     <>
+  //       {buttons}
+  //       <HintButton
+  //         isActive={isHintVisible}
+  //         onClick={toggleHint}
+  //         tooltip={showCloseTooltip ? 'Click here to see video again' : undefined}
+  //         onTooltipClose={() => toggleCloseTooltip()}
+  //         isOnTop={showCloseTooltip}
+  //       />
+  //     </>
+  //   ),
+  //   [isHintVisible, showCloseTooltip]
+  // )
+  //
+  // const video = useMemo(
+  //   () => (
+  //     <AnimatePresence>
+  //       {isHintVisible && <VideoHint videoURI={videoHints[hintType]} onClose={closeHint} />}
+  //     </AnimatePresence>
+  //   ),
+  //   [hintType, closeHint, isHintVisible]
+  // )
+
+  return (
+    <PageHeader
+      title={title}
+      tabs={tabs}
+      // buttons={compiledButtons} video={video}
+    />
   )
-
-  const video = useMemo(
-    () => (
-      <AnimatePresence>
-        {isHintVisible && <VideoHint videoURI={videoHints[hintType]} onClose={closeHint} />}
-      </AnimatePresence>
-    ),
-    [hintType, closeHint, isHintVisible]
-  )
-
-  return <PageHeader title={title} tabs={tabs} buttons={compiledButtons} video={video} />
 }
