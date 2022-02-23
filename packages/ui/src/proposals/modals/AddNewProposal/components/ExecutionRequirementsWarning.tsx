99f62278acf328f6766ad51cf4f36cde290617ea
WRadoslaw <r.wyszynski00@gmail.com>
2022-02-23 12:55:20 +0100

    Message refactor

--- packages/ui/src/proposals/modals/AddNewProposal/components/ExecutionRequirementsWarning.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/components/ExecutionRequirementsWarning.tsx
@@ -1,44 +1,23 @@
-import React, { useEffect, useMemo } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { State, Typestate } from 'xstate'
 
 import { AlertSymbol } from '@/common/components/icons/symbols'
 import { TextMedium } from '@/common/components/typography'
 import { Colors } from '@/common/constants'
-import { AddNewProposalContext, AddNewProposalEvent } from '@/proposals/modals/AddNewProposal/machine'
 
-interface Props {
-  state: State<AddNewProposalContext, AddNewProposalEvent, any, Typestate<AddNewProposalContext>>
-  setValid: (state: boolean) => void
-}
-
-// They `state.matches` fn couldn't accept `as const` for `specificParameters` values and `any` was only solution I saw
-export const proposalsWithExecutionRequirements: any[] = ['specificParameters.fundingRequest']
-
-export const ExecutionRequirementsWarning = ({ state, setValid }: Props) => {
-  const children = useMemo(() => {
-    switch (true) {
-      case state.matches('specificParameters.fundingRequest'):
-        return (
-          <TextMedium>
-            The council budget at the time of proposal execution cannot be lower than the sum of amounts
-          </TextMedium>
-        )
-      default:
-        return null
-    }
-  }, [state.value])
-
-  useEffect(() => {
-    setValid(!children)
-  }, [children, state.value])
-
-  if (!children) {
-    return null
-  }
-
-  return <WarningContainer children={children} />
-}
+export const ExecutionRequirementsWarning = React.memo(() => {
+  return (
+    <Container>
+      <TextMedium bold>
+        <AlertSymbol /> Warning
+      </TextMedium>
+      <TextMedium>
+        Proposal parameters provided do not satisfy current runtime execution constraints. If the proposal was executed
+        with the current runtime state, then it would get rejected automatically.
+      </TextMedium>{' '}
+    </Container>
+  )
+})
 
 const Container = styled.div`
   padding: 20px;
@@ -54,18 +33,3 @@ const Container = styled.div`
     column-gap: 4px;
   }
 `
-
-interface WarningContainerProps {
-  children: React.ReactNode
-}
-
-const WarningContainer = ({ children }: WarningContainerProps) => {
-  return (
-    <Container>
-      <TextMedium bold>
-        <AlertSymbol /> Warning
-      </TextMedium>
-      {children}
-    </Container>
-  )
-}
