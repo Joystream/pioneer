976415d5075a930cae95491d8780a5772a79b338
RafalPloszka <rafalploszka@live.com>
2022-02-23 08:45:17 +0100

    Fix fill-rule and clip-rule in icons

--- packages/ui/src/common/components/icons/BinIcon.tsx
+++ packages/ui/src/common/components/icons/BinIcon.tsx
@@ -13,8 +13,8 @@ export const BinIcon = React.memo(({ className, onClick }: { className?: string;
     onClick={onClick}
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M7 3L8 2H16L17 3V6H21V8H20V21L19 22H5L4 21V8H3V6H7V3ZM9 6H15V4H9V6ZM6 8V20H18V8H6Z"
       fill="currentColor"
     />
--- packages/ui/src/common/components/icons/workingGroup/Bandwidth.tsx
+++ packages/ui/src/common/components/icons/workingGroup/Bandwidth.tsx
@@ -7,40 +7,40 @@ export const Bandwidth = () => (
     <rect x="18.8284" y="18.8284" width="22" height="22" fill="#5D6B80" />
     <circle cx="30" cy="30" r="2" fill="white" />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M29.8284 0L36.2426 6.41421L33.4142 9.24264L31.8284 7.65685V23.8284H27.8284V7.65685L26.2426 9.24264L23.4142 6.41421L29.8284 0Z"
       fill="#3F38FF"
     />
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M31.8284 18.8284V23.8284H27.8284V18.8284H31.8284Z" fill="black" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M31.8284 18.8284V23.8284H27.8284V18.8284H31.8284Z" fill="black" />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M29.8284 59.6569L36.2426 53.2426L33.4142 50.4142L31.8284 52V35.8284H27.8284V52L26.2426 50.4142L23.4142 53.2426L29.8284 59.6569Z"
       fill="#3F38FF"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M59.6567 29.8284L53.2425 36.2426L50.4141 33.4142L51.9999 31.8284L35.8283 31.8284L35.8283 27.8284L51.9999 27.8284L50.4141 26.2426L53.2425 23.4142L59.6567 29.8284Z"
       fill="#3F38FF"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M2.80374e-07 29.8284L6.41421 36.2426L9.24264 33.4142L7.65685 31.8284L23.8284 31.8284L23.8284 27.8284L7.65685 27.8284L9.24264 26.2426L6.41421 23.4142L2.80374e-07 29.8284Z"
       fill="#3F38FF"
     />
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M31.8284 35.8284V40.8284H27.8284V35.8284H31.8284Z" fill="black" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M31.8284 35.8284V40.8284H27.8284V35.8284H31.8284Z" fill="black" />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M23.8284 31.8284L18.8284 31.8284L18.8284 27.8284L23.8284 27.8284L23.8284 31.8284Z"
       fill="black"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M40.8284 31.8284L35.8284 31.8284L35.8284 27.8284L40.8284 27.8284L40.8284 31.8284Z"
       fill="black"
     />
