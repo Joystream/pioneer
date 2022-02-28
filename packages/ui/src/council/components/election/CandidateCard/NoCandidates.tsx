0f3586464fe8d105d2fc4434540b2dfdeec02897
tomekstepien <tomasz.stepien@4soft.co>
2022-02-28 15:01:29 +0100

    fixed 404 response for election tab

--- packages/ui/src/council/components/election/CandidateCard/NoCandidates.tsx
+++ packages/ui/src/council/components/election/CandidateCard/NoCandidates.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { generatePath } from 'react-router'
+import { useHistory } from 'react-router-dom'
 
+import { TransactionButton } from '@/common/components/buttons/TransactionButton'
+import { ArrowRightIcon } from '@/common/components/icons'
 import { NoData } from '@/common/components/NoData'
 import { TextMedium } from '@/common/components/typography'
-import { AnnounceCandidacyButton } from '@/council/components/election/announcing/AnnounceCandidacyButton'
+import { CouncilRoutes } from '@/council/constants'
 
-export const NoCandidates = () => (
-  <NoData>
-    <h3>There are no candidates yet</h3>
-    <TextMedium>Be the first one to announce your candidacy. </TextMedium>
-    <AnnounceCandidacyButton />
-  </NoData>
-)
+export const NoCandidates = () => {
+  const history = useHistory()
+
+  const goToCouncilPage = () => {
+    history.push(generatePath(CouncilRoutes.council))
+  }
+  return (
+    <NoData>
+      <h3>There is no election now</h3>
+      <TextMedium>
+        Announcing period for new candidates starts when council period changes to Election. Elections are not held
+        during the Normal state of council. Please refer to the council period length in the council module
+      </TextMedium>
+      <TransactionButton style="primary" size="medium" onClick={goToCouncilPage}>
+        View current council <ArrowRightIcon white />
+      </TransactionButton>
+    </NoData>
+  )
+}
--- packages/ui/test/council/pages/Election.test.tsx
+++ packages/ui/test/council/pages/Election.test.tsx
@@ -142,7 +142,7 @@ describe('UI: Election page', () => {
       it('Displays stage and round', async () => {
         const { queryByText } = await renderComponent()
 
-        expect(queryByText(/Announcing period/i)).toBeInTheDocument()
+        expect(queryByText('Announcing Period')).toBeInTheDocument()
       })
 
       describe('Tabs', () => {
@@ -150,7 +150,7 @@ describe('UI: Election page', () => {
           it('No candidates', async () => {
             const { queryByText } = await renderComponent()
 
-            expect(queryByText(/There are no candidates yet/i)).not.toBeNull()
+            expect(queryByText(/There is no election now/i)).not.toBeNull()
           })
 
           it('Has candidates', async () => {
@@ -170,7 +170,7 @@ describe('UI: Election page', () => {
 
             const { queryByText } = await renderComponent()
 
-            expect(queryByText(/There are no candidates yet/i)).toBeNull()
+            expect(queryByText(/There is no election now/i)).toBeNull()
             expect(queryByText('My candidates')).toBeNull()
           })
 
