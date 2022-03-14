9a258f4bda3211f266503f86665690f742a0e8b8
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-03-14 17:34:34 +0400

    handle edge case where network selection from localStorage is invalid

--- packages/ui/src/common/hooks/useNetwork.ts
+++ packages/ui/src/common/hooks/useNetwork.ts
@@ -1,10 +1,19 @@
-import { NetworkType } from '@/app/config'
+import { NetworkType, configuredNetworks } from '@/app/config'
 
 import { useLocalStorage } from './useLocalStorage'
 
 export const useNetwork = () => {
   const [network, setNetwork] = useLocalStorage<NetworkType>('network')
-  const resolvedNetwork = network ?? 'local'
+  const networks: NetworkType[] = configuredNetworks()
 
-  return [resolvedNetwork, setNetwork] as const
+  // Ensure we have endpoints configured for selected network
+  if (networks.find((n) => n === network)) {
+    return [network!, setNetwork] as const
+  } else {
+    // Either the network was not specified,
+    // network was once available but is not recognized by pioneer anymore,
+    // or network has no endpoints configured in the current build of pioneer.
+    // Fallback to 'local'
+    return ['local', setNetwork] as const
+  }
 }
