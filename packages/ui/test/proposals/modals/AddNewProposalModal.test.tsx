5f19bace88925aec75290fc0434263b309b6bdbc
tomekstepien <tomasz.stepien@elliptic.co>
2022-03-18 14:05:38 +0100

    working icons updated and mapping added

--- packages/ui/test/proposals/modals/AddNewProposalModal.test.tsx
+++ packages/ui/test/proposals/modals/AddNewProposalModal.test.tsx
@@ -1006,20 +1006,6 @@ describe('UI: AddNewProposalModal', () => {
           expect(await screen.queryByLabelText(/^Opening/i, { selector: 'input' })).toHaveValue('')
           expect(await getCreateButton()).toBeDisabled()
         })
-
-        it.only('Valid form', async () => {
-          await SpecificParameters.FillWorkingGroupLeadOpening.selectedOpening('forumWorkingGroup-1337')
-          await SpecificParameters.FillWorkingGroupLeadOpening.selectApplication('forumWorkingGroup-1337')
-          // const [, txSpecificParameters] = last(createProposalTxMock.mock.calls)
-          // const parameters = txSpecificParameters.asFillWorkingGroupLeadOpening.toJSON()
-          // console.log(parameters)
-          // expect(parameters).toEqual({
-          //   opening_id: 1337,
-          //   successful_application_id: 1337,
-          //   working_group: 'Forum',
-          // })
-          // expect(await getCreateButton()).toBeEnabled()
-        })
       })
       describe('Type - Set Initial Invitation Count', () => {
         beforeAll(() => {
