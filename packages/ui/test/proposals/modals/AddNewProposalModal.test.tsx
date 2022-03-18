f2e6134fb09488fde1b1ced661413123db77f832
tomekstepien <tomasz.stepien@elliptic.co>
2022-03-18 16:23:56 +0100

    improvements added

--- packages/ui/test/proposals/modals/AddNewProposalModal.test.tsx
+++ packages/ui/test/proposals/modals/AddNewProposalModal.test.tsx
@@ -1006,6 +1006,19 @@ describe('UI: AddNewProposalModal', () => {
           expect(await screen.queryByLabelText(/^Opening/i, { selector: 'input' })).toHaveValue('')
           expect(await getCreateButton()).toBeDisabled()
         })
+
+        it('Valid form', async () => {
+          await SpecificParameters.FillWorkingGroupLeadOpening.selectedOpening('forumWorkingGroup-1337')
+          await SpecificParameters.FillWorkingGroupLeadOpening.selectApplication('forumWorkingGroup-1337')
+          const [, txSpecificParameters] = last(createProposalTxMock.mock.calls)
+          const parameters = txSpecificParameters.asFillWorkingGroupLeadOpening.toJSON()
+          expect(parameters).toEqual({
+            opening_id: 1337,
+            successful_application_id: 1337,
+            working_group: 'Forum',
+          })
+          expect(await getCreateButton()).toBeEnabled()
+        })
       })
       describe('Type - Set Initial Invitation Count', () => {
         beforeAll(() => {
