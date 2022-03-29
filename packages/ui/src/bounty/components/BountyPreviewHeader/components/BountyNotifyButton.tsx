fcfc6fe4fb7b6d6384016d6914877454798289cf
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2022-03-29 16:30:07 +0200

    🥾  Hiding bounty notification button (#2720)
    * Fix
    
    * Typing fix

--- packages/ui/src/bounty/components/BountyPreviewHeader/components/BountyNotifyButton.tsx
+++ packages/ui/src/bounty/components/BountyPreviewHeader/components/BountyNotifyButton.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
 
-import { ButtonGhost } from '@/common/components/buttons'
-import { BellIcon } from '@/common/components/icons/BellIcon'
 // todo TODO: https://github.com/Joystream/pioneer/issues/1937
 export const BountyNotifyButton = React.memo(() => {
-  const { t } = useTranslation('bounty')
+  // const { t } = useTranslation('bounty')
+  // till notifications doesn't work we won't display this button, used div instead of null to prevent typing issues
+  return <div />
 
-  return (
-    <ButtonGhost size="large">
-      <BellIcon /> {t('common:buttons.notifyAboutChanges')}
-    </ButtonGhost>
-  )
+  // return (
+  //   <ButtonGhost size="large">
+  //     <BellIcon /> {t('common:buttons.notifyAboutChanges')}
+  //   </ButtonGhost>
+  // )
 })
