b99c98698bd9335decd66703c5eb76efdc72186b
WRadoslaw <r.wyszynski00@gmail.com>
2022-03-29 14:04:11 +0200

    Typing fix

--- packages/ui/src/bounty/components/BountyPreviewHeader/components/BountyNotifyButton.tsx
+++ packages/ui/src/bounty/components/BountyPreviewHeader/components/BountyNotifyButton.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 // todo TODO: https://github.com/Joystream/pioneer/issues/1937
 export const BountyNotifyButton = React.memo(() => {
   // const { t } = useTranslation('bounty')
-  // till notifications doesn't work we won't display this button
-  return null
+  // till notifications doesn't work we won't display this button, used div instead of null to prevent typing issues
+  return <div />
 
   // return (
   //   <ButtonGhost size="large">
