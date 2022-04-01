1aff829eb1af37c41e7abf8367ec81bbe6801262
WRadoslaw <r.wyszynski00@gmail.com>
2022-04-01 16:16:34 +0200

    useSchema improvement

--- packages/ui/src/common/hooks/useSchema.ts
+++ packages/ui/src/common/hooks/useSchema.ts
@@ -1,20 +1,24 @@
 import { useMemo, useState } from 'react'
 import { AnyObjectSchema, ValidationError } from 'yup'
 
-export const useSchema = <T>(fields: Record<string, any>, schema: AnyObjectSchema) => {
+export const useSchema = <T>(fields: Record<string, any>, schema: AnyObjectSchema, path?: string) => {
   const [errors, setErrors] = useState<ValidationError[]>([])
   const [context, setContext] = useState<T>()
 
   const isValid = useMemo(() => {
     try {
-      schema.validateSync(fields, { abortEarly: false, stripUnknown: true, context: context })
+      if(path) {
+        schema.validateSyncAt(path, fields, { abortEarly: false, stripUnknown: true, context: context })
+      } else {
+        schema.validateSync(fields, { abortEarly: false, stripUnknown: true, context: context })
+      }
       setErrors([])
       return true
     } catch (error) {
       setErrors((error as any).inner)
       return false
     }
-  }, [JSON.stringify(fields), JSON.stringify(context), schema])
+  }, [JSON.stringify(fields), JSON.stringify(context), schema, path])
 
   return {
     isValid,
