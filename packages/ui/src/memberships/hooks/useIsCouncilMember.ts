ee3417290dbea46855a7ad2b6616fcdd69e3c507
RafalPloszka <rafalploszka@live.com>
2022-04-14 09:00:47 +0200

    Refactor useIsCouncilMember hook

--- packages/ui/src/memberships/hooks/useIsCouncilMember.ts
+++ packages/ui/src/memberships/hooks/useIsCouncilMember.ts
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+
 import { useApi } from '@/common/hooks/useApi'
 import { useObservable } from '@/common/hooks/useObservable'
 
@@ -7,6 +9,5 @@ export const useIsCouncilMember = (member?: Member) => {
   const { api, connectionState } = useApi()
   const council = useObservable(api?.query.council.councilMembers(), [connectionState])
   const councilMembersIds = council?.map(({ membership_id }) => membership_id.toNumber()) ?? []
-  const isCouncil = member && councilMembersIds.includes(parseInt(member.id))
-  return isCouncil
+  return useMemo(() => !!member && councilMembersIds.includes(parseInt(member.id)), [member, councilMembersIds])
 }
