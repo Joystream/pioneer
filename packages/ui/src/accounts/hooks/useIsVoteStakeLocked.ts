132141e3bc852616e06306244a78df569919ebd9
RafalPloszka <rafalploszka@live.com>
2022-04-14 09:01:23 +0200

    Apply useIsCouncilMember hook

--- packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
+++ packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
@@ -1,12 +1,14 @@
 import { useMemo } from 'react'
 
 import { LatestElection } from '@/council/types/LatestElection'
+import { useIsCouncilMember } from '@/memberships/hooks/useIsCouncilMember'
 import { Member } from '@/memberships/types'
 
 export const useIsVoteStakeLocked = (latestElection?: LatestElection, candidate?: Member) => {
+  const isCouncilMember = useIsCouncilMember(candidate)
   return useMemo(() => {
     // Lock stake if the vote was cast: in current election or to winning candidate
     // Enable stake recovery if election is finished
-    return !!candidate && (!latestElection?.isFinished || candidate.isCouncilMember)
+    return !!candidate && (!latestElection?.isFinished || isCouncilMember)
   }, [latestElection, candidate])
 }
