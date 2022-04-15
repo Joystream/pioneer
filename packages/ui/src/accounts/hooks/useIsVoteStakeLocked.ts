56248a747d7743860acf07073fd2363e18c92a46
RafalPloszka <rafalploszka@live.com>
2022-04-15 09:29:57 +0200

    Use isCouncilMember instead of hook

--- packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
+++ packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
@@ -1,10 +1,9 @@
 import { useMemo } from 'react'
 
-import { useIsCouncilMember } from '@/memberships/hooks/useIsCouncilMember'
 import { Member } from '@/memberships/types'
 
 export const useIsVoteStakeLocked = (candidate?: Member, electionFinished?: boolean) => {
-  const isCouncilMember = useIsCouncilMember(candidate)
+  const isCouncilMember = candidate?.isCouncilMember
   return useMemo(() => {
     // Lock stake if the vote was cast: in current election or to winning candidate
     // Enable stake recovery if election is finished
