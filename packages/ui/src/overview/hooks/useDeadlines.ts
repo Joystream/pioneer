093205f9585320f4d5e99cada9b822284c9b1ae7
tomekstepien <tomasz.stepien@elliptic.co>
2022-04-22 16:17:05 +0200

    query and hook updated

--- packages/ui/src/overview/hooks/useDeadlines.ts
+++ packages/ui/src/overview/hooks/useDeadlines.ts
@@ -1,26 +1,34 @@
 import { useMemo } from 'react'
 
 import { useLocalStorage } from '@/common/hooks/useLocalStorage'
+import { useElectionStage } from '@/council/hooks/useElectionStage'
 import { asElection } from '@/council/types/Election'
+import { Member } from '@/memberships/types'
 import { useGetAllDeadLinesQuery } from '@/overview/queries'
 import { asUpcomingWorkingGroupOpening } from '@/working-groups/types'
 
 interface UseDeadlinesParams {
   groupId?: string
+  member: Member
 }
 
-export type DeadlineNamespace = 'election' | 'proposals' | 'openings' | 'upcomingOpenings'
+export type DeadlineNamespace = 'elections' | 'proposals' | 'openings' | 'upcomingOpenings'
 
 const initializer: Record<DeadlineNamespace, string[]> = {
   proposals: [],
   openings: [],
   upcomingOpenings: [],
-  election: [],
+  elections: [],
 }
 
-export const useDeadlines = (params?: UseDeadlinesParams) => {
-  const options = params?.groupId || {}
-  const { loading, data } = useGetAllDeadLinesQuery(options)
+export const useDeadlines = (params: UseDeadlinesParams) => {
+  const variables = {
+    groupId: params.groupId || {},
+    proposalCreator: params.member.isCouncilMember ? undefined : { id_eq: params.member.id },
+    isLead: false,
+  }
+  const { stage: electionStage } = useElectionStage()
+  const { loading, data } = useGetAllDeadLinesQuery({ variables })
   const [storageDeadlines = initializer, setStorageDeadlines] =
     useLocalStorage<Record<DeadlineNamespace, string[]>>('deadlines')
 
@@ -33,13 +41,15 @@ export const useDeadlines = (params?: UseDeadlinesParams) => {
 
   const deadlines = useMemo((): Record<DeadlineNamespace, any[]> => {
     const rawElection = data?.electionRounds[0]
-    const election = rawElection && asElection(rawElection)
+    const election = rawElection && [asElection(rawElection)]
     const proposals = data?.proposals || []
-    const upcomingOpenings = data?.upcomingWorkingGroupOpenings.map(asUpcomingWorkingGroupOpening) || []
+    const upcomingOpenings = data?.upcomingWorkingGroupOpenings?.map(asUpcomingWorkingGroupOpening) || []
     const openings = data?.workingGroupOpenings || []
     return {
       proposals: proposals.filter((proposal) => !storageDeadlines.proposals.includes(proposal.id)),
-      election: election?.candidates.filter((candidate) => !storageDeadlines.election.includes(candidate.id)) ?? [],
+      elections:
+        election?.filter((election) => !storageDeadlines.elections.includes(`${election.cycleId}:${electionStage}`)) ??
+        [],
       openings: openings.filter((opening) => !storageDeadlines.openings.includes(opening.id)),
       upcomingOpenings: upcomingOpenings.filter(
         (upcomingOpening) => !storageDeadlines.upcomingOpenings.includes(upcomingOpening.id)
