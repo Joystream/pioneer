04af816d7c9857e96dcb0a6785fd03a1bcd85e94
WRadoslaw <r.wyszynski00@gmail.com>
2022-04-25 13:38:33 +0200

    Test update

--- packages/ui/test/bounty/modals/ContributeFundsModal.test.tsx
+++ packages/ui/test/bounty/modals/ContributeFundsModal.test.tsx
@@ -90,8 +90,8 @@ describe('UI: ContributeFundsModal', () => {
     expect(screen.getByText('modals.contribute.title')).toBeInTheDocument()
   })
 
-  it('Displays correct bounty id', () => {
-    expect(screen.getByDisplayValue(bounty.id)).toBeInTheDocument()
+  it('Displays correct bounty title', () => {
+    expect(screen.getByDisplayValue(bounty.title)).toBeInTheDocument()
   })
 
   it('Displays correct transaction fee', () => {
