453103c8738034b6b978616ebafe1ee69f95d88a
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-05-05 18:16:24 +0200

    Update the schema

--- packages/ui/src/forum/queries/__generated__/forumEvents.generated.tsx
+++ packages/ui/src/forum/queries/__generated__/forumEvents.generated.tsx
@@ -110,7 +110,6 @@ export type GetForumEventsQuery = {
     | { __typename: 'AuctionBidCanceledEvent' }
     | { __typename: 'AuctionBidMadeEvent' }
     | { __typename: 'AuctionCanceledEvent' }
-    | { __typename: 'AuctionStartedEvent' }
     | { __typename: 'BidMadeCompletingAuctionEvent' }
     | { __typename: 'BountyCanceledEvent' }
     | { __typename: 'BountyCreatedEvent' }
@@ -128,6 +127,7 @@ export type GetForumEventsQuery = {
     | { __typename: 'BudgetSpendingEvent' }
     | { __typename: 'BudgetUpdatedEvent' }
     | { __typename: 'BuyNowCanceledEvent' }
+    | { __typename: 'BuyNowPriceUpdatedEvent' }
     | { __typename: 'CandidacyNoteSetEvent' }
     | { __typename: 'CandidacyStakeReleaseEvent' }
     | { __typename: 'CandidacyWithdrawEvent' }
@@ -157,7 +157,8 @@ export type GetForumEventsQuery = {
     | { __typename: 'CategoryMembershipOfModeratorUpdatedEvent' }
     | { __typename: 'CategoryStickyThreadUpdateEvent' }
     | { __typename: 'CouncilorRewardUpdatedEvent' }
-    | { __typename: 'EnglishAuctionCompletedEvent' }
+    | { __typename: 'EnglishAuctionSettledEvent' }
+    | { __typename: 'EnglishAuctionStartedEvent' }
     | { __typename: 'InitialInvitationBalanceUpdatedEvent' }
     | { __typename: 'InitialInvitationCountUpdatedEvent' }
     | { __typename: 'InvitesTransferredEvent' }
@@ -176,11 +177,13 @@ export type GetForumEventsQuery = {
     | { __typename: 'NewMissedRewardLevelReachedEvent' }
     | { __typename: 'NftBoughtEvent' }
     | { __typename: 'NftIssuedEvent' }
+    | { __typename: 'NftSlingedBackToTheOriginalArtistEvent' }
     | { __typename: 'NotEnoughCandidatesEvent' }
     | { __typename: 'OfferAcceptedEvent' }
     | { __typename: 'OfferCanceledEvent' }
     | { __typename: 'OfferStartedEvent' }
     | { __typename: 'OpenAuctionBidAcceptedEvent' }
+    | { __typename: 'OpenAuctionStartedEvent' }
     | { __typename: 'OpeningAddedEvent' }
     | { __typename: 'OpeningCanceledEvent' }
     | { __typename: 'OpeningFilledEvent' }
