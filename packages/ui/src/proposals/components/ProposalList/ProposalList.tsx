6421eba14b3998fcaa012881724cbaf261461737
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2022-05-13 23:40:12 +0200

    Sorting of proposals

--- packages/ui/src/proposals/components/ProposalList/ProposalList.tsx
+++ packages/ui/src/proposals/components/ProposalList/ProposalList.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
+import { sortingOptions } from '@/bounty/helpers'
+import { ProposalOrderByInput } from '@/common/api/queries'
 import { List } from '@/common/components/List'
 import { RowGapBlock } from '@/common/components/page/PageContent'
+import { SimpleSelect } from '@/common/components/selects'
 import { NotFoundText } from '@/common/components/typography/NotFoundText'
+import { GetSortProps } from '@/common/hooks/useSort'
 import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 import { ProposalColLayout, ProposalsListHeaders, ProposalListHeader } from '@/proposals/constants'
 import { Proposal } from '@/proposals/types'
@@ -11,11 +16,17 @@ import { ProposalListItem } from './ProposalListItem'
 
 export interface ProposalListProps {
   proposals: Proposal[]
+  getSortProps?: GetSortProps<ProposalOrderByInput>
   isPast?: boolean
 }
 
-export const ProposalList = ({ proposals, isPast }: ProposalListProps) => {
+const SelectWrapper = styled.div`
+  max-width: 150px;
+`
+
+export const ProposalList = ({ proposals, getSortProps, isPast }: ProposalListProps) => {
   const { active } = useMyMemberships()
+  const { onSort, isDescending } = getSortProps?.('createdAt') || {}
   const isCouncilMember = active?.isCouncilMember
 
   if (!proposals.length) {
@@ -28,6 +39,15 @@ export const ProposalList = ({ proposals, isPast }: ProposalListProps) => {
         <ProposalListHeader>Stage</ProposalListHeader>
         <ProposalListHeader>Proposer</ProposalListHeader>
         {isCouncilMember && <ProposalListHeader>My vote</ProposalListHeader>}
+        {getSortProps && onSort ? (
+          <SelectWrapper>
+            <SimpleSelect
+              options={sortingOptions}
+              value={isDescending ? sortingOptions[0] : sortingOptions[1]}
+              onChange={onSort}
+            />
+          </SelectWrapper>
+        ) : null}
       </ProposalsListHeaders>
       <List as="div">
         {proposals.map((proposal) => (
