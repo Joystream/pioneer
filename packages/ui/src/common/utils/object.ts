54ac2db7385631018481b105a9e6cba0e49381f3
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-05-19 19:13:03 +0200

    Clean forgotten code

--- packages/ui/src/common/utils/object.ts
+++ packages/ui/src/common/utils/object.ts
@@ -15,38 +15,6 @@ export const mapObject = (obj: AnyObject, transform: ObjTransform<ObjTransformPa
   }
 }
 
-// const objectEntries = (obj: AnyObject): [string, any][] => {
-//   try {
-//     return Object.entries(obj)
-//   } catch {
-//     return Object.keys(obj).flatMap((key) => {
-//       try {
-//         return [[key, obj[key]]]
-//       } catch {
-//         return []
-//       }
-//     })
-//   }
-//   // if (!obj) return []
-//   // const ownEntries =
-//   //   obj.constructor.name === 'Object'
-//   //     ? Object.entries(obj)
-//   //     : Object.getOwnPropertyNames(obj)
-//   //         .filter((key) => key !== 'constructor')
-//   //         .map((key) => [key, obj[key]])
-
-//   // if (Array.isArray(obj)) {
-//   //   // THIS SHOUDN'T HAPPEN
-//   //   console.error("THIS SHOUDN'T HAPPEN")
-//   //   return []
-//   // }
-//   // if (obj.constructor.name !== 'Object') {
-//   //   console.log('obj:', obj.constructor.name, Object.getOwnPropertyNames(obj), Object.keys(obj), obj)
-//   // }
-
-//   // return [...Object.entries(obj), ...objectEntries(Object.getPrototypeOf(obj))]
-// }
-
 const isIterable = (value: any): value is Iterable<any> => typeof value[Symbol.iterator] === 'function'
 
 const mapEntries = (
