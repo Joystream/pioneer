b730455cacfe494a5315e33100f0c0d037f2113d
WRadoslaw <r.wyszynski00@gmail.com>
2022-05-25 11:18:14 +0200

    Fix on not voted count

--- packages/ui/src/proposals/components/VotesPreview/VotesPreview.tsx
+++ packages/ui/src/proposals/components/VotesPreview/VotesPreview.tsx
@@ -22,7 +22,11 @@ export const VotesPreview = ({ votes, notVoted }: VotesPreviewProps) => (
     <VotePreview kind={Reject} count={votes.count.reject} votes={votes.map.get(Reject)} />
     <VotePreview kind={Slash} count={votes.count.slash} votes={votes.map.get(Slash)} />
     <VotePreview kind={Abstain} count={votes.count.abstain} votes={votes.map.get(Abstain)} />
-    <VotePreview kind="Not Voted" count={notVoted?.length ?? 0} votes={notVoted?.map((voter) => ({ voter })) ?? []} />
+    <VotePreview
+      kind="Not Voted"
+      count={notVoted?.length ?? votes.count.remain}
+      votes={notVoted?.map((voter) => ({ voter })) ?? []}
+    />
   </VotesContainer>
 )
 
