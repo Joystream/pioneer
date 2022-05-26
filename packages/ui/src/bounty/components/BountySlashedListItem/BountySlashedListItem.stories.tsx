2ae8145d7d536fa14d023bf4d5522087a737d7a5
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2022-05-26 10:28:57 +0200

    🦝 Fix on bounty judgement rationale (#3080)
    * Adding Slashed amount to list item
    
    * Adding rationale on terminated and successful bounties
    
    * Copy change
    
    * Fix on sidebar infobox
    
    * Improvement on BountySlashedListItem

--- packages/ui/src/bounty/components/BountySlashedListItem/BountySlashedListItem.stories.tsx
+++ packages/ui/src/bounty/components/BountySlashedListItem/BountySlashedListItem.stories.tsx
@@ -6,6 +6,7 @@ import {
   BountySlashedListItem,
   BountySlashedListItemProps,
 } from '@/bounty/components/BountySlashedListItem/BountySlashedListItem'
+import { BN_ZERO } from '@/common/constants'
 import { Member } from '@/memberships/types'
 import members from '@/mocks/data/raw/members.json'
 import { randomBlock } from '@/mocks/helpers/randomBlock'
@@ -25,7 +26,7 @@ const Template: Story<BountySlashedListItemProps> = (args) => {
 
 export const Default = Template.bind({})
 Default.args = {
-  entrant: members[0] as unknown as Member,
+  entrant: (members[0] as unknown) as Member,
   inBlock: randomBlock(),
-  link: 'url',
+  stake: BN_ZERO,
 }
