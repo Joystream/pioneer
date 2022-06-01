8b23b153d9073f25d21ef57e2bab1a982657fb16
RafalPloszka <rafalploszka@live.com>
2022-06-01 11:41:59 +0200

    Calculate recoveryTime for ProposalLockItem

--- packages/ui/src/accounts/components/AccountItem/components/lockItems/ProposalLockItem.tsx
+++ packages/ui/src/accounts/components/AccountItem/components/lockItems/ProposalLockItem.tsx
@@ -1,7 +1,8 @@
-import faker from 'faker'
 import React, { useMemo } from 'react'
 import { generatePath } from 'react-router-dom'
 
+import { useApi } from '@/common/hooks/useApi'
+import { MILLISECONDS_PER_BLOCK } from '@/common/model/formatters'
 import { asBlock } from '@/common/types'
 import { ProposalsRoutes } from '@/proposals/constants/routes'
 import { useGetLatestProposalByMemberIdQuery } from '@/proposals/queries'
@@ -11,12 +12,24 @@ import { LockLinkButton } from '../LockLinkButton'
 import { LockDetailsProps } from '../types'
 
 export const ProposalLockItem = ({ lock, address, isRecoverable }: LockDetailsProps) => {
+  const { api } = useApi()
   const { data } = useGetLatestProposalByMemberIdQuery({ variables: { lockAccount: address } })
   const proposal = data?.proposals[0]
   const eventData = proposal?.createdInEvent
   const createdInEvent = eventData && asBlock(eventData)
 
-  const recoveryTime = faker.date.soon(1).toISOString()
+  const recoveryTime = useMemo(() => {
+    if (!eventData || !api || isRecoverable) {
+      return null
+    }
+    const startTime = new Date(eventData.createdAt).getTime()
+    // All proposal types has the same constants so any can be used
+    const proposalParameters = api.consts.proposalsCodex.signalProposalParameters
+    const duration = proposalParameters.votingPeriod.toNumber() + proposalParameters.gracePeriod.toNumber()
+    const endDate = new Date(startTime + duration * MILLISECONDS_PER_BLOCK).toISOString()
+
+    return endDate
+  }, [eventData?.createdAt, api, isRecoverable])
 
   const proposalId = proposal?.id
   const goToProposalButton = useMemo(() => {
