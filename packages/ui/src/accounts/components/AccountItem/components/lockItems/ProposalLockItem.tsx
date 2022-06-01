e6426d7cab1e08051d3ea6d2902049ded8536cc9
RafalPloszka <rafalploszka@live.com>
2022-06-01 14:03:43 +0200

    Fix recoveryTime for ProposalLockItem

--- packages/ui/src/accounts/components/AccountItem/components/lockItems/ProposalLockItem.tsx
+++ packages/ui/src/accounts/components/AccountItem/components/lockItems/ProposalLockItem.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react'
 import { generatePath } from 'react-router-dom'
 
 import { useApi } from '@/common/hooks/useApi'
+import { useCurrentBlockNumber } from '@/common/hooks/useCurrentBlockNumber'
 import { MILLISECONDS_PER_BLOCK } from '@/common/model/formatters'
 import { asBlock } from '@/common/types'
 import { ProposalsRoutes } from '@/proposals/constants/routes'
@@ -13,23 +14,31 @@ import { LockDetailsProps } from '../types'
 
 export const ProposalLockItem = ({ lock, address, isRecoverable }: LockDetailsProps) => {
   const { api } = useApi()
+  const currentBlockNumber = useCurrentBlockNumber()
   const { data } = useGetLatestProposalByMemberIdQuery({ variables: { lockAccount: address } })
   const proposal = data?.proposals[0]
   const eventData = proposal?.createdInEvent
   const createdInEvent = eventData && asBlock(eventData)
 
+  // All proposal types has the same constants so any can be used
+  const proposalParameters = api?.consts.proposalsCodex.signalProposalParameters
+  const votingPeriodDuration = proposalParameters?.votingPeriod.toNumber()
+  const gracePeriodDuration = proposalParameters?.gracePeriod.toNumber()
+
   const recoveryTime = useMemo(() => {
-    if (!eventData || !api || isRecoverable) {
+    if (!eventData || !votingPeriodDuration || !gracePeriodDuration || !currentBlockNumber) {
       return null
     }
-    const startTime = new Date(eventData.createdAt).getTime()
-    // All proposal types has the same constants so any can be used
-    const proposalParameters = api.consts.proposalsCodex.signalProposalParameters
-    const duration = proposalParameters.votingPeriod.toNumber() + proposalParameters.gracePeriod.toNumber()
-    const endDate = new Date(startTime + duration * MILLISECONDS_PER_BLOCK).toISOString()
 
-    return endDate
-  }, [eventData?.createdAt, api, isRecoverable])
+    if (proposal.exactExecutionBlock) {
+      const startTime = Date.now()
+      const duration = proposal.exactExecutionBlock - currentBlockNumber?.toNumber()
+      return new Date(startTime + duration * MILLISECONDS_PER_BLOCK).toISOString()
+    }
+
+    const startTime = Date.parse(eventData.createdAt)
+    return new Date(startTime + votingPeriodDuration + gracePeriodDuration * MILLISECONDS_PER_BLOCK).toISOString()
+  }, [eventData?.createdAt, votingPeriodDuration, gracePeriodDuration, currentBlockNumber])
 
   const proposalId = proposal?.id
   const goToProposalButton = useMemo(() => {
