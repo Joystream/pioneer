f5a6058d1895805eb81a267d0f9e637c1ee21f43
WRadoslaw <r.wyszynski00@gmail.com>
2022-06-07 10:28:55 +0200

    Show all entries on sidebar for all bounties

--- packages/ui/src/bounty/hooks/useBountyEntrants.ts
+++ packages/ui/src/bounty/hooks/useBountyEntrants.ts
@@ -1,14 +1,10 @@
 import { useMemo } from 'react'
 
-import { Bounty, BountyEntryStatus, Entrant } from '../types/Bounty'
+import { Bounty, Entrant } from '../types/Bounty'
 
 export const useBountyEntrants = (bounty: Bounty): Entrant[] | undefined => {
-  const disallowedStates: BountyEntryStatus[] = ['BountyEntryStatusWithdrawn', 'BountyEntryStatusRejected']
   return useMemo(
-    () =>
-      bounty.entries
-        ?.filter((entry) => !disallowedStates.includes(entry.status))
-        .map((entry) => ({ actor: entry.worker, count: entry.works?.length ?? 0 })),
+    () => bounty.entries?.map((entry) => ({ actor: entry.worker, count: entry.works?.length ?? 0 })) ?? [],
     [bounty.entries]
   )
 }
