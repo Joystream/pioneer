d4030ec3892af8065e215ac1c70206336b8d45bb
RafalPloszka <rafalploszka@live.com>
2022-06-09 20:50:14 +0200

    Delete candidate checking in useIsVoteStakeLocked

--- packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
+++ packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
@@ -6,5 +6,5 @@ export const useIsVoteStakeLocked = (
 ) => {
   // Lock stake if the vote was cast: in current election or to winning candidate
   // Enable stake recovery if election is finished
-  return isLatestElection && !!candidate && (!isElectionFinished || candidate.isCouncilMember)
+  return isLatestElection && (!isElectionFinished || !!candidate?.isCouncilMember)
 }
