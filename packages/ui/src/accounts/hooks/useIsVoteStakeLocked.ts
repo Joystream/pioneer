a4844ed37b286e0d42a755a3a11db4121226257f
RafalPloszka <rafalploszka@live.com>
2022-06-10 10:10:23 +0200

    Update comment in useIsVoteStakeLocked

--- packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
+++ packages/ui/src/accounts/hooks/useIsVoteStakeLocked.ts
@@ -4,7 +4,7 @@ export const useIsVoteStakeLocked = (
   candidate: Member | undefined,
   { isElectionFinished = true, isLatestElection = true }
 ) => {
-  // Lock stake if the vote was cast: in current election or to winning candidate
-  // Enable stake recovery if election is finished
+  // Always allow stake recovery for votes not cast in the latest election
+  // also allow when both the election is over and the voted for candidate lost
   return isLatestElection && (!isElectionFinished || !!candidate?.isCouncilMember)
 }
