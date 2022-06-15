c8922e1696194928df50f7b09316b35d97dd8252
WRadoslaw <r.wyszynski00@gmail.com>
2022-06-15 13:59:48 +0200

    Introduction of logic for selecting wallet

--- packages/ui/src/accounts/providers/accounts/provider.tsx
+++ packages/ui/src/accounts/providers/accounts/provider.tsx
@@ -1,4 +1,12 @@
-import { web3Accounts, web3AccountsSubscribe, web3Enable } from '@polkadot/extension-dapp'
+import { web3Accounts, web3AccountsSubscribe } from '@polkadot/extension-dapp'
+import { documentReadyPromise } from '@polkadot/extension-dapp/util'
+import {
+  Injected,
+  InjectedWindowProvider,
+  InjectedExtensionInfo,
+  InjectedAccount,
+  InjectedExtension,
+} from '@polkadot/extension-inject/types'
 import { Keyring } from '@polkadot/ui-keyring'
 import React, { ReactNode, useEffect, useState } from 'react'
 import { debounceTime, filter, skip } from 'rxjs/operators'
@@ -6,6 +14,7 @@ import { debounceTime, filter, skip } from 'rxjs/operators'
 import { useKeyring } from '@/common/hooks/useKeyring'
 import { useObservable } from '@/common/hooks/useObservable'
 import { error } from '@/common/logger'
+import { SelectWalletModal } from '@/common/modals/SelectWalletModal/SelectWalletModal'
 
 import { Account } from '../../types'
 
@@ -24,6 +33,47 @@ interface Props {
   children: ReactNode
 }
 
+type InjectedExtensionRaw = Record<string, InjectedWindowProvider>
+
+function getWindowExtension(
+  originName: string,
+  extensionName: string
+): Promise<[InjectedExtensionInfo, Injected | void]> {
+  const injectedExtensions = Object.entries((window as any).injectedWeb3 as InjectedExtensionRaw)
+
+  const [name, { version, enable }] =
+    injectedExtensions.find(([name]) => name === extensionName) ?? injectedExtensions[0]
+
+  return Promise.all([
+    { name, version },
+    // eslint-disable-next-line no-console
+    enable(originName).catch((error): void => console.error(`Error initializing ${extensionName}: ${error.message}`)),
+  ])
+}
+
+const enableSingleExtension = ([info, extension]: [
+  InjectedExtensionInfo,
+  Injected | void
+]): Promise<InjectedExtension> => {
+  return documentReadyPromise((): Promise<InjectedExtension> => {
+    if (extension) {
+      extension.accounts.subscribe = (cb: (accounts: InjectedAccount[]) => void | Promise<void>) => {
+        // eslint-disable-next-line no-console
+        extension.accounts.get().then(cb).catch(console.error)
+
+        return () => {
+          // no need for unsub
+        }
+      }
+    }
+
+    return Promise.resolve({
+      ...info,
+      ...extension,
+    } as InjectedExtension)
+  })
+}
+
 function isKeyringLoaded(keyring: Keyring) {
   try {
     return !!keyring.keyring
@@ -32,8 +82,18 @@ function isKeyringLoaded(keyring: Keyring) {
   }
 }
 
-const loadKeysFromExtension = async (keyring: Keyring) => {
-  await web3Enable('Pioneer')
+const loadKeysFromExtension = async (
+  keyring: Keyring,
+  selectedExtension: string,
+  onInitializationFailure: () => void
+) => {
+  const extension = await getWindowExtension('Pioneer', selectedExtension)
+  if (!extension[1]) {
+    return onInitializationFailure()
+  }
+
+  await enableSingleExtension(extension)
+
   const injectedAccounts = await web3Accounts()
 
   if (!isKeyringLoaded(keyring)) {
@@ -56,15 +116,16 @@ const loadKeysFromExtension = async (keyring: Keyring) => {
 }
 
 // Extensions is not always ready on application load, hence the check
-const onExtensionLoaded = (onSuccess: () => void, onFail: () => void) => () => {
+const onExtensionLoaded = (onSuccess: (selectedExtension?: string) => void, onFail: () => void) => () => {
   const interval = 20
   const timeout = 1000
   let timeElapsed = 0
 
   const intervalId = setInterval(() => {
-    if (Object.keys((window as any).injectedWeb3).length) {
+    const extensionsKeys = Object.keys((window as any).injectedWeb3)
+    if (extensionsKeys.length) {
       clearInterval(intervalId)
-      onSuccess()
+      onSuccess(extensionsKeys.length === 1 ? extensionsKeys[0] : undefined)
     } else {
       timeElapsed += interval
       if (timeElapsed >= timeout) {
@@ -81,22 +142,30 @@ export const AccountsContextProvider = (props: Props) => {
   const keyring = useKeyring()
   const [isExtensionLoaded, setIsExtensionLoaded] = useState(false)
   const [extensionUnavailable, setExtensionUnavailable] = useState(false)
+  const [selectedWallet, setSelectedWallet] = useState<string | undefined>(undefined)
+  const [failedWallet, setFailedWallet] = useState<string[]>([])
 
   useEffect(
     onExtensionLoaded(
-      () => setIsExtensionLoaded(true),
+      (possibleExtension) => {
+        setIsExtensionLoaded(true)
+        setSelectedWallet(possibleExtension)
+      },
       () => setExtensionUnavailable(true)
     ),
     []
   )
 
   useEffect(() => {
-    if (!isExtensionLoaded) {
+    if (!isExtensionLoaded || !selectedWallet) {
       return
     }
 
-    loadKeysFromExtension(keyring).catch(error)
-  }, [isExtensionLoaded])
+    loadKeysFromExtension(keyring, selectedWallet, () => {
+      setFailedWallet((prev) => [...prev, selectedWallet])
+      setSelectedWallet(undefined)
+    }).catch(error)
+  }, [isExtensionLoaded, selectedWallet])
 
   const accounts = useObservable(
     keyring.accounts.subject.asObservable().pipe(
@@ -126,5 +195,17 @@ export const AccountsContextProvider = (props: Props) => {
     value.isLoading = false
   }
 
-  return <AccountsContext.Provider value={value}>{props.children}</AccountsContext.Provider>
+  return (
+    <AccountsContext.Provider value={value}>
+      {!selectedWallet && isExtensionLoaded && (
+        <SelectWalletModal
+          availableWallets={Object.keys((window as any).injectedWeb3).filter(
+            (wallet) => !failedWallet.includes(wallet)
+          )}
+          onWalletSelect={(wallet) => setSelectedWallet(wallet)}
+        />
+      )}
+      {props.children}
+    </AccountsContext.Provider>
+  )
 }
