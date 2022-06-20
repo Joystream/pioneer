0f7ed58ef09e164c6a7a609f21da61efa7a00931
RafalPloszka <rafalploszka@live.com>
2022-06-20 11:49:14 +0200

    New UI for accounts with locks

--- packages/ui/src/accounts/components/SelectAccount/OptionListAccount.tsx
+++ packages/ui/src/accounts/components/SelectAccount/OptionListAccount.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 import { Option, OptionsListComponent } from '../../../common/components/selects'
-import { Account } from '../../types'
+import { AccountOption } from '../../types'
 
 import { OptionAccount } from './OptionAccount'
 
 interface Props {
-  options: Account[]
-  onChange: (option: Account) => void
+  options: AccountOption[]
+  onChange: (option: AccountOption) => void
 }
 
 export const OptionListAccount = React.memo(({ options, onChange }: Props) => (
   <OptionsListComponent>
     {options.map((option) => (
-      <Option key={option.address} onClick={() => onChange && onChange(option)}>
+      <Option key={option.address} onClick={() => onChange && onChange(option)} disabled={!!option.optionLocks?.length}>
         <OptionAccount option={option} />
       </Option>
     ))}
