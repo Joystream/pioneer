5f38876c5ebb351c2b07b74e16a151ade59111a1
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-06-23 15:38:20 +0200

    Keep the fix from #3233

--- packages/ui/src/common/components/CKEditor/CKEditorStylesOverrides.tsx
+++ packages/ui/src/common/components/CKEditor/CKEditorStylesOverrides.tsx
@@ -11,6 +11,10 @@ export const CKEditorStylesOverrides = createGlobalStyle<{ minRows: number; maxR
     width: 100%;
   }
 
+  .ckeditor-anchor:not(.ck-content) {
+    display: none;
+  }
+
   .ck.ck-content {
     line-height: ${EDITOR_LINE_HEIGHT}em;
     width: 100%;
