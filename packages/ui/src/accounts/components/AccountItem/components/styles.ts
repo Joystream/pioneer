3cbfa77a54d9e10501bb6f46b2b4b050692fd670
RafalPloszka <rafalploszka@live.com>
2022-06-28 22:53:31 +0200

    Fix buttons label align

--- packages/ui/src/accounts/components/AccountItem/components/styles.ts
+++ packages/ui/src/accounts/components/AccountItem/components/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
+import { ButtonGhost } from '@/common/components/buttons'
 import { DropDownToggle } from '@/common/components/buttons/DropDownToggle'
+import { LinkButtonGhost } from '@/common/components/buttons/LinkButtons'
 import { Label } from '@/common/components/typography'
 import { BorderRad, Colors } from '@/common/constants'
 
@@ -77,4 +79,12 @@ export const DetailLabel = styled(Label)`
 export const RecoveryTimeWrapper = styled.div`
   display: flex;
   column-gap: 10px;
+`
+
+export const LockInternalLinkButton = styled(LinkButtonGhost)`
+  white-space: nowrap;
+`
+
+export const LockExternalLinkButton = styled(ButtonGhost)`
+  white-space: nowrap;
 `
