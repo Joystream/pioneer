44e30a89544630796de4208ee6fffebd719893b5
Ken-tech-max <81464477+Ken-tech-max@users.noreply.github.com>
2022-07-01 13:53:34 -0700

    udpate for new feedback

--- packages/ui/src/forum/hooks/useLatestForumThreads.ts
+++ packages/ui/src/forum/hooks/useLatestForumThreads.ts
@@ -8,7 +8,7 @@ import { asForumThread } from '../types'
 export const useLatestForumThreads = (limit: number) => {
   const status_json = {
     isTypeOf_not: 'ThreadStatusModerated',
-    visiblePostsCount: { _gt: 0 },
+    visibleThreadsCount_gte: 0,
   }
   const { data, loading } = useGetForumThreadsQuery({
     variables: { orderBy: ForumThreadOrderByInput.CreatedAtDesc, limit, where: { status_json } },
