6a3afcc92714795d292761c588d3406f095251ef
RafalPloszka <rafalploszka@live.com>
2022-07-07 19:53:23 +0200

    Implement WorkerGroupLock logic

--- packages/ui/src/accounts/components/AccountItem/components/LocksDetails.tsx
+++ packages/ui/src/accounts/components/AccountItem/components/LocksDetails.tsx
@@ -14,15 +14,19 @@ interface LocksDetailsProps {
 }
 
 export const LocksDetails = ({ balance, address }: LocksDetailsProps) => {
-  const { isActiveCandidate, isVoteStakeLocked } = useRecoveryConditions(address)
+  const { isActiveCandidate, isVoteStakeLocked, isWorkerStakeLocked } = useRecoveryConditions(address)
 
   if (!balance || !balance.locks.length) {
     return <TextMedium light>No locks found.</TextMedium>
   }
 
   const allLocks = balance.locks
-  const recoverable = allLocks.filter(({ type }) => isRecoverable(type, { isActiveCandidate, isVoteStakeLocked }))
-  const nonRecoverable = allLocks.filter(({ type }) => !isRecoverable(type, { isActiveCandidate, isVoteStakeLocked }))
+  const recoverable = allLocks.filter(({ type }) =>
+    isRecoverable(type, { isActiveCandidate, isVoteStakeLocked, isWorkerStakeLocked })
+  )
+  const nonRecoverable = allLocks.filter(
+    ({ type }) => !isRecoverable(type, { isActiveCandidate, isVoteStakeLocked, isWorkerStakeLocked })
+  )
 
   return (
     <>
