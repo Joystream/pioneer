68e3f87a3964f97568860d73a3749b3601a8c5ae
RafalPloszka <rafalploszka@live.com>
2022-07-08 13:44:21 +0200

    Change info copy + add info icon

--- packages/ui/src/accounts/modals/MoveFoundsModal/MoveFundsModalInfo.tsx
+++ packages/ui/src/accounts/modals/MoveFoundsModal/MoveFundsModalInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { InfoIcon } from '@/common/components/icons/InfoIcon'
 import { TextMedium } from '@/common/components/typography'
 
 import { InfoList } from './styles'
@@ -18,16 +19,20 @@ export const MoveFundsModalInfo = ({ insufficientBalances, noFreeAccounts }: Mov
     return (
       <>
         <TextMedium light>
-          All your accounts have locks. It is strongly recommended to link all accounts to single membership in order to
-          correctly attribute accumulated reputation on the network.
+          All your accounts for current membership have locks preventing from using them for this action.
           <br />
           <br />
-          In order to apply to this role:
+          In order to apply to proceed:
         </TextMedium>
         <InfoList>
-          <li>Create new account using Polkadot extension</li>
-          <li>Move funds to your new account making it suitable</li>
+          <li>Create new account using your wallet extension</li>
+          <li>Move the required amount of tokens to your new account</li>
+          <li>Start again and choose the newly created account</li>
         </InfoList>
+        <TextMedium light>
+          <InfoIcon /> It is strongly recommended to link all accounts to single membership in order to correctly
+          attribute accumulated reputation on the network.
+        </TextMedium>
       </>
     )
   }
