42973f7ec7a237e2f408032f6d3022d3b14caede
WRadoslaw <r.wyszynski00@gmail.com>
2022-07-28 14:41:49 +0200

    Vesting hook

---
+++ packages/ui/src/accounts/hooks/useVesting.ts
@@ -0,0 +1,24 @@
+import { DeriveBalancesAll } from '@polkadot/api-derive/types'
+
+import { useApi } from '@/common/hooks/useApi'
+import { useObservable } from '@/common/hooks/useObservable'
+
+export const useVesting = (address: string) => {
+  const { api, connectionState } = useApi()
+
+  const balances = useObservable(address ? api?.derive.balances.all(address) : undefined, [connectionState, address])
+  if (balances === undefined) {
+    return null
+  }
+
+  return asVesting(balances)
+}
+
+type Vesting = Pick<DeriveBalancesAll, 'vesting' | 'vestingTotal' | 'vestedBalance' | 'vestedClaimable'>
+
+const asVesting = (balances: DeriveBalancesAll): Vesting => ({
+  vesting: balances.vesting,
+  vestingTotal: balances.vestingTotal,
+  vestedClaimable: balances.vestedClaimable,
+  vestedBalance: balances.vestedBalance,
+})
