da7c2a869de5de8da7d5dbd70ca038e5174a2065
Joshua Akinsuyi <akinsuyi.joshua84@gmail.com>
2022-08-01 19:10:20 +0100

    Fix: https://github.com/Joystream/pioneer/issues/3163

--- packages/ui/src/forum/modals/ReplyThreadModal/index.ts
+++
@@ -1,5 +0,0 @@
-import { ModalWithDataCall } from '@/common/providers/modal/types'
-import { ForumPost } from '@/forum/types'
-
-export * from './ReplyThreadModal'
-export type ReplyThreadModalCall = ModalWithDataCall<'ReplyThreadModal', { post: ForumPost }>
