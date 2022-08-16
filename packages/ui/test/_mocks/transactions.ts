4a2154d9be35b419dacb75994b502cd58065f913
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2022-08-16 11:10:02 +0200

    🧞‍♂️ Applying proposal consts to form validation (#3485)
    * New packages
    
    * Typing fix
    
    * Applying new consts to form validation
    
    * Test fix
    
    * New test for max funding amount
    
    * Slight optimization
    
    * Removing unnecessary comment

--- packages/ui/test/_mocks/transactions.ts
+++ packages/ui/test/_mocks/transactions.ts
@@ -153,7 +153,7 @@ export const stubConst = <T>(api: UseApi, constSubPath: string, value: T) => {
 
 export const stubApi = () => {
   const api: UseApi = {
-    api: {} as unknown as Api,
+    api: ({} as unknown) as Api,
     isConnected: true,
     connectionState: 'connected',
   }
@@ -215,6 +215,8 @@ export const stubProposalConstants = (api: UseApi, constants?: { requiredStake: 
     })
   }
   set(api, 'api.consts.members.referralCutMaximumPercent', new BN(50))
+  set(api, 'api.consts.proposalsCodex.fundingRequestProposalMaxAmount', new BN(10_000))
+  set(api, 'api.consts.proposalsCodex.setMaxValidatorCountProposalMaxValidators', new BN(300))
   set(
     api,
     'api.query.members.membershipPrice',
