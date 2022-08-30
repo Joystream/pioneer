cd107e57228976b6b8fa0c6d549c7a047bee6edb
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-08-30 17:46:57 +0200

    Fix `api-rx.ts` for the Rhodes network

--- packages/ui/src/api/api-rx.ts
+++ packages/ui/src/api/api-rx.ts
@@ -1,12 +1,14 @@
-import '@joystream/types'
-import { ApiRx } from '@polkadot/api'
-import { WsProvider } from '@polkadot/rpc-provider'
+import { registry, types } from '@joystream/types'
+import '@joystream/types/augment/augment-api'
+import '@joystream/types/augment/augment-types'
+import { ApiRx, WsProvider } from '@polkadot/api'
+import rpc from '@polkadot/types/interfaces/jsonrpc'
 
 export type Api = ApiRx
 
 export const Api = {
   create: (nodeRpcEndpoint: string) => {
     const provider = new WsProvider(nodeRpcEndpoint)
-    return ApiRx.create({ provider })
+    return ApiRx.create({ provider, rpc, types, registry })
   },
 }
