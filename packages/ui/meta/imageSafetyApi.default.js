f29031884900d9dd6330b057cadfc8e85a588387
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-09-05 20:05:33 +0200

    Potentially report the page the image is on too

--- packages/ui/meta/imageSafetyApi.default.js
+++ packages/ui/meta/imageSafetyApi.default.js
@@ -17,13 +17,13 @@ module.exports = {
         return (await res.text()).split('\n')
       }
     },
-    report: async (url) => fetch('$REPORT_ENDPOINT$', {
+    report: async (image) => fetch('$REPORT_ENDPOINT$', {
       headers: {
         'Content-Type': 'application/json',
         ...ReportBearer ? { Authorization: `Bearer ${ReportBearer}` } : {},
       },
       method: 'POST',
-      body: '$REPORT_BODY$'.replace('{value}', url),
+      body: '$REPORT_BODY$'.replace('{image}', image).replace('{page}', window.location.href),
     })
   }
 }
