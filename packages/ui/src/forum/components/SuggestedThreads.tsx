863b96e1d313ebbad1a8595bd765d537dd2f4ea1
gyroflaw <ryo.kanazawa34@gmail.com>
2022-09-14 21:13:13 +0900

    fix: Removed ThreadItemWraper import issue

--- packages/ui/src/forum/components/SuggestedThreads.tsx
+++ packages/ui/src/forum/components/SuggestedThreads.tsx
@@ -4,7 +4,8 @@ import styled from 'styled-components'
 import { Loading } from '@/common/components/Loading'
 import { ContentWithTabs } from '@/common/components/page/PageContent'
 import { Label } from '@/common/components/typography'
-import { ThreadItem, ThreadItemWrapper } from '@/forum/components/threads/ThreadItem'
+import { CardItem } from '@/forum/components/CardItem'
+import { ThreadItem } from '@/forum/components/threads/ThreadItem'
 import { useForumSuggestedThreads } from '@/forum/hooks/useForumSuggestedThreads'
 
 export const SuggestedThreads = () => {
@@ -40,7 +41,7 @@ export const ThreadsList = styled.div`
 export const SuggestedThreadsWrapper = styled(ContentWithTabs)`
   margin-top: 12px;
 
-  ${ThreadItemWrapper} {
+  ${CardItem} {
     &:before {
       content: unset;
     }
