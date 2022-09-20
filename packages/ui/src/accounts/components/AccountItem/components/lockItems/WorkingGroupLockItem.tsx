db85505d46e9b77fa34b6b47aa009eb59ac4e6da
WRadoslaw <r.wyszynski00@gmail.com>
2022-09-20 23:26:24 +0200

    Hiding recover button and displaying info

--- packages/ui/src/accounts/components/AccountItem/components/lockItems/WorkingGroupLockItem.tsx
+++ packages/ui/src/accounts/components/AccountItem/components/lockItems/WorkingGroupLockItem.tsx
@@ -1,6 +1,9 @@
 import React, { useMemo } from 'react'
 import { generatePath } from 'react-router-dom'
 
+import { LinkSymbol } from '@/common/components/icons/symbols'
+import { TooltipExternalLink } from '@/common/components/Tooltip'
+import { TextMedium } from '@/common/components/typography'
 import { asBlock } from '@/common/types'
 import { WorkingGroupsRoutes } from '@/working-groups/constants'
 import { useOpening } from '@/working-groups/hooks/useOpening'
@@ -10,7 +13,7 @@ import { LockItem } from '../LockItem'
 import { LockLinkButton } from '../LockLinkButton'
 import { LockDetailsProps } from '../types'
 
-export const WorkingGroupLockItem = ({ lock, address, isRecoverable }: LockDetailsProps) => {
+export const WorkingGroupLockItem = ({ lock, address }: LockDetailsProps) => {
   const { data } = useGetWorkingGroupApplicationsQuery({
     variables: {
       where: {
@@ -26,7 +29,7 @@ export const WorkingGroupLockItem = ({ lock, address, isRecoverable }: LockDetai
 
   const recoveryTime = useMemo(() => {
     if (opening?.status === 'OpeningStatusOpen') {
-      return { unrecoverableLabel: 'Based on hiring decision' }
+      return { unrecoverableLabel: 'Recoverable after withdrawing from application' }
     } else {
       if (application?.status.__typename === 'ApplicationStatusAccepted') {
         return { unrecoverableLabel: 'Recoverable after released from role' }
@@ -48,9 +51,27 @@ export const WorkingGroupLockItem = ({ lock, address, isRecoverable }: LockDetai
     <LockItem
       lock={lock}
       address={address}
-      isRecoverable={isRecoverable}
+      isRecoverable={false}
       createdInEvent={createdInEvent}
-      lockRecovery={recoveryTime}
+      lockRecovery={{
+        ...recoveryTime,
+        tooltipLabel: (
+          <>
+            Stake for WG role application and active role participation as worker or lead will be recovered at the time
+            of:
+            <br /> For application - at the time of withdrawal.
+            <br />
+            For active role - after leaving the role and elapsed leaving unstaking period.
+            <br />
+            <TooltipExternalLink
+              href="https://joystream.gitbook.io/testnet-workspace/system/working-groups#concepts"
+              target="_blank"
+            >
+              <TextMedium>More details</TextMedium> <LinkSymbol />
+            </TooltipExternalLink>
+          </>
+        ),
+      }}
       linkButtons={goToOpeningButton}
     />
   )
