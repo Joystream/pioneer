f4b93e9a0feeafb916b29ca0a812e1a67109cd5b
WRadoslaw <r.wyszynski00@gmail.com>
2022-09-29 13:02:20 +0200

    Adapting ThreadCard to interface

--- packages/ui/src/forum/components/ThreadCard/ThreadCard.tsx
+++ packages/ui/src/forum/components/ThreadCard/ThreadCard.tsx
@@ -7,26 +7,38 @@ import { ReplyIcon } from '@/common/components/icons'
 import { ColumnGapBlock } from '@/common/components/page/PageContent'
 import { TextBig, TextExtraSmall, TextMedium } from '@/common/components/typography'
 import { BorderRad, Colors } from '@/common/constants'
+import { relativeIfRecent } from '@/common/model/relativeIfRecent'
+import { ForumThread } from '@/forum/types'
 import { MemberInfo } from '@/memberships/components'
+import { useMember } from '@/memberships/hooks/useMembership'
 
-export const ThreadCard = () => {
+interface ThreadCardProps {
+  thread: ForumThread
+}
+
+export const ThreadCard = ({ thread }: ThreadCardProps) => {
+  const { member: author } = useMember(thread.authorId)
+
+  if (!author) return null
+
   return (
     <Box>
       <div>
-        <MemberInfo size="s" hideGroup onlyTop member={DevMember} />
+        <MemberInfo size="s" hideGroup onlyTop member={author} />
         <div>
           <TextExtraSmall inter lighter>
-            20 mins ago
+            {relativeIfRecent(thread.createdInBlock.timestamp)}
           </TextExtraSmall>
-          <BadgeStatus size="m">BOUNTIES</BadgeStatus>
+          {thread.tags.map((tag) => (
+            <BadgeStatus size="m">{tag.title.toUpperCase()}</BadgeStatus>
+          ))}
         </div>
       </div>
       <TextBig bold value>
-        Welcome and Forum Guidelines Second line of text
+        {thread.title}
       </TextBig>
       <TextMedium light truncateLines={3}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna
+        {thread.initialPostText}
       </TextMedium>
       <ColumnGapBlock gap={8}>
         <ReplyIcon />
@@ -39,7 +51,6 @@ export const ThreadCard = () => {
 const Box = styled.div`
   display: grid;
   row-gap: 16px;
-  max-width: 332px;
   border: 1px solid ${Colors.Black[100]};
   border-radius: ${BorderRad.s};
   padding: 24px;
@@ -77,42 +88,3 @@ const Box = styled.div`
     }
   }
 `
-
-const DevMember = {
-  id: '0',
-  rootAccount: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
-  controllerAccount: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-  boundAccounts: [
-    '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-    '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
-  ],
-  boundAccountsEvents: [],
-  handle: 'alice',
-  metadata: {
-    name: 'nesciunt ea',
-    about:
-      'Hic qui esse ea error eum enim recusandae nisi a. Sit consequatur expedita quo repellat rem facere itaque quia. Et maiores nobis et ut nihil. Id est soluta sunt necessitatibus tempora vitae quas labore possimus. Ullam necessitatibus corporis omnis quod dolore recusandae. Nesciunt at tempora odio ipsum ipsam consequatur velit.\n \rLaboriosam ut minima. Veritatis omnis quam quo saepe. Ut dolorum id. Sint dicta earum maiores et. Ipsum hic optio sunt magnam neque rerum ut possimus et. Adipisci sed officiis rerum inventore omnis rem rerum provident.',
-  },
-  isVerified: false,
-  isFoundingMember: false,
-  isCouncilMember: false,
-  inviteCount: 5,
-  avatar: 'https://raw.githubusercontent.com/Joystream/founding-members/main/avatars/primary-avatar/15.png',
-  entry: {
-    __typename: 'MembershipEntryPaid',
-    membershipBoughtEvent: {
-      inBlock: 19471,
-      createdAt: '2021-12-12T11:57:16.322Z',
-      network: 'OLYMPIA',
-    },
-  },
-  createdAt: 'asd',
-  roles: [
-    {
-      id: '1',
-      groupName: 'forumWorkingGroup',
-      createdAt: undefined,
-      isLead: true,
-    },
-  ],
-}
