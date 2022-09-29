7cb015a6d3595e017b79ec1f8830c6cbaec02ee7
WRadoslaw <r.wyszynski00@gmail.com>
2022-09-29 13:07:36 +0200

    Layout changes

--- packages/ui/src/forum/components/threads/ThreadList.tsx
+++ packages/ui/src/forum/components/threads/ThreadList.tsx
@@ -3,17 +3,12 @@ import styled from 'styled-components'
 
 import { ForumThreadOrderByInput } from '@/common/api/queries'
 import { List } from '@/common/components/List'
-import { ListHeader, ListHeaders } from '@/common/components/List/ListHeader'
-import { SortHeader } from '@/common/components/List/SortHeader'
 import { Loading } from '@/common/components/Loading'
-import { RowGapBlock } from '@/common/components/page/PageContent'
 import { Pagination } from '@/common/components/Pagination'
 import { NotFoundText } from '@/common/components/typography/NotFoundText'
 import { GetSortProps } from '@/common/hooks/useSort'
-import { ThreadsColLayout } from '@/forum/constant'
+import { ThreadCard } from '@/forum/components/ThreadCard/ThreadCard'
 import { ForumThread } from '@/forum/types'
-
-import { ThreadListItem } from './ThreadListItem'
 
 interface ThreadListProps {
   threads: ForumThread[]
@@ -25,50 +20,28 @@ interface ThreadListProps {
   isArchive?: boolean
 }
 
-export const ThreadList = ({
-  threads,
-  getSortProps,
-  isLoading,
-  isArchive,
-  page,
-  pageCount,
-  setPage,
-}: ThreadListProps) => {
+export const ThreadList = ({ threads, isLoading, isArchive, page, pageCount, setPage }: ThreadListProps) => {
   if (threads.length <= 0 && !isLoading) {
     return <NotFoundText>No threads found</NotFoundText>
   }
 
-  return (
-    <ThreadListStyles gap={4}>
-      <ListHeaders $colLayout={ThreadsColLayout}>
-        <SortHeader {...getSortProps('title')}>Threads</SortHeader>
-        <ListHeader>Replies</ListHeader>
-        <SortHeader {...getSortProps('updatedAt')}>Last Activity</SortHeader>
-        <SortHeader {...getSortProps('author')}>Author</SortHeader>
-        {isArchive ? (
-          <ListHeader>Archived</ListHeader>
-        ) : (
-          <SortHeader {...getSortProps('createdAt')}>Created</SortHeader>
-        )}
-      </ListHeaders>
-
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <List as="div" isArchive={isArchive}>
-          {setPage && <Pagination pageCount={pageCount} handlePageChange={setPage} page={page} />}
-          {threads.map((thread, index) => (
-            <ThreadListItem key={index} thread={thread} isArchive={isArchive} />
-          ))}
-          {setPage && <Pagination pageCount={pageCount} handlePageChange={setPage} page={page} />}
-        </List>
-      )}
-    </ThreadListStyles>
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <List as="div" isArchive={isArchive}>
+      <ThreadListStyles>
+        {threads.map((thread) => (
+          <ThreadCard thread={thread} />
+        ))}
+      </ThreadListStyles>
+      {setPage && <Pagination pageCount={pageCount} handlePageChange={setPage} page={page} />}
+    </List>
   )
 }
 
-const ThreadListStyles = styled(RowGapBlock)`
-  ${ListHeaders} {
-    padding: 0 24px;
-  }
+const ThreadListStyles = styled.div`
+  display: grid;
+  padding: 20px 0;
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
 `
