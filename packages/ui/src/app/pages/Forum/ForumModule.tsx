591e433f5ee48e8dfce1ac530bc15908236123bd
WRadoslaw <r.wyszynski00@gmail.com>
2022-10-03 16:23:13 +0200

    New forum tab

--- packages/ui/src/app/pages/Forum/ForumModule.tsx
+++ packages/ui/src/app/pages/Forum/ForumModule.tsx
@@ -5,6 +5,7 @@ import { Forum } from '@/app/pages/Forum/Forum'
 import { ForumArchived } from '@/app/pages/Forum/ForumArchived'
 import { ForumCategory } from '@/app/pages/Forum/ForumCategory'
 import { ForumThread } from '@/app/pages/Forum/ForumThread'
+import { ForumWatchlist } from '@/app/pages/Forum/ForumWatchlist'
 import { ForumRoutes } from '@/forum/constant'
 
 import { ForumMyThreads } from './ForumMyThreads'
@@ -14,6 +15,7 @@ export const ForumModule = () => {
     <Switch>
       <Route path={ForumRoutes.forum} exact component={Forum} />
       <Route path={ForumRoutes.myThreads} exact component={ForumMyThreads} />
+      <Route path={ForumRoutes.watchlist} exact component={ForumWatchlist} />
       <Route path={ForumRoutes.archived} exact component={ForumArchived} />
       <Route path={ForumRoutes.category} exact component={ForumCategory} />
       <Route path={ForumRoutes.thread} exact component={ForumThread} />
