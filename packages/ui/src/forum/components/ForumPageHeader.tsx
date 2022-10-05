08563cc51f4de20a3cb0d324d9bb10269ad60603
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-10-05 11:18:16 +0200

    Revert "🏛️ Implement new forum category page design (#3562)"
    This reverts commit 87a5cacc86129b2d58a16b7569b169a0b7e86eba.

--- packages/ui/src/forum/components/ForumPageHeader.tsx
+++ packages/ui/src/forum/components/ForumPageHeader.tsx
@@ -12,18 +12,22 @@ interface ForumPageHeaderProps {
 }
 export const ForumPageHeader = ({ title, children, buttons }: ForumPageHeaderProps) => {
   const [search, setSearch] = useState('')
+
   const { showModal } = useModal()
+
   return (
     <PageHeaderWrapper>
       <PageHeaderRow>
         {title}
-        <ButtonsGroup>{buttons}</ButtonsGroup>
+        <ButtonsGroup>
+          <SearchBox
+            value={search}
+            onChange={setSearch}
+            onApply={() => showModal({ modal: 'SearchResults', data: { search } })}
+          />
+          {buttons}
+        </ButtonsGroup>
       </PageHeaderRow>
-      <SearchBox
-        value={search}
-        onChange={setSearch}
-        onApply={() => showModal({ modal: 'SearchResults', data: { search } })}
-      />
       {children}
     </PageHeaderWrapper>
   )
--- packages/ui/src/forum/components/category/ForumCategoryList.tsx
+++ packages/ui/src/forum/components/category/ForumCategoryList.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { List } from '@/common/components/List'
+import { ListHeader, ListHeaders } from '@/common/components/List/ListHeader'
+import { RowGapBlock } from '@/common/components/page/PageContent'
+import { categoriesColLayout } from '@/forum/constant'
 import { ForumCategory } from '@/forum/types'
 
 import { CategoryListItem } from './CategoryListItem'
@@ -11,15 +15,30 @@ export interface ForumCategoryListProps {
 }
 
 export const ForumCategoryList = ({ categories, isArchive = false }: ForumCategoryListProps) => (
-  <StyledList>
-    {categories.map((category, index) => (
-      <CategoryListItem key={index} category={category} isArchive={isArchive} />
-    ))}
-  </StyledList>
+  <ForumCategoryListStyles gap={4}>
+    <ListHeaders $colLayout={categoriesColLayout(isArchive)}>
+      <ListHeader>Category</ListHeader>
+      <ListHeader>Total threads</ListHeader>
+      <ListHeader>Last Post</ListHeader>
+      {isArchive ? (
+        <ListHeader>Archived</ListHeader>
+      ) : (
+        <>
+          <ListHeader>Popular Thread</ListHeader>
+          <ListHeader>Moderators</ListHeader>
+        </>
+      )}
+    </ListHeaders>
+    <List as="div" isArchive={isArchive}>
+      {categories.map((category, index) => (
+        <CategoryListItem key={index} category={category} isArchive={isArchive} />
+      ))}
+    </List>
+  </ForumCategoryListStyles>
 )
 
-const StyledList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-  gap: 12px;
+const ForumCategoryListStyles = styled(RowGapBlock)`
+  ${ListHeaders} {
+    padding: 0px 24px;
+  }
 `
