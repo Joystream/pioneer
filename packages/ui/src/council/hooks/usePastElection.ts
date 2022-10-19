df0458a102f738ddadf7420c9f49a6d3e6eef253
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2022-10-19 08:39:01 +0200

    🔒 New hook to group account lock (#3671)
    * New hook for locks
    
    * Logic to recover WG application stake
    
    * Adding WithdrawApplicationButton.tsx based on RecoverBalanceModal.tsx
    
    * Delete unused hook
    
    * test fix
    
    * Improvements on loading handling
    
    * Change modal title when withdrawing application
    
    * Test fix
    
    * CR fixes
    
    * Further CR fixes
    
    * Test fix
    
    * Final CR fixes

--- packages/ui/src/council/hooks/usePastElection.ts
+++ packages/ui/src/council/hooks/usePastElection.ts
@@ -2,8 +2,8 @@ import { useGetPastElectionQuery } from '@/council/queries'
 
 import { asPastElectionWithDetails } from '../types/PastElection'
 
-export const usePastElection = (id: string) => {
-  const { loading, data } = useGetPastElectionQuery({ variables: { id } })
+export const usePastElection = (id?: string) => {
+  const { loading, data } = useGetPastElectionQuery({ variables: { id: id ?? '' }, skip: typeof id === 'undefined' })
 
   return {
     isLoading: loading,
