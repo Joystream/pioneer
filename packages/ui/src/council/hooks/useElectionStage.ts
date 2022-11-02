11eebc6ba67891c7e7bbd1b724abe7234d327527
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-11-02 19:06:50 +0100

    Fix the recovery time of the voting and candidacy locks

--- packages/ui/src/council/hooks/useElectionStage.ts
+++ packages/ui/src/council/hooks/useElectionStage.ts
@@ -1,5 +1,6 @@
 import { concatMap, EMPTY, merge, Observable, of } from 'rxjs'
 
+import { Api } from '@/api'
 import { useApi } from '@/api/hooks/useApi'
 import { useObservable } from '@/common/hooks/useObservable'
 import { ElectionStage } from '@/council/types/Election'
@@ -11,33 +12,32 @@ interface UseElectionStage {
 
 export const useElectionStage = (): UseElectionStage => {
   const { api } = useApi()
-
-  const stageObservable = () => {
-    if (!api) return
-
-    const councilObservable = api.query.council.stage().pipe(
-      concatMap(({ stage: councilStage }): Observable<ElectionStage> => {
-        if (councilStage.isIdle) {
-          return of('inactive')
-        } else if (councilStage.isAnnouncing) {
-          return of('announcing')
-        }
-        return EMPTY
-      })
-    )
-    const referendumObservable = api.query.referendum.stage().pipe(
-      concatMap((referendumStage): Observable<ElectionStage> => {
-        if (referendumStage.isVoting) {
-          return of('voting')
-        } else if (referendumStage.isRevealing) {
-          return of('revealing')
-        }
-        return EMPTY
-      })
-    )
-    return merge(councilObservable, referendumObservable)
-  }
-  const stage = useObservable(stageObservable, [api?.isConnected])
+  const stage = useObservable(() => api && electionStageObservable(api), [api?.isConnected])
 
   return { isLoading: !stage, stage: stage ?? 'inactive' }
+}
+
+export const electionStageObservable = (api: Api) => {
+  const councilObservable = api.query.council.stage().pipe(
+    concatMap(({ stage: councilStage }): Observable<ElectionStage> => {
+      if (councilStage.isIdle) {
+        return of('inactive')
+      } else if (councilStage.isAnnouncing) {
+        return of('announcing')
+      }
+      return EMPTY
+    })
+  )
+  const referendumObservable = api.query.referendum.stage().pipe(
+    concatMap((referendumStage): Observable<ElectionStage> => {
+      if (referendumStage.isVoting) {
+        return of('voting')
+      } else if (referendumStage.isRevealing) {
+        return of('revealing')
+      }
+      return EMPTY
+    })
+  )
+
+  return merge(councilObservable, referendumObservable)
 }
