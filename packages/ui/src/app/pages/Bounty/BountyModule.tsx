a7d16103d5e51151fb141b628513a025a6f28d11
Joystream Stats <dev@joystreamstats.live>
2022-11-03 17:07:38 +0000

    new bounty routes

--- packages/ui/src/app/pages/Bounty/BountyModule.tsx
+++ packages/ui/src/app/pages/Bounty/BountyModule.tsx
@@ -7,17 +7,26 @@ import { BountyRoutes } from '@/bounty/constants'
 import { TextExtraHuge, TextMedium } from '@/common/components/typography'
 import { Colors } from '@/common/constants'
 
+import { BountiesCurrent } from './BountiesCurrent'
+import { BountiesMyBounties } from './BountiesMyBounties'
+import { BountiesMyContributions } from './BountiesMyContributions'
+import { BountiesMyEntries } from './BountiesMyEntries'
+import { BountiesPast } from './BountiesPast'
+import { BountiesTags } from './BountiesTags'
+import { Bounty } from './Bounty'
+
 export const BountyModule = () => {
   return (
     <Switch>
-      {/*<Route exact path={BountyRoutes.currentBounties} component={BountiesCurrent} />*/}
-      {/*<Route exact path={BountyRoutes.pastBounties} component={BountiesPast} />*/}
-      {/*<Route exact path={BountyRoutes.myBounties} component={BountiesMyBounties} />*/}
-      {/*<Route exact path={BountyRoutes.myContributions} component={BountiesMyContributions} />*/}
-      {/*<Route exact path={BountyRoutes.myEntries} component={BountiesMyEntries} />*/}
-      {/*<Route exact path={BountyRoutes.bountyTags} component={BountiesTags} />*/}
-      {/*<Route exact path={BountyRoutes.bounty} component={Bounty} />*/}
-      {/*<Redirect exact path={BountyRoutes.bounties} to={BountyRoutes.currentBounties} />*/}
+      <Route exact path={BountyRoutes.currentBounties} component={BountiesCurrent} />
+      <Route exact path={BountyRoutes.pastBounties} component={BountiesPast} />
+      <Route exact path={BountyRoutes.myBounties} component={BountiesMyBounties} />
+      <Route exact path={BountyRoutes.myContributions} component={BountiesMyContributions} />
+      <Route exact path={BountyRoutes.myEntries} component={BountiesMyEntries} />
+      <Route exact path={BountyRoutes.bountyTags} component={BountiesTags} />
+      <Route exact path={BountyRoutes.bountyTab} component={Bounty} />
+      <Route exact path={BountyRoutes.bounty} component={Bounty} />
+      <Redirect exact path={BountyRoutes.bounties} to={BountyRoutes.currentBounties} />
       <Route exact path={BountyRoutes.bounties} component={EmptyPageComponent} />
       <Redirect from="*" to={BountyRoutes.bounties} />
     </Switch>
--- packages/ui/src/bounty/constants/routes.ts
+++ packages/ui/src/bounty/constants/routes.ts
@@ -1,10 +1,17 @@
 export const BountyRoutes = {
+  // new shortcuts
+  show: '/bounties/:id',
+  tab: '/bounties/:id/:tab',
+  page: '/bounty/:page',
+
+  // legacy
   currentBounties: '/bounty/current',
   pastBounties: '/bounty/past',
   myBounties: '/bounty/my-bounties',
   myContributions: '/bounty/my-contributions',
   myEntries: '/bounty/my-entries',
   bountyTags: '/bounty/tags',
+  bountyTab: '/bounty/preview/:id/:tab',
   bounty: '/bounty/preview/:id',
   bounties: '/bounty',
 } as const
