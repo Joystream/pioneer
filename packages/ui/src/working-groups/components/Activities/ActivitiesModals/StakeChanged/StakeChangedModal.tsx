9839372915e1bf42209e0c175f02555ef7d5163f
Joystream Stats <dev@joystreamstats.live>
2022-11-10 15:48:19 +0000

    Stake changed Activities (#3458)
     - Add 'Read more' on `StakeSlashedContent` to open `StakeChangedModal`
     - Rename 'Slashed with' to 'Amount'
     - Rename 'Status' with 'Action'
     - Hide Amount if empty
     - Hide Rationale if empty

--- packages/ui/src/working-groups/components/Activities/ActivitiesModals/StakeChanged/StakeChangedModal.tsx
+++ packages/ui/src/working-groups/components/Activities/ActivitiesModals/StakeChanged/StakeChangedModal.tsx
@@ -23,39 +23,46 @@ import { useEscape } from '@/common/hooks/useEscape'
 interface StakeChangedModalProps {
   onClose: () => void
   amount?: BN
-  eventType?: 'StakeIncreasedEvent' | 'StakeDecreasedEvent'
+  eventType?: 'StakeIncreasedEvent' | 'StakeDecreasedEvent' | 'StakeSlashedEvent'
   id?: string
+}
+
+const actions = {
+  StakeIncreasedEvent: { past: 'increased', badge: 'increase' },
+  StakeDecreasedEvent: { past: 'decreased', badge: 'decrease' },
+  StakeSlashedEvent: { past: 'slashed', badge: 'slash' },
 }
 
 export const StakeChangedModal = ({ onClose, amount, eventType, id }: StakeChangedModalProps) => {
   useEscape(() => onClose())
   const slashingRationaleInfo = '' // hidden until needed
+  const action = eventType ? actions[eventType] : { past: 'changes', badge: 'change' }
 
   return (
     <SidePaneGlass onClick={onClose}>
       <SidePane topSize="xs">
         <SidePaneHeader>
           <SidePanelTop>
-            <SidePaneTitle>
-              Stake has been {eventType === 'StakeDecreasedEvent' ? 'reduced' : 'increased'}
-            </SidePaneTitle>
+            <SidePaneTitle>Stake has been {action.past}</SidePaneTitle>
             <CloseButton onClick={onClose} />
           </SidePanelTop>
         </SidePaneHeader>
         <SidePaneBody>
-          {amount && eventType && id ? (
+          {eventType && id ? (
             <SidePaneTable>
               <SidePaneRow>
-                <SidePaneLabel text="status" />
-                <StatusBadge>{eventType === 'StakeDecreasedEvent' ? 'reduce' : 'increase'}</StatusBadge>
+                <SidePaneLabel text="action" />
+                <StatusBadge>{action.badge}</StatusBadge>
               </SidePaneRow>
-              <SidePaneRow>
-                <SidePaneLabel text="slashed by" />
-                <SidePaneText>
-                  <TokenValue value={amount} />
-                </SidePaneText>
-              </SidePaneRow>
-              {false && (
+              {amount && (
+                <SidePaneRow>
+                  <SidePaneLabel text="amount" />
+                  <SidePaneText>
+                    <TokenValue value={amount} />
+                  </SidePaneText>
+                </SidePaneRow>
+              )}
+              {slashingRationaleInfo.length > 0 && (
                 <SidePaneRow>
                   <SidePaneLabel text="rationale" />
                   <SidePaneText>{slashingRationaleInfo}</SidePaneText>
