b411b0c2dbe1346f696b18cf25d83ec2e1c4e4b0
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-11-10 11:01:58 +0100

    Fix the `node-mock` election scripts

--- packages/ui/dev/node-mocks/commands/council/announce.ts
+++ packages/ui/dev/node-mocks/commands/council/announce.ts
@@ -3,7 +3,7 @@ import { ApiPromise } from '@polkadot/api'
 import { lockLookup } from '../../../../src/accounts/model/lockTypes'
 import { flatMapP, mapP } from '../../../../src/common/utils'
 import memberData from '../../../../src/mocks/data/raw/members.json'
-import { accountsMap } from '../../data/addresses'
+import { accountsMap, getSudoAccount } from '../../data/addresses'
 import { signAndSend, withApi } from '../../lib/api'
 import { createMembersCommand } from '../members/create'
 
@@ -11,6 +11,16 @@ export const announceCandidaciesCommand = async (api: ApiPromise) => {
   const candidateCount = api.consts.council.councilSize.toNumber() + 1
   const announceStake = api.consts.council.minCandidateStake
   const members = memberData.slice(0, candidateCount)
+
+  // Unstake all accounts to be used
+  const unstakingTxs = await flatMapP(members, async ({ controllerAccount: address }) => {
+    const locks = await api.query.balances.locks(address)
+    if (!locks.some(({ id }) => lockLookup(id) === 'Staking')) return []
+
+    const forceUnstake = api.tx.staking.forceUnstake(address, 0)
+    return api.tx.sudo.sudo(forceUnstake)
+  })
+  await signAndSend(api.tx.utility.batch(unstakingTxs), getSudoAccount())
 
   // Fund the empty accounts
   const requiredBalance = announceStake.add(api.consts.referendum.minimumStake).muln(1.5) // 1.5 extra margin for potential transaction fees
