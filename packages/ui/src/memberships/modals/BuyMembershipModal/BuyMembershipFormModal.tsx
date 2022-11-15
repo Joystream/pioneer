58fd0c5645aae192128c168f414233a7f4810a76
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-11-15 16:35:19 +0100

    Fix the faucet CAPTCHA

--- packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipFormModal.tsx
+++ packages/ui/src/memberships/modals/BuyMembershipModal/BuyMembershipFormModal.tsx
@@ -120,7 +120,6 @@ export const BuyMembershipForm = ({
   type,
 }: BuyMembershipFormProps) => {
   const { allAccounts } = useMyAccounts()
-  const [captchaToken, setCaptchaToken] = useState<string | undefined>()
   const [formHandleMap, setFormHandleMap] = useState('')
   const { isUploading, uploadAvatarAndSubmit } = useUploadAvatarAndSubmit(onSubmit)
   const { data } = useGetMembersCountQuery({ variables: { where: { handle_eq: formHandleMap } } })
@@ -136,7 +135,7 @@ export const BuyMembershipForm = ({
     },
   })
 
-  const [handle, isReferred, referrer] = form.watch(['handle', 'isReferred', 'referrer'])
+  const [handle, isReferred, referrer, captchaToken] = form.watch(['handle', 'isReferred', 'referrer', 'captchaToken'])
 
   useEffect(() => {
     if (handle) {
@@ -229,7 +228,7 @@ export const BuyMembershipForm = ({
                   sitekey={process.env.REACT_APP_CAPTCHA_SITE_KEY}
                   theme="light"
                   languageOverride="en"
-                  onVerify={setCaptchaToken}
+                  onVerify={(token) => form.setValue('captchaToken', token)}
                 />
               </Row>
             )}
