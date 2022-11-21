40114a81bc1e1ad86bd7b39355299f76ad259753
mkbeefcake <mkblockchaindev@outlook.com>
2022-11-21 04:15:50 -0500

    force refresh election page after votes_successed

--- packages/ui/src/council/modals/VoteForCouncil/VoteForCouncilSuccessModal.tsx
+++ packages/ui/src/council/modals/VoteForCouncil/VoteForCouncilSuccessModal.tsx
@@ -21,7 +21,7 @@ export const VoteForCouncilSuccessModal = ({ onClose, candidateId }: Props) => {
   const { candidate } = useCandidate(candidateId)
 
   const goToElection = useCallback(() => {
-    history.push(generatePath(ElectionRoutes.currentElection))
+    history.go(0)
     onClose()
   }, [onClose])
 
