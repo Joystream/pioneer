bc4171873b342c4ad42f9a6819eff8c2ac9878d8
Joystream Stats <dev@joystreamstats.live>
2022-11-25 23:02:01 +0000

    stories

---
+++ packages/ui/src/working-groups/modals/CreateOpening/CreateOpening.stores.tsx
@@ -0,0 +1,51 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { MockApolloProvider } from '@/mocks/components/storybook/MockApolloProvider'
+import {
+  ApplicationForm,
+  DurationAndProcess,
+  StakingPolicyAndReward,
+  WorkingGroupAndDescription,
+} from '@/proposals/modals/AddNewProposal/components/SpecificParameters/WorkingGroupLeadOpening/CreateWorkingGroupLeadOpening'
+
+export default {
+  title: 'WorkingGroups/CreateOpeningModal',
+  components: [ApplicationForm, DurationAndProcess, StakingPolicyAndReward, WorkingGroupAndDescription],
+} as Meta
+
+const WorkingGroupAndDescriptionTemplate: Story = () => {
+  return (
+    <MockApolloProvider members council forum proposals workers workingGroups>
+      <WorkingGroupAndDescription />
+    </MockApolloProvider>
+  )
+}
+
+const DurationAndProcessTemplate: Story = () => {
+  return (
+    <MockApolloProvider>
+      <DurationAndProcess />
+    </MockApolloProvider>
+  )
+}
+
+const ApplicationFormTemplate: Story = () => {
+  return <ApplicationForm />
+}
+
+const StakingTemplate: Story = () => {
+  return (
+    <MockApolloProvider members workingGroups>
+      <StakingPolicyAndReward />
+    </MockApolloProvider>
+  )
+}
+
+export const Step1WorkingGroupAndDescription = WorkingGroupAndDescriptionTemplate.bind({})
+
+export const Step2DurationAndProcess = DurationAndProcessTemplate.bind({})
+
+export const Step3ApplicationForm = ApplicationFormTemplate.bind({})
+
+export const Step4StakingPolicyAndRewardStep = StakingTemplate.bind({})
