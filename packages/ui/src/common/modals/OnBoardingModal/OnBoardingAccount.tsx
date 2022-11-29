54bf5823238adfcdcf8084031c825305c595c4ad
WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
2022-11-29 17:29:03 +0100

    🧏 Onboarding wallet selection without RPC connection (#3886)
    * Change provider logic
    
    * Adapt onboarding components to the new logic
    
    * Test fix

--- packages/ui/src/common/modals/OnBoardingModal/OnBoardingAccount.tsx
+++ packages/ui/src/common/modals/OnBoardingModal/OnBoardingAccount.tsx
@@ -1,8 +1,10 @@
 import React, { useMemo } from 'react'
 
 import { useMyAccounts } from '@/accounts/hooks/useMyAccounts'
+import { useApi } from '@/api/hooks/useApi'
 import { Loading } from '@/common/components/Loading'
 import { NoAccountStep } from '@/common/modals/OnBoardingModal/components/NoAccountStep'
+import { NoNodeConnection } from '@/common/modals/OnBoardingModal/components/NoNodeConnection'
 import { SelectAccountStep } from '@/common/modals/OnBoardingModal/components/SelectAccountStep'
 
 interface Props {
@@ -11,14 +13,19 @@ interface Props {
 
 export const OnBoardingAccount = ({ onAccountSelect }: Props) => {
   const { hasAccounts, isLoading } = useMyAccounts()
+  const { api } = useApi()
 
   const step = useMemo(() => {
+    if (!api?.isConnected) {
+      return <NoNodeConnection />
+    }
+
     if (!hasAccounts) {
       return <NoAccountStep />
     }
 
     return <SelectAccountStep onAccountSelect={onAccountSelect} />
-  }, [hasAccounts])
+  }, [hasAccounts, api?.isConnected])
 
   if (isLoading) {
     return <Loading />
