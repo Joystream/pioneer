2f0be61e5a3c1808ee60ef59ce79dfb2d7857eb5
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-12-02 10:54:00 +0100

    Revert "🩹 Temp fix for the revealing stage (#3881)"
    This reverts commit 6f67161163a311461f95cd8c25a7a85766ffb23a.

--- packages/ui/src/council/hooks/useElectionVotes.ts
+++ packages/ui/src/council/hooks/useElectionVotes.ts
@@ -29,7 +29,7 @@ export interface CandidateStats {
 export const useElectionVotes = (election?: Election) => {
   const { allAccounts } = useMyAccounts()
   const { data, loading } = useGetCouncilVotesQuery({
-    variables: { where: { electionRound: { cycleId_eq: election?.cycleId } }, limit: 500 },
+    variables: { where: { electionRound: { cycleId_eq: election?.cycleId } } },
   })
   const votes = useMemo(() => data?.castVotes.map(asVote), [data?.castVotes])
 
