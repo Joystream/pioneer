fac64269d206c10f80d70060583a34f90fff9850
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-12-06 22:01:30 +0800

    feat: add external links for WGs (#3709)
    * feat: add external links for WGs
    
    * fix: show tooltip on hover & add links for WGs
    
    * style: group tooltip
    
    * refactor wg tooltips (#3709)
    
    * fix: external link issue and tooltip style
    
    * refactor: remove GroupTooltip
    
    * Remove unused variable
    
    Co-authored-by: Theophile Sandoz <theophile.sandoz@gmail.com>
    
    Co-authored-by: Joystream Stats <dev@joystreamstats.live>
    Co-authored-by: Theophile Sandoz <theophile.sandoz@gmail.com>

--- packages/ui/src/common/helpers.ts
+++ packages/ui/src/common/helpers.ts
@@ -37,25 +37,25 @@ export const wgListItemMappings = (value: string) => {
       return {
         subtitle:
           'A diverse set of contributors, such as Developers, Designers and Product Managers, responsible for development of infrastructure and user facing applications.',
-        tooltipLink: undefined,
+        tooltipLink: 'https://joystream.gitbook.io/testnet-workspace/system/builders',
       }
     case 'Storage':
       return {
         subtitle:
           'Broadly responsible for ensuring storage infrastructure uptime, namely running complete and up-to-date copy of the content directory and accept inbound uploads from end users.',
-        tooltipLink: undefined,
+        tooltipLink: 'https://joystream.gitbook.io/testnet-workspace/system/storage',
       }
     case 'Content':
       return {
         subtitle:
           'Monitor publishing of the new content into the content directory, respond to the reported publications and adjudicate possible dispute processes.',
-        tooltipLink: undefined,
+        tooltipLink: 'https://joystream.gitbook.io/testnet-workspace/system/content-directory',
       }
     case 'Distribution':
       return {
         subtitle:
           'Run and maintain distributor nodes that deliver large volumes of upstream data to a large number of simultaneous end users.',
-        tooltipLink: undefined,
+        tooltipLink: 'https://joystream.gitbook.io/testnet-workspace/system/storage#distributor',
       }
     case 'Gateway':
       return {
@@ -67,25 +67,27 @@ export const wgListItemMappings = (value: string) => {
       return {
         subtitle:
           'Human Resources working group is responsible for integrating new members greeting, onboarding, catalyzing and nurturing, as well as managing bounties.',
-        tooltipLink: undefined,
+        tooltipLink:
+          'https://joystream.gitbook.io/testnet-workspace/testnet/council-period-scoring/human-resources-score',
       }
     case 'Operations Gamma':
       return {
         subtitle:
           'Marketing group is responsible for increasing the outreach, sharing the content from the platform with the world, spreading the news about platform development, new members acquisition and overall growth.',
-        tooltipLink: undefined,
+        tooltipLink: 'https://joystream.gitbook.io/testnet-workspace/system/marketers',
       }
     case 'Membership':
       return {
         subtitle:
           'Membership group is responsible for new memberships invitations, referral rewards for existing members and overall process of adding more members via referral scheme.',
-        tooltipLink: undefined,
+        tooltipLink:
+          'https://joystream.gitbook.io/testnet-workspace/testnet/council-period-scoring/human-resources-score',
       }
     case 'Forum':
       return {
         subtitle:
           'Monitor and supervise public communication channels for compliance with usage policies as decided through the governance system.',
-        tooltipLink: undefined,
+        tooltipLink: 'https://joystream.gitbook.io/testnet-workspace/system/forum',
       }
     default:
       return {
