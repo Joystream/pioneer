d30638271543d8ab4172710c41df65287410488b
Traumschule <31551045+traumschule@users.noreply.github.com>
2022-12-12 12:11:55 +0000

    Stake changed Activities (#3458) (#3826)
    - Add 'Read more' on `StakeSlashedContent` to open `StakeChangedModal`
     - Rename 'Slashed with' to 'Amount'
     - Rename 'Status' with 'Action'
     - Hide Amount if empty
     - Hide Rationale if empty
    
    Co-authored-by: Joystream Stats <dev@joystreamstats.live>

--- packages/ui/src/working-groups/components/Activities/StakeSlashedContent.tsx
+++ packages/ui/src/working-groups/components/Activities/StakeSlashedContent.tsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { ActivityContentComponent } from '@/common/components/Activities/ActivityContent'
 import { ButtonLink } from '@/common/components/buttons/Buttons'
 import { MemberModalLink } from '@/memberships/components/MemberModalLink'
 import { StakeSlashedActivity } from '@/working-groups/types'
 
+import { StakeChangedModal } from './ActivitiesModals/StakeChanged/StakeChangedModal'
+
 export const StakeSlashedContent: ActivityContentComponent<StakeSlashedActivity> = ({ activity, isOwn }) => {
-  const { member, groupName } = activity
+  const { eventType, member, groupName, id } = activity
+  const [isStakeSlashedModalOpen, setStakeSlashedModalOpen] = useState(false)
 
   if (isOwn) {
-    return <>Your stake was reduced by the {groupName} Working Group Lead.</>
+    return (
+      <>
+        Your stake have been slashed by the {groupName} lead.{' '}
+        <ButtonLink size="small" inline onClick={() => setStakeSlashedModalOpen(!isStakeSlashedModalOpen)}>
+          Read more
+        </ButtonLink>
+        {isStakeSlashedModalOpen && (
+          <StakeChangedModal
+            onClose={() => setStakeSlashedModalOpen(!isStakeSlashedModalOpen)}
+            eventType={eventType}
+            id={id}
+          />
+        )}
+      </>
+    )
   }
 
   return (
     <>
-      <MemberModalLink call={{ modal: 'Member', data: { id: member.id } }}>{member.handle}</MemberModalLink> has been
-      reduced by the {groupName} Working Group Lead.{' '}
-      <ButtonLink size="small" inline>
+      The stake of{' '}
+      <MemberModalLink call={{ modal: 'Member', data: { id: member.id } }}>{member.handle}</MemberModalLink> was slashed
+      by the {groupName} lead.{' '}
+      <ButtonLink size="small" inline onClick={() => setStakeSlashedModalOpen(!isStakeSlashedModalOpen)}>
         Read more
       </ButtonLink>
+      {isStakeSlashedModalOpen && (
+        <StakeChangedModal
+          onClose={() => setStakeSlashedModalOpen(!isStakeSlashedModalOpen)}
+          eventType={eventType}
+          id={id}
+        />
+      )}
     </>
   )
 }
