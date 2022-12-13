6519000d5a2aeefb97395b8e71071c51882aa7c1
Theophile Sandoz <theophile.sandoz@gmail.com>
2022-12-13 19:19:20 +0100

    Decode metadata

--- packages/ui/dev/helpers/decode.ts
+++ packages/ui/dev/helpers/decode.ts
@@ -1,19 +1,38 @@
+import * as MetaClasses from '@joystream/metadata-protobuf'
+import { AnyMetadataClass } from '@joystream/metadata-protobuf/types'
 import { createType } from '@joystream/types'
 import chalk from 'chalk'
 import yargs from 'yargs'
 
+import { metadataFromBytes } from '../../src/common/model/JoystreamNode'
+
+const MetaClassAliases = {
+  post: 'ForumPostMetadata',
+  opening: 'OpeningMetadata',
+  thread: 'ForumThreadMetadata',
+  bounty: 'BountyMetadata',
+  candidacy: 'CouncilCandidacyNoteMetadata',
+  candidate: 'CouncilCandidacyNoteMetadata',
+  application: 'ApplicationMetadata',
+  member: 'MembershipMetadata',
+  membership: 'MembershipMetadata',
+} as Record<string, keyof typeof MetaClasses>
+
 const options = {
-  type: { choices: ['text'], alias: 't', default: 'text' },
+  type: { type: 'string', alias: 't', example: ['text', 'post', 'ForumPostMetadata'], default: 'text' },
   value: { type: 'string', alias: 'b', demand: true },
 } as const
 
 type CommandOptions = yargs.InferredOptionTypes<typeof options>
 type Args = yargs.Arguments<CommandOptions>
 
-const decode = (type: Args['type'], value: string) => {
-  switch (type) {
-    case 'text':
-      return createType('Text', value).toHuman()
+const decode = (type: string, value: string): string => {
+  if (type === 'text') {
+    return createType('Text', value).toHuman()
+  } else {
+    const metaType = (MetaClassAliases[type as any] ?? type) as keyof typeof MetaClasses
+    const metadata = metadataFromBytes(MetaClasses[metaType] as AnyMetadataClass<any>, value)
+    return `${metaType} ${JSON.stringify(metadata, null, 2)}`
   }
 }
 const handler = ({ type, value }: Args) => {
