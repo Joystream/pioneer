35ddc5778ccc2fc543f35dd6a6d7b820e4861f77
l1.media <31551045+traumschule@users.noreply.github.com>
2022-12-21 16:40:35 +0000

    remove duplicate `WorkerRewardAmountUpdated`
    Co-authored-by: Theophile Sandoz <theophile.sandoz@gmail.com>

--- packages/ui/src/working-groups/types/WorkingGroupActivity/types.ts
+++ packages/ui/src/working-groups/types/WorkingGroupActivity/types.ts
@@ -20,7 +20,6 @@ export type WorkingGroupActivity =
   | OpeningFilledActivity
   | OpeningAnnouncedActivity
   | WorkerTerminatedActivity
-  | WorkerRewardAmountUpdatedActivity
   | WorkerRewardAccountUpdatedActivity
   | WorkerRewardAmountUpdatedActivity
 
