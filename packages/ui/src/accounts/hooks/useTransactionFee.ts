0950d22960dbe2590d4f8b8bbe2697fd21aef833
l1.media <31551045+traumschule@users.noreply.github.com>
2022-12-21 15:07:34 +0000

    Restore `Use Max` button on `TransferFormModal` (#1816) (#3741)
    * Restore `Use Max` button on `TransferFormModal` (#1816)
    
    * add ED
    
     https://wiki.polkadot.network/docs/build-protocol-info#existential-deposit
    
    * refactor
    
     - handle cancelled Tx
     - use ED (existential deposit) if fee is unset
    
    * fixup
    
    * types
    
    * cleanup
    
    * caniuse
    
    * deps
    
    * revert yarn.lock
    
    * revert yarn.lock
    
    Co-authored-by: Joystream Stats <dev@joystreamstats.live>

--- packages/ui/src/accounts/hooks/useTransactionFee.ts
+++ packages/ui/src/accounts/hooks/useTransactionFee.ts
@@ -1,13 +1,13 @@
 import { DependencyList, useContext, useEffect } from 'react'
 
-import { Transaction, TransactionFeesContext } from '@/common/providers/transactionFees/context'
+import { Transaction, TransactionFeesContext, Fee } from '@/common/providers/transactionFees/context'
 import { Address } from '@/common/types'
 
 export function useTransactionFee(
   signer?: Address,
   getTransaction?: () => Transaction | undefined,
   deps: DependencyList = []
-) {
+): Fee {
   const { transaction, feeInfo, setSigner, setTransaction } = useContext(TransactionFeesContext)
 
   useEffect(() => {
