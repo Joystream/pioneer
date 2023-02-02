56440c2389c852efc97bfb3b9e0053440ac09818
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-02-02 12:33:05 +0100

    Address CRs

--- packages/ui/src/common/utils/crypto/worker/client.ts
+++ packages/ui/src/common/utils/crypto/worker/client.ts
@@ -1,7 +1,7 @@
 import { uniqueId } from 'lodash'
 import { filter, firstValueFrom, fromEvent, Observable } from 'rxjs'
 
-import { compute, WorkerRequest, WorkerRequestType, WorkerResponse } from './utils'
+import { WorkerRequest, WorkerRequestType, WorkerResponse } from './utils'
 
 let worker: Worker
 let messages: Observable<MessageEvent<WorkerResponse>>
@@ -13,20 +13,16 @@ export const merkleRootFromBinary = computeInWorker('MERKLE_ROOT')
 function computeInWorker(type: WorkerRequestType) {
   return async (file: Blob): Promise<string> => {
     if (!worker) {
-      try {
-        worker = new Worker(new URL('./worker', import.meta.url), { type: 'module' })
-        messages = fromEvent<MessageEvent<WorkerResponse>>(worker, 'message')
-      } catch {
-        return compute(type, file)
-      }
+      worker = new Worker(new URL('./worker', import.meta.url), { type: 'module' })
+      messages = fromEvent<MessageEvent<WorkerResponse>>(worker, 'message')
     }
 
     const request: WorkerRequest = { type, id: uniqueId(), file }
-    const result = messages.pipe(filter(({ data }) => data.id === request.id))
+    const resultObservable = messages.pipe(filter(({ data }) => data.id === request.id))
 
     worker.postMessage(request)
 
-    const { data } = await firstValueFrom(result)
+    const { data } = await firstValueFrom(resultObservable)
     if (data.error) throw Error(data.value)
     return data.value
   }
