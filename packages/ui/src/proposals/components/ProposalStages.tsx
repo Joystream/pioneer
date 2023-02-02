4d72ce3136a8bc64d747269bc1002a6451963542
charli <chrlschwb77@gmail.com>
2023-02-02 04:55:01 +0100

    test

--- packages/ui/src/proposals/components/ProposalStages.tsx
+++ packages/ui/src/proposals/components/ProposalStages.tsx
@@ -13,7 +13,10 @@ export interface ProposalStagesProps extends ControlProps<number> {
   constitutionality?: number | string
 }
 
+const iconMap = { approved: <CheckboxIcon />, rejected: <CrossIcon />, deciding: undefined, disabled: undefined }
+
 export const ProposalStages = ({ status, updates, constitutionality = '-', value, onChange }: ProposalStagesProps) => {
+
   const rounds = useMemo(() => {
     const decidingCount = updates.filter(({ status }) => status === 'deciding').length
 
@@ -34,31 +37,35 @@ export const ProposalStages = ({ status, updates, constitutionality = '-', value
       },
       ...(isDormant
         ? repeat(
-            () => ({ icon: false, onClick: undefined }),
-            totalRoundCount !== undefined ? totalRoundCount - decidingCount - 1 : 1
-          )
+          () => ({ icon: false, onClick: undefined }),
+          totalRoundCount !== undefined ? totalRoundCount - decidingCount - 1 : 1
+        )
         : []),
     ]
   }, [updates.length, status, constitutionality])
 
+  console.log(rounds);
+
   return (
     <TabsContainer>
-      {rounds.map(({ icon, onClick }, round) => (
-        <Tooltip
-          tooltipText={
-            round !== value
-              ? 'This proposal must undergo the voting of multiple consequent councils. The result of each council vote will be displayed in the separate tabs. For the proposal to be approved, each of the councils must approve it.'
-              : 'The number of councils in that must approve the proposal in a row before it has its intended effect is more than one. The overall execution of proposal will be triggered after the last council completes the voting. Outcomes of each council voting is displayed in a separate tab.'
-          }
-        >
-          <TabContainer key={round} active={round === value} disabled={!onClick} onClick={onClick}>
-            {icon}
-            <span>
-              Council approvals {round + 1}/{constitutionality}
-            </span>
-          </TabContainer>
-        </Tooltip>
-      ))}
+      {rounds.map(({ icon, onClick }, round) => {
+        return (
+          <Tooltip
+            tooltipText={
+              round !== value
+                ? 'This proposal must undergo the voting of multiple consequent councils. The result of each council vote will be displayed in the separate tabs. For the proposal to be approved, each of the councils must approve it.'
+                : 'The number of councils in that must approve the proposal in a row before it has its intended effect is more than one. The overall execution of proposal will be triggered after the last council completes the voting. Outcomes of each council voting is displayed in a separate tab.'
+            }
+          >
+            <TabContainer key={round} active={round === value} disabled={!onClick} onClick={onClick}>
+              {icon}
+              <span>
+                Council approvals {round + 1}/{constitutionality}
+              </span>
+            </TabContainer>
+          </Tooltip>
+        )
+      })}
     </TabsContainer>
   )
 }
