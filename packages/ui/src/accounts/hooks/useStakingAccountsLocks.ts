c7557eac23fa1fccfed21ab36b76b88ee5e88008
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-02-08 12:54:47 +0100

    Set the `optOutLock` when it applies

--- packages/ui/src/accounts/hooks/useStakingAccountsLocks.ts
+++ packages/ui/src/accounts/hooks/useStakingAccountsLocks.ts
@@ -1,4 +1,5 @@
 import BN from 'bn.js'
+import { useEffect, useState } from 'react'
 
 import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
 
@@ -7,7 +8,9 @@ import { AccountOption, LockType, OptionLock } from '../types'
 
 import { useMyAccounts } from './useMyAccounts'
 import { useMyBalances } from './useMyBalances'
+import { useVotingOptOutAccounts } from './useVotingOptOutAccounts'
 
+type StakingAccount = AccountOption & { optionLocks: OptionLock[] }
 interface UseStakingAccountsLocks {
   requiredStake: BN
   lockType: LockType
@@ -20,7 +23,7 @@ export const useStakingAccountsLocks = ({
   lockType,
   recoveryConditions,
   filterByBalance,
-}: UseStakingAccountsLocks) => {
+}: UseStakingAccountsLocks): StakingAccount[] => {
   const { allAccounts } = useMyAccounts()
   const balances = useMyBalances()
   const {
@@ -28,30 +31,45 @@ export const useStakingAccountsLocks = ({
     helpers: { getMemberIdByBoundAccountAddress },
   } = useMyMemberships()
 
-  const accountsWithLocks = allAccounts.map((account) => {
-    const optionLocks: OptionLock[] = []
-    const accountAddress = account.address
-    const accountBalances = balances?.[accountAddress]
+  const [accountsWithLocks, setAccountWithLocks] = useState<StakingAccount[]>(
+    allAccounts.map((account) => {
+      const optionLocks: OptionLock[] = []
+      const accountAddress = account.address
+      const accountBalances = balances?.[accountAddress]
 
-    const boundMembershipId = getMemberIdByBoundAccountAddress(accountAddress)
-    if (accountBalances && accountBalances.total?.lt(requiredStake) && filterByBalance) {
-      optionLocks.push('insufficientFunds')
-    }
-
-    if (boundMembershipId && boundMembershipId !== activeMember?.id && lockType !== 'Voting') {
-      optionLocks.push('boundMembership')
-    }
-
-    if (accountBalances && areLocksConflicting(lockType, accountBalances.locks)) {
-      if (isRecoverable(lockType, recoveryConditions)) {
-        optionLocks.push('recoverableLock')
-      } else {
-        optionLocks.push('rivalrousLock')
+      const boundMembershipId = getMemberIdByBoundAccountAddress(accountAddress)
+      if (accountBalances && accountBalances.total?.lt(requiredStake) && filterByBalance) {
+        optionLocks.push('insufficientFunds')
       }
-    }
 
-    return { ...account, optionLocks }
-  })
+      if (boundMembershipId && boundMembershipId !== activeMember?.id && lockType !== 'Voting') {
+        optionLocks.push('boundMembership')
+      }
 
-  return accountsWithLocks as AccountOption[]
+      if (accountBalances && areLocksConflicting(lockType, accountBalances.locks)) {
+        if (isRecoverable(lockType, recoveryConditions)) {
+          optionLocks.push('recoverableLock')
+        } else {
+          optionLocks.push('rivalrousLock')
+        }
+      }
+
+      return { ...account, optionLocks }
+    })
+  )
+
+  const isVoting = lockType === 'Voting'
+  const votingOptOutAccounts = useVotingOptOutAccounts({ skip: !isVoting })
+  useEffect(() => {
+    if (!isVoting) return
+
+    setAccountWithLocks(
+      accountsWithLocks.map(({ address, optionLocks, ...account }): StakingAccount => {
+        const isOptOut = votingOptOutAccounts.includes(address)
+        return { address, ...account, optionLocks: isOptOut ? [...optionLocks, 'optOutLock'] : optionLocks }
+      })
+    )
+  }, [votingOptOutAccounts])
+
+  return accountsWithLocks
 }
