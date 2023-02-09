7d0f49a07c620d991c7e28fcd9eb0b9d4b1e871a
Victor <emmyvic98@gmail.com>
2023-02-09 22:57:08 +0000

    refactored custom hook

--- packages/ui/src/forum/hooks/useForumCategoryThreadPage.ts
+++ packages/ui/src/forum/hooks/useForumCategoryThreadPage.ts
@@ -2,9 +2,5 @@ export const useForumCategoryThreadPage = () => {
   const locationHash = window.location.hash
   const queryString = locationHash.substring(locationHash.indexOf('?'))
   const urlParams = new URLSearchParams(queryString)
-  if (urlParams.has('page')) {
-    const num = Number(urlParams.get('page')) || 1
-    return num
-  }
-  return 1
+  return Number(urlParams.get('page')) || 1
 }
