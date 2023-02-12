357635e804f911f9cc98f2d355794b7d9c8cdd7d
charli <chrlschwb77@gmail.com>
2023-02-12 13:30:46 +0100

    fix test|

--- packages/ui/src/proposals/modals/AddNewProposal/components/TriggerAndDiscussionStep.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/components/TriggerAndDiscussionStep.tsx
@@ -68,7 +68,7 @@ export const TriggerAndDiscussionStep = () => {
               id="triggerBlock"
               placeholder="0"
               name="triggerAndDiscussion.triggerBlock"
-              maxAllowedValue={2}
+              maxAllowedValue={Math.pow(2, 32)}
             />
           </InputComponent>
         )}
