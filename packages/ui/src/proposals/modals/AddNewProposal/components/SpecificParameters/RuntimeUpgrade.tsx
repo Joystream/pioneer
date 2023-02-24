323fc71f8c5f6c4df37e6e5bdd6b1986f3abe5b6
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2023-02-24 11:45:07 +0400

    better comments for maybeDecompressRuntimeBlob()

--- packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/RuntimeUpgrade.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/RuntimeUpgrade.tsx
@@ -26,17 +26,20 @@ const isDragEvent = (event: any): event is React.DragEvent<HTMLElement> => !!eve
 const isChangeEvent = (event: any): event is React.ChangeEvent<HTMLInputElement> => !!event?.target?.files
 
 // https://github.com/paritytech/substrate/blob/master/primitives/maybe-compressed-blob/src/lib.rs
-// Look for 8 byte magic prefix in blob that indicates it is compressed with zstd algorithm
-// If is compressed, strips the prefix and decompresses remaining bytes, otherwise returns original blob
-const maybeDecompressRuntimeBlob = async (blob: ArrayBuffer): Promise<Buffer> => {
+// Convert blob to Buffer type to make it easier work with (compare|subarray),
+// although it is adding a copy step.
+// Looks for 8-byte magic prefix in blob to determine if it is compressed with zstd algorithm.
+// If compressed, strips the prefix and decompresses remaining bytes returning
+// decompressed value, otherwise returns original blob.
+const maybeDecompressRuntimeBlob = async (blob: ArrayBuffer): Promise<Buffer | Uint8Array> => {
   const ZSTD_PREFIX = Buffer.from([82, 188, 83, 118, 70, 219, 142, 5])
-  let wasm = Buffer.from(blob)
-  const prefix = wasm.slice(0, 8)
+  let wasm: Buffer | Uint8Array = Buffer.from(blob)
+  const prefix = wasm.subarray(0, 8)
   const isCompressed = Buffer.compare(prefix, ZSTD_PREFIX) === 0
   if (isCompressed) {
     const { ZstdStream } = await ZstdInit()
     // strip the prefix and decompress the rest
-    wasm = Buffer.from(ZstdStream.decompress(wasm.subarray(8)))
+    wasm = ZstdStream.decompress(wasm.subarray(8))
   }
   return wasm
 }
@@ -59,9 +62,10 @@ const getValidatedFiles = async (event: DropEvent): Promise<ValidatedFile[]> => 
   for (let i = 0; i < fileList.length; i++) {
     const file = fileList.item(i)
     if (file) {
-      const wasm = await maybeDecompressRuntimeBlob(await file.arrayBuffer())
-      const isValidWASM = await WebAssembly.validate(wasm)
+      const uncompressedRuntime = await maybeDecompressRuntimeBlob(await file.arrayBuffer())
+      const isValidWASM = await WebAssembly.validate(uncompressedRuntime)
       Object.assign(file, { isValidWASM })
+      // Original file (not the decompressed one)
       files.push(file)
     }
   }
