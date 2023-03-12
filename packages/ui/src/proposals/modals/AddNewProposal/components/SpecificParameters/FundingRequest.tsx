ebd58a393fb5cc4f6bc2c6867d112580b5d38914
Victor <emmyvic98@gmail.com>
2023-03-12 08:56:53 +0000

    toggle and prompt added

--- packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/FundingRequest.tsx
+++ packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/FundingRequest.tsx
@@ -1,19 +1,61 @@
 import React from 'react'
+import { useFormContext } from 'react-hook-form'
 
 import { SelectAccount } from '@/accounts/components/SelectAccount'
 import { CurrencyName } from '@/app/constants/currency'
-import { InputComponent, TokenInput } from '@/common/components/forms'
+import { InlineToggleWrap, InputComponent, Label, ToggleCheckbox, TokenInput } from '@/common/components/forms'
 import { Row } from '@/common/components/Modal'
 import { RowGapBlock } from '@/common/components/page/PageContent'
-import { TextMedium } from '@/common/components/typography'
+import { Tooltip, TooltipDefault } from '@/common/components/Tooltip'
+import { TextMedium, TextSmall, TextInlineSmall } from '@/common/components/typography'
+import { Prompt } from './Prompt'
 
 export const FundingRequest = () => {
+  const { watch, setValue } = useFormContext()
+  const [payMultiple] = watch(['fundingRequest.payMultiple'])
   return (
     <RowGapBlock gap={24}>
       <Row>
         <RowGapBlock gap={8}>
           <h4>Specific parameters</h4>
           <TextMedium lighter>Funding Request</TextMedium>
+        </RowGapBlock>
+      </Row>
+      <Row>
+        <RowGapBlock gap={24}>
+          <Row>
+            <InlineToggleWrap>
+              <Label>Pay multiple</Label>
+              <Tooltip
+                tooltipTitle="Pay multiple"
+                tooltipText="For multiple accounts and amounts, follow this CSV pattern:<br/> 
+              account1, amount1<br/> 
+              account2, amount2<br/> 
+              ...<br/> 
+              account20, amount20"
+              >
+                <TooltipDefault />
+              </Tooltip>
+            </InlineToggleWrap>
+            <ToggleCheckbox falseLabel="No" trueLabel="Yes" name="fundingRequest.payMultiple" />
+          </Row>
+          {payMultiple && (
+            <Row>
+              <Prompt>
+                <TextSmall>
+                  For <TextInlineSmall bold>multiple accounts and amounts</TextInlineSmall>, follow this CSV pattern:
+                  <br />
+                  account1, amount1
+                  <br />
+                  account2, amount2
+                  <br />
+                  ...
+                  <br />
+                  account20, amount20
+                </TextSmall>
+              </Prompt>
+            </Row>
+          )}
         </RowGapBlock>
       </Row>
       <Row>
---
+++ packages/ui/src/proposals/modals/AddNewProposal/components/SpecificParameters/Prompt.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import { QuestionIcon } from '@/common/components/icons'
+import { BorderRad, Colors } from '@/common/constants'
+
+export interface PromptProps {
+  children: React.ReactNode
+}
+
+export const Prompt = ({ children }: PromptProps) => {
+  return (
+    <PromptContainer>
+      <IconSection>
+        <PromptQuestion>
+          <QuestionIcon />
+        </PromptQuestion>
+      </IconSection>
+      {children}
+    </PromptContainer>
+  )
+}
+const PromptContainer = styled.div`
+  display: grid;
+  grid-template-columns: 48px 1fr;
+  border-left: 4px solid ${Colors.Blue[200]};
+  color: ${Colors.Black[500]};
+`
+const IconSection = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+const PromptQuestion = styled.div`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  width: 16px;
+  height: 16px;
+  border-radius: ${BorderRad.full};
+  border: 1px solid ${Colors.Black[900]};
+`
