693d754d11b83b8c95b0654d2c213686e6e1918c
Joystream Stats <dev@joystreamstats.live>
2023-03-13 19:33:14 +0000

    fix offset

--- packages/ui/src/app/pages/Forum/components/ForumTabs.tsx
+++ packages/ui/src/app/pages/Forum/components/ForumTabs.tsx
@@ -8,7 +8,7 @@ import { useMyThreads, UseMyThreadsProps } from '@/forum/hooks/useMyThreads'
 const order = { orderKey: 'updatedAt', isDescending: true }
 
 export const ForumTabs = () => {
-  const { totalCount } = useMyThreads({ page: 0, threadsPerPage: 1000, order } as UseMyThreadsProps)
+  const { totalCount } = useMyThreads({ page: 1, order } as UseMyThreadsProps)
   const myThreadsCount = totalCount
   const tabs = usePageTabs([
     ['Forum', ForumRoutes.forum],
