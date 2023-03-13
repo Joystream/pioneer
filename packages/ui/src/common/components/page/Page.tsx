3284fdd48e2f53d8d5f7329d6641c6549d954884
Joystream Stats <dev@joystreamstats.live>
2023-03-13 16:37:42 +0000

    shrink Sidebar

--- packages/ui/src/common/components/page/Page.tsx
+++ packages/ui/src/common/components/page/Page.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Page = styled.div`
   display: grid;
   position: relative;
-  grid-template-columns: 226px 1fr;
+  grid-template-columns: 10px 1fr;
   grid-template-rows: auto;
   grid-template-areas: 'navbar page';
   width: 100vw;
