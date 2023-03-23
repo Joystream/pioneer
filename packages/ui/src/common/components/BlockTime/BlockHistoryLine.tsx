ac5f87a34cd126a64c9d88b9c79dc1894bb91417
charli <chrlschwb77@gmail.com>
2023-03-23 13:58:10 +0100

    fix histoy of propsals

--- packages/ui/src/common/components/BlockTime/BlockHistoryLine.tsx
+++ packages/ui/src/common/components/BlockTime/BlockHistoryLine.tsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Tooltip, TooltipDefault } from '@/common/components/Tooltip'
+import { Link } from '@/common/components/Link'
 import { TextInlineSmall } from '@/common/components/typography'
-import { Fonts } from '@/common/constants'
+import { Fonts, Colors } from '@/common/constants'
+import { useNetworkEndpoints } from '@/common/hooks/useNetworkEndpoints'
 import { formatDateString } from '@/common/model/formatters'
 
-import { BlockInfo, BlockInfoProp } from './BlockInfo'
+import { BlockInfoProp } from './BlockInfo'
 
-export const BlockHistoryLine = ({ block }: BlockInfoProp) => (
-  <BlockHistoryLineContainer>
-    <TextInlineSmall lighter>{formatDateString(block.timestamp)}</TextInlineSmall>
-    <Tooltip popupContent={<BlockInfo block={block} />}>
-      <TooltipDefault />
-    </Tooltip>
-  </BlockHistoryLineContainer>
-)
+export const BlockHistoryLine = ({ block }: BlockInfoProp) => {
+  const [endpoints] = useNetworkEndpoints()
+  return (
+    <BlockHistoryLineContainer>
+      <BlockLink
+        href={`https://polkadot.js.org/apps/?rpc=${endpoints.nodeRpcEndpoint}/ws-rpc#/explorer/query/${block.number}`}
+      >
+        <TextInlineSmall lighter>{formatDateString(block.timestamp)}</TextInlineSmall>
+      </BlockLink>
+    </BlockHistoryLineContainer>
+  )
+}
 
+const BlockLink = styled(Link)`
+  svg,
+  span {
+    color: ${Colors.Black[900]};
+  }
+  :hover {
+    svg,
+    span {
+      color: ${Colors.LogoPurple};
+    }
+  }
+`
 const BlockHistoryLineContainer = styled.div`
   display: flex;
   align-items: center;
