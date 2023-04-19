c102dce1c5364372762f51199cedb4d76c4c5c5a
eshark9312 <eshark9312@outlook.com>
2023-04-19 10:11:24 -0700

    Fix subscribe button, trigger membership modal

--- packages/ui/src/app/pages/Settings/EmailNotifications.tsx
+++ packages/ui/src/app/pages/Settings/EmailNotifications.tsx
@@ -6,25 +6,54 @@ import { SettingsTabs } from './components/SettingsTabs'
 import { EmptyPagePlaceholder } from '@/common/components/EmptyPagePlaceholder/EmptyPagePlaceholder'
 import { TransactionButton } from '@/common/components/buttons/TransactionButton'
 
-export const EmailNotifications = () => (
-  <PageLayout
-    header={
-      <PageHeader
-        title="Settings"
-        tabs={<SettingsTabs />}
-      />
-    }
-    main={
-      <EmptyPagePlaceholder
-        title="Subscribe to email notifications"
-        copy="We use your email only to send you important notifications.
+import { useMyMemberships } from '@/memberships/hooks/useMyMemberships'
+import { MainPanel } from '@/common/components/page/PageContent'
+
+export const EmailNotifications = () => {
+  const { members, active, hasMembers } = useMyMemberships()
+  // The informations related to notification setttings like EmailAddress
+  //   can be in the MembershipsContext
+  /*
+  const { email, verified, notifyset } = active.email
+ */
+  ///////////////////////////////////////////////////////////////////////       mock      /////////////////
+  const { email, verified, notifyset } = { email: null, verified: true, notifyset: true }
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const [subscribed, setSubscribed] = useState(!!email)
+  const [_email, setEmail] = useState(email ?? '')
+
+  const subscribe = () => {
+    let inputstr
+    !hasMembers
+      ? (alert('here openModal function comes'), //here openModal function comes
+        (inputstr = prompt('Email notifications verify modal', 'default@email.com')), //here email verify modal comes
+        setEmail(inputstr ?? ''))
+      : ((inputstr = prompt('Email notifications verify modal', 'default@email.com')), //here email verify modal comes
+        setEmail(inputstr ?? ''))
+    setSubscribed(true)
+  }
+	
+  return (
+    <PageLayout
+      header={<PageHeader title="Settings" tabs={<SettingsTabs />} />}
+      main={
+        !subscribed ? (
+          <EmptyPagePlaceholder
+            title="Subscribe to email notifications"
+            copy="We use your email only to send you important notifications.
       You can customize what kind of notifications you receive anytime in settings."
-        button={
-          <TransactionButton style="primary" size="medium" >
-            Subscribe
-          </TransactionButton>
-        }
-      />
-    }
-  />
-)
+            button={
+              <TransactionButton style="primary" size="medium" onClick={subscribe}>
+                Subscribe
+              </TransactionButton>
+            }
+          />
+        ) : (
+          <MainPanel>
+          </MainPanel>
+        )
+      }
+    />
+  )
+}
