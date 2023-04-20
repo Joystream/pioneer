caa7d38f0ca4819cdea8fa79626c918c3f611ba0
AnttoLion <anttolion@gmail.com>
2023-04-20 18:44:43 +0900

    fix/4236-Email_Noti-Backend_unavailable_errors -1

--- packages/ui/src/app/components/BackendError.tsx
+++ packages/ui/src/app/components/BackendError.tsx
@@ -4,15 +4,17 @@ import { CenterNotification } from '@/common/components/page/CenterNotification'
 
 export function BackendErrorNotification() {
 
-  // return (
-  //   <CenterNotification
-  //     onClick={() => setShowNotification(undefined)}
-  //     title="Error"
-  //     message={`The backend server is down. Please try again later`}
-  //     isError
-  //     showClose
-  //   />
-  // )
+  /* --- diplay BE error modal component ---/
+  return (
+    <CenterNotification
+      onClick={() => setShowNotification(undefined)}
+      title="Error"
+      message={`The backend server is down. Please try again later`}
+      isError
+      showClose
+    />
+  )
+  */
 
   return null
 }
