0512aef90ef5e071ef7748c67cfa46456992ecfa
charli <chrlschwb77@gmail.com>
2023-04-21 03:41:19 +0200

    fix

--- packages/ui/src/common/components/BlockTime/BlockHistoryLine.tsx
+++ packages/ui/src/common/components/BlockTime/BlockHistoryLine.tsx
@@ -2,40 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { BlockTime } from '@/common/components/BlockTime'
-import { Link } from '@/common/components/Link'
 import { TextInlineSmall } from '@/common/components/typography'
-import { Fonts, Colors } from '@/common/constants'
-import { useNetworkEndpoints } from '@/common/hooks/useNetworkEndpoints'
+import { Fonts } from '@/common/constants'
 
 // import { formatDateString } from '@/common/model/formatters'
 import { BlockInfoProp } from './BlockInfo'
 
 export const BlockHistoryLine = ({ block }: BlockInfoProp) => {
-  const [endpoints] = useNetworkEndpoints()
   return (
     <BlockHistoryLineContainer>
-      <BlockLink
-        href={`https://polkadot.js.org/apps/?rpc=${endpoints.nodeRpcEndpoint}/ws-rpc#/explorer/query/${block.number}`}
-      >
-        <BlockTime block={block} layout="reverse-start" lessInfo />
-      </BlockLink>
+      <BlockTime block={block} layout="reverse-start" lessInfo />
+
       {/* <TextInlineSmall lighter>{formatDateString(block.timestamp)}</TextInlineSmall> */}
     </BlockHistoryLineContainer>
   )
 }
 
-const BlockLink = styled(Link)`
-  svg,
-  span {
-    color: ${Colors.Black[900]};
-  }
-  :hover {
-    svg,
-    span {
-      color: ${Colors.LogoPurple};
-    }
-  }
-`
 const BlockHistoryLineContainer = styled.div`
   display: flex;
   align-items: center;
--- packages/ui/src/common/components/BlockTime/BlockInfo.tsx
+++ packages/ui/src/common/components/BlockTime/BlockInfo.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 
 import { BlockIcon } from '@/common/components/icons'
+import { Link } from '@/common/components/Link'
 import { TextSmall } from '@/common/components/typography'
 import { Colors } from '@/common/constants'
+import { useNetworkEndpoints } from '@/common/hooks/useNetworkEndpoints'
 import { formatTokenValue } from '@/common/model/formatters'
 import { Block } from '@/common/types'
-
 export interface BlockInfoProp {
   block: Block
   lessInfo?: boolean
@@ -14,18 +15,34 @@ export interface BlockInfoProp {
 }
 
 export const BlockInfo = ({ block, lessInfo, inline }: BlockInfoProp) => {
+  const [endpoints] = useNetworkEndpoints()
   return (
-    <BlockInfoContainer lessInfo={lessInfo} inline={inline}>
-      <BlockIcon />
-      <span>
-        {formatTokenValue(block.number)} {lessInfo && 'block'}
-      </span>
-      {/* {!lessInfo && <BlockNetworkInfo>on {block.network} network</BlockNetworkInfo>} */}
-    </BlockInfoContainer>
+    <BlockLink
+      href={`https://polkadot.js.org/apps/?rpc=${endpoints.nodeRpcEndpoint}/ws-rpc#/explorer/query/${block.number}`}
+    >
+      <BlockInfoContainer lessInfo={lessInfo} inline={inline}>
+        <BlockIcon />
+        <span>
+          {formatTokenValue(block.number)} {lessInfo && 'block'}
+        </span>
+        {/* {!lessInfo && <BlockNetworkInfo>on {block.network} network</BlockNetworkInfo>} */}
+      </BlockInfoContainer>
+    </BlockLink>
   )
 }
 
-
+const BlockLink = styled(Link)`
+  svg,
+  span {
+    color: ${Colors.Black[900]};
+  }
+  :hover {
+    svg,
+    span {
+      color: ${Colors.LogoPurple};
+    }
+  }
+`
 export const BlockInfoContainer = styled.span<Omit<BlockInfoProp, 'block'>>`
   display: ${({ inline }) => (inline ? 'inline-grid' : 'grid')};
   grid-auto-flow: column;
