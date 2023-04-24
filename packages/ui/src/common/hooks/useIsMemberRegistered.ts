e32b8bbe2213fc3c0b913028d2df23f838c2b8ec
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-04-24 13:30:58 +0200

    Add `UseIsMemberRegistered` hook

---
+++ packages/ui/src/common/hooks/useIsMemberRegistered.ts
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client'
+import { isUndefined } from 'lodash'
+
+import { useBackend } from './useBackend'
+
+const query = gql`
+  query MemberExist($id: Int) {
+    memberExist(id: $id)
+  }
+`
+
+type UseIsMemberRegistered = { isRegistered?: boolean; error?: Error }
+
+export const useIsMemberRegistered = (memberId: number | undefined): UseIsMemberRegistered => {
+  const { data, error } = useBackend<{ memberExist: boolean | null }>(
+    { query, variables: { id: memberId } },
+    isUndefined(memberId)
+  )
+
+  return { isRegistered: data?.memberExist ?? undefined, error }
+}
