2478cb1d647528cb72bd8465e166446e2ce8d8ae
ilya Smiyukha <ilya.smiyukha@touchcast.com>
2023-05-15 16:13:19 +0300

    lint fix

--- packages/ui/src/memberships/modals/EmailSubscriptionModal/EmailSubscriptionModal.stories.tsx
+++ packages/ui/src/memberships/modals/EmailSubscriptionModal/EmailSubscriptionModal.stories.tsx
@@ -8,38 +8,37 @@ import { MockApolloProvider } from '@/mocks/components/storybook/MockApolloProvi
 export default {
   title: 'Member/Modals/EmailSubscriptionModal',
   component: EmailSubscriptionModal,
-  member: {}
+  member: {},
 } as Meta
 
 const Template: Story = () => {
   return (
-      <MockApolloProvider members>
-         <ModalContext.Provider
-          value={{
-            hideModal: () => undefined,
-            modal: 'foo',
-            showModal: () => undefined,
-            modalData: {
-              id: '0',
-              name: 'Alice member',
-              rootAccount: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-              controllerAccount: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-              handle: 'alice',
-              isVerified: false,
-              isFoundingMember: false,
-              isCouncilMember: false,
-              roles: [],
-              boundAccounts: [],
-              inviteCount: 0,
-              createdAt: '',
-            },
-          }}
-        >
-          <EmailSubscriptionModal />
-        </ModalContext.Provider>
-      </MockApolloProvider>
+    <MockApolloProvider members>
+      <ModalContext.Provider
+        value={{
+          hideModal: () => undefined,
+          modal: 'foo',
+          showModal: () => undefined,
+          modalData: {
+            id: '0',
+            name: 'Alice member',
+            rootAccount: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+            controllerAccount: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+            handle: 'alice',
+            isVerified: false,
+            isFoundingMember: false,
+            isCouncilMember: false,
+            roles: [],
+            boundAccounts: [],
+            inviteCount: 0,
+            createdAt: '',
+          },
+        }}
+      >
+        <EmailSubscriptionModal />
+      </ModalContext.Provider>
+    </MockApolloProvider>
   )
 }
 
 export const Default = Template.bind({})
-
