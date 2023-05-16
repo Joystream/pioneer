62df0b3586b935ace8961be3c0bf14782dd17863
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-05-16 10:34:47 +0200

    ⏳ Pass block time to `council:elect` (#4229)
    * Pass block time to `council:elect`
    
    * Pick a stage with `node-mocks council:elect`
    
    * Fix on chains without sudo

--- packages/ui/dev/node-mocks/commands/council/elect.ts
+++ packages/ui/dev/node-mocks/commands/council/elect.ts
@@ -1,3 +1,6 @@
+import yargs from 'yargs'
+
+import { MILLISECONDS_PER_BLOCK } from '../../../../src/common/model/formatters'
 import { nextCouncilStageCommand } from '../../../helpers/nextCouncilStage'
 import { withApi } from '../../lib/api'
 import { createMembersCommand } from '../members/create'
@@ -6,17 +9,35 @@ import { announceCandidaciesCommand } from './announce'
 import { revealVotesCommand } from './reveal'
 import { castVotesCommand } from './vote'
 
+const options = {
+  blockTime: {
+    number: true,
+    default: MILLISECONDS_PER_BLOCK,
+    alias: 'd',
+  },
+  to: {
+    choices: ['VOTE', 'REVEAL', 'IDLE'],
+    default: 'IDLE',
+    alias: 't',
+  },
+}
+
 export const electCouncilModule = {
   command: 'council:elect',
   describe: 'Elect a full council',
-  handler: async () => {
+  handler: async ({ blockTime, to }: yargs.InferredOptionTypes<typeof options>) => {
     await createMembersCommand()
     await withApi(async (api) => {
       await announceCandidaciesCommand(api)
-      await nextCouncilStageCommand(api)
+      if (to === 'VOTE') return
+      await nextCouncilStageCommand(api, blockTime)
+
       await castVotesCommand(api)
-      await nextCouncilStageCommand(api)
+      if (to === 'REVEAL') return
+      await nextCouncilStageCommand(api, blockTime)
+
       await revealVotesCommand(api)
     })
   },
+  builder: (argv: yargs.Argv<unknown>) => argv.options(options),
 }
