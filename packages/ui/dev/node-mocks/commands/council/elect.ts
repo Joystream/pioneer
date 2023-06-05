d46fce3748c33421d7154598ecd66cd235f28f7f
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-06-05 10:08:31 +0200

    🗝️ Fix `opening:create` on the liberated chain (#4383)
    * Enable ts aliases in scripts
    
    * Make `council:elect` callable from other scripts
    
    * Create proposals with new staking accounts
    Because all wellknown accounts are locked once the council is elected.
    
    * Add a `approveProposal` script
    
    * Fix `opening:create` on the liberated chain
    
    * Document `-d BLOCK_TIME`
    
    * Fix `useProposalConstants`

--- packages/ui/dev/node-mocks/commands/council/elect.ts
+++ packages/ui/dev/node-mocks/commands/council/elect.ts
@@ -1,15 +1,16 @@
+import { ApiPromise } from '@polkadot/api'
 import yargs from 'yargs'
 
-import { MILLISECONDS_PER_BLOCK } from '../../../../src/common/model/formatters'
+import { MILLISECONDS_PER_BLOCK } from '@/common/model/formatters'
+
 import { nextCouncilStageCommand } from '../../../helpers/nextCouncilStage'
 import { withApi } from '../../lib/api'
-import { createMembersCommand } from '../members/create'
 
 import { announceCandidaciesCommand } from './announce'
 import { revealVotesCommand } from './reveal'
 import { castVotesCommand } from './vote'
 
-const options = {
+export const electCouncilOptions = {
   blockTime: {
     number: true,
     default: MILLISECONDS_PER_BLOCK,
@@ -22,22 +23,33 @@ const options = {
   },
 }
 
+type Args = yargs.InferredOptionTypes<typeof electCouncilOptions>
+type Props = Partial<Args> & { replaceCurrent?: boolean }
+
+export const electCouncilCommand = async (
+  api: ApiPromise,
+  { blockTime = MILLISECONDS_PER_BLOCK, to = 'IDLE', replaceCurrent = true }: Props
+) => {
+  if (!replaceCurrent) {
+    const councilors = await api.query.council.councilMembers()
+    if (councilors.length > 0) return
+  }
+
+  await announceCandidaciesCommand(api)
+  await nextCouncilStageCommand(api, blockTime)
+  if (to === 'VOTE') return
+
+  await castVotesCommand(api)
+  await nextCouncilStageCommand(api, blockTime)
+  if (to === 'REVEAL') return
+
+  await revealVotesCommand(api)
+  await nextCouncilStageCommand(api, blockTime)
+}
+
 export const electCouncilModule = {
   command: 'council:elect',
   describe: 'Elect a full council',
-  handler: async ({ blockTime, to }: yargs.InferredOptionTypes<typeof options>) => {
-    await createMembersCommand()
-    await withApi(async (api) => {
-      await announceCandidaciesCommand(api)
-      if (to === 'VOTE') return
-      await nextCouncilStageCommand(api, blockTime)
-
-      await castVotesCommand(api)
-      if (to === 'REVEAL') return
-      await nextCouncilStageCommand(api, blockTime)
-
-      await revealVotesCommand(api)
-    })
-  },
-  builder: (argv: yargs.Argv<unknown>) => argv.options(options),
+  handler: ({ blockTime, to }: Args) => withApi((api) => electCouncilCommand(api, { blockTime, to })),
+  builder: (argv: yargs.Argv<unknown>) => argv.options(electCouncilOptions),
 }
--- packages/ui/dev/node-mocks/commands/proposals/create.ts
+++ packages/ui/dev/node-mocks/commands/proposals/create.ts
@@ -2,12 +2,18 @@
 import { access, readFile } from 'fs/promises'
 import { isAbsolute, resolve } from 'path'
 
+import { ApiPromise } from '@polkadot/api'
+import { PalletProposalsCodexProposalDetails } from '@polkadot/types/lookup'
+import { mnemonicGenerate } from '@polkadot/util-crypto'
 import yargs from 'yargs'
 
-import { createType } from '../../../../src/common/model/createType'
-import { getDataFromEvent } from '../../../../src/common/model/JoystreamNode'
-import memberData from '../../../../src/mocks/data/raw/members.json'
-import { signAndSend, withApi } from '../../lib/api'
+import { createType } from '@/common/model/createType'
+import { getDataFromEvent } from '@/common/model/JoystreamNode'
+import memberData from '@/mocks/data/raw/members.json'
+import { proposalConstants } from '@/proposals/hooks/useProposalConstants'
+import { typenameToProposalDetails } from '@/proposals/model/proposalDetails'
+
+import { keyring, signAndSend, withApi } from '../../lib/api'
 import { createMembersCommand } from '../members/create'
 
 const options = {
@@ -21,36 +27,37 @@ const options = {
 type CommandOptions = yargs.InferredOptionTypes<typeof options>
 export type Args = yargs.Arguments<CommandOptions>
 
-const createProposal = (args: Args) => {
-  withApi(async (api) => {
-    const aliceMember = memberData[0]
+export const createProposal = async (api: ApiPromise, proposalDetails: PalletProposalsCodexProposalDetails) => {
+  const { id: memberId, controllerAccount } = memberData[0]
 
-    // Create accounts
-    const nextId = await api.query.members.nextMemberId()
-    if (Number(nextId) < 1) {
-      await createMembersCommand()
-    }
+  const proposalType = typenameToProposalDetails(proposalDetails.type)
+  const requiredStake = proposalConstants(api, proposalType)?.requiredStake
+  if (!requiredStake) throw `Not supported proposal ${proposalDetails.type}`
 
-    // Create proposal
-    const id = aliceMember.id
-    const address = aliceMember.controllerAccount
+  // 1. Create and fund the staking account
+  const stakingSigner = keyring.createFromUri(mnemonicGenerate(), { name: 'proposal author' })
+  const stackingAccount = stakingSigner.address
+  const fundingTx = api.tx.balances.transfer(stackingAccount, requiredStake.muln(1.5))
+  await signAndSend(fundingTx, controllerAccount)
 
-    const commonParams = {
-      memberId: id,
-      title: `Lorem ${Object.keys(args)[0]}`,
-      description: JSON.stringify(args, null, 2),
-      stakingAccountId: address,
-    }
-    const proposalDetails = await specificParams(args)
+  // 2. Bind the staking account to Alice
+  await signAndSend(api.tx.members.addStakingAccountCandidate(memberId), stakingSigner)
+  await signAndSend(api.tx.members.confirmStakingAccount(memberId, stackingAccount), controllerAccount)
 
-    const tx = api.tx.proposalsCodex.createProposal(commonParams, proposalDetails)
-    const events = await signAndSend(tx, address)
+  // 3. Create the proposal
+  const commonParams = {
+    memberId,
+    title: `Lorem ${proposalDetails.type}`,
+    description: JSON.stringify(proposalDetails.toJSON(), null, 2),
+    stakingAccountId: stackingAccount,
+  }
+  const tx = api.tx.proposalsCodex.createProposal(commonParams, proposalDetails)
+  const events = await signAndSend(tx, controllerAccount)
 
-    const proposalId = Number(getDataFromEvent(events, 'proposalsCodex', 'ProposalCreated'))
-    const proposalData = getDataFromEvent(events, 'proposalsCodex', 'ProposalCreated', 1)
-    const threadId = Number(getDataFromEvent(events, 'proposalsDiscussion', 'ThreadCreated'))
-    console.log({ proposalId, ...proposalData?.toJSON(), threadId })
-  })
+  const proposalId = Number(getDataFromEvent(events, 'proposalsCodex', 'ProposalCreated'))
+  const proposalData = getDataFromEvent(events, 'proposalsCodex', 'ProposalCreated', 1)
+  const threadId = Number(getDataFromEvent(events, 'proposalsDiscussion', 'ThreadCreated'))
+  return [proposalId, threadId, proposalData] as const
 }
 
 const specificParams = async (args: Args) => {
@@ -82,6 +89,13 @@ const filePath = async (path: string) => {
 export const createProposalModule = {
   command: 'proposal:create',
   describe: 'Create a proposal',
-  handler: createProposal,
+  handler: async (args: Args) => {
+    withApi(async (api) => {
+      await createMembersCommand(api)
+      const proposalDetails = await specificParams(args)
+      const [proposalId, threadId, proposalData] = await createProposal(api, proposalDetails)
+      console.log({ proposalId, ...(proposalData?.toJSON() ?? []), threadId })
+    })
+  },
   builder: (argv: yargs.Argv<unknown>) => argv.options(options),
 }
