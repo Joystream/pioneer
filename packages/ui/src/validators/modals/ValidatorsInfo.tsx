f972f42367341babde9e0fd0672615dffeff79d0
eshark9312 <eshark9312@outlook.com>
2023-06-12 18:15:21 -0700

    fix validator page notification modal

--- packages/ui/src/validators/modals/ValidatorsInfo.tsx
+++ packages/ui/src/validators/modals/ValidatorsInfo.tsx
@@ -1,73 +1,75 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import { Modal, ModalBody, ModalFooter, ModalHeader } from '../../common/components/Modal'
 import { RowGapBlock } from '@/common/components/page/PageContent'
-import { TextMedium } from '../../common/components/typography'
-import { Link } from 'react-router-dom'
-import { Colors } from '../../common/constants'
+import { useLocalStorage } from '@/common/hooks/useLocalStorage'
+import { useModal } from '@/common/hooks/useModal'
+
 import { ButtonPrimary } from '../../common/components/buttons'
 import { Checkbox } from '../../common/components/forms'
 import { ArrowRightIcon } from '../../common/components/icons'
-import { useModal } from '@/common/hooks/useModal'
-import { useLocalStorage } from '@/common/hooks/useLocalStorage'
+import { Modal, ModalBody, ModalFooter, ModalHeader } from '../../common/components/Modal'
+import { TextMedium } from '../../common/components/typography'
+import { Colors } from '../../common/constants'
+import { useToggle } from '@/common/hooks/useToggle'
 
 export const ValidatorsInfo = () => {
-  
   const title = 'Nominating validators on Joystream'
   const buttonName = 'Start nominating'
-
+  const [check, setCheck] = useToggle(false)
   const { hideModal } = useModal()
-  const [ pageCheck, showInfo] = useLocalStorage<Boolean>('ValidatorsPageCheck')
+  const [pageCheck, showInfo] = useLocalStorage<boolean>('ValidatorsPageCheck')
 
   const closeModal = () => {
     hideModal()
-    showInfo(true)
+    if (check) showInfo(true)
   }
-  
-  if (!pageCheck) return (
-    <Modal modalSize="s" onClose={closeModal}>
-      <ModalHeader title={title} onClick={closeModal} />
-      <ModalBody>
-        <RowGapBlock gap={16}>
-          <RowGapBlock gap={24}>
+
+  if (!pageCheck)
+    return (
+      <Modal modalSize="s" onClose={closeModal}>
+        <ModalHeader title={title} onClick={closeModal} />
+        <ModalBody>
+          <RowGapBlock gap={16}>
+            <RowGapBlock gap={24}>
+              <TextMedium>
+                The Joystream blockchain is a PoS system relying on validators. Nominating validators allows you to
+                participate in the Joystream governance system and earn rewards.
+              </TextMedium>
+              <TextMedium>
+                When nominating, you are at risk of having parts of your staked funds lost if the validator malfunctions
+                or does a poor job, resulting in a reduced return on investment. To manage your risk, we advice you to
+                nominate several validators (up to 16). This allows you to spread out your risk and increase your
+                chances of earning rewards. You can choose how much to stake with each validator, and you can change
+                your staking percentages at any time.
+              </TextMedium>
+              <TextMedium>
+                To begin, review each validator's performance metrics by clicking on their name in the list. When you're
+                ready to nominate, add the validators you'd like to nominate to by clicking the "Nominate" button on the
+                list or directly on the validator’s profile. Once you've selected your validators, click the "Proceed"
+                button to initiate the nomination process.
+              </TextMedium>
+            </RowGapBlock>
             <TextMedium>
-              The Joystream blockchain is a PoS system relying on validators. Nominating validators allows you to
-              participate in the Joystream governance system and earn rewards.
-            </TextMedium>
-            <TextMedium>
-              When nominating, you are at risk of having parts of your staked funds lost if the validator malfunctions
-              or does a poor job, resulting in a reduced return on investment. To manage your risk, we advice you to
-              nominate several validators (up to 16). This allows you to spread out your risk and increase your chances
-              of earning rewards. You can choose how much to stake with each validator, and you can change your staking
-              percentages at any time.
-            </TextMedium>
-            <TextMedium>
-              To begin, review each validator's performance metrics by clicking on their name in the list. When you're
-              ready to nominate, add the validators you'd like to nominate to by clicking the "Nominate" button on the
-              list or directly on the validator’s profile. Once you've selected your validators, click the "Proceed"
-              button to initiate the nomination process.
+              You can learn more about the Pioneer nomination{' '}
+              <Link to="#" style={{ color: Colors.Blue[500], textDecoration: 'underline' }}>
+                system here
+              </Link>
+              .
             </TextMedium>
           </RowGapBlock>
-          <TextMedium>
-            You can learn more about the Pioneer nomination{' '}
-            <Link to="#" style={{ color: Colors.Blue[500], textDecoration: 'underline' }}>
-              system here
-            </Link>
-            .
-          </TextMedium>
-        </RowGapBlock>
-      </ModalBody>
-      <InfoModalFooter>
-        <Checkbox id="" onChange={() => {}} isChecked={false}>
-          Do not show this again.
-        </Checkbox>
-        <ButtonPrimary size="medium" onClick={closeModal}>
-          {buttonName} <ArrowRightIcon white />
-        </ButtonPrimary>
-      </InfoModalFooter>
-    </Modal>
-  )
+        </ModalBody>
+        <InfoModalFooter>
+          <Checkbox id="" onChange={setCheck} isChecked={check}>
+            Do not show this again.
+          </Checkbox>
+          <ButtonPrimary size="medium" onClick={closeModal}>
+            {buttonName} <ArrowRightIcon white />
+          </ButtonPrimary>
+        </InfoModalFooter>
+      </Modal>
+    )
   return null
 }
 
