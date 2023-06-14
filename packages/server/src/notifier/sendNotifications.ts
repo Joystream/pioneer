c105a615a25ead198b1826ed77af074ce369bc22
RC6799 <richcool.6799@gmail.com>
2023-06-14 13:02:34 +0900

    scope of #4275 implemented

--- packages/server/src/notifier/sendNotifications.ts
+++ packages/server/src/notifier/sendNotifications.ts
@@ -5,6 +5,13 @@ import { prisma } from '@/common/prisma'
 import { configEmailProvider, errorMessage, NO_EMAIL_PROVIDER } from '@/common/utils'
 
 import { notifyByEmail } from './model/email'
+enum EmailStatus {
+  SENT = 'sent',
+  FAILED_TO_SEND = 'failed_to_send',
+  PENDING_RETRY = 'pending_retry'
+}
+
+const MAX_RETRY_COUNT = 3
 
 export const sendNotifications = async (): Promise<any[]> => {
   if (NO_EMAIL_PROVIDER) {
@@ -12,21 +19,26 @@ export const sendNotifications = async (): Promise<any[]> => {
     return []
   }
 
-  const notifications = await prisma.notification.findMany({ where: { isSent: false }, include: { member: true } })
+  const notifications = await prisma.notification.findMany({ where: { emailStatus: EmailStatus.PENDING_RETRY }, include: { member: true } })
   const sendEmail = notifyByEmail(configEmailProvider())
 
   info('Email notifications', 'Attempt to email', notifications.length, 'notifications')
 
   return Promise.all(
     notifications.map(async (notification) => {
+      const { id, retryCount } = notification
       try {
         await sendEmail(notification)
-        return await prisma.notification.update({ where: pick(notification, 'id'), data: { isSent: true } })
+        await prisma.notification.update({ where: { id }, data: { emailStatus: EmailStatus.SENT, isSent: true, retryCount: 0 } })
+        return
       } catch (errData) {
-        error('Email notification failure', `Failed to email ${notification.id} with error:`, errorMessage(errData))
+        error('Email notification failure', `Failed to email ${id} with error:`, errorMessage(errData))
+        if (retryCount <MAX_RETRY_COUNT) {
+          await prisma.notification.update({ where: { id }, data: { emailStatus: EmailStatus.PENDING_RETRY, retryCount: retryCount + 1 } })
+          return
+        }
+        await prisma.notification.update({ where: { id }, data: { emailStatus: EmailStatus.FAILED_TO_SEND, retryCount: 0 } })
       }
-      // TODO: update a fail counter instead so it can be retried N time later
-      await prisma.notification.update({ where: pick(notification, 'id'), data: { isSent: true } })
     })
   )
 }
