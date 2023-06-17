4ce02efdfd77eca626a692adf2b111a5a7e954f9
l1.media <31551045+traumschule@users.noreply.github.com>
2023-06-17 18:53:21 +0100

    restore insert table button

--- packages/ui/src/common/components/CKEditor/CKEditor.tsx
+++ packages/ui/src/common/components/CKEditor/CKEditor.tsx
@@ -58,7 +58,7 @@ export const BaseCKEditor = React.forwardRef(
               'undo',
               'redo',
               '|',
-              // 'insertTable',
+              'insertTable',
               'specialCharacters',
             ],
           },
