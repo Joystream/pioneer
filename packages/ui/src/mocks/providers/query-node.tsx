54ab1875d0684b1f7e08fe24835511792369c33b
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-06-28 15:51:44 +0200

    Warn about missing QN queries

--- packages/ui/src/mocks/providers/query-node.tsx
+++ packages/ui/src/mocks/providers/query-node.tsx
@@ -1,6 +1,8 @@
 import { DocumentNode } from '@apollo/client/core'
 import React, { FC, createContext, useCallback, useContext, useMemo, useState } from 'react'
 
+import { warning } from '@/common/logger'
+
 import { BLOCK_HEAD } from './api'
 
 export { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client/core'
@@ -14,7 +16,13 @@ type QueryMap = Map<DocumentNode, Resolver>
 const QNMockContext = createContext<QueryMap>(new Map())
 
 export const useQuery = (query: DocumentNode, options?: Options): Result => {
-  const result = useContext(QNMockContext).get(query)?.(options) ?? { loading: false, data: undefined }
+  const qnMocks = useContext(QNMockContext)
+
+  if (!qnMocks.has(query)) {
+    warning('Missing mock query:', (query.definitions[0] as any).name.value ?? query.loc?.source.body)
+  }
+
+  const result = qnMocks.get(query)?.(options) ?? { loading: false, data: undefined }
   return useMemo<Result>(() => result, [JSON.stringify(result)])
 }
 
