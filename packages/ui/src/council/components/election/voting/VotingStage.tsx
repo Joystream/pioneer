a9f13a86aec11e57e3513d704256f541c71baf88
Victor <emmyvic98@gmail.com>
2023-07-13 10:36:57 +0100

    lint fix

--- packages/ui/src/council/components/election/voting/VotingStage.tsx
+++ packages/ui/src/council/components/election/voting/VotingStage.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-console */
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import { useMyAccounts } from '@/accounts/hooks/useMyAccounts'
 import { NoData } from '@/common/components/NoData'
@@ -37,23 +36,18 @@ export const VotingStage = ({ election, isLoading }: VotingStageProps) => {
       }))
     const votedForCandidates = allCandidates
       ?.filter(({ voted }) => voted)
-      // .map((candidate) => {
-      //   const myVotesForCandidate = votes?.filter((vote) => vote.optionId === candidate.member.id) ?? []
-      //   return {
-      //     ...candidate,
-      //     myStake: myVotesForCandidate.reduce((prev, next) => prev.add(next.stake), BN_ZERO),
-      //   }
-      // })
+      .map((candidate) => {
+        const myVotesForCandidate = votes?.filter((vote) => vote.optionId === candidate.member.id) ?? []
+
+        return {
+          ...candidate,
+          myVotes: myVotesForCandidate,
+          myStake: myVotesForCandidate.reduce((prev, next) => prev.add(next.stake), BN_ZERO),
+        }
+      })
 
     return [allCandidates, votedForCandidates]
-  }, [optionIds?.size, election?.candidates, votes, election])
-
-  useEffect(() => {
-    console.log('My Cast Votes ', votes)
-    console.log('My Votes length ', myVotes)
-    console.log('All candidates ', allCandidates)
-    console.log('Voted for candidates ', votedForCandidates)
-  })
+  }, [optionIds?.size, election?.candidates])
 
   return (
     <>
