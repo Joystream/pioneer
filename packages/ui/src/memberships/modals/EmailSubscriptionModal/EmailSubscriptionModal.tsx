c1e3e3f3a64fc267a14765ef4fc01b1d140cd035
ilya Smiyukha <ilya.smiyukha@touchcast.com>
2023-08-09 10:14:59 +0200

    add api call to email subscription

--- packages/ui/src/memberships/modals/EmailSubscriptionModal/EmailSubscriptionModal.tsx
+++ packages/ui/src/memberships/modals/EmailSubscriptionModal/EmailSubscriptionModal.tsx
@@ -1,9 +1,12 @@
+import { gql } from '@apollo/client'
 import React, { useEffect } from 'react'
 
 import { useMyAccounts } from '@/accounts/hooks/useMyAccounts'
 import { FailureModal } from '@/common/components/FailureModal'
 import { ResultText } from '@/common/components/Modal'
 import { WaitModal } from '@/common/components/WaitModal'
+import { useBackend } from '@/common/hooks/useBackend'
+import { useLocalStorage } from '@/common/hooks/useLocalStorage'
 import { useMachine } from '@/common/hooks/useMachine'
 import { useModal } from '@/common/hooks/useModal'
 import { EmailSubscriptionModalCall } from '@/memberships/modals/EmailSubscriptionModal/index'
@@ -12,14 +15,27 @@ import { EmailSubscriptionFormModal } from './EmaiSubscriptionFormModal'
 import { EmailSubscriptionMachine } from './machine'
 import { EmailSubscriptionForm } from './types'
 
+const SIGNUP_MUTATION = gql`
+  mutation signup($memberId: Int!, $name: String!, $email: String!, $signature: String!, $timestamp: BigInt!) {
+    signup(memberId: $memberId, name: $name, email: $email, signature: $signature, timestamp: $timestamp)
+  }
+`
+
 export const EmailSubscriptionModal = () => {
   const {
     hideModal,
     modalData: { member },
   } = useModal<EmailSubscriptionModalCall>()
-
+  const [, setMembersEmail] = useLocalStorage<Record<string, string>>('membersEmail')
   const { wallet } = useMyAccounts()
   const [state, send] = useMachine(EmailSubscriptionMachine)
+  const {
+    data,
+    error,
+    send: mutate,
+  } = useBackend({
+    mutation: SIGNUP_MUTATION,
+  })
 
   const signEmail = async () => {
     const timestamp = Date.now()
@@ -36,11 +52,37 @@ export const EmailSubscriptionModal = () => {
     }
   }
 
+  const signUp = async () => {
+    if (state.event.type === 'SIGNED') {
+      await mutate({
+        memberId: parseFloat(member.id),
+        name: member.name,
+        email: state.context.email,
+        signature: state.event.signature,
+        timestamp: state.event.timestamp,
+      })
+    }
+  }
+
   useEffect(() => {
     if (state.matches('signature')) {
       signEmail()
     }
+    if (state.matches('transaction')) {
+      signUp()
+    }
   }, [state])
+
+  useEffect(() => {
+    if (data) {
+      send('SUCCESS')
+      setMembersEmail((emails) => ({ ...emails, [member.id]: state.context.email || '' }))
+      hideModal()
+    }
+    if (error) {
+      send('ERROR')
+    }
+  }, [data, error])
 
   if (state.matches('prepare')) {
     return (
