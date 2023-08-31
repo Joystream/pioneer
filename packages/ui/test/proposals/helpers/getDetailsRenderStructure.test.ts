733d99bd02566783af2427414402171e4f921f39
Victor <emmyvic98@gmail.com>
2023-08-31 07:43:42 +0100

    build fix

--- packages/ui/test/proposals/helpers/getDetailsRenderStructure.test.ts
+++ packages/ui/test/proposals/helpers/getDetailsRenderStructure.test.ts
@@ -60,49 +60,27 @@ describe('getDetailsRenderStructure()', () => {
     expect(structure).toEqual({
       structure: [
         {
-          label: 'amount',
-          value: new BN(20),
+          label: 'Total Payment',
+          value: new BN(60),
           renderType: 'Amount',
         },
         {
-          label: 'destination',
-          value: alice.address,
-          renderType: 'Address',
-        },
-        {
-          label: '',
-          value: undefined,
-          renderType: 'Divider',
-        },
-        {
-          label: 'amount',
-          value: new BN(30),
-          renderType: 'Amount',
-        },
-        {
-          label: 'destination',
-          value: bob.address,
-          renderType: 'Address',
-        },
-        {
-          label: '',
-          value: undefined,
-          renderType: 'Divider',
-        },
-        {
-          label: 'amount',
-          value: new BN(10),
-          renderType: 'Amount',
-        },
-        {
-          label: 'destination',
-          value: aliceStash.address,
-          renderType: 'Address',
-        },
-        {
-          label: '',
-          value: undefined,
-          renderType: 'Divider',
+          label: 'Payment Details',
+          value: [
+            {
+              account: alice.address,
+              amount: new BN(20),
+            },
+            {
+              account: bob.address,
+              amount: new BN(30),
+            },
+            {
+              account: aliceStash.address,
+              amount: new BN(10),
+            },
+          ],
+          renderType: 'DestinationsPreview',
         },
       ],
     })
