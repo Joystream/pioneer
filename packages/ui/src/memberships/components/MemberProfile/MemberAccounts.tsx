edf7a731d9f286066c30a3f346fbbe689f788449
chrlschwb <chrlschwb77@gmail.com>
2023-09-11 11:46:38 +0200

    fix lint error

--- packages/ui/src/memberships/components/MemberProfile/MemberAccounts.tsx
+++ packages/ui/src/memberships/components/MemberProfile/MemberAccounts.tsx
@@ -1,21 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { useBalance } from '@/accounts/hooks/useBalance'
+import { TokenValue } from '@/common/components/typography'
+import { isDefined } from '@/common/utils'
+
 import { UnknownAccountInfo } from '../../../accounts/components/UnknownAccountInfo'
-import { AccountRow } from '../../../common/components/Modal'
 import { RowGapBlock } from '../../../common/components/page/PageContent'
 import { SidePaneLabel } from '../../../common/components/SidePane'
 import { Member } from '../../types'
-import { TokenValue } from '@/common/components/typography'
-import { isDefined } from '@/common/utils'
-import { useBalance } from '@/accounts/hooks/useBalance'
 
-export const MemberAccounts = ({ member }: { member: Member }) => {
-  const rootBalance = useBalance(member.rootAccount);
-  const countrolBalance = useBalance(member.controllerAccount);
+interface BoundAccountProps {
+  account: string
+}
+
+const BoundAccount = ({ account }: BoundAccountProps) => {
+  const boundBalance = useBalance(account)
 
   return (
-    < AccountsDisplay gap={16} >
+    <AccountMemberRow key={account}>
+      <UnknownAccountInfo address={account} placeholderName="Bound Account" />
+      <TokenValue value={boundBalance?.total} isLoading={!isDefined(boundBalance?.total)} />
+    </AccountMemberRow>
+  )
+}
+
+export const MemberAccounts = ({ member }: { member: Member }) => {
+  const rootBalance = useBalance(member.rootAccount)
+  const countrolBalance = useBalance(member.controllerAccount)
+
+  return (
+    <AccountsDisplay gap={16}>
       <SidePaneLabel text="Root account" />
       {!!member.rootAccount && (
         <AccountMemberRow>
@@ -24,32 +39,22 @@ export const MemberAccounts = ({ member }: { member: Member }) => {
         </AccountMemberRow>
       )}
       <SidePaneLabel text="Controller account" />
-      {
-        !!member.controllerAccount && (
-          <AccountMemberRow>
-            <UnknownAccountInfo address={member.controllerAccount} placeholderName="Controller Account" />
-            <TokenValue value={countrolBalance?.total} isLoading={!isDefined(countrolBalance?.total)} />
-          </AccountMemberRow>
-        )
-      }
+      {!!member.controllerAccount && (
+        <AccountMemberRow>
+          <UnknownAccountInfo address={member.controllerAccount} placeholderName="Controller Account" />
+          <TokenValue value={countrolBalance?.total} isLoading={!isDefined(countrolBalance?.total)} />
+        </AccountMemberRow>
+      )}
 
-      {
-        !!member.boundAccounts.length && (
-          <>
-            <SidePaneLabel text="Bound accounts" />
-            {member.boundAccounts.map((account) => {
-              const bountBalance = useBalance(account);
-              return (
-                <AccountMemberRow key={account}>
-                  <UnknownAccountInfo address={account} placeholderName="Bound Account" />
-                  <TokenValue value={bountBalance?.total} isLoading={!isDefined(bountBalance?.total)} />
-                </AccountMemberRow>
-              )
-            })}
-          </>
-        )
-      }
-    </ AccountsDisplay>
+      {!!member.boundAccounts.length && (
+        <>
+          <SidePaneLabel text="Bound accounts" />
+          {member.boundAccounts.map((account) => (
+            <BoundAccount account={account} />
+          ))}
+        </>
+      )}
+    </AccountsDisplay>
   )
 }
 
@@ -57,19 +62,20 @@ const AccountsDisplay = styled(RowGapBlock)`
   padding: 24px;
 `
 
-export const AccountMemberRow = styled.div
-  `    display: grid;
-grid-template-columns: 1fr 1fr;
-grid-template-rows: 2fr;
--webkit-align-items: center;
--webkit-box-align: center;
--ms-flex-align: center;
-align-items: center;
-justify-items: center;
-width: 100%;
-min-height: 94px;
-max-height: 94px;
-padding: 8px 13px 8px 14px;
-border: 1px solid #C4CCD6;
-border-radius: 2px;
-background-color: #FFFFFF;`
+export const AccountMemberRow = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 2fr;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  justify-items: center;
+  width: 100%;
+  min-height: 94px;
+  max-height: 94px;
+  padding: 8px 13px 8px 14px;
+  border: 1px solid #c4ccd6;
+  border-radius: 2px;
+  background-color: #ffffff;
+`
