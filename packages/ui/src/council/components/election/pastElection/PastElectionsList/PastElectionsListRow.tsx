9cd3501489c11cc9ff7ae9b6f076ae354eca8f8c
Josh Groban <goldwolf.dev@gmail.com>
2023-10-14 17:30:56 -0600

    fix/past-election-1

--- packages/ui/src/council/components/election/pastElection/PastElectionsList/PastElectionsListRow.tsx
+++ packages/ui/src/council/components/election/pastElection/PastElectionsList/PastElectionsListRow.tsx
@@ -29,7 +29,7 @@ export const PastElectionsListRow = ({ election }: PastElectionsListRowProps) =>
       ) : (
         <></>
       )}
-      <TokenValue value={election.totalStake} />
+      <TokenValue value={election.totalCandidatesStake} />
       <Fraction numerator={election.revealedVotes} denominator={election.totalVotes} sameSize />
       <CountInfo count={election.totalCandidates} />
     </PastElectionsListRowItem>
