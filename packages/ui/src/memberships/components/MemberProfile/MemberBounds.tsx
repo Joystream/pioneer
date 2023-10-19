198e9d65e92d2a692b039c19785ed0d75659151c
chrlschwb <chrlschwb77@gmail.com>
2023-10-19 02:00:25 -0700

    finish fixed

---
+++ packages/ui/src/memberships/components/MemberProfile/MemberBounds.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { useBalance } from '@/accounts/hooks/useBalance'
+import { TokenValue } from '@/common/components/typography'
+import { isDefined } from '@/common/utils'
+
+import { UnknownAccountInfo } from '../../../accounts/components/UnknownAccountInfo'
+
+import { AccountMemberRow } from './MemberAccounts'
+
+type Props = {
+  account: string
+}
+
+export const MemberBounds = ({ account }: Props) => {
+  const balance = useBalance(account)
+  return (
+    <div>
+      {!!account && (
+        <AccountMemberRow>
+          <UnknownAccountInfo address={account} placeholderName="Root Account" />
+          <TokenValue value={balance?.total} isLoading={!isDefined(balance?.total)} />
+        </AccountMemberRow>
+      )}
+    </div>
+  )
+}
