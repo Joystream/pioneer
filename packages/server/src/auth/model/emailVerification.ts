8774e818b874183c4fb3d7eb418aaea2d92e4302
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-10-27 20:52:53 +0200

    Log email activity

--- packages/server/src/auth/model/emailVerification.ts
+++ packages/server/src/auth/model/emailVerification.ts
@@ -1,3 +1,5 @@
+import { error } from 'npmlog'
+
 import { emailProvider } from '@/common/api/email'
 import { PIONEER_URL } from '@/common/config'
 import { renderPioneerEmail } from '@/common/email-templates/pioneer-email'
@@ -16,17 +18,22 @@ export const sendVerificationEmail = async ({ email, memberId, name, referer }: 
   const baseUrl = (referer || PIONEER_URL).replace(/\/$/, '') // get rid of trailing slash
   const verificationUrl = `${baseUrl}/#/settings?emailVerificationToken=${token}`
 
-  await emailProvider.sendEmail({
-    to: email,
-    subject: 'Confirm your email for Pioneer',
-    html: renderPioneerEmail({
-      memberHandle: name,
-      summary: 'Confirm your email for Pioneer',
-      text: `Please use the link below to confirm your email address "${email}" for Pioneer notifications.`,
-      button: {
-        label: 'Confirm email',
-        href: verificationUrl,
-      },
-    }),
-  })
+  try {
+    await emailProvider.sendEmail({
+      to: email,
+      subject: 'Confirm your email for Pioneer',
+      html: renderPioneerEmail({
+        memberHandle: name,
+        summary: 'Confirm your email for Pioneer',
+        text: `Please use the link below to confirm your email address "${email}" for Pioneer notifications.`,
+        button: {
+          label: 'Confirm email',
+          href: verificationUrl,
+        },
+      }),
+    })
+  } catch (err) {
+    error('Auth', JSON.stringify(error, null, '2'))
+    throw error
+  }
 }
