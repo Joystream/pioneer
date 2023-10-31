cf8f5be261a666364f097ddfa91f942f4ca6b3ad
chrlschwb <chrlschwb77@gmail.com>
2023-10-31 14:02:16 +0900

    fixed link event error

--- packages/ui/src/council/components/election/pastElection/PastElectionsList/PastElectionsListRow.tsx
+++ packages/ui/src/council/components/election/pastElection/PastElectionsList/PastElectionsListRow.tsx
@@ -22,6 +22,7 @@ export const PastElectionsListRow = ({ election }: PastElectionsListRowProps) =>
       $colLayout={PastElectionsColLayout}
       as={GhostRouterLink}
       to={generatePath(ElectionRoutes.pastElection, { id: election.id })}
+      target="_blank"
     >
       <Info>#{election.cycleId}</Info>
       {election.finishedAtBlock ? (
