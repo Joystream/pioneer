45c427dbb1fdbd1c8b44508e89071dd45b159ab8
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-11-09 11:18:39 +0100

    📜 Log email activity (#4608)
    * Log email activity
    
    * Do not create members when verification email fails
    
    * Fix `emailVerification` error loging
    
    * Revert "Do not create members when verification email fails"
    
    This reverts commit 4d97fcb63fa89b6924be5eeff4ee1223458d7f11.

--- packages/server/src/auth/model/emailVerification.ts
+++ packages/server/src/auth/model/emailVerification.ts
@@ -1,6 +1,9 @@
+import { error } from 'npmlog'
+
 import { emailProvider } from '@/common/api/email'
 import { PIONEER_URL } from '@/common/config'
 import { renderPioneerEmail } from '@/common/email-templates/pioneer-email'
+import { errorMessage } from '@/common/utils'
 
 import { createEmailToken } from './token'
 
@@ -16,17 +19,22 @@ export const sendVerificationEmail = async ({ email, memberId, name, referer }: 
   const baseUrl = (referer || PIONEER_URL).replace(/\/$/, '') // get rid of trailing slash
   const verificationUrl = `${baseUrl}/#/settings?emailVerificationToken=${token}`
 
-  await emailProvider.sendEmail({
-    to: email,
-    subject: 'Confirm your email for Pioneer',
-    html: renderPioneerEmail({
-      memberHandle: name,
-      summary: 'Confirm your email for Pioneer',
-      text: `Please use the link below to confirm your email address "${email}" for Pioneer notifications.`,
-      button: {
-        label: 'Confirm email',
-        href: verificationUrl,
-      },
-    }),
-  })
+  try {
+    await emailProvider.sendEmail({
+      to: email,
+      subject: 'Confirm your email for Pioneer',
+      html: renderPioneerEmail({
+        memberHandle: name,
+        summary: 'Confirm your email for Pioneer',
+        text: `Please use the link below to confirm your email address "${email}" for Pioneer notifications.`,
+        button: {
+          label: 'Confirm email',
+          href: verificationUrl,
+        },
+      }),
+    })
+  } catch (err) {
+    error('Auth', errorMessage(err), JSON.stringify(err, null, 2))
+    throw err
+  }
 }
