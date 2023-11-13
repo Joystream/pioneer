e02b18616b6b002f4c60fa346665c630233e0b4c
Victor <emmyvic98@gmail.com>
2023-11-13 20:27:25 +0100

    required changes

--- packages/ui/src/app/pages/Proposals/CurrentProposals.stories.tsx
+++ packages/ui/src/app/pages/Proposals/CurrentProposals.stories.tsx
@@ -1410,32 +1410,32 @@ export const SpecificParametersSetMembershipPrice: Story = {
     })
   }),
 }
-// export const SpecificParametersUpdatePalletFrozenStatus: Story = {
-//   play: specificParametersTest('Update Pallet Frozen Status', async ({ args, createProposal, modal, step }) => {
-//     await createProposal(async () => {
-//       const nextButton = getButtonByText(modal, 'Create proposal')
+export const SpecificParametersUpdatePalletFrozenStatus: Story = {
+  play: specificParametersTest('Update Pallet Frozen Status', async ({ args, createProposal, modal, step }) => {
+    await createProposal(async () => {
+      const nextButton = getButtonByText(modal, 'Create proposal')
 
-//       await userEvent.click(modal.getByTestId('crt-feature-select'))
-//       expect(
-//         await modal.findByText(
-//           /The ProjectToken pallet is currently enabled, so presently this proposal would fail due to execution constraints./
-//         )
-//       )
-//       expect(await modal.findByText(/Warning/))
-//       expect(nextButton).toBeDisabled()
+      await userEvent.click(modal.getByTestId('crt-feature-select'))
+      expect(
+        await modal.findByText(
+          /The ProjectToken pallet is currently enabled, so presently this proposal would fail due to execution constraints./
+        )
+      )
+      expect(await modal.findByText(/Warning/))
+      expect(nextButton).toBeDisabled()
 
-//       await userEvent.click(modal.getByTestId('crt-feature-select'))
-//       await waitFor(() => expect(nextButton).toBeEnabled())
-//     })
+      await userEvent.click(modal.getByTestId('crt-feature-select'))
+      await waitFor(() => expect(nextButton).toBeEnabled())
+    })
 
-//     step('Transaction parameters', () => {
-//       const [, specificParameters] = args.onCreateProposal.mock.calls.at(-1)
-//       expect(specificParameters.toJSON()).toEqual({
-//         setPalletFozenStatus: [true, 'ProjectToken'],
-//       })
-//     })
-//   }),
-// }
+    step('Transaction parameters', () => {
+      const [, specificParameters] = args.onCreateProposal.mock.calls.at(-1)
+      expect(specificParameters.toJSON()).toEqual({
+        setPalletFozenStatus: [true, 'ProjectToken'],
+      })
+    })
+  }),
+}
 export const SpecificParametersUpdateWorkingGroupBudget: Story = {
   parameters: {
     councilSize: 3,
