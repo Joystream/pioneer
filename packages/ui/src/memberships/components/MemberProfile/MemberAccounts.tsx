8b4187fdd4d0d731aa4e7a4cfb481e756e56363a
chrlschwb <chrlschwb77@gmail.com>
2023-11-15 23:00:23 +0900

    update

--- packages/ui/src/memberships/components/MemberProfile/MemberAccounts.tsx
+++ packages/ui/src/memberships/components/MemberProfile/MemberAccounts.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { useBalance } from '@/accounts/hooks/useBalance'
-import { AccountRow } from '@/common/components/Modal'
-import { TokenValue } from '@/common/components/typography'
-import { isDefined } from '@/common/utils'
-
-import { UnknownAccountInfo } from '../../../accounts/components/UnknownAccountInfo'
 import { RowGapBlock } from '../../../common/components/page/PageContent'
 import { SidePaneLabel } from '../../../common/components/SidePane'
 import { Member } from '../../types'
@@ -14,31 +8,18 @@ import { Member } from '../../types'
 import { MemberBounds } from './MemberBounds'
 
 export const MemberAccounts = ({ member }: { member: Member }) => {
-  const rootBalance = useBalance(member.rootAccount)
-  const countrolBalance = useBalance(member.controllerAccount)
-
   return (
     <AccountsDisplay gap={16}>
       <SidePaneLabel text="Root account" />
-      {!!member.rootAccount && (
-        <AccountMemberRow>
-          <UnknownAccountInfo address={member.rootAccount} placeholderName="Root Account" />
-          <TokenValue value={rootBalance?.total} isLoading={!isDefined(rootBalance?.total)} />
-        </AccountMemberRow>
-      )}
+      {!!member.rootAccount && <MemberBounds account={member.rootAccount} name="Root Account" />}
       <SidePaneLabel text="Controller account" />
-      {!!member.controllerAccount && (
-        <AccountMemberRow>
-          <UnknownAccountInfo address={member.controllerAccount} placeholderName="Controller Account" />
-          <TokenValue value={countrolBalance?.total} isLoading={!isDefined(countrolBalance?.total)} />
-        </AccountMemberRow>
-      )}
+      {!!member.controllerAccount && <MemberBounds account={member.controllerAccount} name="Controller Account" />}
 
       {!!(member.boundAccounts.length !== 0) && (
         <>
           <SidePaneLabel text="Bound accounts" />
           {member.boundAccounts.map((account) => {
-            return <MemberBounds account={account} />
+            return <MemberBounds account={account} name="Bound Account" />
           })}
         </>
       )}
@@ -48,12 +29,4 @@ export const MemberAccounts = ({ member }: { member: Member }) => {
 
 const AccountsDisplay = styled(RowGapBlock)`
   padding: 24px;
-`
-
-export const AccountMemberRow = styled(AccountRow)`
-  grid-template-rows: 2fr;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  justify-items: end;
-  padding: 8px 14px 8px 14px;
 `
