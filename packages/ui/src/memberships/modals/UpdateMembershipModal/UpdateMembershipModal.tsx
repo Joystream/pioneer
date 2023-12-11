2614cfbe35faa5fa00ae550a6cc31bd6f6552585
goldwolf <goldwolf.dev@gmail.com>
2023-12-11 09:43:56 -0400

    isURL

--- packages/ui/src/memberships/modals/UpdateMembershipModal/UpdateMembershipModal.tsx
+++ packages/ui/src/memberships/modals/UpdateMembershipModal/UpdateMembershipModal.tsx
@@ -14,9 +14,13 @@ import { updateMembershipMachine } from './machine'
 import { UpdateMembershipFormModal } from './UpdateMembershipFormModal'
 import { UpdateMembershipSuccessModal } from './UpdateMembershipSuccessModal'
 
-export const isURL = (input: any): boolean => {
-  const urlRegex = /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,})(\/\S*)?$/i
-  return urlRegex.test(input)
+export const isURL = (str: string) => {
+  try {
+    const newUrl = new URL(str)
+    return newUrl.protocol === 'http:' || newUrl.protocol === 'https:'
+  } catch (err) {
+    return false
+  }
 }
 
 export const UpdateMembershipModal = () => {
@@ -33,7 +37,7 @@ export const UpdateMembershipModal = () => {
       <UpdateMembershipFormModal
         onClose={hideModal}
         onSubmit={(params) => {
-          if (params.externalResources?.TWITTER && params.externalResources?.TWITTER != '') {
+          if (params.externalResources?.TWITTER && params.externalResources?.TWITTER) {
             params.externalResources?.TWITTER.match(/@/g)
               ? (params.externalResources.TWITTER = params.externalResources.TWITTER.split('@')[1])
               : params.externalResources.TWITTER
@@ -41,7 +45,7 @@ export const UpdateMembershipModal = () => {
               ? (params.externalResources.TWITTER = params.externalResources.TWITTER.split('twitter.com/')[1])
               : params.externalResources?.TWITTER
           }
-          if (params.externalResources?.GITHUB && params.externalResources?.GITHUB != '') {
+          if (params.externalResources?.GITHUB && params.externalResources?.GITHUB) {
             params.externalResources?.GITHUB.match(/@/g)
               ? (params.externalResources.GITHUB = params.externalResources.GITHUB.split('@')[1])
               : params.externalResources.GITHUB
@@ -49,19 +53,19 @@ export const UpdateMembershipModal = () => {
               ? (params.externalResources.GITHUB = params.externalResources.GITHUB.split('github.com/')[1])
               : params.externalResources?.GITHUB
           }
-          if (params.externalResources?.TELEGRAM && params.externalResources?.TELEGRAM != '') {
+          if (params.externalResources?.TELEGRAM && params.externalResources?.TELEGRAM) {
             params.externalResources?.TELEGRAM.match(/@/g)
               ? (params.externalResources.TELEGRAM = params.externalResources.TELEGRAM.split('@')[1])
               : params.externalResources.TELEGRAM
           }
 
-          if (params.externalResources?.YOUTUBE && params.externalResources?.YOUTUBE != '') {
+          if (params.externalResources?.YOUTUBE && params.externalResources?.YOUTUBE) {
             params.externalResources?.YOUTUBE.match(/@/g)
               ? (params.externalResources.YOUTUBE = params.externalResources.YOUTUBE.split('@')[1])
               : params.externalResources.YOUTUBE
           }
 
-          if (params.externalResources?.FACEBOOK && params.externalResources?.FACEBOOK != '') {
+          if (params.externalResources?.FACEBOOK && params.externalResources?.FACEBOOK) {
             params.externalResources?.FACEBOOK.match(/@/g)
               ? (params.externalResources.FACEBOOK = params.externalResources.FACEBOOK.split('@')[1])
               : params.externalResources.FACEBOOK
@@ -70,7 +74,7 @@ export const UpdateMembershipModal = () => {
               : params.externalResources?.FACEBOOK
           }
 
-          if (params.externalResources?.LINKEDIN && params.externalResources?.LINKEDIN != '') {
+          if (params.externalResources?.LINKEDIN && params.externalResources?.LINKEDIN) {
             params.externalResources?.LINKEDIN.match(/@/g)
               ? (params.externalResources.LINKEDIN = params.externalResources.LINKEDIN.split('@')[1])
               : params.externalResources.LINKEDIN
