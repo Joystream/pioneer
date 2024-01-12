408c0e7a27af07580ba66105e6114df4d79d4985
eshark9312 <eshark9312@outlook.com>
2024-01-12 01:49:22 -0500

    fix useValidators hook

--- packages/ui/src/validators/hooks/useValidators.ts
+++ packages/ui/src/validators/hooks/useValidators.ts
@@ -10,7 +10,7 @@ export const useValidators = ({ skip = false }: Props = {}) => {
 
   useEffect(() => {
     if (!skip) setShouldFetchValidators(true)
-  }, [])
+  }, [skip])
 
   return { allValidators, allValidatorsWithCtrlAcc, validatorsWithMembership }
 }
