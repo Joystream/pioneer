96cd00852ae66d1b78fafa613d6b3a0ad6049ff5
Theophile Sandoz <theophile.sandoz@gmail.com>
2024-03-14 15:30:56 +0100

    Test the Blake3 hashing directly

--- packages/ui/test/common/utils/crypto.test.ts
+++ packages/ui/test/common/utils/crypto.test.ts
@@ -1,4 +1,6 @@
 import { generateJsonPayloadFromPayoutsVector, generateSerializedPayload } from '@joystream/js/content'
+import { blake3 } from '@noble/hashes/blake3'
+import { encode as encodeHash, toB58String } from 'multihashes'
 
 import { merkleRootFromBinary, hashFile } from '@/common/utils/crypto'
 
@@ -14,6 +16,27 @@ describe('Utils: Crypto', () => {
   })
 
   it('File hash', async () => {
+    expect(await hashFile(new Blob(['foo']))).toBe('gVwzhfDKQjym61HfkEEQr1tZtNH6Lwk52eziQLVdmRriit')
     expect(await hashFile(file)).toBe('gW22Sg9hMpHzog1XGwPAM7pz4As1NHDKuRoQvUpDybR6W5')
   })
+
+  it('Blake3', () => {
+    const digest = blake3(new Uint8Array([1, 2, 3]))
+    const hashBytes = encodeHash(digest, 'blake3')
+    const hashB58 = toB58String(hashBytes)
+
+    expect(stringifyU8A(digest)).toBe(
+      'b1 77 ec 1b f2 6d fb 3b 70 10 d4 73 e6 d4 47 13 b2 9b 76 5b 99 c6 e6 0e cb fa e7 42 de 49 65 43'
+    )
+    expect(stringifyU8A(hashBytes)).toBe(
+      '1e 20 b1 77 ec 1b f2 6d fb 3b 70 10 d4 73 e6 d4 47 13 b2 9b 76 5b 99 c6 e6 0e cb fa e7 42 de 49 65 43'
+    )
+    expect(hashB58).toBe('gW9cRVpYEEwRnkdZR5ibFPDssBHBNJDHYo6rJNrhTeanjt')
+  })
 })
+
+function stringifyU8A(bytes: Uint8Array): string {
+  return Array.from(bytes)
+    .map((byte) => byte.toString(16).padStart(2, '0'))
+    .join(' ')
+}
