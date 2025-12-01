8a49078863bbc78905901656f42a72d4b57494c8
goldstarhigher <heinrich.olf@gmail.com>
2025-12-01 07:12:09 -0500

    remove the ClaimStakingRewardsModal.

--- packages/ui/src/validators/modals/ClaimStakingRewardsModal/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './ClaimStakingRewardsModal'
-export * from './types'
-
--- packages/ui/src/validators/modals/ClaimStakingRewardsModal/types.ts
+++
@@ -1,4 +0,0 @@
-import { ModalCall } from '@/common/providers/modal/types'
-
-export type ClaimStakingRewardsModalCall = ModalCall<'ClaimStakingRewardsModal'>
-
