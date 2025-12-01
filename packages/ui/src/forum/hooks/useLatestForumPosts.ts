12abca17ba16b9f292540b2a58c7b52f5825a65d
goldstarhigher <heinrich.olf@gmail.com>
2025-12-01 08:05:53 -0500

    pushed.

--- packages/ui/src/forum/hooks/useLatestForumPosts.ts
+++ packages/ui/src/forum/hooks/useLatestForumPosts.ts
@@ -1,6 +1,7 @@
 import { useMemo } from 'react'
 
 import { ForumPostOrderByInput } from '@/common/api/queries'
+import { ActiveStatus } from '@/forum/hooks/useForumCategories'
 import { useGetLatestForumPostsQuery } from '@/forum/queries'
 import { asMember, Member } from '@/memberships/types'
 
@@ -31,6 +32,11 @@ export const useLatestForumPosts = (limit: number) => {
         thread: {
           status_json: {
             isTypeOf_eq: 'ThreadStatusActive',
+          },
+          category: {
+            status_json: {
+              isTypeOf_eq: ActiveStatus,
+            },
           },
         },
       },
